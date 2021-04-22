380bb646512fe989576f768e29c1854e80691f02
Tiago Garcia <tiagojsag@gmail.com>
2021-04-22 14:33:55 +0200

    Remove GH pages directory (so we can use the TLD URL)

--- marketing/next.config.js
+++ marketing/next.config.js
@@ -1,5 +1,5 @@
 const debug = process.env.NODE_ENV === 'development';
-const GH_PAGES_DIRECTORY = 'landgriffon'; // change this in case you don't use GH Pages
+const GH_PAGES_DIRECTORY = ''; // change this in case you don't use GH Pages
 
 module.exports = {
   basePath: debug ? '' : `/${GH_PAGES_DIRECTORY}`,

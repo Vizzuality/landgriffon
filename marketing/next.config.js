59ed6c58b0dff89a4a7d78c16818968a99569f2f
Tiago Garcia <tiagojsag@gmail.com>
2021-04-22 14:36:31 +0200

    Remove GH pages directory (so we can use the TLD URL)

--- marketing/next.config.js
+++ marketing/next.config.js
@@ -1,7 +1,4 @@
-const debug = process.env.NODE_ENV === 'development';
-const GH_PAGES_DIRECTORY = ''; // change this in case you don't use GH Pages
-
 module.exports = {
-  basePath: debug ? '' : `/${GH_PAGES_DIRECTORY}`,
-  assetPrefix: debug ? '' : `/${GH_PAGES_DIRECTORY}/`,
+  basePath: '',
+  assetPrefix: '',
 };

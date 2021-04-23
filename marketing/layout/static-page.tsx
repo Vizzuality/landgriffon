44530aa44ca1fd3140744d43c91c7fb6517c25e4
David Inga <david.inga@vizzuality.com>
2021-04-23 08:42:33 +0200

    added support for mdx files; also created privacy policy page

---
+++ marketing/layout/static-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StaticPage: React.FC = ({ children }) => (
+  <div className="">
+    {children}
+  </div>
+);
+
+export default StaticPage;
--- marketing/next.config.js
+++ marketing/next.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withMDX = require('@next/mdx')({
+  extension: /\.(md|mdx)$/,
+});
+
+module.exports = withMDX({
   basePath: '',
   assetPrefix: '',
-};
+  pageExtensions: ['js', 'jsx', 'mdx'],
+});

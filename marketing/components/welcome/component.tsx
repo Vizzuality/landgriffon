744123434ab9b2ed309b30e768847fda6a3607a7
David Inga <david.inga@vizzuality.com>
2021-04-27 09:46:42 +0200

    added animation for read more link, and also updated the link

--- marketing/components/welcome/component.tsx
+++ marketing/components/welcome/component.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './styles.module.css';
 
 const Welcome: React.FC = () => (
   <div>
@@ -7,7 +9,14 @@ const Welcome: React.FC = () => (
       chain sustainability
       targets
     </h1>
-    <p className="mt-4"><a className="text-3xl lg:text-4xl font-bold underline" href="/">Learn more</a></p>
+    <p className="mt-4">
+      <a
+        className={classnames('text-3xl lg:text-4xl font-bold', styles.linkAnimation)}
+        href="https://medium.com/@Vizzuality/sustainable-supply-chain-ec-funding-announcement-78819a53ff3d"
+      >
+        Learn more
+      </a>
+    </p>
   </div>
 );
 

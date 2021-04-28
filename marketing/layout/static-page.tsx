baa6030eb54d87e962c433a8ed5b70962b1bd08c
David Inga <david.inga@vizzuality.com>
2021-04-28 10:28:28 +0200

    title for privacy policy page

--- marketing/layout/static-page.tsx
+++ marketing/layout/static-page.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
 import Logo from 'components/logo';
 
-const StaticPage: React.FC = ({ children }) => (
-  <div>
+type LayoutProps = {
+  children: React.ReactChildren;
+  title: string;
+}
+
+const StaticPage: React.FC = ({ children, title }: LayoutProps) => (
+  <>
+    <Head>
+      <title>{`Landgriffon - ${title}`}</title>
+    </Head>
     <header className="px-10 py-7 lg:p-10 flex justify-center items-start xl:justify-start lg:w-1/5">
       <Link href="/">
         <a>
@@ -12,11 +21,11 @@ const StaticPage: React.FC = ({ children }) => (
       </Link>
     </header>
     <div className="flex justify-center">
-      <div className="max-w-screen-lg prose lg:prose-x my-8 mx-4">
+      <div className="max-w-screen-lg my-8 mx-4 prose">
         {children}
       </div>
     </div>
-  </div>
+  </>
 );
 
 export default StaticPage;
--- marketing/tailwind.config.js
+++ marketing/tailwind.config.js
@@ -4,7 +4,11 @@ const forms = require('@tailwindcss/forms');
 const typography = require('@tailwindcss/typography');
 
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './layout/**/*.{js,ts,jsx,tsx,md,mdx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {

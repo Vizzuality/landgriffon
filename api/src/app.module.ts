ca196691ed1b63fd548a8406c4c5cf75b68b754e
Tiago Garcia <tiagojsag@gmail.com>
2021-05-10 12:54:52 +0200

    Add code coverage testing to the api - Enable coverage debg

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './typeorm.config';
+import { typeOrmConfig } from 'typeorm.config';
 import { AuthenticationModule } from 'modules/authentication/authentication.module';
 import { UsersModule } from 'modules/users/users.module';
 import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from './filters/all-exceptions.exception.filter';
+import { AllExceptionsFilter } from 'filters/all-exceptions.exception.filter';
 
 @Module({
   imports: [
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
-import { AppModule } from './app.module';
+import { AppModule } from 'app.module';
 import * as config from 'config';
 
 async function bootstrap(): Promise<void> {

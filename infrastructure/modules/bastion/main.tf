6d1ba04430125698b76e8ad0978c3291c0a5df21
Tiago Garcia <tiagojsag@gmail.com>
2021-05-12 15:12:44 +0200

    Remove commented code

--- infrastructure/modules/bastion/main.tf
+++ infrastructure/modules/bastion/main.tf
@@ -33,28 +33,6 @@ resource "aws_security_group_rule" "default_ssh_egress" {
   security_group_id = aws_security_group.bastion_security_group.id
 }
 
-//resource "aws_security_group_rule" "default_http_egress" {
-//  type             = "egress"
-//  from_port        = "80"
-//  to_port          = "80"
-//  protocol         = "tcp"
-//  cidr_blocks      = ["0.0.0.0/0"]
-//  ipv6_cidr_blocks = ["::/0"]
-//
-//  security_group_id = aws_security_group.bastion_security_group.id
-//}
-//
-//resource "aws_security_group_rule" "default_https_egress" {
-//  type             = "egress"
-//  from_port        = "443"
-//  to_port          = "443"
-//  protocol         = "tcp"
-//  cidr_blocks      = ["0.0.0.0/0"]
-//  ipv6_cidr_blocks = ["::/0"]
-//
-//  security_group_id = aws_security_group.bastion_security_group.id
-//}
-
 #
 # Bastion resources
 #

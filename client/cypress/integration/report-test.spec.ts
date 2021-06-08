0d1f39c2a38a7b0b20e6f9f405d007b6929ca8ab
alexeh <alexurola@gmail.com>
2021-06-08 08:28:04 +0200

    Test LCOV file copy to /github/home

--- client/cypress/integration/report-test.spec.ts
+++ client/cypress/integration/report-test.spec.ts
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+
+
 // report-test.spec.ts created with Cypress
 //
 // Start writing your Cypress tests below!
@@ -6,9 +8,16 @@
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
 
+/**
+ * @debt: This is a dumb test just to run through the actual FE code and generate a non-empty report
+ * to avoid a failing CI stage (uploading report to Code-Climate)
+ *
+ * Feel free to delete this suite as soon as you generate an actual unit/integration/e2e test
+ */
 
+// @ts-ignore
 describe('Dumb test to generate report', ()=> {
-  it('do nothing', ()=>{
+  it('Should pass to generate a minimum report to pass CI stage', ()=>{
     cy.visit('localhost:3000')
     cy.get('h1').contains('Welcome to Vizzuality')
   })

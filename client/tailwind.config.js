eee5f20640bda69939b518510148f81240c10c9d
David Inga <david.inga@vizzuality.com>
2021-06-17 11:17:56 +0200

    fixed production build for tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,11 +6,7 @@ const lineClamp = require('./lib/tailwind/line-clamp');
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV !== 'development',
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-      './containers/**/*.{js,ts,jsx,tsx}',
-    ],
+    content: ['./**/*.ts', './**/*.tsx'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {

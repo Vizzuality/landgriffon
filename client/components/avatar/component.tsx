dfae9afa33d938d1e04f5b61a2749d24b1fb9532
David Inga <david.inga@vizzuality.com>
2021-06-17 18:03:05 +0200

    components

--- client/components/avatar/component.tsx
+++ client/components/avatar/component.tsx
@@ -1,25 +1,14 @@
-import { ButtonHTMLAttributes, FC } from 'react';
+import { ImgHTMLAttributes, FC } from 'react';
 import cx from 'classnames';
 
-export interface AvatarProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  children?: React.ReactNode;
-  className?: string;
-  bgImage?: string;
-}
+export type AvatarProps = ImgHTMLAttributes<HTMLImageElement>;
 
-export const Avatar: FC<AvatarProps> = ({ children, className, bgImage }: AvatarProps) => (
-  <div
-    className={cx({
-      'relative z-0 hover:z-10 flex items-center justify-center bg-transparent bg-cover bg-no-repeat bg-center border-2 border-gray-700 w-10 h-10 rounded-full':
-        true,
-      [className]: !!className,
-    })}
-    style={{
-      ...(bgImage && { backgroundImage: `url(${bgImage})` }),
-    }}
-  >
-    {children}
-  </div>
+export const Avatar: FC<AvatarProps> = ({ className, ...props }: AvatarProps) => (
+  <img
+    alt={props.alt || 'Profile avatar'}
+    className={cx('inline-block h-10 w-10 rounded-full', className)}
+    {...props}
+  />
 );
 
 export default Avatar;
---
+++ client/components/breadcrumb/component.stories.tsx
@@ -0,0 +1,34 @@
+import { Story } from '@storybook/react/types-6-0';
+import Breadcrumb, { BreadcrumbProps } from './component';
+
+export default {
+  title: 'Components/Breadcrumb',
+  component: Breadcrumb,
+  argTypes: {},
+};
+
+const Template: Story<BreadcrumbProps> = ({ pages }: BreadcrumbProps) => (
+  <Breadcrumb pages={pages} />
+);
+
+export const Default = Template.bind({});
+
+Default.args = {
+  pages: [
+    {
+      current: false,
+      href: '#',
+      name: 'Analysis',
+    },
+    {
+      current: true,
+      href: '#',
+      name: 'Scenarios',
+    },
+    {
+      current: false,
+      href: '#',
+      name: 'Edit',
+    },
+  ],
+};

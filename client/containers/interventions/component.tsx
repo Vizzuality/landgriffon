af5bd07e3f7a7c4bbd163975484f7cce3fc2e1b8
David Inga <david.inga@vizzuality.com>
2021-06-28 11:11:04 +0200

    added comparison and color tweaks on the scenario component

--- client/containers/interventions/component.tsx
+++ client/containers/interventions/component.tsx
@@ -2,6 +2,7 @@ import { useCallback } from 'react';
 import { PlusIcon } from '@heroicons/react/solid';
 import { useAppDispatch } from 'store/hooks';
 import { setSubContentCollapsed } from 'store/features/analysis';
+import { Button } from 'components/button';
 
 const items = [
   {
@@ -28,14 +29,10 @@ const Interventions = () => {
     <div>
       <div className="flex items-center content-between">
         <div className="flex-1">{items.length} interventions</div>
-        <button
-          type="button"
-          className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          onClick={handleNewIntervention}
-        >
-          <PlusIcon className="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+        <Button onClick={handleNewIntervention}>
+          <PlusIcon className="-ml-1 mr-1 h-5 w-5" aria-hidden="true" />
           New
-        </button>
+        </Button>
       </div>
       <div className="bg-white border border-gray-300 overflow-hidden rounded-md mt-4">
         <ul className="divide-y divide-gray-300">

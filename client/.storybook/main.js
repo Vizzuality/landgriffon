4e940bdacdb063c49a260e0d1b5623f6fbe78f3a
David Inga <david.inga@vizzuality.com>
2021-07-05 11:17:28 +0200

    fixed storybook configuration after src folder migration

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -2,8 +2,8 @@ const path = require("path");
 
 module.exports = {
   stories: [
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
-    "../containers/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/containers/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   /* nextjs -> no need to import React and can use alias modules */
@@ -12,7 +12,7 @@ module.exports = {
     // RESOLVE node_modules
     // If you want to add a directory to search in that takes precedence over node_modules/:
     // https://webpack.js.org/configuration/resolve/#resolvemodules
-    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"];
+    config.resolve.modules = [path.resolve(__dirname, "../src"), "node_modules"];
 
     // *************************
     // assets

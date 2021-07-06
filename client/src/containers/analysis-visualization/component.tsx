b0946996c949615a3cbfe4d1ed702fd050b8c362
David Inga <david.inga@vizzuality.com>
2021-07-06 16:34:39 +0200

    show visualization mode in the URL

--- client/src/containers/analysis-visualization/component.tsx
+++ client/src/containers/analysis-visualization/component.tsx
@@ -1,9 +1,12 @@
-import { useCallback } from 'react';
+import { useEffect } from 'react';
+import Link from 'next/link';
+import classNames from 'classnames';
+import { useRouter } from 'next/router';
 import { ChartPieIcon, MapIcon, TableIcon } from '@heroicons/react/outline';
 import { useAppSelector, useAppDispatch } from 'store/hooks';
 import { visualizationMode, setVisualizationMode } from 'store/features/analysis';
+import type { AnalysisState } from 'store/features/analysis';
 import Map from 'components/map';
-import classNames from 'classnames';
 
 const MAPBOX_API_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN;
 
@@ -12,43 +15,55 @@ const impactFactors = [{ id: '1', name: 'Rice', 2021: 342, 2022: 632, 2023: 1332
 const AnalysisVisualization = () => {
   const currentMode = useAppSelector(visualizationMode);
   const dispatch = useAppDispatch();
+  const { query } = useRouter();
+  const { mode } = query;
 
-  const handleChange = useCallback((event) => {
-    const selectedMode = event.currentTarget.dataset.mode;
+  useEffect(() => {
+    const selectedMode = (mode as AnalysisState['visualizationMode']) || 'map';
     dispatch(setVisualizationMode(selectedMode));
-  }, []);
+  }, [mode]);
 
   return (
     <section className="min-w-0 flex-1 h-full flex flex-col overflow-hidden lg:order-last bg-white">
       {/* Vis selector: map, table or chart */}
       <div className="absolute right-6 top-6 z-10 inline-flex shadow-sm rounded-md">
-        <button
-          type="button"
-          data-mode="map"
-          className={classNames(
-            'relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500',
-            { 'z-10 outline-none ring-1 ring-indigo-500 border-indigo-500': currentMode === 'map' }
-          )}
-          onClick={handleChange}
+        <Link
+          href={{
+            pathname: '/analysis',
+            query: { ...query, mode: 'map' },
+          }}
         >
-          <MapIcon className="h-6 w-6" aria-hidden="true" />
-        </button>
-        <button
-          type="button"
-          data-mode="table"
-          className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-          onClick={handleChange}
+          <a
+            className={classNames(
+              'relative inline-flex items-center px-4 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700',
+              {
+                'z-10 outline-none ring-1 ring-green-700 border-green-700': currentMode === 'map',
+              }
+            )}
+          >
+            <MapIcon className="h-6 w-6" aria-hidden="true" />
+          </a>
+        </Link>
+        <Link
+          href={{
+            pathname: '/analysis',
+            query: { ...query, mode: 'table' },
+          }}
         >
-          <TableIcon className="h-6 w-6" aria-hidden="true" />
-        </button>
-        <button
-          type="button"
-          data-mode="chart"
-          className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500"
-          onClick={handleChange}
+          <a className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700">
+            <TableIcon className="h-6 w-6" aria-hidden="true" />
+          </a>
+        </Link>
+        <Link
+          href={{
+            pathname: '/analysis',
+            query: { ...query, mode: 'chart' },
+          }}
         >
-          <ChartPieIcon className="h-6 w-6" aria-hidden="true" />
-        </button>
+          <a className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700">
+            <ChartPieIcon className="h-6 w-6" aria-hidden="true" />
+          </a>
+        </Link>
       </div>
 
       {currentMode === 'map' && (

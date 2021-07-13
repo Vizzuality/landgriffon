81048707101ceb81cf12aa3b8679b4ff7634575f
David Inga <david.inga@vizzuality.com>
2021-07-13 11:23:48 +0200

    scenarios list connected with the API

--- client/src/containers/scenarios/component.tsx
+++ client/src/containers/scenarios/component.tsx
@@ -1,25 +1,37 @@
 import Link from 'next/link';
-import { PlusIcon } from '@heroicons/react/solid';
+import { PlusIcon, XCircleIcon } from '@heroicons/react/solid';
 import ScenariosList from 'containers/scenarios/list';
 import { Anchor } from 'components/button';
+import type { UseQueryResult } from 'react-query';
 import type { Scenarios } from './types';
 
 type ScenariosProps = {
-  scenarios: {
-    isLoading: boolean;
-    data: Scenarios;
-  };
+  scenarios: UseQueryResult;
 };
 
 const ScenariosComponent: React.FC<ScenariosProps> = ({ scenarios }: ScenariosProps) => {
-  const { data, isLoading } = scenarios;
+  const { data, isLoading, error } = scenarios;
 
   return (
     <div>
       <h1>Analyse impact</h1>
       <p className="text-sm mt-2 mb-2">Select the scenario you want to analyse</p>
       {isLoading && <p>Loading scenarios...</p>}
-      {!isLoading && data && <ScenariosList data={data} />}
+      {!isLoading && data && <ScenariosList data={data as Scenarios} />}
+      {!isLoading && error && (
+        <div className="rounded-md bg-red-50 p-4 my-4">
+          <div className="flex">
+            <div className="flex-shrink-0">
+              <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+            </div>
+            <div className="ml-3">
+              <h3 className="text-sm font-medium text-red-800">
+                There was an error with your request
+              </h3>
+            </div>
+          </div>
+        </div>
+      )}
       <Link href="/analysis?new_scenario=true" shallow>
         <Anchor size="xl">
           <PlusIcon className="-ml-5 mr-3 h-5 w-5" aria-hidden="true" />
--- client/src/containers/scenarios/index.tsx
+++ client/src/containers/scenarios/index.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
+import { getScenarios } from 'services/scenarios';
 import Component from './component';
-import type { Scenario, Scenarios } from './types';
+import type { Scenario } from './types';
 
 /**
  * Actual data to the data response
@@ -10,80 +11,18 @@ const ACTUAL_DATA: Scenario = {
   title: 'Actual data',
 };
 
-/**
- * Scenarios mock data
- */
-const DATA: Scenarios = [
-  {
-    title: 'Scenario name 1',
-    description: null,
-    id: 1,
-    updatedAt: Date.now(),
-    // user: { name: 'Francis' },
-  },
-  {
-    title: 'Scenario name 2',
-    description: null,
-    id: 2,
-    updatedAt: Date.now(),
-    // user: { name: 'Elena' },
-  },
-  {
-    title: 'Scenario name 3',
-    description: null,
-    id: 3,
-    updatedAt: Date.now(),
-    // user: { name: 'Pedro' },
-  },
-  {
-    title: 'Scenario name 4',
-    description: null,
-    id: 4,
-    updatedAt: Date.now(),
-    // user: { name: 'Er Mendo' },
-  },
-];
-
-type Response = {
-  isLoading: boolean;
-  data: Scenarios;
-  error: unknown;
-};
-
-const fetch = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () =>
-        resolve({
-          isLoading: false,
-          error: null,
-          data: [ACTUAL_DATA, ...DATA], // Injecting Actual data
-        } as Response),
-      1000
-    )
-  );
-
 const ScenariosContainer: React.FC = () => {
-  // Simulating React-Query
-  const [response, setResponse] = useState({
-    isLoading: false,
-    data: null,
-    error: null,
-  } as Response);
+  const response = useQuery('scenariosList', getScenarios, { retry: false });
+  const result =
+    response.isSuccess && response.data
+      ? {
+          ...response,
+          // injecting actual data
+          data: [ACTUAL_DATA, ...response.data],
+        }
+      : response;
 
-  useEffect(() => {
-    const fetchScenarios = async () => {
-      setResponse({
-        ...response,
-        isLoading: true,
-      });
-      const mock = await fetch();
-      setResponse(mock as Response);
-    };
-    fetchScenarios();
-  }, []);
-
-  return <Component scenarios={response} />;
+  return <Component scenarios={result} />;
 };
 
 export default ScenariosContainer;

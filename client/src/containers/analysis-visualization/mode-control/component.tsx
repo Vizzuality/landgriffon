e3b10b2df4a7d8c2783a83b165854ca33b419bf5
Pedro Pimenta <pedro@pimenta.co>
2021-07-28 10:09:33 +0200

    UI adjustments on Mode Control and Collapse Button

--- client/src/containers/analysis-visualization/mode-control/component.tsx
+++ client/src/containers/analysis-visualization/mode-control/component.tsx
@@ -5,7 +5,7 @@ import type { AnalysisState } from 'store/features/analysis';
 import type { ParsedUrlQuery } from 'querystring';
 
 const CONTROL_ITEM_CLASS_NAMES =
-  'relative inline-flex items-center px-4 py-2 border border-gray-100 bg-white text-sm font-medium text-gray-600 hover:bg-green-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700';
+  'relative inline-flex items-center px-4 py-1.5 border border-gray-100 bg-white text-sm font-medium text-gray-600 hover:bg-green-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700';
 const CONTROL_ITEM_ACTIVE_CLASS_NAMES =
   'z-10 outline-none ring-1 ring-green-700 text-green-700 border-green-700 bg-green-50';
 

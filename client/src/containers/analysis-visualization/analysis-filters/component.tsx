2e2a3dcc023fa86ea48fd25d9af472b772ddcfeb
Pedro Pimenta <pedro@pimenta.co>
2021-08-02 11:24:20 +0200

    Add Filters dropdown + Materials and Origin Regions

--- client/src/containers/analysis-visualization/analysis-filters/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/component.tsx
@@ -1,12 +1,18 @@
 import ImpactIndicatorsFilter from './impact-indicators';
 import GroupByFilter from './group-by';
 import YearsFilter from './years';
+import MoreFilters from './more-filters';
+import Materials from './materials';
+import OriginRegions from './origin-regions';
+
+const FILTERS = [<Materials />, <OriginRegions />];
 
 const AnalysisFilters: React.FC = () => (
   <div className="absolute left-12 top-6 z-10 inline-flex gap-2">
     <ImpactIndicatorsFilter />
     <GroupByFilter />
     <YearsFilter />
+    <MoreFilters filters={FILTERS} />
   </div>
 );
 

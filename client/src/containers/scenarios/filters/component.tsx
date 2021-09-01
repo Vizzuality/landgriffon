28659782a83ce11e9a76993599dd3a037acd1a13
anamontiaga <amontiaga@gmail.com>
2021-09-01 17:37:45 +0200

    expand rows and set custom icon

--- client/src/containers/scenarios/filters/component.tsx
+++ client/src/containers/scenarios/filters/component.tsx
@@ -50,29 +50,29 @@ const ScenariosFilters = () => {
   }, [term]);
 
   return (
-    <div className="flex justify-between items-center">
+    <div className="flex items-center justify-between">
       {isSearchEnable ? (
         <div className="w-full sm:max-w-xs">
           <div className="relative">
-            <div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-              <SearchIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <SearchIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
             </div>
             <input
               id="search"
               name="search"
-              className="block w-full bg-white border border-gray-300 rounded-md py-2 pl-10 pr-3 text-sm placeholder-gray-500 focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-green-700 focus:border-green-700 sm:text-sm"
+              className="block w-full py-2 pl-10 pr-3 text-sm placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-green-700 focus:border-green-700 sm:text-sm"
               placeholder="Search"
               type="search"
               onChange={handleSearchByTerm}
             />
           </div>
           <button type="button" onClick={toggleSearch}>
-            <XIcon className="h-6 w-6" />
+            <XIcon className="w-6 h-6" />
           </button>
         </div>
       ) : (
         <button type="button" onClick={toggleSearch}>
-          <SearchIcon className="h-6 w-6" />
+          <SearchIcon className="w-6 h-6" />
         </button>
       )}
 

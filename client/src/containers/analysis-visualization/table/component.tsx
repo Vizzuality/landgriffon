91ae0054585c192afa3a124c02b793902c150731
anamontiaga <amontiaga@gmail.com>
2021-09-01 17:37:45 +0200

    expand rows and set custom icon

--- client/src/containers/analysis-visualization/table/component.tsx
+++ client/src/containers/analysis-visualization/table/component.tsx
@@ -1,29 +1,42 @@
+/* eslint-disable react/prop-types */
 import { Table as TableAntd } from 'antd';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline';
 
 export interface TableProps {
   columns: Array<object>;
-  dataSource: Array<object>;
+  dataSource: Array<{
+    key: string;
+    commodity: string;
+    children?: React.ReactNode;
+    [key: number]: number;
+  }>;
 }
 
 export const Table: React.FC<TableProps> = ({ columns, dataSource }: TableProps) => (
   <TableAntd
-    // expandable={{
-    //   expandedRowRender: (record) => <p style={{ margin: 0 }}>{record.description}</p>,
-    //   expandIcon: ({ expanded, onExpand, record }) =>
-    //     expanded ? (
-    //       <button type="button" onClick={(e) => onExpand(record, e)}>
-    //         A
-    //       </button>
-    //     ) : (
-    //       <button type="button" onClick={(e) => onExpand(record, e)}>
-    //         B
-    //       </button>
-    //     ),
-    // }}
+    expandable={{
+      rowExpandable: (record) => !!record.children,
+
+
+      expandIcon: ({ expanded, onExpand, record }) => {
+        if (!record.children) return null;
+        if (expanded)
+          return (
+            <button type="button" onClick={(e) => onExpand(record, e)}>
+              <ChevronUpIcon className="w-5 h-4 mr-2 text-gray-800" />
+            </button>
+          );
+        return (
+          <button type="button" onClick={(e) => onExpand(record, e)}>
+            <ChevronDownIcon className="w-5 h-4 mr-2 text-gray-800" />
+          </button>
+        );
+      },
+    }}
     dataSource={dataSource}
     columns={columns}
     pagination={false}
-    scroll={{ x: 2500 }}
+    scroll={{ x: 2000 }}
   />
 );
 

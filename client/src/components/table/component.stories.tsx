17394bf710d38ff8425ad56f8f9a4d7967127144
anamontiaga <amontiaga@gmail.com>
2021-09-01 14:39:11 +0200

    add basic less styles

--- client/src/components/table/component.stories.tsx
+++ client/src/components/table/component.stories.tsx
@@ -14,48 +14,77 @@ export const Default = Template.bind({});
 Default.args = {
   columns: [
     {
+      title: 'YEAR',
+      dataIndex: 'commodity',
+      key: 'commodity',
+      width: 250,
+      fixed: 'left',
+    },
+    {
       title: '2021',
       dataIndex: '2021',
       key: '2021',
-      width: 150,
+      width: 100,
       fixed: 'left',
     },
     {
       title: '2022',
       dataIndex: '2022',
       key: '2022',
-      width: 150,
+      width: 100,
     },
     {
       title: '2023',
       dataIndex: '2023',
       key: '2023',
-      width: 150,
+      width: 100,
     },
     {
       title: '2024',
       dataIndex: '2024',
       key: '2024',
-      width: 150,
+      width: 100,
     },
     {
       title: '2025',
       dataIndex: '2025',
       key: '2025',
-      width: 150,
+      width: 100,
     },
   ],
   dataSource: [
     {
       key: '1',
+      commodity: 'Carbon emissions (tCO2)',
       2021: 20,
       2022: 30,
       2023: 50,
       2024: 30,
       2025: 36,
+      children: [
+        {
+          key: 11,
+          commodity: 'Rice',
+          2021: 220,
+          2022: 230,
+          2023: 250,
+          2024: 230,
+          2025: 236,
+        },
+        {
+          key: 12,
+          commodity: 'Palm Oil',
+          2021: 20,
+          2022: 130,
+          2023: 20,
+          2024: 30,
+          2025: 336,
+        },
+      ],
     },
     {
       key: '2',
+      commodity: 'Deforestation (Ha)',
       2021: 20,
       2022: 30,
       2023: 150,
@@ -64,6 +93,7 @@ Default.args = {
     },
     {
       key: '3',
+      commodity: 'Unsustainable water use (M3)',
       2021: 200,
       2022: 30,
       2023: 150,
@@ -72,6 +102,7 @@ Default.args = {
     },
     {
       key: '4',
+      commodity: 'Biodiversity (X)',
       2021: 50,
       2022: 30,
       2023: 100,
--- client/src/components/table/component.tsx
+++ client/src/components/table/component.tsx
@@ -7,10 +7,23 @@ export interface TableProps {
 
 export const Table: React.FC<TableProps> = ({ columns, dataSource }: TableProps) => (
   <TableAntd
+    // expandable={{
+    //   expandedRowRender: (record) => <p style={{ margin: 0 }}>{record.description}</p>,
+    //   expandIcon: ({ expanded, onExpand, record }) =>
+    //     expanded ? (
+    //       <button type="button" onClick={(e) => onExpand(record, e)}>
+    //         A
+    //       </button>
+    //     ) : (
+    //       <button type="button" onClick={(e) => onExpand(record, e)}>
+    //         B
+    //       </button>
+    //     ),
+    // }}
     dataSource={dataSource}
     columns={columns}
     pagination={false}
-    scroll={{ x: 1500, y: 300 }}
+    scroll={{ x: 2500 }}
   />
 );
 

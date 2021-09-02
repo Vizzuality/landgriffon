cb6fd3235b5123d37db6b925f2016f1a3e240080
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2021-09-02 15:53:05 +0200

    Widget: first steps

---
+++ client/src/components/widget/component.stories.tsx
@@ -0,0 +1,38 @@
+import type { Story } from '@storybook/react';
+import Chart from 'components/chart';
+import AreaStacked from 'components/chart/area-stacked';
+
+import Widget, { WidgetProps } from './component';
+
+import MOCK from './mock';
+
+export default {
+  title: 'Components/Widget',
+  component: Widget,
+  argTypes: {},
+};
+
+const Template: Story<WidgetProps> = (props: WidgetProps) => (
+  <div className="w-full h-96">
+    <Widget {...props}>
+      <Chart>
+        <AreaStacked
+          data={MOCK}
+          margin={{ top: 12, right: 12, bottom: 30, left: 30 }}
+          keys={['beef', 'coal', 'corn', 'duck', 'mint', 'poultry', 'soy']}
+          target={120}
+          settings={{
+            tooltip: true,
+            projection: true,
+            target: true,
+          }}
+        />
+      </Chart>
+    </Widget>
+  </div>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  title: 'Carbon emissions (CO2e)',
+};

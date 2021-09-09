28aa3e2834b06f2cf34ec8f029faa7aeb90806cc
anamontiaga <amontiaga@gmail.com>
2021-09-09 17:52:15 +0200

    import indicator and by filters

--- client/src/containers/analysis-visualization/analysis-filters/group-by/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/group-by/component.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { analysis, setFilter } from 'store/features/analysis';
 
@@ -5,7 +6,6 @@ import { Select } from 'antd';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 
 import type { Group } from 'types';
-import { useCallback } from 'react';
 
 const ALL_GROUPS: Group[] = [
   {
@@ -42,29 +42,27 @@ const GroupByFilter: React.FC<GroupByFilterProps> = () => {
   }, []);
 
   return (
-    <>
-      <Select
-        value={filters.by}
-        onChange={handleChange}
-        className="w-40"
-        optionLabelProp="label"
-        suffixIcon={<ChevronDownIcon />}
-      >
-        {ALL_GROUPS.map((group) => (
-          <Select.Option
-            key={group.id}
-            value={group.id}
-            label={
-              <>
-                <span className="text-gray-500">by</span> {group.name}
-              </>
-            }
-          >
-            {group.name}
-          </Select.Option>
-        ))}
-      </Select>
-    </>
+    <Select
+      value={filters.by}
+      onChange={handleChange}
+      className="w-40"
+      optionLabelProp="label"
+      suffixIcon={<ChevronDownIcon />}
+    >
+      {ALL_GROUPS.map((group) => (
+        <Select.Option
+          key={group.id}
+          value={group.id}
+          label={
+            <>
+              <span className="text-gray-500">by</span> {group.name}
+            </>
+          }
+        >
+          {group.name}
+        </Select.Option>
+      ))}
+    </Select>
   );
 };
 

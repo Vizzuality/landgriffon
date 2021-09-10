d4c15141246a09e8dee55b9bd231366f9d2b44cd
anamontiaga <amontiaga@gmail.com>
2021-09-10 09:37:48 +0200

    remove custom order button set antd one

--- client/src/containers/analysis-visualization/analysis-table/table-title/component.tsx
+++ client/src/containers/analysis-visualization/analysis-table/table-title/component.tsx
@@ -1,4 +1,4 @@
-import { SwitchVerticalIcon } from '@heroicons/react/outline';
+// import { SwitchVerticalIcon } from '@heroicons/react/outline';
 
 interface TitleProps {
   title: string;
@@ -7,10 +7,10 @@ interface TitleProps {
 const Title: React.FC<TitleProps> = ({ title }: TitleProps) => (
   <div className="flex items-center">
     <p className="m-0">{title}</p>
-    <button type="button" onClick={() => console.log('sort column')}>
+    {/* <button type="button" onClick={() => console.log('sort column')}>
       {' '}
       <SwitchVerticalIcon className="w-3 h-3 mx-4 text-black" />
-    </button>
+    </button> */}
   </div>
 );
 

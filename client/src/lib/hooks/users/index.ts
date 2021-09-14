9130a870e9d72dd72a902570bdb7dc0a94bbf766
Alvaro Leal <pursuance@gmail.com>
2021-09-14 18:30:37 +0200

    Move users to hooks

---
+++ client/src/lib/hooks/users/index.ts
@@ -0,0 +1,22 @@
+import { useMemo } from 'react';
+import { useQueryClient } from 'react-query';
+import USERS from 'services/users';
+
+export function useUsers(session) {
+  const queryClient = useQueryClient();
+  queryClient.prefetchQuery('me', () =>
+    USERS.request({
+      method: 'GET',
+      url: '/me',
+      headers: {
+        Authorization: `Bearer ${session.accessToken}`,
+      },
+    }).then((response) => response.data)
+  );
+
+  return useMemo(() => {
+    return {
+      queryClient,
+    };
+  }, [session, queryClient]);
+}

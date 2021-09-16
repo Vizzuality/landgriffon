00dbd5b3606ed93ff769addb93140d541165d0f0
David Inga <david.inga@vizzuality.com>
2021-09-16 10:33:59 +0200

    fixing map style warning; also removed sortable list on the legends

--- client/src/components/map/legend/component.tsx
+++ client/src/components/map/legend/component.tsx
@@ -3,8 +3,6 @@ import cx from 'classnames';
 
 import { ArrowUpIcon } from '@heroicons/react/outline';
 
-import SortableList from './sortable/list';
-
 export interface LegendProps {
   className?: string;
   children: React.ReactNode;
@@ -16,7 +14,6 @@ export const Legend: React.FC<LegendProps> = ({
   children,
   className = '',
   maxHeight,
-  onChangeOrder,
 }: LegendProps) => {
   const [active, setActive] = useState(true);
 
@@ -55,9 +52,7 @@ export const Legend: React.FC<LegendProps> = ({
           }}
         >
           <div className="absolute top-0 left-0 z-10 w-full h-4 pointer-events-none bg-gradient-to-b from-white via-white" />
-          <div className="overflow-x-hidden overflow-y-auto">
-            <SortableList onChangeOrder={onChangeOrder}>{children}</SortableList>
-          </div>
+          <div className="overflow-x-hidden overflow-y-auto">{children}</div>
           <div className="absolute bottom-0 left-0 z-10 w-full h-3 pointer-events-none bg-gradient-to-t from-white via-white" />
         </div>
       )}

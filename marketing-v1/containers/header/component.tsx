477abec5af2bcf27c577b0b538a1d7e7ee56ad8e
anamontiaga <amontiaga@gmail.com>
2021-09-24 19:14:07 +0200

    header

--- marketing-v1/containers/header/component.tsx
+++ marketing-v1/containers/header/component.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-import cx from 'classnames';
-
 import Link from 'next/link';
 
-import Wrapper from 'containers/wrapper';
+import Button from 'components/button';
 
 export const Header = () => (
-  <header
-    className={cx({
-      'w-full row-auto': true,
-    })}
-  >
-    <Wrapper>
-      <nav className="relative flex flex-wrap items-center justify-between mt-10 md:mt-0 navbar-expand-lg">
-        <Link href="/">LANDGRIFFON</Link>
-        <Link href="/about-us">About Us</Link>
-      </nav>
-    </Wrapper>
+  <header className="w-full row-auto pt-4 pb-5">
+    <nav className="relative flex flex-wrap items-center justify-between mt-1 md:mt-0 navbar-expand-lg">
+      <Link href="/">
+        <h1 className="text-lg tracking-widest font-heading">LANDGRIFFON</h1>
+      </Link>
+      <div className="flex items-center space-x-12 font-sans">
+        <Link href="/about-us">
+          <div className="font-sans text-base cursor-pointer">
+            <p>About Us</p>
+          </div>
+        </Link>
+        <Button
+          theme="primary"
+          size="l"
+          className="flex-shrink-0 ml-5 h-11 w-44"
+          onClick={() => console.info('Contact us')}
+        >
+          Contact us
+        </Button>
+      </div>
+    </nav>
   </header>
 );
 

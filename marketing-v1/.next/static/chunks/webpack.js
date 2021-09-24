b440d54fc1f3d230bc7f88a5b8fa7fd6d4d29d04
anamontiaga <amontiaga@gmail.com>
2021-09-24 16:32:03 +0200

    fix build

--- marketing-v1/.next/static/chunks/webpack.js
+++ marketing-v1/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0dda84c727c7284b0d96"; }
+/******/ 		__webpack_require__.h = function() { return "9bb74c8faffb02c51b8a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

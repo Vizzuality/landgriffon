45a66680e86c0fff4be6a49ab3f3b7b507c3750c
anamontiaga <amontiaga@gmail.com>
2021-09-24 17:30:51 +0200

    fonts install

--- marketing-v1/pages/index.tsx
+++ marketing-v1/pages/index.tsx
@@ -5,7 +5,14 @@ const Home: React.FC = () => (
     <Head>
       <title>Welcome</title>
     </Head>
-    <h1 className="font-mono text-3xl bg-orange">LANDGRIFFON.</h1>
+    <h1 className="font-heading text-7xl bg-orange">LANDGRIFFON.</h1>
+
+    <div className="w-96">
+      <p className="font-sans text-xl font-semibold bg-white">
+        We help companies become sustainable by understanding and planning strategies to manage
+        environmental impacts and risks in food supply chains.{' '}
+      </p>
+    </div>
   </div>
 );
 

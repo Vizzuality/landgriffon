892d8126e17159692de6ab2f6a5837679df54d8f
anamontiaga <amontiaga@gmail.com>
2021-09-27 11:43:07 +0200

    measure, analize and forecast basic page

---
+++ marketing-v1/pages/measure.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+
+import { motion } from 'framer-motion';
+
+import Footer from 'containers/footer';
+import Header from 'containers/header';
+import Hero from 'containers/hero';
+import Wrapper from 'containers/wrapper';
+
+const Measure: React.FC = () => (
+  <div>
+    <Wrapper>
+      <Head>
+        <title>Measure</title>
+      </Head>
+      <Header />
+      <motion.div layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+        <Hero />
+      </motion.div>
+      <Footer />
+    </Wrapper>
+  </div>
+);
+
+export default Measure;

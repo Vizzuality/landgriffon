b82f16f4ce176b6385241917fbe30c25af450de6
anamontiaga <amontiaga@gmail.com>
2021-09-27 13:18:54 +0200

    hero

--- marketing-v1/pages/measure.tsx
+++ marketing-v1/pages/measure.tsx
@@ -15,7 +15,12 @@ const Measure: React.FC = () => (
       </Head>
       <Header />
       <motion.div layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-        <Hero />
+        <Hero
+          title="Measure"
+          subtitle="Map where your raw materials come from."
+          description="Do you know all your farm locations or are you just starting out? No matter your level of value chain information, our accurate probabilistic sourcing model tells you where your materials are most likely to come from."
+          imageURL="/images/measure/measure-map.jpg"
+        />
       </motion.div>
       <Footer />
     </Wrapper>

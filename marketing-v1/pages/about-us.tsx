0cca52082580f76fac9439787ade70a587938e07
anamontiaga <amontiaga@gmail.com>
2021-10-01 10:18:30 +0200

    about-us: herostyles

--- marketing-v1/pages/about-us.tsx
+++ marketing-v1/pages/about-us.tsx
@@ -6,21 +6,18 @@ import AboutHero from 'containers/about/hero';
 import TeamCarousel from 'containers/about/team-carousel';
 import Footer from 'containers/footer';
 import Header from 'containers/header';
-import Wrapper from 'containers/wrapper';
 
 const AboutUs: React.FC = () => (
   <div>
-    <Wrapper>
-      <Head>
-        <title>About Us</title>
-      </Head>
-      <Header />
-      <motion.div layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
-        <AboutHero />
-        <TeamCarousel />
-      </motion.div>
-      <Footer />
-    </Wrapper>
+    <Head>
+      <title>About Us</title>
+    </Head>
+    <Header />
+    <motion.div layout initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+      <AboutHero />
+      <TeamCarousel />
+    </motion.div>
+    <Footer />
   </div>
 );
 

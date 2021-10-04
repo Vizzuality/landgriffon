05aad5d2ce73f1db0b883b202c9d420e2649c44e
anamontiaga <amontiaga@gmail.com>
2021-10-04 09:22:40 +0200

    home: services

--- marketing-v1/pages/index.tsx
+++ marketing-v1/pages/index.tsx
@@ -6,8 +6,8 @@ import { motion } from 'framer-motion';
 import Footer from 'containers/footer';
 import Header from 'containers/header';
 import Features from 'containers/home/features';
+import Services from 'containers/home/services';
 import MetaFooter from 'containers/meta-footer';
-import Wrapper from 'containers/wrapper';
 
 const Home: React.FC = () => (
   <div>
@@ -41,68 +41,7 @@ const Home: React.FC = () => (
           </div>
         </div>
       </section>
-
-      <section className="pt-40 pb-20">
-        <Wrapper>
-          <h2 className="pb-12 text-5xl font-sans-semibold text-green">Services</h2>
-          <div className="flex items-center justify-between font-sans">
-            <h3 className="w-4/6 text-7xl">Measure and manage supply chain impacts.</h3>
-            <p className="w-2/6 text-xl">
-              LandGriffon is a powerful modeling tool backed by a team of experts to empower you to
-              change your business for the better.
-            </p>
-          </div>
-        </Wrapper>
-      </section>
-
-      <section className="bg-bege">
-        <Wrapper>
-          <div className="flex justify-between space-x-64 p-28">
-            <Image
-              alt="plantation from the sky"
-              height="942px"
-              width="588px"
-              src="/images/home/home-2.jpg"
-            />
-            <nav className="flex flex-col self-center h-full space-y-10">
-              <div className="border-b border-black">
-                <a
-                  className="text-5xl underline font-sans-semibold hover:no-underline"
-                  href="/measure"
-                >
-                  01 Measure
-                </a>
-                <p className="font-sans text-xl py-9">
-                  Turn procurement data into accurate estimates of environmental impacts.
-                </p>
-              </div>
-              <div className="border-b border-black">
-                <a
-                  className="text-5xl underline font-sans-semibold hover:no-underline"
-                  href="/analyze"
-                >
-                  02 Analyze
-                </a>
-                <p className="font-sans text-xl py-9">
-                  Identify where your supply chain impacts are and uncover the key drivers.{' '}
-                </p>
-              </div>
-              <div className="border-b border-black">
-                <a
-                  className="text-5xl underline font-sans-semibold hover:no-underline"
-                  href="/forecast"
-                >
-                  03 Forecast
-                </a>
-                <p className="font-sans text-xl py-9">
-                  Prioritize areas for change and plot a path to sustainability.
-                </p>
-              </div>
-            </nav>
-          </div>
-        </Wrapper>
-      </section>
-
+      <Services />
       <Features />
     </motion.div>
 

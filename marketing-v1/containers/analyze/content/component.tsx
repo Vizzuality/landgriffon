da9216189ac9ccf06054e070d5df02249f154034
anamontiaga <amontiaga@gmail.com>
2021-10-05 10:39:15 +0200

    feat(measure, analyse and forecast): add content and finish desktop

---
+++ marketing-v1/containers/analyze/content/component.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import ContentCard from 'containers/content-card';
+import Wrapper from 'containers/wrapper';
+
+
+export const AnalyzeContent = () => (
+  <Wrapper>
+    <section className='flex flex-col justify-center px-16 py-36 space-y-36'>
+      <ContentCard
+        orientation="horizontal"
+        reverse
+        imageURL='/images/analyze/analyze-2.jpg'
+        title="See your whole supply chain sourcing area on one map."
+        description="Map out sourcing areas, analyze by total impact, risk metrics, or volumes, drill down to individual suppliers, and see the scientific data sources that were used to calculate their impacts."
+      />
+      <ContentCard
+        orientation="horizontal"
+        imageURL='/images/analyze/analyze-3.jpg'
+        title="Customized tools to meet the needs of your organization."
+        description="Use the power of diverse data visualization through charts, tables, and map views. Export reports, figures, and KPIs for financial reporting standards such as CDP, GRI, and SASB."
+      />
+    </section>
+  </Wrapper>
+);
+
+export default AnalyzeContent;
---
+++ marketing-v1/containers/content-card/component.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+export interface ContentCardProps {
+  description: string;
+  imageURL: string;
+  title: string;
+  hyperlink?: string;
+  orientation: 'vertical' | 'horizontal';
+  reverse?: boolean,
+}
+
+export const ContentCard: React.FC<ContentCardProps> = ({
+  description,
+  imageURL,
+  title,
+  hyperlink,
+  orientation = 'vertical',
+  reverse = false,
+}: ContentCardProps) => (
+  <>
+    {orientation === 'vertical' && (
+      <div>
+        <Image width="466px" height="575px" src={imageURL} />
+        <div className="flex flex-col py-8 space-y-4 w-96">
+          <h6 className="text-5xl font-sans-semibold">{title}</h6>
+          <p>{description}</p>
+          {hyperlink && (
+            <Link href={hyperlink}>
+              <a
+                className="text-2xl text-black underline font-sans-semibold hover:no-underline"
+                target="_blank"
+                href={hyperlink}
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </Link>
+          )}
+        </div>
+      </div>
+    )}
+    {orientation === 'horizontal' && (
+
+      <div className={reverse ? 'flex flex-row-reverse' : 'flex'}>
+        <Image width="657px" height="351px" src={imageURL} />
+        <div className="flex flex-col w-2/4 px-20 py-4 space-y-4">
+
+          <h6 className="text-5xl font-sans-semibold">{title}</h6>
+          <p>{description}</p>
+          {hyperlink && (
+            <Link href={hyperlink}>
+              <a
+                className="text-2xl text-black underline font-sans-semibold hover:no-underline"
+                target="_blank"
+                href={hyperlink}
+                rel="noreferrer"
+              >
+                Learn more
+              </a>
+            </Link>
+          )}
+        </div>
+      </div>
+    )}
+  </>
+
+);
+
+export default ContentCard;

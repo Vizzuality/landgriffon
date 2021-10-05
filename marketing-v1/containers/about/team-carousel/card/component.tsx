bb301503432e4f9f81aca103cbc5523e20991633
anamontiaga <amontiaga@gmail.com>
2021-10-05 17:13:44 +0200

    feat(team-carousel):responsive desktop devices

--- marketing-v1/containers/about/team-carousel/card/component.tsx
+++ marketing-v1/containers/about/team-carousel/card/component.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
+import cx from 'classnames';
+
 import Image from 'next/image';
 
 export interface CardProps {
-  role: string;
-  name: string;
-  photo: string;
+  className?: string;
+  role?: string;
+  name?: string;
+  photo?: string;
 }
 
-export const Card: React.FC<CardProps> = ({ role, name, photo }: CardProps) => (
-  <div className="flex flex-col justify-between bg-white font-sans-semibold w-72 h-max rounded-3xl">
+export const Card: React.FC<CardProps> = ({ role, name, photo, className }: CardProps) => (
+  <div
+    className={cx({
+      'flex flex-col justify-between bg-white font-sans-semibold w-72 h-max rounded-3xl': true,
+      [className]: !!className,
+    })}
+  >
     <div className="p-5">
       <p className="text-sm text-green">{role}</p>
       <p className="text-xl text-black">{name}</p>

168cd5fc910ec1cd74df862ba86a33c4155bcf63
anamontiaga <amontiaga@gmail.com>
2021-10-06 17:59:15 +0200

    style(about us):mobile devices responsive

--- marketing-v1/containers/about/hero/component.tsx
+++ marketing-v1/containers/about/hero/component.tsx
@@ -2,56 +2,72 @@ import React from 'react';
 
 import Image from 'next/image';
 
+import { Media } from 'containers/media';
 import Wrapper from 'containers/wrapper';
 
 export const AboutHero = () => (
   <section className="relative w-full row-auto pb-5 pt-28">
     <div className="absolute top-0 left-0 w-full bg-white bg-center bg-cover h-4/6" />
-    <div className="absolute bottom-0 left-0 w-full h-2/6 bg-bege" />
+    <div className="absolute bottom-0 left-0 w-full h-2/6 md:bg-bege" />
 
     <Wrapper>
       <div className="relative">
-        <div className="flex justify-center pt-8 pb-10 mx-16 border-b border-black border-b-px">
-          <h1 className="font-sans-semibold text-7xl">Supported by a team of experts</h1>
+        <div className="flex justify-center pb-4 border-b border-black md:pb-10 md:pt-8 md:mx-16 border-b-px">
+          <h1 className="font-sans text-5xl font-semibold md:font-normal md:font-sans-semibold md:text-7xl">
+            Supported by a team of experts
+          </h1>
         </div>
-        <div className="flex justify-between px-16 pt-16 pb-24">
-          <h4 className="w-2/4 text-5xl font-sans-semibold">
+        <div className="flex flex-col justify-between py-10 space-y-4 md:pb-24 md:space-y-0 md:pt-16 md:flex-row md:px-16">
+          <h4 className="font-sans text-xl font-semibold md:text-5xl md:w-2/4 md:font-normal md:font-sans-semibold">
             Our purpose is the creation of a better future for our planet and society.
           </h4>
-          <p className="w-2/4 font-sans text-xl xl:w-2/5">
+          <p className="font-sans text-xl md:w-2/4 xl:w-2/5">
             We work closely with many of the organizations that are leading corporate sustainability
             standards, and can help you understand industry best practices.
           </p>
         </div>
       </div>
       <div className="flex flex-col space-y-2">
-        <div className="flex justify-between space-x-4">
-          <div>
-            <Image width="473px" height="330px" src="/images/about/about-1.jpg" />
+        <Media lessThan="md">
+          <div className="flex justify-between space-x-4">
+            <div>
+              <Image width="473px" height="330px" src="/images/about/about-1.jpg" />
+            </div>
+            <div>
+              <Image width="473px" height="330px" src="/images/about/about-3.jpg" />
+            </div>
           </div>
-          <div>
-            <Image width="473px" height="330px" src="/images/about/about-2.jpg" />
+          <Image width="768px" height="412px" src="/images/about/about-5.jpg" />
+        </Media>
+        <Media greaterThanOrEqual="md">
+          <div className="flex justify-between space-x-4">
+            <div>
+              <Image width="473px" height="330px" src="/images/about/about-1.jpg" />
+            </div>
+            <div>
+              <Image width="473px" height="330px" src="/images/about/about-2.jpg" />
+            </div>
+            <div>
+              <Image width="473px" height="330px" src="/images/about/about-3.jpg" />
+            </div>
           </div>
-          <div>
-            <Image width="473px" height="330px" src="/images/about/about-3.jpg" />
+          <div className="flex justify-between space-x-4">
+            <div>
+              <Image width="720px" height="386px" src="/images/about/about-4.jpg" />
+            </div>
+            <div>
+              <Image width="720px" height="386px" src="/images/about/about-5.jpg" />
+            </div>
           </div>
-        </div>
-        <div className="flex justify-between space-x-4">
-          <div>
-            <Image width="720px" height="386px" src="/images/about/about-4.jpg" />
-          </div>
-          <div>
-            <Image width="720px" height="386px" src="/images/about/about-5.jpg" />
-          </div>
-        </div>
+        </Media>
       </div>
-      <div className="relative grid w-full grid-cols-2">
+      <div className="relative w-full md:grid md:grid-cols-2">
         <div />
-        <div className="flex flex-col py-24 pl-4 pr-16 space-y-4">
-          <h4 className="text-5xl font-sans-semibold">
+        <div className="flex flex-col py-10 space-y-4 md:pl-4 md:py-24 md:pr-16">
+          <h4 className="font-sans text-xl font-semibold md:text-5xl md:font-normal md:font-sans-semibold">
             We are on a mission to make supply chains more sustainable.{' '}
           </h4>
-          <p className="font-sans text-xl">
+          <p className="font-sans text-base md:text-xl">
             The world is moving to a zero-carbon future and nature positive. We offer our expertise
             in environmental monitoring, designing user-centric scientific applications, and
             artificial intelligence based satellite monitoring to help companies reach this
--- marketing-v1/containers/about/values/component.tsx
+++ marketing-v1/containers/about/values/component.tsx
@@ -7,19 +7,21 @@ import Wrapper from 'containers/wrapper';
 import { VALUES } from './constants';
 
 export const AboutValues = () => (
-  <section className="w-full bg-lightBlue pb-36 pt-28">
+  <section className="w-full bg-lightBlue py-28 md:pb-36 md:pt-28">
     <Wrapper>
-      <div className="grid grid-cols-2">
-        <h4 className="font-sans text-7xl">Our values</h4>
+      <div className="md:grid md:grid-cols-2">
+        <h4 className="mb-10 font-sans text-5xl font-semibold md:mb-0 md:font-normal md:text-7xl">
+          Our values
+        </h4>
         <ul className="flex flex-col space-y-16">
           {VALUES.map((v) => (
-            <li key={v.key} className="flex items-center space-x-14">
-              <div>
+            <li key={v.key} className="flex flex-col md:items-center md:flex-row md:space-x-14">
+              <div className="mb-4 md:mb-0">
                 <Image height="80px" width="80px" src={v.icon} />
               </div>
-              <div className="space-y-8">
-                <h5 className="text-5xl font-sans-semibold">{v.title}</h5>
-                <p className="font-sans text-xl">{v.description}</p>
+              <div className="space-y-4 md:space-y-8">
+                <h5 className="text-xl md:text-5xl font-sans-semibold">{v.title}</h5>
+                <p className="font-sans text-base md:text-xl">{v.description}</p>
               </div>
             </li>
           ))}

d4bd2355879174b2793a00af189c7290555822a2
Tiago Garcia <tiagojsag@gmail.com>
2021-10-07 08:06:15 +0200

    Improvements to CI process

--- client/cypress/integration/common/pages.ts
+++ client/cypress/integration/common/pages.ts
@@ -1,14 +1,11 @@
 /// <reference types="cypress" />
-import { Given, Then } from "cypress-cucumber-preprocessor/steps";
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
-beforeEach(()=>
-  cy.viewport(1280, 720)
-)
-Given("I am on the landing page", () => {
-
-  cy.visit("http://localhost:3000");
+beforeEach(() => cy.viewport(1280, 720));
+Given('I am on the landing page', () => {
+  cy.visit('http://localhost:3000');
 });
 
-Then('I navigate to {string} page', (page:string)=>{
-  cy.url().should('include', page)
-})
+Then('I navigate to {string} page', (page: string) => {
+  cy.url().should('include', page);
+});

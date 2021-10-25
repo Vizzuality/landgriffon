3c4cc152bcb876a58dc8101bbaa6f194771f3e5c
David Inga <david.inga@vizzuality.com>
2021-10-25 16:40:18 +0200

    replace tolsma picture; replaced home animation with lottie

---
+++ marketing-v1/src/components/animation-player/component.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useRef } from 'react';
+
+import lottie from 'lottie-web';
+import type { AnimationConfigWithData } from 'lottie-web';
+
+type AnimationPlayerProps = Partial<AnimationConfigWithData> & {
+  children?: JSX.Element | JSX.Element[];
+  className?: string;
+  isAnimating?: boolean;
+};
+
+const animationSettings: Partial<AnimationConfigWithData> = {
+  loop: true,
+  // renderer: 'canvas',
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+const AnimationPlayer: React.FC<AnimationPlayerProps> = (props: Partial<AnimationPlayerProps>) => {
+  const elementRef = useRef(null);
+  const { className, ...restProps } = props;
+
+  useEffect(() => {
+    let playerInstance;
+
+    if (elementRef) {
+      playerInstance = lottie.loadAnimation({
+        ...animationSettings,
+        ...restProps,
+        autoplay: true, // force autoplay
+        container: elementRef.current,
+      });
+    }
+
+    return () => {
+      if (playerInstance) {
+        playerInstance.stop();
+        playerInstance.destroy();
+      }
+    };
+  }, [elementRef]);
+
+  return <div ref={elementRef} className={className} />;
+};
+
+export default AnimationPlayer;

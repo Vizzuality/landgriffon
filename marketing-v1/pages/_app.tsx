ab5b3b3c03f69352e184ebf2a44a38c416a35a8e
David Inga <david.inga@vizzuality.com>
2021-10-25 10:41:38 +0200

    removed not needed libs on package json

--- marketing-v1/pages/_app.tsx
+++ marketing-v1/pages/_app.tsx
@@ -1,26 +1,13 @@
 import type { AppProps } from 'next/app';
 
-import { OverlayProvider } from '@react-aria/overlays';
-import { Provider as AuthenticationProvider } from 'next-auth/client';
-
 import { MediaContextProvider } from 'containers/media';
 
 import 'styles/tailwind.css';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
-  <AuthenticationProvider
-    session={pageProps.session}
-    options={{
-      clientMaxAge: 5 * 60, // Re-fetch session if cache is older than 60 seconds
-      keepAlive: 10 * 60, // Send keepAlive message every 10 minutes
-    }}
-  >
-    <MediaContextProvider>
-      <OverlayProvider>
-        <Component {...pageProps} />
-      </OverlayProvider>
-    </MediaContextProvider>
-  </AuthenticationProvider>
+  <MediaContextProvider>
+    <Component {...pageProps} />
+  </MediaContextProvider>
 );
 
 export default MyApp;

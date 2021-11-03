593ce7b4c683ce4a56f4101c2cfe88eea8c01e66
David Inga <david.inga@vizzuality.com>
2021-11-03 16:28:19 +0100

    show an error when there is no data

--- client/src/containers/analysis-visualization/analysis-map/component.tsx
+++ client/src/containers/analysis-visualization/analysis-map/component.tsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 import DeckGL from '@deck.gl/react';
 import { H3HexagonLayer } from '@deck.gl/geo-layers';
 import { StaticMap } from 'react-map-gl';
+import { XCircleIcon } from '@heroicons/react/solid';
 
 import { useAppSelector } from 'store/hooks';
 import { analysis } from 'store/features/analysis';
@@ -27,12 +28,22 @@ const INITIAL_VIEW_STATE = {
 const AnalysisMap: React.FC = () => {
   const { layer, filters } = useAppSelector(analysis);
 
-  const [layers, setLayers] = useState([]);
+  // const [layers, setLayers] = useState([]);
   const [legendItems, setLegendItems] = useState([]);
   const [isRendering, setIsRendering] = useState(false);
 
-  const { data: h3MaterialData, isFetching } = useH3MaterialData();
-  const { data: h3RiskData } = useH3RiskData();
+  const {
+    data: h3MaterialData,
+    isFetching: isH3MaterialFetching,
+    isError: isH3MaterialError,
+  } = useH3MaterialData();
+  const {
+    data: h3RiskData,
+    isFetching: isH3RiskFetching,
+    isError: isH3RiskError,
+  } = useH3RiskData();
+  const isError = isH3MaterialError || isH3RiskError;
+  const isFetching = isH3MaterialFetching || isH3RiskFetching;
 
   const handleAfterRender = useCallback(() => setIsRendering(false), []);
 
@@ -40,51 +51,19 @@ const AnalysisMap: React.FC = () => {
     if (layer === 'material' && filters.materials?.length > 0) {
       return `${filters.materials[0].label} in the year 2010`;
     }
-    if (layer === 'risk' && filters.materials?.length > 0) {
+    if (layer === 'risk' && filters.indicator && filters.materials?.length > 0) {
       return `${filters.indicator.label}, for ${filters.materials[0].label} in 2010`;
     }
     return null;
   }, [layer, filters]);
 
   useEffect(() => {
-    setIsRendering(true);
+    // setIsRendering(true);
 
-    if (h3MaterialData || h3RiskData) {
-      setLayers([
-        new H3HexagonLayer({
-          id: 'h3-layer-material',
-          data: h3MaterialData.data,
-          pickable: true,
-          wireframe: false,
-          filled: true,
-          extruded: true,
-          elevationScale: 1,
-          highPrecision: false,
-          visible: layer === 'material',
-          getHexagon: (d) => d.h,
-          getFillColor: (d) => d.c,
-          getElevation: (d) => d.v,
-        }),
-        new H3HexagonLayer({
-          id: 'h3-layer-risk',
-          data: h3RiskData.data,
-          pickable: true,
-          wireframe: false,
-          filled: true,
-          extruded: true,
-          elevationScale: 1,
-          highPrecision: false,
-          visible: layer === 'risk',
-          getHexagon: (d) => d.h,
-          getFillColor: (d) => d.c,
-          getElevation: (d) => d.v,
-        }),
-      ]);
-
-      // TO-DO: improve this logic
+    if (h3MaterialData?.data.length || h3RiskData?.data.length) {
       const nextLegendItems = [];
 
-      if (layer === 'material' && h3MaterialData.data.length) {
+      if (layer === 'material') {
         nextLegendItems.push({
           id: 'h3-legend-material',
           name: legendName,
@@ -97,7 +76,7 @@ const AnalysisMap: React.FC = () => {
         });
       }
 
-      if (layer === 'risk' && h3RiskData.data.length) {
+      if (layer === 'risk') {
         nextLegendItems.push({
           id: 'h3-legend-risk',
           name: legendName,
@@ -113,6 +92,37 @@ const AnalysisMap: React.FC = () => {
       setLegendItems(nextLegendItems);
     }
   }, [h3MaterialData, h3RiskData, layer]);
+
+  const layers = [
+    new H3HexagonLayer({
+      id: 'h3-layer-material',
+      data: h3MaterialData.data,
+      pickable: true,
+      wireframe: false,
+      filled: true,
+      extruded: true,
+      elevationScale: 1,
+      highPrecision: false,
+      visible: layer === 'material',
+      getHexagon: (d) => d.h,
+      getFillColor: (d) => d.c,
+      getElevation: (d) => d.v,
+    }),
+    new H3HexagonLayer({
+      id: 'h3-layer-risk',
+      data: h3RiskData.data,
+      pickable: true,
+      wireframe: false,
+      filled: true,
+      extruded: true,
+      elevationScale: 1,
+      highPrecision: false,
+      visible: layer === 'risk',
+      getHexagon: (d) => d.h,
+      getFillColor: (d) => d.c,
+      getElevation: (d) => d.v,
+    }),
+  ];
 
   return (
     <>
@@ -141,17 +151,29 @@ const AnalysisMap: React.FC = () => {
           mapboxApiAccessToken={MAPBOX_API_TOKEN}
         />
       </DeckGL>
-      <Legend
-        className="absolute z-10 bottom-10 right-6 w-72"
-        maxHeight={400}
-        onChangeOrder={() => null}
-      >
-        {legendItems.map((i) => (
-          <LegendItem key={i.id} {...i}>
-            <LegendTypeChoropleth className="text-sm text-gray-500" min={i.min} items={i.items} />
-          </LegendItem>
-        ))}
-      </Legend>
+      {isError && (
+        <div className="absolute z-10 top-20 left-12 p-4 rounded-md bg-red-50 p-4">
+          <div className="flex">
+            <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+            <p className="text-red-600 text-sm ml-3 mb-0">
+              No available data for the current filter selection. Please try another one.
+            </p>
+          </div>
+        </div>
+      )}
+      {legendItems?.length > 0 && (
+        <Legend
+          className="absolute z-10 bottom-10 right-6 w-72"
+          maxHeight={400}
+          onChangeOrder={() => null}
+        >
+          {legendItems.map((i) => (
+            <LegendItem key={i.id} {...i}>
+              <LegendTypeChoropleth className="text-sm text-gray-500" min={i.min} items={i.items} />
+            </LegendItem>
+          ))}
+        </Legend>
+      )}
     </>
   );
 };

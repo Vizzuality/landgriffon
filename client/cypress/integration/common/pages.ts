7ac9acfc51b83b90ad6b8af76baf32dfaa8865bd
David Inga <david.inga@vizzuality.com>
2021-11-10 10:10:46 +0100

    updated cypress test for analysis navigation

--- client/cypress/integration/common/pages.ts
+++ client/cypress/integration/common/pages.ts
@@ -6,6 +6,10 @@ Given('I am on the landing page', () => {
   cy.visit('http://localhost:3000');
 });
 
+Given('I am on the sign-in page', () => {
+  cy.visit('http://localhost:3000/auth/sign-in');
+});
+
 Then('I navigate to {string} page', (page: string) => {
   cy.url().should('include', page);
 });

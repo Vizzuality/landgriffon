603d6a0521d5735f497d2d55a8d57996aaf648d4
Tiago Garcia <tiagojsag@gmail.com>
2021-11-12 13:09:29 +0100

    Bump nodejs heap size

--- kubernetes/modules/api/main.tf
+++ kubernetes/modules/api/main.tf
@@ -133,11 +133,11 @@ resource "kubernetes_deployment" "api_deployment" {
           resources {
             limits = {
               cpu    = "1"
-              memory = "1Gi"
+              memory = "2Gi"
             }
             requests = {
               cpu    = "500m"
-              memory = "512Mi"
+              memory = "2Gi"
             }
           }
 

e959bd5977b757a9aed50ec484ae573cc520030e
elpamart <elena.palao@vizzuality.com>
2021-11-17 14:26:10 +0100

    modify mock data

--- client/src/hooks/analysis/mock.ts
+++ client/src/hooks/analysis/mock.ts
@@ -3,16 +3,16 @@ export const DATA = [
     id: 'bd0cf013-05e1-4230-912a-aae592f96c30',
     indicator: 'Carbon emissions due to land use change',
     values: [
-      { year: 2020, value: 300, calculated: false },
-      { year: 2021, value: 200, calculated: false },
-      { year: 2022, value: 300, calculated: true },
-      { year: 2023, value: 100, calculated: true },
-      { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+      { year: 2020, value: 2563, calculated: false },
+      { year: 2021, value: 1869, calculated: false },
+      { year: 2022, value: 2338, calculated: true },
+      { year: 2023, value: 1537, calculated: true },
+      { year: 2024, value: 1937, calculated: true }, // data projected for non-existing years
     ],
     children: [
       {
-        id: 'coal',
-        name: 'Coal',
+        id: 'Bananas & plantains',
+        name: 'Bananas & plantains',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -22,8 +22,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'beef',
-        name: 'Beef',
+        id: 'Coffee tea mate and spices',
+        name: 'Coffee tea mate and spices',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -33,19 +33,19 @@ export const DATA = [
         ],
       },
       {
-        id: 'corn',
-        name: 'Corn',
+        id: 'Cotton',
+        name: 'Cotton',
         values: [
-          { year: 2020, value: 350, calculated: false },
-          { year: 2021, value: 200, calculated: false },
-          { year: 2022, value: 300, calculated: true },
-          { year: 2023, value: 100, calculated: true },
-          { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 944, calculated: false },
+          { year: 2021, value: 955, calculated: false },
+          { year: 2022, value: 966, calculated: true },
+          { year: 2023, value: 977, calculated: true },
+          { year: 2024, value: 989, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'duck',
-        name: 'Duck',
+        id: 'Maize (corn)',
+        name: 'Maize (corn)',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -55,19 +55,19 @@ export const DATA = [
         ],
       },
       {
-        id: 'poultry',
-        name: 'Poultry',
+        id: 'Raw hides, skins and leather',
+        name: 'Raw hides, skins and leather',
         values: [
-          { year: 2020, value: 350, calculated: false },
-          { year: 2021, value: 89, calculated: false },
-          { year: 2022, value: 300, calculated: true },
-          { year: 2023, value: 30, calculated: true },
-          { year: 2024, value: 40, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 189, calculated: false },
+          { year: 2021, value: 192, calculated: false },
+          { year: 2022, value: 194, calculated: true },
+          { year: 2023, value: 196, calculated: true },
+          { year: 2024, value: 198, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'mint',
-        name: 'Mint',
+        id: 'Rubber and articles thereof',
+        name: 'Rubber and articles thereof',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 35, calculated: false },
@@ -77,8 +77,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'soy',
-        name: 'Soy',
+        id: 'Soya beans whether or not broken',
+        name: 'Soya beans whether or not broken',
         values: [
           { year: 2020, value: 30, calculated: false },
           { year: 2021, value: 87, calculated: false },
@@ -93,16 +93,16 @@ export const DATA = [
     id: '457116a8-90f6-4b0b-8517-9965e880b6a8',
     indicator: 'Deforestation loss due to land use change',
     values: [
-      { year: 2020, value: 150, calculated: false },
-      { year: 2021, value: 200, calculated: false },
-      { year: 2022, value: 300, calculated: true },
-      { year: 2023, value: 100, calculated: true },
-      { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+      { year: 2020, value: 2388, calculated: false },
+      { year: 2021, value: 1690, calculated: false },
+      { year: 2022, value: 2157, calculated: true },
+      { year: 2023, value: 1354, calculated: true },
+      { year: 2024, value: 1752, calculated: true }, // data projected for non-existing years
     ],
     children: [
       {
-        id: 'coal',
-        name: 'Coal',
+        id: 'Bananas & plantains',
+        name: 'Bananas & plantains',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -112,8 +112,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'beef',
-        name: 'Beef',
+        id: 'Coffee tea mate and spices',
+        name: 'Coffee tea mate and spices',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -123,19 +123,19 @@ export const DATA = [
         ],
       },
       {
-        id: 'corn',
-        name: 'Corn',
+        id: 'Cotton',
+        name: 'Cotton',
         values: [
-          { year: 2020, value: 350, calculated: false },
-          { year: 2021, value: 200, calculated: false },
-          { year: 2022, value: 300, calculated: true },
-          { year: 2023, value: 100, calculated: true },
-          { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 587, calculated: false },
+          { year: 2021, value: 593, calculated: false },
+          { year: 2022, value: 600, calculated: true },
+          { year: 2023, value: 607, calculated: true },
+          { year: 2024, value: 614, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'duck',
-        name: 'Duck',
+        id: 'Maize (corn)',
+        name: 'Maize (corn)',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -145,19 +145,19 @@ export const DATA = [
         ],
       },
       {
-        id: 'poultry',
-        name: 'Poultry',
+        id: 'Raw hides, skins and leather',
+        name: 'Raw hides, skins and leather',
         values: [
-          { year: 2020, value: 350, calculated: false },
-          { year: 2021, value: 89, calculated: false },
-          { year: 2022, value: 300, calculated: true },
-          { year: 2023, value: 30, calculated: true },
-          { year: 2024, value: 40, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 371, calculated: false },
+          { year: 2021, value: 375, calculated: false },
+          { year: 2022, value: 379, calculated: true },
+          { year: 2023, value: 383, calculated: true },
+          { year: 2024, value: 388, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'mint',
-        name: 'Mint',
+        id: 'Rubber and articles thereof',
+        name: 'Rubber and articles thereof',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 35, calculated: false },
@@ -167,8 +167,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'soy',
-        name: 'Soy',
+        id: 'Soya beans whether or not broken',
+        name: 'Soya beans whether or not broken',
         values: [
           { year: 2020, value: 30, calculated: false },
           { year: 2021, value: 87, calculated: false },
@@ -183,27 +183,27 @@ export const DATA = [
     id: '1224d1d9-c3ae-450f-acb6-67f4ed33b5f7',
     indicator: 'Unsustainable water use',
     values: [
-      { year: 2020, value: 70, calculated: false },
-      { year: 2021, value: 200, calculated: false },
-      { year: 2022, value: 300, calculated: true },
-      { year: 2023, value: 100, calculated: true },
-      { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+      { year: 2020, value: 4584, calculated: false },
+      { year: 2021, value: 6540, calculated: false },
+      { year: 2022, value: 6930, calculated: true },
+      { year: 2023, value: 6439, calculated: true },
+      { year: 2024, value: 6775, calculated: true }, // data projected for non-existing years
     ],
     children: [
       {
-        id: 'coal',
-        name: 'Coal',
+        id: 'Bananas & plantains',
+        name: 'Bananas & plantains',
         values: [
           { year: 2020, value: 230, calculated: false },
-          { year: 2021, value: 5, calculated: false },
-          { year: 2022, value: 56, calculated: true },
-          { year: 2023, value: 100, calculated: true },
-          { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+          { year: 2021, value: 232, calculated: false },
+          { year: 2022, value: 235, calculated: true },
+          { year: 2023, value: 238, calculated: true },
+          { year: 2024, value: 240, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'beef',
-        name: 'Beef',
+        id: 'Coffee tea mate and spices',
+        name: 'Coffee tea mate and spices',
         values: [
           { year: 2020, value: 40, calculated: false },
           { year: 2021, value: 76, calculated: false },
@@ -213,19 +213,19 @@ export const DATA = [
         ],
       },
       {
-        id: 'corn',
-        name: 'Corn',
+        id: 'Cotton',
+        name: 'Cotton',
         values: [
-          { year: 2020, value: 150, calculated: false },
-          { year: 2021, value: 200, calculated: false },
-          { year: 2022, value: 50, calculated: true },
-          { year: 2023, value: 100, calculated: true },
-          { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 1559, calculated: false },
+          { year: 2021, value: 1576, calculated: false },
+          { year: 2022, value: 1595, calculated: true },
+          { year: 2023, value: 1613, calculated: true },
+          { year: 2024, value: 1631, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'duck',
-        name: 'Duck',
+        id: 'Maize (corn)',
+        name: 'Maize (corn)',
         values: [
           { year: 2020, value: 175, calculated: false },
           { year: 2021, value: 200, calculated: false },
@@ -235,35 +235,35 @@ export const DATA = [
         ],
       },
       {
-        id: 'poultry',
-        name: 'Poultry',
+        id: 'Raw hides, skins and leather',
+        name: 'Raw hides, skins and leather',
         values: [
-          { year: 2020, value: 350, calculated: false },
-          { year: 2021, value: 89, calculated: false },
-          { year: 2022, value: 300, calculated: true },
-          { year: 2023, value: 30, calculated: true },
-          { year: 2024, value: 40, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 767, calculated: false },
+          { year: 2021, value: 776, calculated: false },
+          { year: 2022, value: 784, calculated: true },
+          { year: 2023, value: 794, calculated: true },
+          { year: 2024, value: 803, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'mint',
-        name: 'Mint',
+        id: 'Rubber and articles thereof',
+        name: 'Rubber and articles thereof',
         values: [
-          { year: 2020, value: 350, calculated: false },
-          { year: 2021, value: 35, calculated: false },
-          { year: 2022, value: 50, calculated: true },
-          { year: 2023, value: 89, calculated: true },
-          { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+          { year: 2020, value: 1783, calculated: false },
+          { year: 2021, value: 1804, calculated: false },
+          { year: 2022, value: 1824, calculated: true },
+          { year: 2023, value: 1845, calculated: true },
+          { year: 2024, value: 1867, calculated: true }, // data projected for non-existing years
         ],
       },
       {
-        id: 'soy',
-        name: 'Soy',
+        id: 'Soya beans whether or not broken',
+        name: 'Soya beans whether or not broken',
         values: [
           { year: 2020, value: 30, calculated: false },
           { year: 2021, value: 87, calculated: false },
           { year: 2022, value: 78, calculated: true },
-          { year: 2023, value: 50, calculated: true },
+          { year: 2023, value: 50., calculated: true },
           { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
         ],
       },
@@ -273,16 +273,16 @@ export const DATA = [
     id: '73f95f81-eb4b-4a1b-a148-1ff9f67db07f',
     indicator: 'Biodiversity loss due to land use change',
     values: [
-      { year: 2020, value: 50, calculated: false },
-      { year: 2021, value: 200, calculated: false },
-      { year: 2022, value: 300, calculated: true },
-      { year: 2023, value: 100, calculated: true },
-      { year: 2024, value: 150, calculated: true }, // data projected for non-existing years
+      { year: 2020, value: 994, calculated: false },
+      { year: 2021, value: 391, calculated: false },
+      { year: 2022, value: 1514, calculated: true },
+      { year: 2023, value: 494, calculated: true },
+      { year: 2024, value: 940, calculated: true }, // data projected for non-existing years
     ],
     children: [
       {
-        id: 'coal',
-        name: 'Coal',
+        id: 'Bananas & plantains',
+        name: 'Bananas & plantains',
         values: [
           { year: 2020, value: 200, calculated: false },
           { year: 2021, value: 45, calculated: false },
@@ -292,8 +292,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'beef',
-        name: 'Beef',
+        id: 'Coffee tea mate and spices',
+        name: 'Coffee tea mate and spices',
         values: [
           { year: 2020, value: 100, calculated: false },
           { year: 2021, value: 45, calculated: false },
@@ -303,8 +303,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'corn',
-        name: 'Corn',
+        id: 'Cotton',
+        name: 'Cotton',
         values: [
           { year: 2020, value: 350, calculated: false },
           { year: 2021, value: 45, calculated: false },
@@ -314,8 +314,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'duck',
-        name: 'Duck',
+        id: 'Maize (corn)',
+        name: 'Maize (corn)',
         values: [
           { year: 2020, value: 90, calculated: false },
           { year: 2021, value: 45, calculated: false },
@@ -325,8 +325,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'poultry',
-        name: 'Poultry',
+        id: 'Raw hides, skins and leather',
+        name: 'Raw hides, skins and leather',
         values: [
           { year: 2020, value: 98, calculated: false },
           { year: 2021, value: 89, calculated: false },
@@ -336,8 +336,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'mint',
-        name: 'Mint',
+        id: 'Rubber and articles thereof',
+        name: 'Rubber and articles thereof',
         values: [
           { year: 2020, value: 126, calculated: false },
           { year: 2021, value: 35, calculated: false },
@@ -347,8 +347,8 @@ export const DATA = [
         ],
       },
       {
-        id: 'soy',
-        name: 'Soy',
+        id: 'Soya beans whether or not broken',
+        name: 'Soya beans whether or not broken',
         values: [
           { year: 2020, value: 30, calculated: false },
           { year: 2021, value: 87, calculated: false },

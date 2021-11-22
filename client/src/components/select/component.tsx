627ae66e7cee3e7d4b5c0f06ed06e05704fada50
David Inga <david.inga@vizzuality.com>
2021-11-22 13:12:12 +0100

    refactored selector

--- client/src/components/select/component.tsx
+++ client/src/components/select/component.tsx
@@ -1,19 +1,9 @@
 import { Fragment, useEffect, useState } from 'react';
 import { Listbox, Transition } from '@headlessui/react';
-import { CheckIcon, SelectorIcon } from '@heroicons/react/solid';
+import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
 import classNames from 'classnames';
 
-type Option = {
-  label: string;
-  value: string | number;
-};
-
-export type SelectProps = {
-  label?: string;
-  options: Option[];
-  current: Option;
-  onChange?: (selected: Option) => unknown;
-};
+import type { SelectProps } from './types';
 
 const ScenariosComparison: React.FC<SelectProps> = (props: SelectProps) => {
   const { label, options, current = options[0], onChange } = props;
@@ -27,15 +17,19 @@ const ScenariosComparison: React.FC<SelectProps> = (props: SelectProps) => {
     <Listbox value={selectedOption} onChange={setSelectedOption}>
       {({ open }) => (
         <>
-          {/* <Listbox.Label className="block text-sm font-medium text-gray-700">
-            Assigned to
-          </Listbox.Label> */}
           <div className="mt-1 relative">
-            <Listbox.Button className="bg-white relative w-full border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700 sm:text-sm">
+            <Listbox.Button
+              className="bg-white relative w-full flex align-center border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default
+              focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700 text-sm cursor-pointer"
+            >
               {label && <span className="inline-block truncate mr-1 text-gray-400">{label}</span>}
               <span className="inline-block truncate">{selectedOption.label}</span>
               <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                {open ? (
+                  <ChevronUpIcon className="h-4 w-4 text-gray-900" aria-hidden="true" />
+                ) : (
+                  <ChevronDownIcon className="h-4 w-4 text-gray-900" aria-hidden="true" />
+                )}
               </span>
             </Listbox.Button>
 
@@ -48,7 +42,7 @@ const ScenariosComparison: React.FC<SelectProps> = (props: SelectProps) => {
             >
               <Listbox.Options
                 static
-                className="absolute z-10 mt-1 w-full bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm"
+                className="absolute z-10 mt-1 w-48 bg-white shadow-lg max-h-60 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none text-sm"
               >
                 {options.map((option) => (
                   <Listbox.Option
@@ -56,7 +50,7 @@ const ScenariosComparison: React.FC<SelectProps> = (props: SelectProps) => {
                     className={({ active }) =>
                       classNames(
                         active ? 'text-white bg-green-700' : 'text-gray-900',
-                        'cursor-default select-none relative py-2 pl-3 pr-9',
+                        'cursor-pointer select-none relative py-2 pl-3 pr-9',
                       )
                     }
                     value={option}
@@ -66,7 +60,7 @@ const ScenariosComparison: React.FC<SelectProps> = (props: SelectProps) => {
                         <span
                           className={classNames(
                             selected ? 'font-semibold' : 'font-normal',
-                            'block truncate',
+                            'block text-sm truncate',
                           )}
                         >
                           {option.label}
@@ -79,7 +73,7 @@ const ScenariosComparison: React.FC<SelectProps> = (props: SelectProps) => {
                               'absolute inset-y-0 right-0 flex items-center pr-4',
                             )}
                           >
-                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                            <CheckIcon className="h-4 w-4" aria-hidden="true" />
                           </span>
                         ) : null}
                       </>

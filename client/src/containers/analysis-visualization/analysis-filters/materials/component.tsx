b1799cfec1e99fb058d5f4f734aba993dc83075f
David Inga <david.inga@vizzuality.com>
2021-11-23 16:53:53 +0100

    tree select refactored

--- client/src/containers/analysis-visualization/analysis-filters/materials/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/materials/component.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
-import TreeSelect, { TreeSelectProps } from 'rc-tree-select';
+// import TreeSelect, { TreeSelectProps } from 'rc-tree-select';
 import { ChevronDownIcon, XIcon } from '@heroicons/react/solid';
 import { sortBy } from 'lodash';
 
@@ -7,9 +7,12 @@ import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { analysis, setFilter } from 'store/features/analysis';
 import { useMaterialsTrees } from 'hooks/materials';
 
-type MaterialsFilterProps = TreeSelectProps<unknown>;
+import TreeSelect from 'components/tree-select';
 
-const MaterialsFilter: React.FC<MaterialsFilterProps> = (props: MaterialsFilterProps) => {
+// type MaterialsFilterProps = TreeSelectProps<unknown>;
+
+const MaterialsFilter: React.FC = (props) => {
+  console.log(props);
   const { multiple } = props;
   const dispatch = useAppDispatch();
   const { filters } = useAppSelector(analysis);
@@ -24,37 +27,40 @@ const MaterialsFilter: React.FC<MaterialsFilterProps> = (props: MaterialsFilterP
     () =>
       sortBy(
         data?.map(({ name, id, children }) => ({
-          label: name,
+          title: name,
           key: id,
-          children: children?.map(({ name, id }) => ({ label: name, key: id })),
+          children: children?.map(({ name, id }) => ({ title: name, key: id })),
         })),
-        'label',
+        'title',
       ),
     [data],
   );
 
+  console.log(treeData);
+
   return (
     <TreeSelect
-      onChange={handleChange}
-      labelInValue
-      className="w-64"
-      loading={isLoading}
-      placeholder={error ? 'Something went wrong' : 'Select materials'}
-      value={multiple ? filters.materials : filters.materials[0]}
-      multiple={false}
-      treeData={treeData}
-      showArrow
-      showCheckedStrategy={TreeSelect.SHOW_PARENT}
-      treeDefaultExpandAll={false}
-      treeCheckable={false}
-      disabled={!!error}
-      treeNodeFilterProp="title"
-      inputIcon={<ChevronDownIcon />}
-      removeIcon={<XIcon />}
-      maxTagCount={5}
-      maxTagPlaceholder={(e) => `${e.length} more...`}
-      showSearch
-      {...props}
+      data={treeData}
+      // onChange={handleChange}
+      // labelInValue
+      // className="w-64"
+      // loading={isLoading}
+      // placeholder={error ? 'Something went wrong' : 'Select materials'}
+      // value={multiple ? filters.materials : filters.materials[0]}
+      // multiple={false}
+      // treeData={treeData}
+      // showArrow
+      // showCheckedStrategy={TreeSelect.SHOW_PARENT}
+      // treeDefaultExpandAll={false}
+      // treeCheckable={false}
+      // disabled={!!error}
+      // treeNodeFilterProp="title"
+      // inputIcon={<ChevronDownIcon />}
+      // removeIcon={<XIcon />}
+      // maxTagCount={5}
+      // maxTagPlaceholder={(e) => `${e.length} more...`}
+      // showSearch
+      // {...props}
     />
   );
 };

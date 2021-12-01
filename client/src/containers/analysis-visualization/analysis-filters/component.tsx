c5bf9240b465e104a01e03a820c66cc51a7341d6
David Inga <david.inga@vizzuality.com>
2021-12-01 09:26:34 +0100

    only show group by selector for impact layer and mode table or chart

--- client/src/containers/analysis-visualization/analysis-filters/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/component.tsx
@@ -8,12 +8,13 @@ import Materials from './materials';
 import MoreFilters from './more-filters';
 
 const AnalysisFilters: React.FC = () => {
-  const { layer } = useAppSelector(analysis);
+  const { layer, visualizationMode } = useAppSelector(analysis);
 
   return (
     <div className="inline-flex gap-2 flex-wrap">
       {layer !== 'material' && <ImpactIndicatorsFilter />}
       {layer !== 'impact' && <Materials />}
+      {layer === 'impact' && visualizationMode !== 'map' && <GroupByFilter />}
       <YearsFilter />
       {layer === 'impact' && <MoreFilters />}
     </div>

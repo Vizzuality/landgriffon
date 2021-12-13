f44b9561b7f5f0f64934ee272e5d356b43605440
David Inga <david.inga@vizzuality.com>
2021-12-13 16:01:24 +0100

    removed table from antd

--- client/src/containers/analysis-visualization/analysis-table/table/component.tsx
+++ client/src/containers/analysis-visualization/analysis-table/table/component.tsx
@@ -1,4 +1,4 @@
-import { Table as TableAntd } from 'antd';
+// import { Table as TableAntd } from 'antd';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/outline';
 
 export interface TableProps {
@@ -13,30 +13,31 @@ export interface TableProps {
 }
 
 export const Table: React.FC<TableProps> = ({ columns, dataSource, onChange }: TableProps) => (
-  <TableAntd
-    expandable={{
-      rowExpandable: (record) => !!record.children,
-      expandIcon: ({ expanded, onExpand, record }) => {
-        if (!record.children) return null;
-        if (expanded)
-          return (
-            <button type="button" onClick={(e) => onExpand(record, e)}>
-              <ChevronUpIcon className="w-5 h-4 mr-2 text-gray-800" />
-            </button>
-          );
-        return (
-          <button type="button" onClick={(e) => onExpand(record, e)}>
-            <ChevronDownIcon className="w-5 h-4 mr-2 text-gray-800" />
-          </button>
-        );
-      },
-    }}
-    dataSource={dataSource}
-    columns={columns}
-    pagination={false}
-    scroll={{ x: '110%', scrollToFirstRowOnChange: true }}
-    onChange={onChange}
-  />
+  <div>Table</div>
+  // <TableAntd
+  //   expandable={{
+  //     rowExpandable: (record) => !!record.children,
+  //     expandIcon: ({ expanded, onExpand, record }) => {
+  //       if (!record.children) return null;
+  //       if (expanded)
+  //         return (
+  //           <button type="button" onClick={(e) => onExpand(record, e)}>
+  //             <ChevronUpIcon className="w-5 h-4 mr-2 text-gray-800" />
+  //           </button>
+  //         );
+  //       return (
+  //         <button type="button" onClick={(e) => onExpand(record, e)}>
+  //           <ChevronDownIcon className="w-5 h-4 mr-2 text-gray-800" />
+  //         </button>
+  //       );
+  //     },
+  //   }}
+  //   dataSource={dataSource}
+  //   columns={columns}
+  //   pagination={false}
+  //   scroll={{ x: '110%', scrollToFirstRowOnChange: true }}
+  //   onChange={onChange}
+  // />
 );
 
 export default Table;

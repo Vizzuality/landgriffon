bcffacf83fb1abe1f9d6f0cb105bd96902b0827b
David Inga <david.inga@vizzuality.com>
2021-12-14 16:33:55 +0100

    upgraded tailwind to version 3

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,8 @@ const colors = require('tailwindcss/colors');
 const lineClamp = require('./src/lib/tailwind/line-clamp');
 
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV !== 'development',
-    content: ['./**/*.ts', './**/*.tsx'],
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ['./**/*.ts', './**/*.tsx'],
+  darkMode: 'media', // 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {

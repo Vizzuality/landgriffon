5db90e96ace4cfebf95a4d7e9ec6bf7c20ae7426
Yulia Belyakova <yuliya.beliakova@gmail.com>
2021-12-20 22:43:22 +0100

    Update error message when Indicator h3 data is not found

--- api/src/modules/h3-data/h3-data.service.ts
+++ api/src/modules/h3-data/h3-data.service.ts
@@ -170,7 +170,7 @@ export class H3DataService {
 
     if (!indicatorH3Data)
       throw new NotFoundException(
-        `There is no H3 Data for Indicator with ID: ${indicatorId}`,
+        `There is no H3 Data for Indicator with ID: ${indicatorId} for the requested year`,
       );
 
     const harvestMaterialH3Data: H3Data | undefined =

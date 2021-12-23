9745e6f4239cae2d4995e483bbac38fc550a409a
David Inga <david.inga@vizzuality.com>
2021-12-23 17:10:15 +0100

    migrates tables to ka-table

--- client/src/containers/analysis-visualization/analysis-table/component.tsx
+++ client/src/containers/analysis-visualization/analysis-table/component.tsx
@@ -1,81 +1,43 @@
+import { useMemo } from 'react';
 import { DownloadIcon } from '@heroicons/react/outline';
 import { InformationCircleIcon } from '@heroicons/react/solid';
+import { DataType } from 'ka-table/enums';
 
-import { useIndicators } from 'hooks/indicators';
 import { useImpactData } from 'hooks/impact';
-import { useAnalysisData, useIndicatorAnalysisTable } from 'hooks/analysis';
-import { useAppSelector } from 'store/hooks';
-import { analysis } from 'store/features/analysis';
 
 import Button from 'components/button';
 import Loading from 'components/loading';
-
-import Table from 'containers/analysis-visualization/analysis-table/table';
-import TableTitle from 'containers/analysis-visualization/analysis-table/table-title';
+import Table from 'components/table';
 
 const AnalysisTable: React.FC = () => {
-  const { filters } = useAppSelector(analysis);
+  const { data: impactData, isLoading } = useImpactData();
 
-  const { data: tableData, isFetched: tableDataIsFetched } = useImpactData();
-  console.log('data', tableData);
+  // initial value of the *props
+  const tableData = useMemo(() => {
+    const {
+      data: { impactTable },
+    } = impactData;
 
-  const { data: indicatorTableData, isFetched: indicatorTableDataIsFetched } =
-    useIndicatorAnalysisTable();
-
-  const getValueByYear = (columnYear, record) => {
-    if (record && record.values) {
-      const dataIndex = record.values.find((el) => el.year === columnYear)?.value;
-      return dataIndex;
-    }
-    return null;
-  };
-
-  const FILTERED_DATA = filters.indicator?.value === 'all' ? tableData : indicatorTableData;
-
-  const filteredDataIsFetched = tableDataIsFetched || indicatorTableDataIsFetched;
-
-  const tableInitialColumns = [
-    {
-      title: 'YEAR',
-      dataIndex: 'indicator',
-      key: 'indicator',
-      width: 150,
-      fixed: 'left',
-    },
-    {
-      title: `${filters.startYear}-${filters.endYear}`,
-      dataIndex: 'all',
-      key: 'all',
-      width: 100,
-      fixed: 'left',
-    },
-  ];
-
-  const rangeOfYears = (start, end) =>
-    Array(end - start + 1)
-      .fill(start)
-      .map((year, index) => year + index);
-
-  // Substitute parameters by (filters.startYear, filters.endYear);
-  const filteredYears = rangeOfYears(2020, 2024);
-
-  const tableYearColumns = filteredYears.map((y, i) => {
-    const isPastOrCurrentYear = y <= new Date().getFullYear();
-    return {
-      title: () => <TableTitle title={y} />,
-      render: (record) => getValueByYear(y, record),
-      key: 'values',
-      width: 100,
-      sorter: (a, b) => a.values[i].value - b.values[i].value,
-      ...(isPastOrCurrentYear && { fixed: 'left' }),
-    };
-  });
-
-  const TABLE_COLUMNS = (tableInitialColumns as []).concat(tableYearColumns as []);
-
-  const onChange = (pagination: any, sorter: any, extra: any) => {
-    console.info('params', pagination, sorter, extra);
-  };
+    return impactTable.map(({ indicatorId, yearSum, rows }) => ({
+      key: indicatorId,
+      rowKeyField: 'id',
+      columns: [
+        { key: 'material', title: 'Material', dataType: DataType.String },
+        ...yearSum.map(({ year }) => ({
+          key: year.toString(),
+          title: year.toString(),
+          DataType: DataType.Number,
+        })),
+      ],
+      data: rows.map((row, rowIndex) => ({
+        id: `${indicatorId}-${rowIndex}`,
+        material: row.name,
+        ...row.values
+          .map(({ year, value }) => ({ [year as string]: value }))
+          .reduce((a, b) => ({ ...a, ...b })),
+      })),
+    }));
+  }, [impactData]);
 
   return (
     <>
@@ -97,10 +59,14 @@ const AnalysisTable: React.FC = () => {
         </Button>
       </div>
       <div className="relative">
-        <Loading className="text-white" />
-        {/* {FILTERED_DATA && filteredDataIsFetched && (
-          <Table columns={TABLE_COLUMNS} dataSource={FILTERED_DATA} onChange={onChange} />
-        )} */}
+        {isLoading && <Loading className="text-green-700" />}
+        {impactData &&
+          !isLoading &&
+          tableData.map(({ key, ...tableProps }) => (
+            <div key={key} className="my-4">
+              <Table {...tableProps} />
+            </div>
+          ))}
       </div>
     </>
   );

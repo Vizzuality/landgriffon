a72969921391e8660a99f30e57416909bc5b258d
alexeh <alexurola@gmail.com>
2022-01-04 15:31:44 +0100

    Add job event handling

--- api/src/modules/import-data/import-data.service.ts
+++ api/src/modules/import-data/import-data.service.ts
@@ -3,50 +3,44 @@ import {
   Logger,
   ServiceUnavailableException,
 } from '@nestjs/common';
-import {
-  ExcelImportJob,
-  ImportDataProducer,
-} from 'modules/import-data/workers/import-data.producer';
+import { ImportDataProducer } from 'modules/import-data/workers/import-data.producer';
 import { Job } from 'bull';
+import { ExcelImportJob } from 'modules/import-data/workers/import-data.producer';
 import { SourcingDataImportService } from 'modules/import-data/sourcing-data/sourcing-data-import.service';
-import { JobEventsService } from 'modules/job-events/job-events.service';
 
 @Injectable()
 export class ImportDataService {
   logger: Logger = new Logger(ImportDataService.name);
   constructor(
     private readonly importDataProducer: ImportDataProducer,
-    private readonly sourcingDataImportService: SourcingDataImportService,
-    private readonly jobEvents: JobEventsService,
+    private readonly sourcingDataImport: SourcingDataImportService,
   ) {}
 
   async loadXlsxFile(
     userId: string,
     xlsxFileData: Express.Multer.File,
   ): Promise<void> {
-    const { filename, path: xlsxFilePath } = xlsxFileData;
     try {
-      await this.importDataProducer.addExcelImportJob(xlsxFilePath, userId);
-    } catch ({ message }) {
-      this.logger.warn(
-        `Job for file: ${filename} sent by user: ${userId} could not been added to queue`,
+      await this.importDataProducer.addExcelImportJob(xlsxFileData, userId);
+    } catch (error: any) {
+      this.logger.error(
+        `Job for file: ${
+          xlsxFileData.filename
+        } sent by user: ${userId} could not been added to queue: ${error.toString()}`,
       );
+      /**
+       * @note: This error would be sent back to the consumer in case the job hasn't been pushed to queue.
+       * What would we want to add as message?
+       */
       throw new ServiceUnavailableException(
-        `File: ${filename} could not have been loaded`,
+        `File: ${xlsxFileData.filename} could not have been loaded`,
       );
     }
   }
 
   async processImportJob(job: Job<ExcelImportJob>): Promise<void> {
-    try {
-      // TODO: Handle event
-      await this.sourcingDataImportService.importSourcingRecords(
-        job.data.xlsxFilePath,
-      );
-    } catch (error: any) {
-      this.logger.error(
-        `Import Failed for ${job.data.xlsxFilePath} sent by user ${job.data.userId}: ${error.message}`,
-      );
-    }
+    await this.sourcingDataImport.importSourcingData(
+      job.data.xlsxFileData.path,
+    );
   }
 }

f8f706d703c64777b0d190d5876d8cc7dcd2cf1f
alexeh <alexurola@gmail.com>
2022-01-06 06:50:25 +0100

    Fix conflicts at rebase

---
+++ api/test/integration/import-data/xlsx-uploads/import-data-produce-jobs.spec.ts
@@ -0,0 +1,58 @@
+import { Queue } from 'bull';
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppModule } from '../../../../src/app.module';
+import { ImportDataModule } from '../../../../src/modules/import-data/import-data.module';
+import { getQueueToken } from '@nestjs/bull';
+import { importQueueName } from '../../../../src/modules/import-data/workers/import-data.producer';
+import { ImportDataService } from '../../../../src/modules/import-data/import-data.service';
+
+describe('XLSX Upload Feature Job Producer Tests', () => {
+  const importQueue: any = { add: jest.fn() };
+  const importQueueFail: any = { add: jest.fn().mockRejectedValue('test') };
+  let queue: Queue;
+  let moduleFixture: TestingModule;
+  let importDataService: ImportDataService;
+
+  const bootstrapTestingApp = async (fakeQueue: any): Promise<void> => {
+    moduleFixture = await Test.createTestingModule({
+      imports: [AppModule, ImportDataModule],
+    })
+      .overrideProvider(getQueueToken(importQueueName.value))
+      .useValue(fakeQueue)
+      .compile();
+
+    queue = moduleFixture.get(getQueueToken(importQueueName.value));
+    importDataService = moduleFixture.get(ImportDataService);
+  };
+
+  afterEach(async () => {
+    jest.clearAllMocks();
+    await moduleFixture.close();
+  });
+
+  test('When loadXlsxFile is called with required file data and a userId, Then a job should be pushed to the queue', async () => {
+    await bootstrapTestingApp(importQueue);
+    const fileData = { path: 'filepath' } as Express.Multer.File;
+    const userId = '29b47625-9ffa-45a9-bfce-7a75abeb820e';
+    await importDataService.loadXlsxFile(userId, fileData);
+
+    expect(queue.add).toHaveBeenCalledWith('excel-import-job', {
+      xlsxFilePath: fileData.path,
+      userId,
+    });
+  });
+  test('When loadXlsxFile is called with required file data and a userId, but the Job can not be added to the queue, a error should be displayed', async () => {
+    await bootstrapTestingApp(importQueueFail);
+    const fileData = {
+      path: 'filepath',
+      filename: 'filename',
+    } as Express.Multer.File;
+    const userId = '29b47625-9ffa-45a9-bfce-7a75abeb820e';
+    expect.assertions(1);
+    try {
+      await importDataService.loadXlsxFile(userId, fileData);
+    } catch ({ message }) {
+      expect(message).toEqual('File: filename could not have been loaded');
+    }
+  });
+});

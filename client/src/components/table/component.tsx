7bbde9129f9029db27ec038e36d85b8e192f4e06
David Inga <david.inga@vizzuality.com>
2022-01-10 11:55:02 +0100

    added summary rows to the custom table

--- client/src/components/table/component.tsx
+++ client/src/components/table/component.tsx
@@ -4,6 +4,7 @@ import { ITableProps, kaReducer, Table } from 'ka-table';
 import { updateData } from 'ka-table/actionCreators';
 import { SortingMode } from 'ka-table/enums';
 import { DispatchFunc } from 'ka-table/types';
+import { ChildComponents } from 'ka-table/Models/ChildComponents';
 
 const defaultTableProps: ITableProps = {
   columns: [],
@@ -12,26 +13,28 @@ const defaultTableProps: ITableProps = {
   sortingMode: SortingMode.Single,
 };
 
-const OverviewDemo: React.FC<ITableProps> = (tablePropsInit) => {
-  // in this case *props are stored in the state of parent component
-  const [tableProps, changeTableProps] = useState({ ...defaultTableProps, ...tablePropsInit });
+const OverviewDemo: React.FC<{ tablePropsInit: ITableProps; childComponents?: ChildComponents }> =
+  ({ tablePropsInit, childComponents = {} }) => {
+    // in this case *props are stored in the state of parent component
+    const [tableProps, changeTableProps] = useState({ ...defaultTableProps, ...tablePropsInit });
 
-  // dispatch has an *action as an argument
-  const dispatch: DispatchFunc = (action) => {
-    // *kaReducer returns new *props according to previous state and *action, and saves new props to the state
-    changeTableProps((prevState: ITableProps) => kaReducer(prevState, action));
+    // dispatch has an *action as an argument
+    const dispatch: DispatchFunc = (action) => {
+      // *kaReducer returns new *props according to previous state and *action, and saves new props to the state
+      changeTableProps((prevState: ITableProps) => kaReducer(prevState, action));
+    };
+
+    useEffect(() => {
+      dispatch(updateData(tablePropsInit.data));
+    }, [tablePropsInit.data]);
+
+    return (
+      <Table
+        {...tableProps} // ka-table UI is rendered according to props
+        dispatch={dispatch} // dispatch is required for obtain new actions from the UI
+        childComponents={childComponents}
+      />
+    );
   };
-
-  useEffect(() => {
-    dispatch(updateData(tablePropsInit.data));
-  }, [tablePropsInit.data]);
-
-  return (
-    <Table
-      {...tableProps} // ka-table UI is rendered according to props
-      dispatch={dispatch} // dispatch is required for obtain new actions from the UI
-    />
-  );
-};
 
 export default OverviewDemo;

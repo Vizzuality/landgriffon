970c0a47fd2e9f00dc96d1287e4a154442831067
alexeh <alexurola@gmail.com>
2022-01-18 07:44:29 +0100

    Add endpoint to retrieve impact materials

--- api/src/modules/impact/impact.controller.ts
+++ api/src/modules/impact/impact.controller.ts
@@ -3,11 +3,17 @@ import { GetImpactTableDto } from 'modules/impact/dto/get-impact-table.dto';
 import { ImpactService } from 'modules/impact/impact.service';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ImpactTable } from 'modules/impact/dto/response-impact-table.dto';
+import { ParseOptionalIntPipe } from 'pipes/parse-optional-int.pipe';
+import { Material } from 'modules/materials/material.entity';
+import { MaterialsService } from 'modules/materials/materials.service';
 
 @Controller('/api/v1/impact')
 @ApiTags('Impact')
 export class ImpactController {
-  constructor(public readonly impactService: ImpactService) {}
+  constructor(
+    public readonly impactService: ImpactService,
+    public readonly materialsService: MaterialsService,
+  ) {}
 
   @ApiOperation({
     description: 'Get data for Impact Table',
@@ -23,5 +29,20 @@ export class ImpactController {
       impactTableDto,
     );
     return { data: impactTable };
+  }
+
+  @ApiOperation({ description: 'Return materials imported by a User' })
+  @ApiOkResponse({
+    type: Material,
+    isArray: true,
+  })
+  @Get('materials')
+  async getMaterialsForImpact(
+    @Query('depth', ParseOptionalIntPipe) depth?: number,
+  ): Promise<Material[]> {
+    const results: Material[] =
+      await this.materialsService.getMaterialsForImpact();
+    //return this.materialsService.serialize(results);
+    return results;
   }
 }
--- api/src/modules/impact/impact.module.ts
+++ api/src/modules/impact/impact.module.ts
@@ -3,10 +3,10 @@ import { ImpactService } from 'modules/impact/impact.service';
 import { ImpactController } from 'modules/impact/impact.controller';
 import { IndicatorsModule } from 'modules/indicators/indicators.module';
 import { SourcingRecordsModule } from 'modules/sourcing-records/sourcing-records.module';
-import { SourcingLocationsModule } from 'modules/sourcing-locations/sourcing-locations.module';
+import { MaterialsModule } from 'modules/materials/materials.module';
 
 @Module({
-  imports: [IndicatorsModule, SourcingRecordsModule, SourcingLocationsModule],
+  imports: [IndicatorsModule, SourcingRecordsModule, MaterialsModule],
   providers: [ImpactService],
   controllers: [ImpactController],
   exports: [ImpactService],

5dd4087533dcbfaca41a76cf6f4e9658a61a7a53
David Inga <david.inga@vizzuality.com>
2022-01-19 13:58:35 +0100

    added custom table for grouping indicators on the table view

--- client/src/containers/analysis-visualization/analysis-table/multiple-indicator-table/component.tsx
+++ client/src/containers/analysis-visualization/analysis-table/multiple-indicator-table/component.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 import { ITableProps } from 'ka-table';
 import { DataType } from 'ka-table/enums';
+import { uniq } from 'lodash';
 
 import Table from 'components/table';
 import { ISummaryCellProps } from 'ka-table/props';
@@ -10,45 +11,78 @@ import type { ImpactTableData } from 'types';
 
 type ITableData = ITableProps & {
   key?: string;
-  yearSum: {
-    year: number;
-    value: number;
-  }[];
+  yearsSum: Record<number, string>;
 };
 
 type CustomSummaryCell = ISummaryCellProps & {
-  yearSum: {
-    year: number;
-    value: number;
-  }[];
+  yearsSum: Record<number, string>;
 };
 
-const AnalysisTable: React.FC<{ data: ImpactTableData }> = ({ data }) => {
-  // initial value of the *props
+const MultipleIndicatorTable: React.FC<{ data: ImpactTableData[] }> = ({ data }) => {
+  // Data parsed for the table
+  const tableData = useMemo(() => {
+    const result = [];
+
+    data.forEach(({ indicatorId, indicatorShortName, rows }) => {
+      rows.forEach(({ name, values }) => {
+        result.push({
+          id: `${indicatorId}-${name}`,
+          name,
+          indicatorName: indicatorShortName,
+          ...values
+            .map(({ year, value }) => ({ [year as string]: DATA_NUMBER_FORMAT(value as number) }))
+            .reduce((a, b) => ({ ...a, ...b })),
+        });
+      });
+    });
+
+    return result;
+  }, [data]);
+
+  // Years from table data
+  const years = useMemo(() => {
+    const result = [];
+    data.forEach(({ yearSum }) => {
+      const years = yearSum.map(({ year }) => year);
+      years.forEach((year) => result.push(year));
+    });
+    return uniq(result);
+  }, [data]);
+
+  // Totals for summary
+  const yearsSum = useMemo(() => {
+    const resultByYear = [];
+    years.forEach((year) => {
+      const result = data.map(({ yearSum }) => {
+        const yearValue = yearSum.find((sum) => sum.year === year);
+        if (yearValue) return yearValue;
+        return { year, value: null };
+      });
+      resultByYear.push(result.reduce((a, b) => ({ year, value: a.value + b.value })));
+    });
+    return resultByYear
+      .map(({ year, value }) => ({ [year as string]: DATA_NUMBER_FORMAT(value as number) }))
+      .reduce((a, b) => ({ ...a, ...b }));
+  }, [data, years]);
+
   const tableProps: ITableData = useMemo(() => {
-    const { indicatorId, yearSum, rows } = data;
     return {
-      key: indicatorId,
       rowKeyField: 'id',
       columns: [
         { key: 'name', title: 'Name', dataType: DataType.String, width: 250 },
-        ...yearSum.map(({ year }) => ({
+        { key: 'indicatorName', title: 'Indicator', dataType: DataType.String },
+        ...years.map((year) => ({
           key: year.toString(),
           title: year.toString(),
           DataType: DataType.Number,
           width: 100,
         })),
       ],
-      data: rows.map((row, rowIndex) => ({
-        id: `${indicatorId}-${rowIndex}`,
-        name: row.name,
-        ...row.values
-          .map(({ year, value }) => ({ [year as string]: DATA_NUMBER_FORMAT(value as number) }))
-          .reduce((a, b) => ({ ...a, ...b })),
-      })),
-      yearSum,
+      data: tableData,
+      groups: [{ columnKey: 'indicatorName' }],
+      yearsSum,
     };
-  }, [data]);
+  }, [tableData, yearsSum, years]);
 
   return (
     <Table
@@ -134,10 +168,9 @@ const AnalysisTable: React.FC<{ data: ImpactTableData }> = ({ data }) => {
                 </div>
               );
             }
-            const totalCell = props.yearSum.find((d) => d.year.toString() === props.column.key);
             return (
               <div className="ka-cell-text text-center font-bold uppercase text-gray-500 text-xs">
-                {totalCell && DATA_NUMBER_FORMAT(totalCell.value)}
+                {props.yearsSum[props.column.key]}
               </div>
             );
           },
@@ -147,4 +180,4 @@ const AnalysisTable: React.FC<{ data: ImpactTableData }> = ({ data }) => {
   );
 };
 
-export default AnalysisTable;
+export default MultipleIndicatorTable;

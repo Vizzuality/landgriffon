f5de988bb7a9f4e4dbaffb1cbb8e2d814b684314
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-01-27 14:14:01 +0000

    Improving file uploader, adding uploader error/success messages + Alerts component

--- client/src/components/uploader/component.tsx
+++ client/src/components/uploader/component.tsx
@@ -1,33 +1,112 @@
-import { useMemo, useState } from 'react';
+import { useMemo, useEffect, useState } from 'react';
 
 import Image from 'next/image';
+// import { useSession } from 'next-auth/client';
 import { useDropzone } from 'react-dropzone';
 
 import { FILE_UPLOADER_MAX_SIZE } from 'constants/file-uploader-size-limits';
+import { apiService } from 'services/api';
 import { bytesToMegabytes } from 'utils/units';
+
+import Alerts, { AlertsItemProps } from 'components/alerts';
 
 import { UploaderProps } from './types';
 
 const Uploader: React.FC<UploaderProps> = ({
   header,
   footer,
+  key = 'file',
+  url,
   fileTypes,
   maxFiles = 1,
   maxSize = FILE_UPLOADER_MAX_SIZE,
 }: UploaderProps) => {
-  const [files, setFiles] = useState([]);
+  // const [session] = useSession();
 
-  const onDropAccepted = async (files) => {
-    setFiles(files);
-    console.log('onDropAccepted', files);
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [alert, setAlert] = useState<AlertsItemProps>(null);
+
+  useEffect(() => {
+    if (isUploading) setAlert(null);
+  }, [isUploading]);
+
+  const uploadFiles = (files: File[]) => {
+    const formData = new FormData();
+    files.forEach((file) => formData.append(key, file));
+
+    apiService
+      .request({
+        method: 'POST',
+        url: url,
+        data: formData,
+        headers: {
+          // Authorization: `Bearer ${session.accessToken}`,
+        },
+      })
+      .then((response) => {
+        console.log('API', { response });
+        setAlert({
+          type: 'success',
+          title: 'Your file was successfully uploaded.',
+        });
+      })
+      .catch(({ request }) => {
+        try {
+          const errors = JSON.parse(request.response).errors.map(({ title }) => title);
+          const alertTitle = `There was ${errors.length} error${
+            errors.length > 1 ? 's' : ''
+          } with your file. Please correct ${errors.length > 1 ? 'them' : 'it'} and try again.`;
+
+          setAlert({
+            type: 'error',
+            title: alertTitle,
+            messages: errors,
+          });
+        } catch {
+          setAlert({
+            type: 'error',
+            title: 'There was an error uploading your file. Please try again.',
+          });
+        }
+      })
+      .finally(() => {
+        setIsUploading(false);
+      });
   };
 
-  const onDropRejected = async (files) => {
-    console.log('onDropAccepted', files);
+  const onDropAccepted = async (files: File[]) => {
+    setIsUploading(true);
+    uploadFiles(files);
+  };
+
+  const onDropRejected = async (dropErrors) => {
+    // TODO: handle all the file's errors.
+    //       Currently we're only dealing with the first file's errors.
+    //       It suits our existing use case but it may not, in the future.
+    const errors = dropErrors[0].errors.map((error) => {
+      switch (error.code) {
+        case 'file-too-large':
+          return `File is larger than ${bytesToMegabytes(maxSize)}MB`;
+        default:
+          return error.message;
+      }
+    });
+
+    const alertTitle = `There was ${errors.length} error${
+      errors.length > 1 ? 's' : ''
+    } with your file. Please correct ${errors.length > 1 ? 'them' : 'it'} and try again.`;
+
+    setIsUploading(false);
+    setAlert({
+      type: 'error',
+      title: alertTitle,
+      messages: errors,
+    });
   };
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: fileTypes && fileTypes.map((f) => '.' + f),
+    disabled: !!isUploading,
     maxSize,
     maxFiles,
     multiple: maxFiles > 1,
@@ -38,10 +117,6 @@ const Uploader: React.FC<UploaderProps> = ({
   const fileTypesString = useMemo(() => {
     return (fileTypes || []).map((fileType) => fileType.toUpperCase()).join(', ');
   }, [fileTypes]);
-
-  const fileNamesString = useMemo(() => {
-    return (files || []).map((file) => file.name).join(', ');
-  }, [files]);
 
   return (
     <div>
@@ -55,12 +130,13 @@ const Uploader: React.FC<UploaderProps> = ({
         <p className="mt-1">
           <span className="text-green-700">Upload a file</span> or drag and drop
         </p>
-        {files?.length > 0 && <p className="text-gray-500 text-xs mt-1">{fileNamesString}</p>}
         <p className="text-gray-500 text-xs mt-1">
-          {fileTypesString} {bytesToMegabytes(maxSize)}MB
+          {isUploading ? 'Uploading file...' : `${fileTypesString} ${bytesToMegabytes(maxSize)}MB`}
         </p>
       </div>
       {footer && <div className="text-sm mt-3">{footer}</div>}
+
+      <Alerts className="-mb-4" items={alert} />
     </div>
   );
 };

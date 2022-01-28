9593cc5a860db5c02f5abef82efb2fe8d5561b69
mluena <maluenarod@gmail.com>
2022-01-28 11:16:29 +0100

    chart data connected to API

--- client/src/hooks/analysis/types.ts
+++ client/src/hooks/analysis/types.ts
@@ -5,3 +5,51 @@ export interface AnalysisChartOptions {
 export interface AnalysisTableOptions {
   filters: Record<string, unknown>;
 }
+
+type ColorsChart = Readonly<{
+  [key: string]: string;
+}>;
+
+type Indicator = Readonly<{
+  label: string;
+  value: string;
+}>;
+
+type Filters = Readonly<{
+  by: string;
+  endYear: number;
+  indicator: Indicator;
+  materials: unknown[];
+  origins: unknown[];
+  startYear: number;
+  suppliers: unknown[];
+}>;
+
+type ChartValues = Readonly<{
+  [key: string]: number | string | boolean;
+  current: boolean;
+  date: string;
+  id: string;
+}>;
+
+type ChartData = Readonly<{
+  colors: ColorsChart;
+  filters: Filters;
+  id: string;
+  indicator: string;
+  keys: string[];
+  values: ChartValues[];
+}>;
+
+type ChartLegend = Readonly<{
+  id: string;
+  name: string;
+  color: string;
+}>;
+
+export interface AnalysisChart {
+  isLoading: boolean;
+  isFetching: boolean;
+  legend: ChartLegend[];
+  data: ChartData[];
+}

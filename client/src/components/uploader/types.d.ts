c616558411943170029b4986e2c46d3f42b2aa89
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-01-31 10:48:23 +0000

    Enable parent components from controlling the uploader (disabling auto-uploading and alerts if needed)

--- client/src/components/uploader/types.d.ts
+++ client/src/components/uploader/types.d.ts
@@ -2,8 +2,14 @@ export type UploaderProps = {
   header?: React.ReactNode;
   footer?: React.ReactNode;
   key?: string;
-  url: string;
+  url?: string;
   fileTypes?: string[];
   maxFiles?: number;
   maxSize?: number;
+  autoUpload?: boolean;
+  showAlerts?: boolean;
+  onSelected?: (files: Files[], { upload: func }) => void;
+  onRejected?: (errors) => void;
+  onUpload?: (response: AxiosResponse) => void;
+  onError?: (response) => void;
 };

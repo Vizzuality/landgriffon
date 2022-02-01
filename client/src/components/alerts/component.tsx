101c50f76e5978c14560ff5d699e99150beb0168
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-01 11:50:57 +0000

    Moving ALERT_CLASSES to a constants.ts file

--- client/src/components/alerts/component.tsx
+++ client/src/components/alerts/component.tsx
@@ -1,28 +1,8 @@
 import classNames from 'classnames';
 import { CheckCircleIcon, XCircleIcon, ExclamationIcon } from '@heroicons/react/solid';
 
+import { ALERT_CLASSES } from './constants';
 import { AlertsProps } from './types';
-
-const ALERT_CLASSES = {
-  success: {
-    iconClassnames: 'mr-2 h-5 w-5 fill-green-600',
-    backgroundColor: 'bg-green-100',
-    titleColor: 'text-green-800',
-    messagesColor: 'text-green-900',
-  },
-  error: {
-    iconClassnames: 'mr-2 h-5 w-5 fill-red-700',
-    backgroundColor: 'bg-red-100',
-    titleColor: 'text-red-800',
-    messagesColor: 'text-red-900',
-  },
-  warning: {
-    iconClassnames: 'mr-2 h-5 w-5 fill-yellow-500',
-    backgroundColor: 'bg-yellow-100',
-    titleColor: 'text-yellow-600',
-    messagesColor: 'text-yellow-900',
-  },
-};
 
 const Alerts: React.FC<AlertsProps> = ({ className, items }: AlertsProps) => {
   if (!items) return null;

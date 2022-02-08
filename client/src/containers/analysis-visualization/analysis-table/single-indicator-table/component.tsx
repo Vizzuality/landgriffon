f58a507f52664f196b175224cc2be41f26cdf558
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-08 12:25:13 +0000

    Using new Table container in the Analysis' indicators

--- client/src/containers/analysis-visualization/analysis-table/single-indicator-table/component.tsx
+++ client/src/containers/analysis-visualization/analysis-table/single-indicator-table/component.tsx
@@ -1,13 +1,12 @@
-import { useMemo, PropsWithChildren } from 'react';
+import { useMemo } from 'react';
 import { DataType } from 'ka-table/enums';
 
-import Table from 'components/table';
-import LineChart from 'components/chart/line';
 import { DATA_NUMBER_FORMAT } from 'containers/analysis-visualization/constants';
+import Table from 'containers/table';
+import SummaryRow from 'containers/table/summary-row';
 
 // types
 import type { ImpactTableData } from 'types';
-import { CustomChartCell, CustomSummaryCellSingleIndicator } from '../types';
 
 const AnalysisTable: React.FC<{ data: ImpactTableData }> = ({ data }) => {
   const { indicatorId, yearSum, rows } = data;
@@ -26,17 +25,54 @@ const AnalysisTable: React.FC<{ data: ImpactTableData }> = ({ data }) => {
     [yearSum],
   );
 
-  const dataValues = useMemo(
-    () =>
-      rows.map((row, rowIndex) => ({
-        id: `${indicatorId}-${rowIndex}`,
-        name: row.name,
-        ...row.values
-          .map(({ year, value }) => ({ [year as number]: value as number }))
-          .reduce((a, b) => ({ ...a, ...b })),
-      })),
-    [rows, indicatorId],
-  );
+  const dataValues = useMemo(() => {
+    const datesRangeChartConfig = (data) => {
+      const chartData = data.map(({ year, value }) => ({
+        x: year,
+        y: value,
+      }));
+
+      const xAxisValues = chartData.map(({ x }) => x);
+      const xMaxValue = Math.max(...xAxisValues);
+      const xMinValue = Math.min(...xAxisValues);
+      const min = xMaxValue - xMinValue;
+
+      return {
+        lines: [
+          {
+            stroke: '#909194',
+            width: '100%',
+            dataKey: 'primary_line',
+            data: chartData.filter(({ x }) => x < xMinValue + min / 2),
+          },
+          {
+            stroke: '#909194',
+            width: '100%',
+            strokeDasharray: '3 3',
+            dataKey: 'secondary_line',
+            data: chartData,
+          },
+        ],
+      };
+    };
+
+    return rows.map((row, rowIndex) => ({
+      id: `${indicatorId} - ${rowIndex}`,
+      name: row.name,
+      datesRangeChart: datesRangeChartConfig(row.values),
+      ...row.values
+        .map(({ year, value }) => ({ [year as number]: value as number }))
+        .reduce((a, b) => ({ ...a, ...b })),
+    }));
+  }, [rows, indicatorId]);
+
+  // Totals for summary
+  const yearsSum = useMemo(() => {
+    return yearSum
+      .map(({ year, value }) => ({ [year]: DATA_NUMBER_FORMAT(value) }))
+      .reduce((a, b) => ({ ...a, ...b }));
+  }, [yearSum]);
+
   const tableProps = useMemo(() => {
     const minYear = Math.min(...years);
     const maxYear = Math.max(...years);
@@ -53,157 +89,27 @@ const AnalysisTable: React.FC<{ data: ImpactTableData }> = ({ data }) => {
       columns: [
         { key: 'name', title: 'Name', dataType: DataType.String, width: 250 },
         {
-          key: 'dates-range',
-          title: `${minYear}-${maxYear}`,
-          chart: true,
-          width: 100,
+          key: 'datesRangeChart',
+          title: `${minYear} - ${maxYear}`,
+          type: 'line-chart',
+          width: 140,
         },
         ...columnValues,
       ],
       data: dataValues,
-      yearSum,
+      childComponents: {
+        summaryRow: {
+          content: (props) => (
+            <SummaryRow rowData={{ name: 'Total impact', ...yearsSum }} {...props} />
+          ),
+        },
+      },
     };
-  }, [yearSum, years, indicatorId, columnValues, dataValues]);
+  }, [yearsSum, years, indicatorId, columnValues, dataValues]);
 
   const tableKey: string = useMemo(() => `${years[0]}-${years[years.length - 1]}`, [years]);
 
-  return (
-    <Table
-      key={tableKey}
-      tablePropsInit={tableProps}
-      childComponents={{
-        tableWrapper: {
-          elementAttributes: () => ({
-            className: 'rounded-md border w-full',
-          }),
-        },
-        tableHead: {
-          elementAttributes: () => ({
-            className: 'border-b border-gray-300',
-          }),
-        },
-        tableFoot: {
-          elementAttributes: () => ({
-            className: 'border-t border-gray-300',
-          }),
-        },
-        headCell: {
-          elementAttributes: (props) => {
-            if (props.column.key === 'name') {
-              return {
-                className: 'h-auto py-3 bg-gray-50 sticky left-0 z-10',
-              };
-            }
-            return {
-              className: 'h-auto py-3 bg-gray-50 text-center',
-            };
-          },
-        },
-        headCellContent: {
-          elementAttributes: () => ({
-            className: 'font-bold uppercase text-xs leading-4 text-gray-500 whitespace-nowrap',
-          }),
-        },
-        dataRow: {
-          elementAttributes: () => ({
-            className: 'border-0 group even:bg-gray-50',
-          }),
-        },
-        cell: {
-          elementAttributes: (props) => {
-            if (props.column.key === 'name') {
-              return {
-                className: 'h-auto py-3 sticky left-0 group-even:bg-gray-50 group-odd:bg-red',
-              };
-            }
-            return {
-              className: 'h-auto py-3',
-            };
-          },
-          content: (props: PropsWithChildren<CustomChartCell>) => {
-            if (props.column.chart) {
-              const chartData = Object.entries(props.rowData).map((row: [string, number]) => ({
-                x: row[0],
-                y: row[1],
-              }));
-              const filtered = chartData.filter((d) => years.includes(new Date(d.x).getFullYear()));
-
-              const xAxisValues = filtered.map((d) => new Date(d.x).getFullYear());
-              const xMaxValue = Math.max(...xAxisValues);
-              const xMinValue = Math.min(...xAxisValues);
-              const min = xMaxValue - xMinValue;
-              const chartConfig = {
-                lines: [
-                  {
-                    stroke: '#909194',
-                    width: '100%',
-                    dataKey: 'primary_line',
-                    data: filtered.filter((d) => Number(d.x) < xMinValue + min / 2),
-                  },
-                  {
-                    stroke: '#909194',
-                    width: '100%',
-                    strokeDasharray: '3 3',
-                    dataKey: 'secondary_line',
-                    data: filtered,
-                  },
-                ],
-              };
-
-              return (
-                <div
-                  style={{ width: props.column.width, height: 50 }}
-                  className="ka-cell-text text-center font-bold uppercase text-xs flex justify-center h-full"
-                >
-                  <LineChart chartConfig={chartConfig} width={props.column.width} />
-                </div>
-              );
-            }
-          },
-        },
-        cellText: {
-          elementAttributes: (props) => {
-            if (props.column.key === 'name') {
-              return {
-                className: 'text-gray-900 leading-5',
-              };
-            }
-            return {
-              className: 'text-gray-500 leading-5 text-center',
-            };
-          },
-        },
-        summaryCell: {
-          elementAttributes: (props) => {
-            if (props.column.key === 'name') {
-              return {
-                className: 'h-auto py-3 bg-gray-50 sticky left-0',
-              };
-            }
-            return {
-              className: 'h-auto py-3 bg-gray-50',
-            };
-          },
-          content: (props: CustomSummaryCellSingleIndicator) => {
-            if (props.column.key === 'name') {
-              return (
-                <div className="ka-cell-text font-bold uppercase bg-gray-50 text-gray-500 text-xs">
-                  Total impact
-                </div>
-              );
-            }
-            const totalCell = props.yearSum.find((d) => d.year.toString() === props.column.key);
-            if (props.column.key === 'dates-range') return null;
-            return (
-              <div className="ka-cell-text text-center font-bold uppercase text-gray-500 text-xs">
-                {totalCell && DATA_NUMBER_FORMAT(totalCell.value)}
-              </div>
-            );
-          },
-        },
-      }}
-    />
-  );
+  return <Table key={tableKey} {...tableProps} />;
 };
 
 export default AnalysisTable;

21ebb032e1ee4823eb6d5e9a50e4780a6336ed5e
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-02-08 18:08:41 +0100

    Refactor sourcing locations Materials endpoint

--- api/src/modules/sourcing-locations/sourcing-locations.module.ts
+++ api/src/modules/sourcing-locations/sourcing-locations.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SourcingLocationRepository } from 'modules/sourcing-locations/sourcing-location.repository';
 import { SourcingLocationsController } from 'modules/sourcing-locations/sourcing-locations.controller';
 import { SourcingLocationsService } from 'modules/sourcing-locations/sourcing-locations.service';
-import { CustomSerializer } from 'modules/sourcing-locations/serializer/custom.serializer';
+import { SourcingLocationsMaterialsSerializer } from 'modules/sourcing-locations/serializer/sourcing-locations-materials.serializer';
 
 @Module({
   imports: [TypeOrmModule.forFeature([SourcingLocationRepository])],
   controllers: [SourcingLocationsController],
-  providers: [SourcingLocationsService, CustomSerializer],
+  providers: [SourcingLocationsService, SourcingLocationsMaterialsSerializer],
   exports: [SourcingLocationsService],
 })
 export class SourcingLocationsModule {}

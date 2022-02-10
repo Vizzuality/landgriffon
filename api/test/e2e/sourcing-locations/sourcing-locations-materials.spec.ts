aef08e0d62e9b80dab7d0d4bcbef6b305696b0a4
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-02-10 10:39:40 +0100

    Add search and orderBy flags to sl materials endpoint

--- api/test/e2e/sourcing-locations/sourcing-locations-materials.spec.ts
+++ api/test/e2e/sourcing-locations/sourcing-locations-materials.spec.ts
@@ -136,4 +136,196 @@ describe('Materials - Get the list of Materials uploaded by User with details', 
     expect(responseWithCustomPagination.body.meta.totalPages).toEqual(2);
     expect(responseWithCustomPagination.body.data.length).toBe(2);
   });
+
+  test('Getting list of materials with search flag to search for material names should be successful', async () => {
+    const supplier1: Supplier = await createSupplier();
+
+    const material1: Material = await createMaterial({ name: 'bananas' });
+    const material2: Material = await createMaterial({ name: 'maize' });
+    const material3: Material = await createMaterial({ name: 'cotton' });
+    const material4: Material = await createMaterial({ name: 'coffee' });
+    await createMaterial({ name: 'cocoa' });
+
+    await createSourcingLocation({
+      t1SupplierId: supplier1.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material1.id,
+    });
+    await createSourcingLocation({
+      producerId: supplier1.id,
+      locationType: LOCATION_TYPES.AGGREGATION_POINT,
+      materialId: material2.id,
+    });
+    await createSourcingLocation({
+      t1SupplierId: supplier1.id,
+      producerId: supplier1.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material3.id,
+    });
+
+    await createSourcingLocation({
+      t1SupplierId: supplier1.id,
+      producerId: supplier1.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material4.id,
+    });
+
+    const response1 = await request(app.getHttpServer())
+      .get(`/api/v1/sourcing-locations`)
+      .query({ materialsData: true, search: 'maize' })
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send()
+      .expect(HttpStatus.OK);
+
+    expect(response1.body.data.length).toEqual(1);
+    expect(response1.body.data[0].attributes.materialName).toEqual(
+      material2.name,
+    );
+
+    const response2 = await request(app.getHttpServer())
+      .get(`/api/v1/sourcing-locations`)
+      .query({ materialsData: true, search: 'co' })
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send()
+      .expect(HttpStatus.OK);
+
+    expect(response2.body.data.length).toEqual(2);
+    expect(response2.body.data[0].attributes.materialName).toEqual(
+      material4.name,
+    );
+  });
+
+  test('Getting list of materials with correct order by flag should be successful', async () => {
+    const supplier1: Supplier = await createSupplier({ name: 'aSupplier' });
+    const supplier2: Supplier = await createSupplier({ name: 'bSupplier' });
+    const supplier3: Supplier = await createSupplier({ name: 'cSupplier' });
+    const supplier4: Supplier = await createSupplier({ name: 'dSupplier' });
+
+    const material1: Material = await createMaterial({ name: 'bananas' });
+    const material2: Material = await createMaterial({ name: 'maize' });
+    const material3: Material = await createMaterial({ name: 'cotton' });
+    const material4: Material = await createMaterial({ name: 'coffee' });
+    await createMaterial({ name: 'cocoa' });
+
+    await createSourcingLocation({
+      t1SupplierId: supplier1.id,
+      producerId: supplier4.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material2.id,
+      locationCountryInput: 'Brazil',
+    });
+    await createSourcingLocation({
+      t1SupplierId: supplier4.id,
+      producerId: supplier1.id,
+      locationType: LOCATION_TYPES.AGGREGATION_POINT,
+      materialId: material1.id,
+      locationCountryInput: 'Argentina',
+    });
+    await createSourcingLocation({
+      t1SupplierId: supplier2.id,
+      producerId: supplier3.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material3.id,
+      locationCountryInput: 'Italy',
+    });
+
+    await createSourcingLocation({
+      t1SupplierId: supplier3.id,
+      producerId: supplier2.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material4.id,
+      locationCountryInput: 'Spain',
+    });
+
+    // Order by country name
+    const response1 = await request(app.getHttpServer())
+      .get(`/api/v1/sourcing-locations`)
+      .query({ materialsData: true, orderBy: 'country' })
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send()
+      .expect(HttpStatus.OK);
+
+    expect(response1.body.data.length).toEqual(4);
+    expect(response1.body.data[0].attributes.materialName).toEqual(
+      material1.name,
+    );
+    expect(response1.body.data[1].attributes.materialName).toEqual(
+      material2.name,
+    );
+    expect(response1.body.data[2].attributes.materialName).toEqual(
+      material3.name,
+    );
+    expect(response1.body.data[3].attributes.materialName).toEqual(
+      material4.name,
+    );
+
+    // Order by supplierT1 name
+    const response2 = await request(app.getHttpServer())
+      .get(`/api/v1/sourcing-locations`)
+      .query({ materialsData: true, orderBy: 't1Supplier' })
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send()
+      .expect(HttpStatus.OK);
+
+    expect(response2.body.data[0].attributes.materialName).toEqual(
+      material2.name,
+    );
+    expect(response2.body.data[1].attributes.materialName).toEqual(
+      material3.name,
+    );
+    expect(response2.body.data[2].attributes.materialName).toEqual(
+      material4.name,
+    );
+    expect(response2.body.data[3].attributes.materialName).toEqual(
+      material1.name,
+    );
+
+    // Order by producer name
+    const response3 = await request(app.getHttpServer())
+      .get(`/api/v1/sourcing-locations`)
+      .query({ materialsData: true, orderBy: 'producer' })
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send()
+      .expect(HttpStatus.OK);
+    expect(response3.body.data[0].attributes.materialName).toEqual(
+      material1.name,
+    );
+    expect(response3.body.data[1].attributes.materialName).toEqual(
+      material4.name,
+    );
+    expect(response3.body.data[2].attributes.materialName).toEqual(
+      material3.name,
+    );
+    expect(response3.body.data[3].attributes.materialName).toEqual(
+      material2.name,
+    );
+  });
+
+  test('Getting list of materials with order by flag applied to incorrect column should return proper error message', async () => {
+    const supplier1: Supplier = await createSupplier({ name: 'aSupplier' });
+
+    const material1: Material = await createMaterial({ name: 'bananas' });
+
+    await createSourcingLocation({
+      t1SupplierId: supplier1.id,
+      producerId: supplier1.id,
+      locationType: LOCATION_TYPES.UNKNOWN,
+      materialId: material1.id,
+    });
+
+    const response = await request(app.getHttpServer())
+      .get(`/api/v1/sourcing-locations`)
+      .query({ materialsData: true, orderBy: 'purcahses' })
+      .set('Authorization', `Bearer ${jwtToken}`)
+      .send()
+      .expect(HttpStatus.BAD_REQUEST);
+
+    expect(response).toHaveErrorMessage(
+      HttpStatus.BAD_REQUEST,
+      'Bad Request Exception',
+      [
+        'Available columns for orderBy: country, material, producer, t1Supplier, locationType',
+      ],
+    );
+  });
 });

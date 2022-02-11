10220e5c93f2c32ae6c2ce705483fb66d8d3296a
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-11 13:31:27 +0000

    Switching to new endpoint to fetching sourcing materials data

--- client/src/hooks/sourcing-locations/index.ts
+++ client/src/hooks/sourcing-locations/index.ts
@@ -5,15 +5,15 @@ import { v4 as uuidv4 } from 'uuid';
 import { apiWithMetadataService } from 'services/api';
 import { SourcingLocation, APIMetadataPagination } from 'types';
 
-type SourcingLocationsAPIResponse = {
+type SourcingLocationsMaterialsAPIResponse = {
   data: SourcingLocation[];
   metadata: APIMetadataPagination;
 };
 
-type SourcingLocationsDataResponse = UseQueryResult & SourcingLocationsAPIResponse;
+type SourcingLocationsMaterialsDataResponse = UseQueryResult &
+  SourcingLocationsMaterialsAPIResponse;
 
 export type SourcingLocationsParams = {
-  materialsData?: boolean;
   'page[number]'?: number;
   'page[size]'?: number;
 };
@@ -24,19 +24,18 @@ const DEFAULT_QUERY_OPTIONS: UseQueryOptions = {
   keepPreviousData: false,
   refetchOnWindowFocus: false,
 };
-
-export function useSourcingLocations(
+export function useSourcingLocationsMaterials(
   params: SourcingLocationsParams,
-): SourcingLocationsDataResponse {
+): SourcingLocationsMaterialsDataResponse {
   // const [session] = useSession();
 
   const query = useQuery(
-    ['sourcingLocations', params],
+    ['sourcingLocationsMaterials', params],
     async () =>
       apiWithMetadataService
         .request({
           method: 'GET',
-          url: '/sourcing-locations',
+          url: '/sourcing-locations/materials',
           params,
           headers: {
             // Authorization: `Bearer ${session.accessToken}`,
@@ -50,7 +49,7 @@ export function useSourcingLocations(
 
   const { data: response } = query;
 
-  return useMemo<SourcingLocationsDataResponse>(
+  return useMemo<SourcingLocationsMaterialsDataResponse>(
     () =>
       ({
         ...query,
@@ -59,12 +58,12 @@ export function useSourcingLocations(
           // are duplicates. This causes issues with our tables, not only errors but also
           // duplicate rows when updating the table data. A workaround is to generate uuids.
           // We won't be loading many rows at once so it shouldn't be a huge performance hit.
-          ((response as SourcingLocationsAPIResponse).data || []).map((data) => ({
+          ((response as SourcingLocationsMaterialsAPIResponse).data || []).map((data) => ({
             id: uuidv4(),
             ...data,
           })) || DEFAULT_QUERY_OPTIONS.placeholderData,
-        metadata: (response as SourcingLocationsAPIResponse).metadata || {},
-      } as SourcingLocationsDataResponse),
+        metadata: (response as SourcingLocationsMaterialsAPIResponse).metadata || {},
+      } as SourcingLocationsMaterialsDataResponse),
     [query, response],
   );
 }

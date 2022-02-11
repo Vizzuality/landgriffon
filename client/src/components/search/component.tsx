9098554439e75dfacd8696cc43628261a4db502a
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-11 14:05:19 +0000

    Adding search input component

---
+++ client/src/components/search/component.tsx
@@ -0,0 +1,43 @@
+import { FC, useRef } from 'react';
+import { SearchIcon, XIcon } from '@heroicons/react/solid';
+import { useButton } from '@react-aria/button';
+import { useSearchField } from '@react-aria/searchfield';
+import { useSearchFieldState } from '@react-stately/searchfield';
+
+import type { SearchProps } from './types';
+
+export const Search: FC<SearchProps> = ({ ...props }: SearchProps) => {
+  const { placeholder } = props;
+  const state = useSearchFieldState(props);
+
+  const ref = useRef();
+  const { inputProps, clearButtonProps } = useSearchField(props, state, ref);
+  const { buttonProps } = useButton(clearButtonProps, null);
+
+  return (
+    <div className="relative flex items-center w-full">
+      <SearchIcon className="absolute w-5 h-5 text-gray-400 transform -translate-y-1/2 top-1/2 left-3" />
+
+      <input
+        {...inputProps}
+        ref={ref}
+        placeholder={placeholder}
+        type="search"
+        className="py-3 pl-10 pr-8 text-sm font-medium leading-4 text-left bg-white border-none rounded-md shadow-sm md:w-auto focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700"
+      />
+
+      {state.value !== '' && (
+        <button
+          {...buttonProps}
+          tabIndex="clear"
+          className="absolute z-10 flex items-center self-center justify-center w-5 h-5 right-3 r-2"
+          type="button"
+        >
+          <XIcon className="w-4 h-4 text-gray-400" />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default Search;

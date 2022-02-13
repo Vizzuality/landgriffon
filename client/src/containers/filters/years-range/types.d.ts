c393401a9ae739f181dcfec9e035711d5819aba2
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-13 21:39:02 +0000

    Adding comments to the years range filter types

--- client/src/containers/filters/years-range/types.d.ts
+++ client/src/containers/filters/years-range/types.d.ts
@@ -1,16 +1,24 @@
 export type YearsRangeParams = {
+  /** Start year option in the filter */
   startYear: number;
+  /** End year option in the filter */
   endYear: number;
 };
 
 export type YearsRangeFilterProps = YearsRangeParams & {
+  /** Array of available years to add to the filter */
   years: number[];
+  /** Whether the filter should enforce a 5 year gap between start and end years */
   fiveYearGap?: boolean;
+  /** Callback when the years range is changed */
   onChange?: ({ startYear, endYear }: YearsRangeParams) => void;
 };
 
 export type UseYearsRangeProps = Partial<YearsRangeParams> & {
+  /** Set the start year */
   setStartYear?: (startYear: number) => void;
+  /** Set the end year */
   setEndYear?: (endYear: number) => void;
+  /** Set the years range */
   setYearsRange?: ({ startYear, endYear }: YearsRangeParams) => void;
 };

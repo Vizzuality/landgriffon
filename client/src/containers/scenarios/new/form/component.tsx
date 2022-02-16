1cfe306c9a2a1df74be1aded8257fb6994d466f2
mluena <maria.luena@vizzuality.com>
2022-02-16 10:01:04 +0100

    scenarios new form

--- client/src/containers/scenarios/new/form/component.tsx
+++ client/src/containers/scenarios/new/form/component.tsx
@@ -1,66 +1,25 @@
-import { useCallback, useMemo } from 'react';
+import { useMemo } from 'react';
 import { useRouter } from 'next/router';
-import { useAppDispatch } from 'store/hooks';
-import Steps from 'components/steps';
-import { setSubContentCollapsed } from 'store/features/analysis';
 
-import type { Step } from 'components/steps/types';
+import InterventionsForm from 'containers/interventions/new';
+import GrowthForm from 'containers/growth';
 
-import Step1 from './step1';
-import Step2 from './step2';
+import CollapseButton from 'containers/collapse-button';
 
-const steps: Step[] = [
-  {
-    id: 'Step 1',
-    name: 'Define scope',
-    href: '#step1',
-    status: 'current',
-  },
-  {
-    id: 'Step 2',
-    name: 'Source from a new supplier or location',
-    description: 'Select a new location or supplier you want to source from in order to analyse changes.',
-    href: '#step2',
-    status: 'upcoming',
-  },
-];
-
-const InterventionForm = () => {
-  const dispatch = useAppDispatch();
+const ScenariosForm = () => {
   const { asPath } = useRouter();
+  const tab = useMemo<string>(() => asPath.split('#')[1], [asPath]);
+  const growthContent = useMemo<boolean>(() => tab && tab.includes('growth'), [tab]);
 
-  const currentStep = useMemo<string>(() => asPath.split('#')[1], [asPath]);
-
-  const handleCancel = useCallback(() => {
-    dispatch(setSubContentCollapsed(true));
-  }, []);
-  console.log(currentStep, currentStep === 'step1', 'ecenario new')
   return (
     <div className="p-12 bg-white z-50 h-full">
-      <Steps steps={steps} className="mb-10 z-20" />
-      <form className="space-y-8">
-        { <Step1 />}
-        {currentStep === 'step2' && <Step2 />}
-        <div className="pt-5">
-          <div className="flex justify-end">
-            <button
-              type="button"
-              className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              onClick={handleCancel}
-            >
-              Cancel
-            </button>
-            <button
-              type="submit"
-              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            >
-              Add intervention
-            </button>
-          </div>
-        </div>
-      </form>
+      {!growthContent && <InterventionsForm />}
+      {growthContent && <GrowthForm />}
+      <div className="absolute top-5 right-0 transform translate-x-1/2 z-30">
+        <CollapseButton />
       </div>
+    </div>
   );
 };
 
-export default InterventionForm;
+export default ScenariosForm;
--- client/src/containers/scenarios/new/index.tsx
+++ client/src/containers/scenarios/new/index.tsx
@@ -1,19 +1,12 @@
-import { useCallback, useState } from 'react';
-import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 import { useQuery } from 'react-query';
 import { createScenario } from 'services/scenarios';
 
-import Interventions from 'containers/interventions';
-import InterventionForm from 'containers/scenarios/new/form';
-import CollapseButton from 'containers/collapse-button';
-import { useAppSelector, useAppDispatch } from 'store/hooks';
-import { analysis, setSubContentCollapsed } from 'store/features/analysis';
-
+import InterventionsAttributes from 'containers/scenarios/attributes';
+import { useAppDispatch } from 'store/hooks';
+import { setSubContentCollapsed } from 'store/features/analysis';
 
 const ScenariosNewContainer: React.FC = () => {
-  const router = useRouter();
-
-  const [newIntervention, setNewIntervention] = useState(false);
   const response = useQuery('scenarioNew', () => createScenario({ title: 'Untitled' }));
 
   if (response.isSuccess) {
@@ -27,47 +20,40 @@ const ScenariosNewContainer: React.FC = () => {
   const dispatch = useAppDispatch();
   const handleNewIntervention = useCallback(() => {
     dispatch(setSubContentCollapsed(false));
-    setNewIntervention(true);
   }, []);
 
   return (
-      <>
-    <form action="#" method="POST" className='z-20 bg-green-500'>
-      <label htmlFor="title" className="block text-sm text-gray-900">
+    <>
+      <form action="#" method="POST" className="z-20">
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Untitled"
+          aria-label="sceanrio title"
+          autoFocus
+          className="flex-1 block w-full md:text-2xl sm:text-sm border-none text-gray-400 p-0 font-semibold mb-6"
+        />
 
-      </label>
-      <input
-        type="text"
-        name="title"
-        id="title"
-        placeholder="Untitled"
-        autoFocus
-        className="flex-1 block w-full md:text-2xl sm:text-sm border-none text-gray-400 p-0 font-semibold mb-6"
-      />
-
-      <div className="sm:col-span-6">
-        <label htmlFor="description" className="block text-sm text-gray-900">
-          Scenario description <span className="text-gray-500">(optional)</span>
-        </label>
-        <div className="mt-1">
-          <textarea
-            id="description"
-            name="description"
-            rows={3}
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md"
-            defaultValue=""
-          />
+        <div className="sm:col-span-6">
+          <label htmlFor="description" className="block text-sm text-gray-900">
+            Scenario description <span className="text-gray-500">(optional)</span>
+          </label>
+          <div className="mt-1">
+            <textarea
+              id="description"
+              name="description"
+              rows={3}
+              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border border-gray-300 rounded-md"
+              defaultValue=""
+            />
+          </div>
         </div>
+      </form>
+      <div className="h-full flex flex-col w-auto bg-white py-6 overflow-auto relative">
+        <InterventionsAttributes handleNewIntervention={handleNewIntervention} />
       </div>
-    </form>
-    <div className="h-full flex flex-col w-auto bg-white py-6 overflow-auto relative">
-          <Interventions handleNewIntervention={handleNewIntervention}/>
-       {newIntervention && <div className="absolute h-full left-125 top-0 right-10">
-          <InterventionForm />
-        </div>}
-        </div>
-
-        </>
+    </>
   );
 };
 

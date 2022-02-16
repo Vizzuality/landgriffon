1c934916b2e231c0798d55516f4006a9cf0a3300
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-02-16 14:15:30 +0100

    Testing geo coding import

--- api/src/modules/scenario-interventions/scenario-interventions.module.ts
+++ api/src/modules/scenario-interventions/scenario-interventions.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GeoCodingModule } from 'modules/geo-coding/geo-coding.module';
+
 import { ScenarioInterventionRepository } from 'modules/scenario-interventions/scenario-intervention.repository';
 import { ScenarioInterventionsController } from 'modules/scenario-interventions/scenario-interventions.controller';
 import { ScenarioInterventionsService } from 'modules/scenario-interventions/scenario-interventions.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ScenarioInterventionRepository])],
+  imports: [
+    TypeOrmModule.forFeature([ScenarioInterventionRepository]),
+    GeoCodingModule,
+  ],
   controllers: [ScenarioInterventionsController],
   providers: [ScenarioInterventionsService],
   exports: [ScenarioInterventionsService],

07e0236cf79ef7fc3769f25e01ca42fa3214ad97
mluena <maria.luena@vizzuality.com>
2022-02-16 10:02:46 +0100

    slug added to steps component

--- client/src/components/steps/component.tsx
+++ client/src/components/steps/component.tsx
@@ -1,28 +1,36 @@
 import type { Step } from './types';
+import cx from 'classnames';
 
 import Link from 'next/link';
 
 type StepsProps = React.HTMLAttributes<unknown> & {
   steps: Step[];
+  current?: string;
 };
 
-const Steps: React.FC<StepsProps> = ({ steps, ...props }: StepsProps) => (
+const Steps: React.FC<StepsProps> = ({ steps, current, ...props }: StepsProps) => (
   <nav aria-label="Progress" {...props}>
     <ol className="space-y-4 md:flex md:space-y-0 md:space-x-8">
       {steps.map((step) => (
         <li key={step.name} className="md:flex-1">
           {step.status === 'complete' && (
             <Link href={step.href}>
-              <a 
+              <a
                 href={step.href}
-                className="group pl-4 py-2 flex flex-col border-l-4 border-indigo-600 hover:border-indigo-800 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+                className={cx('group pl-4 py-2 flex flex-col border-l-4md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4',
+                  {
+                    'border-indigo-600 hover:border-indigo-800': step.slug !== current,
+                    'border-green-700': step.slug === current,
+                  },
+                )}
               >
-
                 <span className="text-xs text-indigo-600 font-semibold tracking-wide uppercase group-hover:text-indigo-800">
                   {step.id}
                 </span>
                 <span className="text-sm font-medium">{step.name}</span>
-                {!!step.description && <p className="text-xs text-gray-500">{step.description}</p>}
+                {!!step.description && step.slug === current && (
+                  <p className="text-xs text-gray-500">{step.description}</p>
+                )}
               </a>
             </Link>
           )}
@@ -30,14 +38,21 @@ const Steps: React.FC<StepsProps> = ({ steps, ...props }: StepsProps) => (
             <Link href={step.href}>
               <a
                 href={step.href}
-                className="pl-4 py-2 flex flex-col border-l-4 border-indigo-600 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+                className={cx('pl-4 py-2 flex flex-col border-l-4 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4',
+                  {
+                    'border-green-70': step.slug !== current,
+                    'border-green-700': step.slug === current,
+                  },
+                )}
                 aria-current="step"
               >
                 <span className="text-xs text-indigo-600 font-semibold tracking-wide uppercase">
                   {step.id}
                 </span>
                 <span className="text-sm font-medium">{step.name}</span>
-                {!!step.description && <p className="text-xs text-gray-500">{step.description}</p>}
+                {!!step.description && step.slug === current && (
+                  <p className="text-xs text-gray-500">{step.description}</p>
+                )}
               </a>
             </Link>
           )}
@@ -45,13 +60,20 @@ const Steps: React.FC<StepsProps> = ({ steps, ...props }: StepsProps) => (
             <Link href={step.href}>
               <a
                 href={step.href}
-                className="group pl-4 py-2 flex flex-col border-l-4 border-gray-200 hover:border-gray-300 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4"
+                className={cx('group pl-4 py-2 flex flex-col border-l-4 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4',
+                  {
+                    'border-gray-200 hover:border-gray-300': step.slug !== current,
+                    'border-green-700': step.slug === current,
+                  },
+                )}
               >
                 <span className="text-xs text-gray-500 font-semibold tracking-wide uppercase group-hover:text-gray-700">
                   {step.id}
                 </span>
                 <span className="text-sm font-medium">{step.name}</span>
-                {!!step.description && <p className="text-xs text-gray-500">{step.description}</p>}
+                {!!step.description && step.slug === current && (
+                  <p className="text-xs text-gray-500">{step.description}</p>
+                )}
               </a>
             </Link>
           )}

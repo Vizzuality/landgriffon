9a034491552a36f00440039053bc6fa5cbeedb52
mluena <maria.luena@vizzuality.com>
2022-02-16 09:50:13 +0100

    analysis index layout for subcontent

--- client/src/pages/analysis/index.tsx
+++ client/src/pages/analysis/index.tsx
@@ -6,12 +6,15 @@ import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
 import ApplicationLayout from 'layouts/application';
+import Breadcrumb from 'components/breadcrumb';
 import Scenarios from 'containers/scenarios';
 import ScenarioNew from 'containers/scenarios/new';
 import ScenarioEdit from 'containers/scenarios/edit';
-import InterventionForm from 'containers/interventions/form';
+import ScenariosForm from 'containers/scenarios/new/form';
 import { analysis, setSubContentCollapsed } from 'store/features/analysis';
 import CollapseButton from 'containers/collapse-button';
+
+import type { Page } from 'components/breadcrumb/types';
 
 const AnalysisVisualizationNoSSR = dynamic(() => import('containers/analysis-visualization'), {
   ssr: false,
@@ -19,6 +22,9 @@ const AnalysisVisualizationNoSSR = dynamic(() => import('containers/analysis-vis
 
 const AnalysisPage: React.FC = () => {
   const { isSidebarCollapsed, isSubContentCollapsed } = useAppSelector(analysis);
+  const dispatch = useAppDispatch();
+  const { query } = useRouter();
+  const { new_scenario, edit_scenario } = query;
 
   const analysisContent = () => {
     if (new_scenario) return <ScenarioNew />;
@@ -26,9 +32,15 @@ const AnalysisPage: React.FC = () => {
     return <Scenarios />;
   };
 
-  const dispatch = useAppDispatch();
-  const { query } = useRouter();
-  const { new_scenario, edit_scenario } = query;
+  // Breadcrumbs
+  let pages: Page[] = [{ name: 'Analysis', href: '/analysis' }]; // Default
+  if (edit_scenario) {
+    pages = [...pages, { name: 'Edit scenario', href: '/analysis?edit_scenario' }];
+  }
+
+  if (new_scenario) {
+    pages = [...pages, { name: 'New scenario', href: '/analysis?new_scenario' }];
+  }
 
   useEffect(() => {
     // Close and cancel interventions creation
@@ -42,37 +54,43 @@ const AnalysisPage: React.FC = () => {
         <title>Analysis - Landgriffon</title>
       </Head>
       <main className="flex-1">
-        <div className="lg:h-screen">
-          <AnalysisVisualizationNoSSR />
-
+        <div className="lg:h-screen flex">
           {/* Analysis content */}
-          <section className="fixed h-full top-0 hidden lg:block lg:flex-shrink-0 lg:order-first z-20">
-            <Transition
-              className="h-full relative flex flex-col border-r border-gray-200 bg-white w-96 overflow-x-hidden px-6"
-              show={!isSidebarCollapsed}
-              enter="transition-opacity duration-75"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="transition-opacity duration-150"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              {analysisContent()}
-            </Transition>
-            <div className="absolute top-5 right-0 transform translate-x-1/2 z-30">
-              <CollapseButton />
-            </div>
-          </section>
+          <div className="relative h-full z-30">
+            <section className="relative h-full top-0 hidden lg:block lg:flex-shrink-0 lg:order-first">
+              <Transition
+                className="h-full relative flex flex-col border-r border-gray-200 bg-white w-96 overflow-x-hidden px-6"
+                show={!isSidebarCollapsed}
+                enter="transition-opacity duration-75"
+                enterFrom="opacity-0"
+                enterTo="opacity-100"
+                leave="transition-opacity duration-150"
+                leaveFrom="opacity-100"
+                leaveTo="opacity-0"
+              >
+                <div className="pb-10">
+                  <Breadcrumb pages={pages} />
+                </div>
+                {analysisContent()}
+              </Transition>
 
-          {/* Analysis aside */}
-          {!isSubContentCollapsed && (
-            <aside className="absolute ml-96 h-full hidden lg:block lg:flex-shrink-0 w-1/2">
-              <div className="h-full relative flex flex-col w-auto border-r border-gray-200 bg-white p-6 overflow-auto">
-                {/* For now, I'm going to assume we will only have the intervention form here */}
-                <InterventionForm />
-              </div>
-            </aside>
-          )}
+              {/* Analysis aside */}
+              {!isSubContentCollapsed && !isSidebarCollapsed && (
+                <aside className="absolute ml-96 top-0 h-full hidden lg:block lg:flex-shrink-0 bg-white z-20 w-max max-w-3xl">
+                  <div className="h-full flex flex-col w-auto border-r border-gray-200 p-6 overflow-auto">
+                    {/* For now, I'm going to assume we will only have the intervention form here */}
+                    <ScenariosForm />
+                  </div>
+                </aside>
+              )}
+              {isSubContentCollapsed && (
+                <div className="absolute top-5 right-0 transform translate-x-1/2 z-30">
+                  <CollapseButton />
+                </div>
+              )}
+            </section>
+          </div>
+          <AnalysisVisualizationNoSSR />
         </div>
       </main>
     </ApplicationLayout>

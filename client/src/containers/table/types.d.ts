54ca5709f4685ebd064792d4e3fc2a4219e7f880
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-21 15:14:06 +0000

    Adding missing PaginationProps type + Adding comments to types

--- client/src/containers/table/types.d.ts
+++ client/src/containers/table/types.d.ts
@@ -3,21 +3,35 @@ import { ITableProps, ICellProps, ChildComponents } from 'ka-table';
 import { SortingMode } from './enums';
 
 export type ColumnProps = ICellProps & {
+  /** Custom cell types */
   type: 'line-chart';
+  /** Whether the column is sortable */
   isSortable: boolean;
 };
 
 export interface TableProps extends ITableProps {
+  /** classNames to apply to the table */
   className?: string;
+  /**
+   * Whether data is currently being loaded. Will cause pagination to freeze its current state.
+   * Defaults to `false`.
+   * */
   isLoading?: boolean;
+  /** Whether to make the first column sticky. Defaults to `true` */
   stickyFirstColumn?: boolean;
+  /** ka-table childComponents */
   childComponents?: ChildComponents;
+  /** ka-table sorting modes + a custom one: `Api` */
   sortingMode?: SortingMode;
+  /** Default sorting time */
   defaultSorting?: ApiSortingType;
+  /** Callback when sorting changes. Applicable to `Api` sorting only. */
   onSortingChange?: func;
 }
 
 export type ApiSortingType = {
+  /** Field to sort by */
   orderBy: string;
+  /** Sort by asc or desc */
   order: ApiSortDirection;
 };

7dca69a6a1e94f95dd394758fdce25393d8bc26b
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-21 13:57:33 +0000

    Small tweak to make it easier to import Tables with NoSSR

--- client/src/containers/table/index.ts
+++ client/src/containers/table/index.ts
@@ -1,3 +1,9 @@
+import dynamic from 'next/dynamic';
+
 export { default } from './component';
 export type { TableProps, ApiSortingType } from './types';
 export { DataType, SortingMode, ApiSortingDirection } from './enums';
+
+export const TableNoSSR = dynamic(() => import('./component'), {
+  ssr: false,
+});

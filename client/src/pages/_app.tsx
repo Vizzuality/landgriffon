a0177b015fb5aa6094ebb1bcec7dd6c731b772ec
David Inga <david.inga@vizzuality.com>
2022-02-22 16:13:55 +0100

    updated next-auth v4 configuration

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { OverlayProvider } from '@react-aria/overlays';
 import { Hydrate } from 'react-query/hydration';
-import { Provider as AuthenticationProvider } from 'next-auth/client';
+import { SessionProvider } from 'next-auth/react';
 import store from 'store';
 
 import 'styles/globals.css';
@@ -14,17 +14,17 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
   <ReduxProvider store={store}>
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
-        <AuthenticationProvider
+        <SessionProvider
           session={pageProps.session}
-          options={{
-            clientMaxAge: 5 * 60, // Re-fetch session if cache is older than 60 seconds
-            keepAlive: 10 * 60, // Send keepAlive message every 10 minutes
-          }}
+          // options={{
+          //   clientMaxAge: 5 * 60, // Re-fetch session if cache is older than 60 seconds
+          //   keepAlive: 10 * 60, // Send keepAlive message every 10 minutes
+          // }}
         >
           <OverlayProvider>
             <Component {...pageProps} />
           </OverlayProvider>
-        </AuthenticationProvider>
+        </SessionProvider>
       </Hydrate>
     </QueryClientProvider>
   </ReduxProvider>

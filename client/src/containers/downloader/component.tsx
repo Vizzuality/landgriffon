567c6995b7ae4b2877656d79bb1c0dac7fa48875
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-22 17:19:19 +0000

    Adding generic Downloader component to help download data as CSV

---
+++ client/src/containers/downloader/component.tsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+// import { useSession } from 'next-auth/client';
+import { CSVDownload } from 'react-csv';
+import { noop } from 'lodash';
+
+import { apiService } from 'services/api';
+
+import { DownloaderProps } from './types';
+
+const DEFAULT_PARAMS = {
+  disablePagination: true,
+};
+
+const Downloader: React.FC<DownloaderProps> = ({
+  className,
+  type = 'csv',
+  url,
+  params,
+  headers: propsHeaders,
+  transformer,
+  onDownloading = noop,
+  onSuccess = noop,
+  onError = noop,
+  children,
+}: DownloaderProps) => {
+  // const [session] = useSession();
+
+  const [headers, setHeaders] = useState<Record<string, string>[]>(null);
+  const [data, setData] = useState<Record<string, string>[]>(null);
+
+  const getDataForExport = async () =>
+    apiService
+      .request({
+        method: 'GET',
+        url: url,
+        params: { ...DEFAULT_PARAMS, ...params },
+        headers: {
+          // Authorization: `Bearer ${session.accessToken}`,
+        },
+      })
+      .then(({ data }) =>
+        transformer
+          ? transformer({
+              headers: propsHeaders,
+              data,
+            })
+          : { headers: propsHeaders, data },
+      );
+
+  const handleClick = async () => {
+    onDownloading();
+
+    switch (type) {
+      case 'csv':
+        getDataForExport()
+          .then(({ headers, data }) => {
+            headers && setHeaders(headers);
+            data && setData(data);
+            onSuccess();
+          })
+          .catch(onError);
+        return;
+      // TODO: Other export formats
+      default:
+        const errorMessage = `Export format ${type} not yet supported`;
+        console.error(errorMessage);
+        onError(errorMessage);
+        return;
+    }
+  };
+
+  useEffect(() => {
+    if (!data) return;
+    setHeaders(null);
+    setData(null);
+  }, [data]);
+
+  const shouldTriggerDownload = data && data.length > 0;
+
+  return (
+    <>
+      <div className={className} onClick={handleClick}>
+        {children}
+      </div>
+      {shouldTriggerDownload && (
+        <>
+          {type === 'csv' && (
+            <CSVDownload target="_blank" data={data} {...(headers && { headers })} />
+          )}
+          {/* TODO: Other export formats */}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Downloader;

29d1a5ff11ef21e51223d5711c7911570e4109ba
David Inga <david.inga@vizzuality.com>
2022-02-23 14:50:22 +0100

    solved warning related to react forward ref

--- client/src/components/forms/input/component.tsx
+++ client/src/components/forms/input/component.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Hint from '../hint';
 import classnames from 'classnames';
 
@@ -5,7 +6,7 @@ type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   error?: string;
 };
 
-const Input: React.FC<InputProps> = ({ error, ...props }) => (
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ error, ...props }, ref) => (
   <>
     <div className="mt-1">
       <input
@@ -13,11 +14,14 @@ const Input: React.FC<InputProps> = ({ error, ...props }) => (
           'appearance-none block w-full px-3 py-2 border border-gray-200 rounded-md shadow-sm text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-green-700 focus:border-green-700 sm:text-sm',
           { 'border-red-600': !!error },
         )}
+        ref={ref}
         {...props}
       />
     </div>
     {error && <Hint>{error}</Hint>}
   </>
-);
+));
+
+Input.displayName = 'Input';
 
 export default Input;

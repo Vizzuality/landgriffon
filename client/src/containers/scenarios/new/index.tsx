5185ec5165d26376edce21eba1a6c4c92e5dfa7e
María Luena Rodríguez <maluenarod@gmail.com>
2022-02-23 17:37:38 +0100

    Update client/src/containers/scenarios/new/index.tsx
    Co-authored-by: David Inga <davidsingal@users.noreply.github.com>

--- client/src/containers/scenarios/new/index.tsx
+++ client/src/containers/scenarios/new/index.tsx
@@ -114,17 +114,13 @@ const ScenariosNewContainer: React.FC = () => {
                 <button
                   type="button"
                   className={cx({ 'border-b-2 border-green-700': !interventionsContent })}
-                  onClick={handleTab}
-                  dataTab="growth"
+                  onClick={() => handleTab('growth')}
                 >
                   Growth rates ({items.length})
                 </button>
               </div>
-<<<<<<< HEAD
+
               <Button onClick={handleNewScenarioFeature}>
-=======
-              <Button onClick={() => handleNewScenarioFeature()}>
->>>>>>> 1701329b (new scen componentized, scen attributes removed)
                 <PlusIcon className="-ml-1 mr-1 h-5 w-5" aria-hidden="true" />
                 New
               </Button>

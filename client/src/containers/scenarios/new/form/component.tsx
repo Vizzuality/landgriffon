c22ef628ef6bdc772b594b1f86cd10e8f8ebc999
mluena <maria.luena@vizzuality.com>
2022-02-23 18:10:17 +0100

    interventions steps redux updated to get just a number

--- client/src/containers/scenarios/new/form/component.tsx
+++ client/src/containers/scenarios/new/form/component.tsx
@@ -8,31 +8,12 @@ import GrowthForm from 'containers/growth';
 import CollapseButton from 'containers/collapse-button';
 
 const ScenariosForm: FC = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
   const { scenarioCurrentTab } = useAppSelector(analysis);
 
   return (
     <div className="p-12 bg-white z-50 h-full">
       {scenarioCurrentTab === 'interventions' && <InterventionsForm />}
       {scenarioCurrentTab === 'growth' && <GrowthForm />}
-=======
-  const { currentTab } = useAppSelector(analysis);
-  const { tab } = currentTab;
-
-  return (
-    <div className="p-12 bg-white z-50 h-full">
-      {tab === 'interventions' && <InterventionsForm />}
-      {tab === 'growth' && <GrowthForm />}
->>>>>>> 872e8ce9 (steps WIP)
-=======
-  const { scenarioCurrentTab } = useAppSelector(analysis);
-
-  return (
-    <div className="p-12 bg-white z-50 h-full">
-      {scenarioCurrentTab === 'interventions' && <InterventionsForm />}
-      {scenarioCurrentTab === 'growth' && <GrowthForm />}
->>>>>>> 1701329b (new scen componentized, scen attributes removed)
       <div className="absolute top-5 right-0 transform translate-x-1/2 z-30">
         <CollapseButton />
       </div>

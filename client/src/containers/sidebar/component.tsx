6d876214b67b3c359a9f166169a35eed74212161
AbdoKousara <abdelkader.kawsara@gmail.com>
2022-02-25 10:35:53 +0100

    DropDown Styling Done

--- client/src/containers/sidebar/component.tsx
+++ client/src/containers/sidebar/component.tsx
@@ -12,7 +12,7 @@ import { ui, setMenuMobileOpen } from 'store/features/ui';
 import MobileNavigation from 'containers/navigation/mobile';
 import DesktopNavigation from 'containers/navigation/desktop';
 import Avatar from 'components/avatar';
-
+import Dropdown from 'containers/dropdown';
 import type { NavigationList } from 'containers/navigation/types';
 
 const user = {
@@ -40,7 +40,7 @@ const Sidebar: React.FC = () => {
         <Dialog
           as="div"
           static
-          className="fixed inset-0 flex z-40 lg:hidden"
+          className="fixed inset-0 z-40 flex lg:hidden"
           open={isMenuMobileOpen}
           onClose={handleOnClose}
         >
@@ -64,7 +64,7 @@ const Sidebar: React.FC = () => {
             leaveFrom="translate-x-0"
             leaveTo="-translate-x-full"
           >
-            <div className="relative max-w-xs w-full bg-green-700 pt-5 pb-4 flex-1 flex flex-col">
+            <div className="relative flex flex-col flex-1 w-full max-w-xs pt-5 pb-4 bg-green-700">
               <Transition.Child
                 as={Fragment}
                 enter="ease-in-out duration-300"
@@ -74,29 +74,29 @@ const Sidebar: React.FC = () => {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <div className="absolute top-1 right-0 -mr-14 p-1">
+                <div className="absolute right-0 p-1 top-1 -mr-14">
                   <button
                     type="button"
-                    className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    className="flex items-center justify-center w-10 h-10 ml-1 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                     onClick={() => dispatch(setMenuMobileOpen(false))}
                   >
-                    <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                    <XIcon className="w-6 h-6 text-white" aria-hidden="true" />
                     <span className="sr-only">Close sidebar</span>
                   </button>
                 </div>
               </Transition.Child>
               <div className="pt-5 pb-4">
-                <div className="flex-shrink-0 flex items-center px-4">
+                <div className="flex items-center flex-shrink-0 px-4">
                   <img
-                    className="h-8 w-auto"
+                    className="w-auto h-8"
                     src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
                     alt="Landgriffon"
                   />
                 </div>
                 <MobileNavigation items={navigationItems} />
               </div>
-              <div className="flex-shrink-0 flex p-4">
-                <a href="#" className="flex-shrink-0 group block">
+              <div className="flex flex-shrink-0 p-4">
+                <a href="#" className="flex-shrink-0 block group">
                   <div className="flex items-center">
                     <Avatar src={user.imageUrl} />
                     <div className="ml-3">
@@ -115,29 +115,31 @@ const Sidebar: React.FC = () => {
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className="hidden lg:flex lg:flex-shrink-0 fixed h-screen w-28 z-10">
-        <div className="flex flex-col w-28 bg-green-700">
-          <div className="flex flex-col h-0 flex-1 overflow-y-auto">
-            <div className="flex-1 flex flex-col">
+      <div className="fixed z-10 hidden h-screen lg:flex lg:flex-shrink-0 w-28">
+        <div className="flex flex-col bg-green-700 w-28">
+          <div className="flex flex-col flex-1 h-0 overflow-y-auto">
+            <div className="flex flex-col flex-1">
               {/* Logo has been removed temporally */}
-              {/* <div className="flex-shrink-0 py-4 flex items-center justify-center">
+              {/* <div className="flex items-center justify-center flex-shrink-0 py-4">
                 <img
-                  className="h-8 w-auto"
+                  className="w-auto h-8"
                   src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
                   alt="Landgriffon"
                 />
               </div> */}
               <DesktopNavigation items={navigationItems} />
             </div>
-            <div className="flex-shrink-0 flex pb-5">
-              <a href="#" className="flex flex-shrink-0 w-full justify-center">
+
+            <Dropdown></Dropdown>
+            {/* <div className="flex flex-shrink-0 pb-5">
+              <a href="#" className="flex justify-center flex-shrink-0 w-full">
                 <Avatar src={user.imageUrl} />
                 <div className="sr-only">
                   <p>{user.name}</p>
                   <p>Account settings</p>
                 </div>
               </a>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>

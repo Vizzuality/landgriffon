123eca2ce3e7617f3d875ca2a0179c3b3f1f5cf4
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-25 13:33:35 +0000

    Styling checkbox component with theme support

--- client/src/components/forms/checkbox/component.tsx
+++ client/src/components/forms/checkbox/component.tsx
@@ -2,21 +2,24 @@ import React from 'react';
 import Hint from '../hint';
 import classnames from 'classnames';
 
+const THEMES = {
+  default:
+    'border rounded text-sm text-green-700 focus:outline-none focus:ring-2 focus:ring-green-700/50 focus:border-green-700 px-0',
+};
+
 type CheckboxProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  theme?: 'default';
   error?: string;
 };
 
 const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
-  ({ error, children, ...props }, ref) => (
-    <div>
+  ({ className, theme = 'default', error, children, ...props }, ref) => (
+    <div className={classnames('mt-1', className)}>
       <div className="flex items-center">
         <input
-          className={classnames(
-            'h-4 w-4 text-green-700 focus:ring-green-700 border-gray-700 rounded',
-            {
-              'border-red-600': !!error,
-            },
-          )}
+          className={classnames([THEMES[theme]], {
+            'border-red-600': !!error,
+          })}
           {...props}
           ref={ref}
           type="checkbox"

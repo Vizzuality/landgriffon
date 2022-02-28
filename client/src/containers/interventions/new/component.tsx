2e13ee6eb614faffce0d081cd66d4af11e0ce668
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-02-28 12:28:17 +0000

    Adding Button component in Scenarios forms instead of html button

--- client/src/containers/interventions/new/component.tsx
+++ client/src/containers/interventions/new/component.tsx
@@ -79,16 +79,14 @@ const InterventionForm: FC = () => {
         {!!currentStep && !isFirstStep && <Step2 />}
         <div className="pt-5">
           <div className="flex justify-end">
-            <button
-              type="button"
+            <Button
               className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={handleCancel}
             >
               Cancel
-            </button>
+            </Button>
             {!!currentStep && !isFirstStep && (
               <Button
-                // type="button"
                 type="submit"
                 className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 onClick={handleIntervention}

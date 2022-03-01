14a279514257d9a434e08578c19f97486957e23b
David Inga <david.inga@vizzuality.com>
2022-03-01 14:10:52 +0100

    user dropdown styles

--- client/src/containers/user-dropdown/component.tsx
+++ client/src/containers/user-dropdown/component.tsx
@@ -1,63 +1,70 @@
-import { useCallback } from 'react';
+import { Fragment, useCallback } from 'react';
 import { signOut, useSession } from 'next-auth/react';
-import { Menu } from '@headlessui/react';
+import { Popover, Transition } from '@headlessui/react';
 import StringAvatar from 'containers/string-avatar';
 import Avatar from 'components/avatar';
 import Loading from 'components/loading';
+
+const MENU_ITEM_CLASSNAME =
+  'block w-full py-2 px-4 text-sm text-left text-gray-900 h-9 hover:bg-green-50';
 
 const UserDropdown: React.FC = () => {
   const handleSignOut = useCallback(() => signOut(), []);
   const { data: session, status } = useSession();
 
-  const renderAvatar = useCallback(() => {
-    if (!session) return null;
-    const { user } = session || {};
-    if (user?.image) return <Avatar src={user?.image} className="cursor-pointer" />;
-    if (user?.name) return <StringAvatar fullName={user.name} className="cursor-pointer" />;
-    return <StringAvatar fullName={user.email} className="cursor-pointer" />;
-  }, [session]);
+  const renderAvatar = useCallback(
+    (className?: string) => {
+      if (!session) return null;
+      const { user } = session || {};
+      if (user?.image) return <Avatar src={user?.image} className={className} />;
+      if (user?.name) return <StringAvatar fullName={user.name} className={className} />;
+      return <StringAvatar fullName={user.email} className={className} />;
+    },
+    [session],
+  );
 
   return (
-    <Menu as="div">
-      <Menu.Button as="div" className="flex pb-5">
-        <div className="flex justify-center w-full">
-          {(!session || status === 'loading') && <Loading className="text-white" />}
+    <Popover as="div" className="flex justify-center w-full mb-5">
+      <div>
+        {(!session || status === 'loading') && <Loading className="text-white" />}
+        <Popover.Button>{renderAvatar()}</Popover.Button>
+      </div>
 
-          {renderAvatar()}
+      {session && (
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Popover.Panel className="absolute p-6 bg-white shadow-lg rounded-md bottom-5 left-24 focus:outline-none">
+            <div className="flex mb-3">
+              {renderAvatar('w-14 h-14')}
 
-          {session && (
-            <Menu.Items className="absolute flex-col h-48 p-6 bg-white rounded-md bottom-5 w-80 left-24">
-              <Menu.Item as="div" className="flex">
-                {session.user.image ? (
-                  <Avatar src={session.user.image} className="cursor-pointer w-14 h-14" />
-                ) : (
-                  <StringAvatar fullName={session.user.name} className="w-14 h-14" />
-                )}
+              <div className="ml-4">
+                <span className="block text-lg leading-8 text-gray-900">{session.user.name}</span>
+                <span className="block text-sm leading-7 text-gray-400">{session.user.email}</span>
+              </div>
+            </div>
 
-                <div className="ml-4">
-                  <h1 className="text-lg leading-8 text-gray-900">{session.user.name}</h1>
-                  <p className="text-sm leading-7 text-gray-400">{session.user.email}</p>
-                </div>
-              </Menu.Item>
+            <div>
+              <a href="#" className={MENU_ITEM_CLASSNAME}>
+                Edit Profile
+              </a>
+            </div>
 
-              <Menu.Item
-                as="div"
-                className="w-64 py-2 pl-4 mt-3 text-sm text-gray-900 cursor-pointer h-9 hover:bg-green-50"
-              >
-                <a href="#">Edit Profile</a>
-              </Menu.Item>
-
-              <Menu.Item
-                as="div"
-                className="w-64 py-2 pl-4 text-sm text-gray-900 cursor-pointer h-9 hover:bg-green-50"
-              >
-                <button onClick={handleSignOut}>Logout</button>
-              </Menu.Item>
-            </Menu.Items>
-          )}
-        </div>
-      </Menu.Button>
-    </Menu>
+            <div>
+              <button type="button" onClick={handleSignOut} className={MENU_ITEM_CLASSNAME}>
+                Logout
+              </button>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      )}
+    </Popover>
   );
 };
 

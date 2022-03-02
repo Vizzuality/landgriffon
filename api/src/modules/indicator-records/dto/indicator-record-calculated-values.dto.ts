7c296f2a1492b22be99451d0f100f26d002ae3c9
alexeh <alexurola@gmail.com>
2022-03-02 12:07:41 +0300

    Add DTOs for updated Indicator Record calculation approach

---
+++ api/src/modules/indicator-records/dto/indicator-record-calculated-values.dto.ts
@@ -0,0 +1,23 @@
+/**
+ * @description: Interface for typing the properties to calculate the values per indicator of a indicator record
+ * on the fly
+ * @note: This could be probably done within the DB in the future
+ */
+import { INDICATOR_TYPES } from 'modules/indicators/indicator.entity';
+
+export class IndicatorRecordCalculatedValuesDto {
+  sourcingRecordId: string;
+  production: number;
+
+  landPerTon: number;
+  landUse: number;
+  deforestationPerHarvestedAreaLandUse: number;
+  biodiversityLossPerHarvestedAreaLandUse: number;
+  carbonLossPerHarvestedAreaLandUse: number;
+
+  // Indicator name codes:
+  [INDICATOR_TYPES.DEFORESTATION]: number;
+  [INDICATOR_TYPES.BIODIVERSITY_LOSS]: number;
+  [INDICATOR_TYPES.CARBON_EMISSIONS]: number;
+  [INDICATOR_TYPES.UNSUSTAINABLE_WATER_USE]: number;
+}

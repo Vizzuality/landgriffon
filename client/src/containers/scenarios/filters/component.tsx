28006d7e795718d207b34f3f4c9f27082de19ecd
mluena <maria.luena@vizzuality.com>
2022-03-03 11:22:24 +0100

    scenarios animation and styles

--- client/src/containers/scenarios/filters/component.tsx
+++ client/src/containers/scenarios/filters/component.tsx
@@ -1,8 +1,10 @@
-import { useCallback, useEffect, useState } from 'react';
+import { FC, Fragment, useCallback, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { SearchIcon, XIcon } from '@heroicons/react/solid';
+import { SearchIcon } from '@heroicons/react/solid';
+import { Transition } from '@headlessui/react';
 import { useDebounce } from '@react-hook/debounce';
+
 import classNames from 'classnames';
 import Select from 'components/select';
 
@@ -15,14 +17,32 @@ const SORT_OPTIONS = [
     label: 'Title',
     value: 'title',
   },
+  {
+    label: 'Shared',
+    value: 'shared',
+  },
 ];
 
-const ScenariosFilters = () => {
+const filtersItems = [
+  {
+    name: 'All',
+    filter: 'all',
+  },
+  {
+    name: 'My scenarios',
+    filter: 'my-scenarios',
+  },
+  {
+    name: 'Shared',
+    filter: 'shared',
+  },
+];
+
+const ScenariosFilters: FC = () => {
   const router = useRouter();
   const { query } = router;
   const [isSearchEnable, setSearchEnable] = useState(false);
   const [term, setTerm] = useDebounce(null, 250); // wait 250ms before set term
-
   const toggleSearch = useCallback(() => setSearchEnable(!isSearchEnable), [isSearchEnable]);
   const handleSort = useCallback(
     (selected) =>
@@ -50,63 +70,66 @@ const ScenariosFilters = () => {
   }, [term]);
 
   return (
-    <div className="flex items-center justify-between">
-      {isSearchEnable ? (
-        <div className="w-full sm:max-w-xs">
-          <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <SearchIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-            </div>
+    <div className="flex items-center">
+      <div
+        className={classNames(
+          'relative w-full',
+          isSearchEnable ? 'sm:max-w-[100px]' : 'sm:max-w-[20px]',
+        )}
+      >
+        <button
+          type="button"
+          onClick={toggleSearch}
+          className="absolute top-1/2 transform -translate-y-1/2 left-0"
+        >
+          <SearchIcon className="w-4 h-4" />
+        </button>
+        {isSearchEnable && (
+          <Transition
+            as={Fragment}
+            enter="transition-opacity ease-linear duration-700"
+            enterFrom="-left-10"
+            enterTo="left-0"
+            leave="transition-opacity ease-linear duration-700"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
             <input
               id="search"
               name="search"
-              className="block w-full py-2 pl-10 pr-3 text-sm placeholder-gray-500 bg-white border border-gray-300 rounded-md focus:outline-none focus:text-gray-900 focus:placeholder-gray-400 focus:ring-1 focus:ring-green-700 focus:border-green-700 sm:text-sm"
               placeholder="Search"
+              className="absolute top-1/2 w-20 transform -translate-y-1/2 left-6 appearance-none text-sm text-green-700 font-bold border-0 border-b-2 border-green-700 px-0 py-0 focus:outline-none focus:border-green-700 focus:ring-0"
               type="search"
               onChange={handleSearchByTerm}
             />
-          </div>
-          <button type="button" onClick={toggleSearch}>
-            <XIcon className="w-6 h-6" />
-          </button>
-        </div>
-      ) : (
-        <button type="button" onClick={toggleSearch}>
-          <SearchIcon className="w-6 h-6" />
-        </button>
-      )}
+          </Transition>
+        )}
+      </div>
 
-      <ul className="flex space-x-2">
-        <li>
-          <Link href={{ pathname: '/analysis', query: { ...query, filter: 'all' } }}>
-            <a
-              className={classNames('block border-b-2 border-white', {
-                'border-green-700': query.filter === 'all' || !query.filter,
-              })}
-            >
-              All
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href={{ pathname: '/analysis', query: { ...query, filter: 'my-scenarios' } }}>
-            <a
-              className={classNames('block border-b-2 border-white', {
-                'border-green-700': query.filter === 'my-scenarios',
-              })}
-            >
-              My scenarios
-            </a>
-          </Link>
-        </li>
+      <ul className="relative flex space-x-4 ml-4 justify-start whitespace-nowrap items-center">
+        {filtersItems.map((item) => (
+          <li key={item.filter}>
+            <Link href={{ pathname: '/analysis', query: { ...query, filter: item.filter } }}>
+              <a
+                className={classNames('block', {
+                  'border-b-2 border-green-700 box-content':
+                    query.filter === item.filter || (!query.filter && item.filter === 'all'),
+                })}
+              >
+                {item.name}
+              </a>
+            </Link>
+          </li>
+        ))}
       </ul>
-
-      <Select
-        theme="default-bordernone"
-        current={SORT_OPTIONS[0]}
-        options={SORT_OPTIONS}
-        onChange={handleSort}
-      />
+      <div className="absolute right-0 bg-white p-2">
+        <Select
+          theme="default-bordernone"
+          current={SORT_OPTIONS[0]}
+          options={SORT_OPTIONS}
+          onChange={handleSort}
+        />
+      </div>
     </div>
   );
 };

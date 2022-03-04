147e8639a45e25dcd3df6b436224a93d8d0191c5
alexeh <alexurola@gmail.com>
2022-03-04 08:23:06 +0300

    Add migration.config.ts and yarn command to revert migraton

--- api/src/typeorm.config.ts
+++ api/src/typeorm.config.ts
@@ -14,13 +14,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   autoLoadEntities: true,
   synchronize: dbConfig.synchronize === true || dbConfig.synchronize === 'true',
   migrations: [__dirname + '/migrations/*.{ts,js}'],
-  /** Migrations will run automatically on startup, unless the
-   * `API_RUN_MIGRATIONS_ON_STARTUP` or `GEOPROCESSING_RUN_MIGRATIONS_ON_STARTUP`
-   * environment variables are set and their value matches, case-insensitively,
-   * the string `false`.
-   *
-   * @debt I think this should be way more resilient to user input.
-   */
   migrationsRun:
     dbConfig.migrationsRun === true || dbConfig.migrationsRun === 'true',
   dropSchema: dbConfig.dropSchema === true || dbConfig.dropSchema === 'true',

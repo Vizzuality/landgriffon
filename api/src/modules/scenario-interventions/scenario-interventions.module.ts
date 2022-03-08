de2e2915d9824734809e3369705553aae95a1bc9
alexeh <alexurola@gmail.com>
2022-03-08 09:30:43 +0300

    Fix import in scenario-interventions.module.ts

--- api/src/modules/scenario-interventions/scenario-interventions.module.ts
+++ api/src/modules/scenario-interventions/scenario-interventions.module.ts
@@ -6,10 +6,12 @@ import { ScenarioInterventionRepository } from 'modules/scenario-interventions/s
 import { ScenarioInterventionsController } from 'modules/scenario-interventions/scenario-interventions.controller';
 import { ScenarioInterventionsService } from 'modules/scenario-interventions/scenario-interventions.service';
 import { SourcingLocationsModule } from 'modules/sourcing-locations/sourcing-locations.module';
+import { IndicatorRecordsModule } from 'modules/indicator-records/indicator-records.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ScenarioInterventionRepository]),
+    IndicatorRecordsModule,
     GeoCodingModule,
     SourcingLocationsModule,
   ],

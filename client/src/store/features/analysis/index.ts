eed8806558ba33246241e1fde4dcd71c416afc4b
mluena <maria.luena@vizzuality.com>
2022-03-10 13:54:39 +0100

    scenarios state update

--- client/src/store/features/analysis/index.ts
+++ client/src/store/features/analysis/index.ts
@@ -1,9 +1,7 @@
 import type { RootState } from 'store';
 import type { AnalysisUIState } from './ui';
 import type { AnalysisFiltersState } from './filters';
-import type { Indicator } from 'types';
 
-import { initialState as scenariosInitialState, analysisScenariosSlice } from './scenarios';
 import type { ScenariosState } from './scenarios';
 
 export { analysisUI } from './ui';
@@ -15,35 +13,6 @@ export type AnalysisState = {
   'analysis/scenarios': ScenariosState;
 };
 
-type FeatureState = RootState & { analysis: AnalysisState; 'analysis/scenarios': ScenariosState };
-
-// Define the initial state using that type
-// export const initialState: AnalysisState = {
-//   'analysis/scenarios': scenariosInitialState,
-// };
-
-// export const analysisSlice = createSlice({
-//   name: 'analysis',
-//   initialState,
-// });
-
-// export const {
-//   setVisualizationMode,
-//   setSidebarCollapsed,
-//   setSubContentCollapsed,
-//   setLayer,
-//   setFilter,
-//   setFilters,
-// } = analysisSlice.actions;
+type FeatureState = RootState & { analysis: AnalysisState };
 
 export const analysis = (state: FeatureState) => state.analysis;
-export const scenarios = (state: FeatureState) => state.analysis['analysis/scenarios'];
-export const {
-  setCurrentScenario,
-  setScenarioToCompare,
-  setComparisonMode,
-  setScenarioTab,
-  setNewInterventionStep,
-} = analysisScenariosSlice.actions;
-
-// export default analysisSlice.reducer;

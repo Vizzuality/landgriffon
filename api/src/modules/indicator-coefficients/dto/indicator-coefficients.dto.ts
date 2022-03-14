952b72a53c451044c7d866fd5bdb96310a270cbc
alexeh <alexurola@gmail.com>
2022-03-14 08:24:14 +0300

    Update DTO's

--- api/src/modules/indicator-coefficients/dto/indicator-coefficients.dto.ts
+++ api/src/modules/indicator-coefficients/dto/indicator-coefficients.dto.ts
@@ -1,24 +1,32 @@
 import { INDICATOR_TYPES } from 'modules/indicators/indicator.entity';
-import { IsNotEmpty, IsNumber } from 'class-validator';
+import { IsNotEmpty, IsNumber, Max, Min } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class IndicatorCoefficientsDto {
   @ApiProperty()
+  @Max(100)
+  @Min(0)
   @IsNotEmpty()
   @IsNumber()
   [INDICATOR_TYPES.DEFORESTATION]: number;
 
   @ApiProperty()
+  @Max(100)
+  @Min(0)
   @IsNotEmpty()
   @IsNumber()
   [INDICATOR_TYPES.UNSUSTAINABLE_WATER_USE]: number;
 
   @ApiProperty()
+  @Max(100)
+  @Min(0)
   @IsNotEmpty()
   @IsNumber()
   [INDICATOR_TYPES.BIODIVERSITY_LOSS]: number;
 
   @ApiProperty()
+  @Max(100)
+  @Min(0)
   @IsNotEmpty()
   @IsNumber()
   [INDICATOR_TYPES.CARBON_EMISSIONS]: number;

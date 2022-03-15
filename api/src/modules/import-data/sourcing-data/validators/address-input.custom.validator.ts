40cc7d4eb47e038053693a5d15ceb880868e298c
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-03-15 17:49:54 +0100

    Fix import validation naming, types and sourcingLocationGroupId

--- api/src/modules/import-data/sourcing-data/validators/address-input.custom.validator.ts
+++ api/src/modules/import-data/sourcing-data/validators/address-input.custom.validator.ts
@@ -4,6 +4,7 @@ import {
   ValidatorConstraintInterface,
 } from 'class-validator';
 import { LOCATION_TYPES } from 'modules/sourcing-locations/sourcing-location.entity';
+import { SourcingDataExcelValidator } from 'modules/import-data/sourcing-data/validators/sourcing-data.class.validator';
 
 @ValidatorConstraint({ name: 'location_address', async: false })
 export class LocationAddressInputValidator
@@ -11,27 +12,28 @@ export class LocationAddressInputValidator
 {
   validate(addressInput: string, args: ValidationArguments): boolean {
     if (
-      (args.object as any).location_type === LOCATION_TYPES.UNKNOWN ||
-      (args.object as any).location_type ===
+      (args.object as SourcingDataExcelValidator).location_type ===
+        LOCATION_TYPES.UNKNOWN ||
+      (args.object as SourcingDataExcelValidator).location_type ===
         LOCATION_TYPES.COUNTRY_OF_PRODUCTION
     ) {
       return !addressInput;
     } else if (
-      ((args.object as any).location_type ===
+      ((args.object as SourcingDataExcelValidator).location_type ===
         LOCATION_TYPES.AGGREGATION_POINT ||
-        (args.object as any).location_type ===
+        (args.object as SourcingDataExcelValidator).location_type ===
           LOCATION_TYPES.POINT_OF_PRODUCTION) &&
-      ((args.object as any).location_latitude_input ||
-        (args.object as any).location_longitude_input)
+      ((args.object as SourcingDataExcelValidator).location_latitude_input ||
+        (args.object as SourcingDataExcelValidator).location_longitude_input)
     ) {
       return !addressInput;
     } else if (
-      ((args.object as any).location_type ===
+      ((args.object as SourcingDataExcelValidator).location_type ===
         LOCATION_TYPES.AGGREGATION_POINT ||
-        (args.object as any).location_type ===
+        (args.object as SourcingDataExcelValidator).location_type ===
           LOCATION_TYPES.POINT_OF_PRODUCTION) &&
-      (!(args.object as any).location_latitude_input ||
-        !(args.object as any).location_longitude_input)
+      (!(args.object as SourcingDataExcelValidator).location_latitude_input ||
+        !(args.object as SourcingDataExcelValidator).location_longitude_input)
     ) {
       return typeof addressInput === 'string' && addressInput.length > 2;
     } else {
@@ -40,27 +42,28 @@ export class LocationAddressInputValidator
   }
   defaultMessage(args: ValidationArguments): string {
     if (
-      (args.object as any).location_type === LOCATION_TYPES.UNKNOWN ||
-      (args.object as any).location_type ===
+      (args.object as SourcingDataExcelValidator).location_type ===
+        LOCATION_TYPES.UNKNOWN ||
+      (args.object as SourcingDataExcelValidator).location_type ===
         LOCATION_TYPES.COUNTRY_OF_PRODUCTION
     ) {
       return `Address must be empty for locations of type ${
         JSON.parse(JSON.stringify(args.object)).location_type
       }`;
     } else if (
-      ((args.object as any).location_type ===
+      ((args.object as SourcingDataExcelValidator).location_type ===
         LOCATION_TYPES.AGGREGATION_POINT ||
-        (args.object as any).location_type ===
+        (args.object as SourcingDataExcelValidator).location_type ===
           LOCATION_TYPES.POINT_OF_PRODUCTION) &&
-      ((args.object as any).location_latitude_input ||
-        (args.object as any).location_longitude_input)
+      ((args.object as SourcingDataExcelValidator).location_latitude_input ||
+        (args.object as SourcingDataExcelValidator).location_longitude_input)
     ) {
       return `Address input OR coordinates are required for locations of type ${
-        (args.object as any).location_type
+        (args.object as SourcingDataExcelValidator).location_type
       }. Address must be empty if coordinates are provided`;
     } else {
       return `Address input or coordinates are required for locations of type ${
-        (args.object as any).location_type
+        (args.object as SourcingDataExcelValidator).location_type
       }.`;
     }
   }

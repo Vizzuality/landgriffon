cd2a9dfadd7b78e5e2cf0d205c47e6a4d889df02
mluena <maria.luena@vizzuality.com>
2022-03-29 17:21:38 +0200

    breadcrumbs to btns, new scenario title inline error

--- client/src/pages/analysis/index.tsx
+++ client/src/pages/analysis/index.tsx
@@ -1,43 +1,26 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Head from 'next/head';
 
-import { useAppDispatch } from 'store/hooks';
-import { setSubContentCollapsed } from 'store/features/analysis/ui';
+import { useAppSelector } from 'store/hooks';
+import { scenarios } from 'store/features/analysis/scenarios';
 
 import AnalysisLayout from 'layouts/analysis';
 import Breadcrumb from 'components/breadcrumb';
 import Scenarios from 'containers/scenarios';
-import ScenarioNew from 'containers/scenarios/new';
 import ScenarioEdit from 'containers/scenarios/edit';
 import type { Page } from 'components/breadcrumb/types';
 
 const AnalysisPage: React.FC = () => {
+  const { mode } = useAppSelector(scenarios);
   const analysisContent = () => {
-    if (scenario === 'new') return <ScenarioNew />;
-    if (scenario === 'edit') return <ScenarioEdit />;
+    if (mode === 'edit') return <ScenarioEdit />;
     return <Scenarios />;
   };
 
-  const dispatch = useAppDispatch();
-  const {
-    query: { scenario },
-  } = useRouter();
-
   // Breadcrumbs
-  let pages: Page[] = [{ name: 'Analysis', href: '/analysis' }]; // Default
-  if (scenario === 'edit') {
-    pages = [...pages, { name: 'Edit scenario', href: '/analysis?scenario=edit' }];
+  let pages: Page[] = [{ name: 'Analysis', mode: 'list' }]; // Default
+  if (mode === 'edit') {
+    pages = [...pages, { name: 'Edit scenario', mode: 'edit' }];
   }
-  if (scenario === 'new') {
-    pages = [...pages, { name: 'New scenario', href: '/analysis?scenario=new' }];
-  }
-
-  useEffect(() => {
-    // Close and cancel interventions creation
-    // when user goes to scenarios list
-    if (!scenario) dispatch(setSubContentCollapsed(true));
-  }, [scenario, dispatch]);
 
   return (
     <AnalysisLayout>

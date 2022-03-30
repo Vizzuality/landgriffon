79e6194a85a7042265897252bfafe175551c0314
David Inga <david.inga@vizzuality.com>
2022-03-30 11:04:33 +0200

    fixed error 500 in the search for admin page

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { OverlayProvider } from '@react-aria/overlays';
+import { SSRProvider } from '@react-aria/ssr';
 import { Hydrate } from 'react-query/hydration';
 import { SessionProvider } from 'next-auth/react';
 import store from 'store';
@@ -16,7 +17,9 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
       <Hydrate state={pageProps.dehydratedState}>
         <SessionProvider session={pageProps.session}>
           <OverlayProvider>
-            <Component {...pageProps} />
+            <SSRProvider>
+              <Component {...pageProps} />
+            </SSRProvider>
           </OverlayProvider>
         </SessionProvider>
       </Hydrate>

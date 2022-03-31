912ee30a4247dfb20754c7fcde838d2f3ee724c1
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-03-31 16:36:48 +0200

    Fix migration file, refactor impact map query

--- api/src/migrations/1645259040554-ImpactStoredFunctions.ts
+++ api/src/migrations/1645259040554-ImpactStoredFunctions.ts
@@ -259,52 +259,6 @@ export class ImpactStoredFunctions1645259040554 implements MigrationInterface {
   $$
   LANGUAGE plpgsql;
   `);
-
-    await queryRunner.query(`
-    CREATE OR REPLACE FUNCTION get_h3_material_table_column(h3DataId uuid)
-    RETURNS TABLE (h3_table_name varchar, h3_column_name varchar) AS
-    $$
-        SELECT h3_data."h3tableName", h3_data."h3columnName"
-        FROM h3_data
-        WHERE h3_data.id = h3DataId 
-        LIMIT 1;
-    $$
-    LANGUAGE SQL;
-    `);
-
-    await queryRunner.query(`
-    CREATE OR REPLACE FUNCTION get_h3_data_over_georegion(
-    geo_region_id uuid, 
-    h3DataId uuid
-    )
-    RETURNS TABLE (h3index h3index, value float) AS
-    $$
-        DECLARE
-            material_h3_table_name varchar;
-            material_h3_column_name varchar;
-            h3_resolution integer := 6;
-            value float;
-
-        BEGIN
-            -- Get h3data table name and column name for given material
-            SELECT * INTO material_h3_table_name, material_h3_column_name
-            FROM get_h3_material_table_column(h3DataId);
-
-            -- Sum table column over region
-            RETURN QUERY EXECUTE format(
-                'SELECT 
-                    h3grid.h3index,
-                    h3grid.%I::float
-                    FROM
-                        get_h3_uncompact_geo_region($1, $2) geo_region
-                        INNER JOIN %I h3grid ON h3grid.h3index = geo_region.h3index
-                WHERE h3grid.%I > 0
-                ', material_h3_column_name, material_h3_table_name, material_h3_column_name)
-                USING geo_region_id, h3_resolution;
-        END;
-    $$
-    LANGUAGE plpgsql;
-    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
@@ -324,11 +278,6 @@ export class ImpactStoredFunctions1645259040554 implements MigrationInterface {
     DROP FUNCTION IF EXISTS sum_weighted_carbon_over_georegion();
 
     DROP FUNCTION IF EXISTS sum_material_over_georegion();
-
-    DROP FUNCTION IF EXISTS get_h3_material_table_column();
-
-    DROP FUNCTION IF EXISTS get_h3_data_over_georegion();
-
     `);
   }
 }

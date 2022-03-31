e27eb0ca17875afb7a566cf33f81be4c7c34d1cd
mluena <maria.luena@vizzuality.com>
2022-03-31 14:00:33 +0200

    visualizationMode removed from url

--- client/src/containers/analysis-visualization/mode-control/component.tsx
+++ client/src/containers/analysis-visualization/mode-control/component.tsx
@@ -1,14 +1,10 @@
-import Link from 'next/link';
+import { useCallback } from 'react';
 import classNames from 'classnames';
 import { ChartPieIcon, MapIcon, TableIcon } from '@heroicons/react/outline';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 
 import { useAppSelector, useAppDispatch } from 'store/hooks';
 import { analysisUI, setVisualizationMode } from 'store/features/analysis/ui';
 import { analysisFilters } from 'store/features/analysis/filters';
-
-import type { AnalysisUIState } from 'store/features/analysis/ui';
 
 const CONTROL_ITEM_CLASS_NAMES =
   'relative inline-flex items-center px-4 py-1.5 border border-gray-200 bg-white text-sm font-medium text-gray-600 hover:bg-green-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700';
@@ -19,62 +15,37 @@ const ModeControl: React.FC = () => {
   const { visualizationMode } = useAppSelector(analysisUI);
   const { layer } = useAppSelector(analysisFilters);
   const dispatch = useAppDispatch();
-  const { query } = useRouter();
-  const { mode } = query;
 
-  useEffect(() => {
-    if (mode) {
-      const selectedMode = mode as AnalysisUIState['visualizationMode'];
-      dispatch(setVisualizationMode(selectedMode));
-    }
-  }, [dispatch, mode]);
+  const handleClick = useCallback((mode) => dispatch(setVisualizationMode(mode)), [dispatch]);
 
-  if (layer !== 'impact' && mode !== 'map') return null;
+  if (layer !== 'impact' && visualizationMode !== 'map') return null;
 
   return (
     <div className="inline-flex shadow-sm rounded-md">
-      <Link
-        href={{
-          pathname: '/analysis',
-          query: { ...query, mode: 'map' },
-        }}
+      <button
+        onClick={() => handleClick('map')}
+        className={classNames(CONTROL_ITEM_CLASS_NAMES, 'rounded-l-md', {
+          [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'map',
+        })}
       >
-        <a
-          className={classNames(CONTROL_ITEM_CLASS_NAMES, 'rounded-l-md', {
-            [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'map',
-          })}
-        >
-          <MapIcon className="h-6 w-6" aria-hidden="true" />
-        </a>
-      </Link>
-      <Link
-        href={{
-          pathname: '/analysis',
-          query: { ...query, mode: 'table' },
-        }}
+        <MapIcon className="h-6 w-6" aria-hidden="true" />
+      </button>
+      <button
+        onClick={() => handleClick('table')}
+        className={classNames(CONTROL_ITEM_CLASS_NAMES, '-ml-px', {
+          [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'table',
+        })}
       >
-        <a
-          className={classNames(CONTROL_ITEM_CLASS_NAMES, '-ml-px', {
-            [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'table',
-          })}
-        >
-          <TableIcon className="h-6 w-6" aria-hidden="true" />
-        </a>
-      </Link>
-      <Link
-        href={{
-          pathname: '/analysis',
-          query: { ...query, mode: 'chart' },
-        }}
+        <TableIcon className="h-6 w-6" aria-hidden="true" />
+      </button>
+      <button
+        onClick={() => handleClick('chart')}
+        className={classNames(CONTROL_ITEM_CLASS_NAMES, '-ml-px rounded-r-md', {
+          [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'chart',
+        })}
       >
-        <a
-          className={classNames(CONTROL_ITEM_CLASS_NAMES, '-ml-px rounded-r-md', {
-            [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'chart',
-          })}
-        >
-          <ChartPieIcon className="h-6 w-6" aria-hidden="true" />
-        </a>
-      </Link>
+        <ChartPieIcon className="h-6 w-6" aria-hidden="true" />
+      </button>
     </div>
   );
 };

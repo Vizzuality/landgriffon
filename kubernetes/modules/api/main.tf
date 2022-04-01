ed10656a416bf4445b38ccc7d24b6a7e92957e9c
alexeh <alexurola@gmail.com>
2022-04-01 08:27:06 +0300

    Bump API K8s pod memory to 4GB

--- kubernetes/modules/api/main.tf
+++ kubernetes/modules/api/main.tf
@@ -177,11 +177,11 @@ resource "kubernetes_deployment" "api_deployment" {
           resources {
             limits = {
               cpu    = "1"
-              memory = "2Gi"
+              memory = "4Gi"
             }
             requests = {
               cpu    = "500m"
-              memory = "2Gi"
+              memory = "4Gi"
             }
           }
 

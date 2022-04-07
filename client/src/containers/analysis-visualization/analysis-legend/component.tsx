65cce823902a60f42a396908f6388cafc2d93e17
David Inga <david.inga@vizzuality.com>
2022-04-07 13:37:02 +0200

    added toggle show legend

--- client/src/containers/analysis-visualization/analysis-legend/component.tsx
+++ client/src/containers/analysis-visualization/analysis-legend/component.tsx
@@ -1,60 +1,75 @@
 import { useCallback, useState } from 'react';
-import cx from 'classnames';
+import { ChevronDoubleLeftIcon, ChevronDoubleRightIcon } from '@heroicons/react/outline';
 
 import ImpactLegendItem from './impact-legend-item';
 import MaterialLegendItem from './material-legend-item';
 import RiskLegendItem from './risk-legend-item';
 
-export interface LegendProps {
-  className?: string;
-}
+export const Legend: React.FC = () => {
+  const [showLegend, setShowLegend] = useState<boolean>(false);
+  const [showContextualLayers, setShowContextualLayers] = useState<boolean>(false);
 
-export const Legend: React.FC<LegendProps> = ({ className = '' }: LegendProps) => {
-  const [active, setActive] = useState(false);
+  const handleShowLegend = useCallback(() => {
+    setShowLegend(!showLegend);
+  }, [showLegend]);
 
   const onToggleActive = useCallback(() => {
-    setActive(!active);
-  }, [active]);
+    setShowContextualLayers(!showContextualLayers);
+  }, [showContextualLayers]);
 
   return (
-    <div
-      className={cx(
-        'flex flex-col flex-grow shadow-sm bg-white border border-gray-200 rounded',
-        className,
-      )}
-    >
-      <div className="flex items-center justify-between px-4 py-2">
-        <div className="font-semibold text-gray-900 text-sm">Legend</div>
-        <button
-          type="button"
-          aria-expanded={active}
-          className="text-green-700 text-xs"
-          onClick={onToggleActive}
-        >
-          <span>{active ? 'Hide' : 'Show'} contextual layers</span>
-        </button>
-      </div>
+    <>
+      {showLegend && (
+        <div className="absolute z-10 bottom-10 right-20 flex flex-col flex-grow shadow-sm bg-white border border-gray-200 rounded w-72">
+          <div className="flex items-center justify-between px-4 py-2">
+            <div className="font-semibold text-gray-900 text-sm">Legend</div>
+            <button
+              type="button"
+              aria-expanded={showContextualLayers}
+              className="text-green-700 text-xs"
+              onClick={onToggleActive}
+            >
+              <span>{showContextualLayers ? 'Hide' : 'Show'} contextual layers</span>
+            </button>
+          </div>
 
-      {/* Contextual layers */}
-      {active && (
-        <div className="relative flex flex-col flex-grow">
-          <MaterialLegendItem />
-          <RiskLegendItem />
+          {/* Contextual layers */}
+          {showContextualLayers && (
+            <div className="relative flex flex-col flex-grow">
+              <div className="border-t border-gray-100">
+                <MaterialLegendItem />
+              </div>
+              <div className="border-t border-gray-100">
+                <RiskLegendItem />
+              </div>
+            </div>
+          )}
+
+          {/* Main layer: it will be always active */}
+          <div
+            className="relative flex flex-col flex-grow overflow-hidden border-t border-gray-100"
+            style={{
+              maxHeight: 400,
+            }}
+          >
+            <div className="overflow-x-hidden overflow-y-auto">
+              <ImpactLegendItem />
+            </div>
+          </div>
         </div>
       )}
-
-      {/* Main layer: it will be always active */}
-      <div
-        className="relative flex flex-col flex-grow overflow-hidden border-t border-gray-100"
-        style={{
-          maxHeight: 400,
-        }}
+      <button
+        type="button"
+        className="absolute z-10 bottom-10 right-6 bg-white border border-gray-100 p-3"
+        onClick={handleShowLegend}
       >
-        <div className="overflow-x-hidden overflow-y-auto">
-          <ImpactLegendItem />
-        </div>
-      </div>
-    </div>
+        {showLegend ? (
+          <ChevronDoubleRightIcon className="w-4 h-4" />
+        ) : (
+          <ChevronDoubleLeftIcon className="w-4 h-4" />
+        )}
+      </button>
+    </>
   );
 };
 

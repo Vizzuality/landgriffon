74174f7dc9aa0d7f11f21e47b45949a45bcd2c13
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-04-08 12:01:16 +0200

    Add early version of opacity controls

---
+++ client/src/components/legend/item/opacityControl.tsx
@@ -0,0 +1,37 @@
+import { EyeIcon } from '@heroicons/react/solid';
+import { Tooltip } from '@visx/tooltip';
+import { Input } from 'components/forms';
+import { useState } from 'react';
+
+interface OpacityControlProps {
+  opacity: number;
+  onChange: (opacity: number) => void;
+}
+
+const OpacityControl: React.FC<OpacityControlProps> = ({ opacity, onChange }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  return (
+    <>
+      <button>
+        <EyeIcon
+          className="w-4 h-4 text-gray-900"
+          onClick={() => {
+            setIsVisible((visible) => !visible);
+          }}
+        />
+        {isVisible && (
+          <Input
+            type="number"
+            defaultValue={opacity * 100}
+            onChange={(e) => {
+              if (Number.isNaN(e.target.valueAsNumber)) return;
+              onChange(e.target.valueAsNumber / 100);
+            }}
+          />
+        )}
+      </button>
+    </>
+  );
+};
+
+export default OpacityControl;

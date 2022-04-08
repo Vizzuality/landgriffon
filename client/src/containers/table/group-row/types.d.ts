7f87e69e6be02407c4653d83bacac010ea95756b
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-04-08 10:43:49 +0100

    Add comments to Table types/props

--- client/src/containers/table/group-row/types.d.ts
+++ client/src/containers/table/group-row/types.d.ts
@@ -1,5 +1,6 @@
 import { IGroupRowProps } from 'ka-table/props';
 
 export type GroupRowProps = IGroupRowProps & {
+  /** Whether the row should be sticky (when scrolling horizontally) */
   sticky: boolean;
 };

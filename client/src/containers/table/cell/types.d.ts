69e6efae19f6462b130f0cf6a5387742e7cb92b9
Simao Rodrigues <arodrigues.simao@gmail.com>
2022-04-08 10:43:49 +0100

    Add comments to Table types/props

--- client/src/containers/table/cell/types.d.ts
+++ client/src/containers/table/cell/types.d.ts
@@ -1,11 +1,16 @@
 export type CellProps = ICellProps & {
+  /** Classnames to apply to the container */
   className?: string;
-  firstColumnKey?: string;
-  isFirstColumnSticky?: boolean;
-  stickyColumnKey?: string;
-  isFirstColumn?: boolean;
+  /** Key of the first column in the table */
+  firstColumnKey: string;
+  /** Whether the first column should be sticky (when scrolling horizontally) */
+  isFirstColumnSticky: boolean;
+  /** Whether the row is being hovered. Defaults to `false` */
   isRowHovered?: boolean;
+  /** onclick callback */
   onClick?: () => void;
+  /** onmouseenter callback */
   onMouseEnter?: () => void;
+  /** onmouseleave callback */
   onMouseLeave?: () => void;
 };

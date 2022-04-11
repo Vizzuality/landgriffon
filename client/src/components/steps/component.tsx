f18ddf4c41c7f621d0d94354cd0efa10dd8416ac
mluena <maria.luena@vizzuality.com>
2022-04-11 13:52:59 +0200

    disable step 2 in interventions

--- client/src/components/steps/component.tsx
+++ client/src/components/steps/component.tsx
@@ -1,8 +1,8 @@
-import { useCallback } from 'react';
+import { useCallback, useMemo } from 'react';
 import cx from 'classnames';
 
-import { useAppDispatch } from 'store/hooks';
-import { setNewInterventionStep } from 'store/features/analysis/scenarios';
+import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { scenarios, setNewInterventionStep } from 'store/features/analysis/scenarios';
 
 import type { Step } from './types';
 
@@ -14,6 +14,43 @@ type StepsProps = React.HTMLAttributes<unknown> & {
 const Steps: React.FC<StepsProps> = ({ steps, current }: StepsProps) => {
   const dispatch = useAppDispatch();
   const handleTab = useCallback((step) => dispatch(setNewInterventionStep(step)), [dispatch]);
+  const { newInterventionData } = useAppSelector(scenarios);
+  const {
+    title,
+    percentage,
+    materialIds,
+    businessUnitIds,
+    startYear,
+    endYear,
+    type,
+    supplierIds,
+    adminRegionIds,
+  } = newInterventionData;
+  const hasErrors = useMemo(
+    () =>
+      [
+        title,
+        percentage,
+        materialIds,
+        businessUnitIds,
+        startYear,
+        endYear,
+        type,
+        supplierIds,
+        adminRegionIds,
+      ].some((el) => !el),
+    [
+      title,
+      percentage,
+      materialIds,
+      businessUnitIds,
+      startYear,
+      endYear,
+      type,
+      supplierIds,
+      adminRegionIds,
+    ],
+  );
 
   return (
     <ol className="space-y-4 md:flex md:space-y-0 md:space-x-8">
@@ -24,10 +61,12 @@ const Steps: React.FC<StepsProps> = ({ steps, current }: StepsProps) => {
               className={cx(
                 'w-full group pl-4 py-2 flex flex-col border-l-4md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4',
                 {
-                  'border-indigo-600 hover:border-indigo-800': step.id !== current,
+                  'border-indigo-600 hover:border-indigo-800 cursor-pointer':
+                    step.id !== current && !hasErrors,
                   'border-green-700': step.id === current,
                 },
               )}
+              disabled={hasErrors}
               onClick={() => handleTab(step.id)}
             >
               <span className="text-indigo-600 font-semibold tracking-wide uppercase group-hover:text-indigo-800 leading-4 text-xs">
@@ -46,11 +85,12 @@ const Steps: React.FC<StepsProps> = ({ steps, current }: StepsProps) => {
               className={cx(
                 'w-full pl-4 py-2 flex flex-col border-l-4 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4',
                 {
-                  'border-green-70': step.id !== current,
+                  'border-green-70 cursor-pointer': step.id !== current && !hasErrors,
                   'border-green-700': step.id === current,
                 },
               )}
               aria-current="step"
+              disabled={hasErrors}
               onClick={() => handleTab(step.id)}
             >
               <span className="text-indigo-600 font-semibold tracking-wide uppercase leading-4 text-xs">
@@ -69,10 +109,12 @@ const Steps: React.FC<StepsProps> = ({ steps, current }: StepsProps) => {
               className={cx(
                 'w-full group pl-4 py-2 flex flex-col border-l-4 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4',
                 {
-                  'border-gray-200 hover:border-gray-300': step.id !== current,
+                  'border-gray-200 hover:border-gray-300 cursor-pointer':
+                    step.id !== current && !hasErrors,
                   'border-green-700': step.id === current,
                 },
               )}
+              disabled={hasErrors}
               onClick={() => handleTab(step.id)}
             >
               <span className="text-gray-500 font-semibold tracking-wide uppercase group-hover:text-gray-700 leading-4 text-xs">

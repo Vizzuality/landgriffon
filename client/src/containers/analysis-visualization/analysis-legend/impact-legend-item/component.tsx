f67b0d1552b78db8f9473cd0f49259a5a24e90db
David Inga <david.inga@vizzuality.com>
2022-04-11 10:46:35 +0200

    added impact layer from hooks

--- client/src/containers/analysis-visualization/analysis-legend/impact-legend-item/component.tsx
+++ client/src/containers/analysis-visualization/analysis-legend/impact-legend-item/component.tsx
@@ -1,67 +1,42 @@
-import { useCallback, useMemo } from 'react';
+import { useCallback } from 'react';
 
 import { useAppSelector, useAppDispatch } from 'store/hooks';
-import { analysisFilters } from 'store/features/analysis/filters';
 import { analysisMap, setLayer } from 'store/features/analysis/map';
-
-import { useH3ImpactData } from 'hooks/h3-data';
 
 import Loading from 'components/loading';
 import LegendItem from 'components/legend/item';
 import LegendTypeChoropleth from 'components/legend/types/choropleth';
 
-import { COLOR_RAMPS, NUMBER_FORMAT } from '../../constants';
-
-import type { Legend, LegendItem as LegendItemProp } from 'types';
-
-const LAYER_ID = 'impact';
-
 const ImpactLayer = () => {
-  const { indicator, startYear } = useAppSelector(analysisFilters);
+  const dispatch = useAppDispatch();
   const {
     layers: { impact },
   } = useAppSelector(analysisMap);
-  const { data, isFetching } = useH3ImpactData();
 
-  const legendData = useMemo<Legend>(() => {
-    if (indicator?.label && data) {
-      return {
-        name: `${indicator.label} in ${startYear}`,
-        unit: data.metadata.unit,
-        min: NUMBER_FORMAT(data.metadata.quantiles[0]),
-        items: data.metadata.quantiles.slice(1).map(
-          (v, index): LegendItemProp => ({
-            value: NUMBER_FORMAT(v),
-            color: COLOR_RAMPS[LAYER_ID][index],
-          }),
-        ),
-        active: true, // this layer is always active
-      };
-    }
-    return null;
-  }, [data, indicator?.label, startYear]);
-
-  const dispatch = useAppDispatch();
   const onChangeOpacity = useCallback(
     (opacity: number) => {
-      dispatch(setLayer({ id: LAYER_ID, layer: { opacity } }));
+      dispatch(setLayer({ id: 'impact', layer: { opacity } }));
     },
     [dispatch],
   );
 
+  const isFetching = false;
+
   return (
     <LegendItem
-      {...legendData}
+      name={impact.legend.name}
+      unit={impact.legend.unit}
       opacity={impact.opacity}
+      active={impact.active}
       onChangeOpacity={onChangeOpacity}
       showToggle={false}
     >
       {isFetching && <Loading />}
-      {!isFetching && !!legendData?.items?.length && (
+      {!isFetching && !!impact.legend?.items?.length && (
         <LegendTypeChoropleth
           className="text-sm text-gray-500 flex-1"
-          min={legendData.min}
-          items={legendData.items}
+          min={impact.legend.min}
+          items={impact.legend.items}
         />
       )}
     </LegendItem>

3dc8d1f6245e07d67682b3ba67dca8fe37cb7a02
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-04-12 11:35:06 +0200

    Only apply filters when apply is clicked

--- client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
@@ -21,51 +21,31 @@ const INITIAL_FILTERS: Partial<AnalysisFiltersState> = {
 };
 
 const MoreFilters: React.FC = () => {
-  const filters = useAppSelector(analysisFilters);
+  const { materials, origins, suppliers } = useAppSelector(analysisFilters);
+  const filters = { materials, origins, suppliers };
   const dispatch = useAppDispatch();
 
-  const { materials, origins, suppliers } = filters;
-
-  const selectedFilters = useMemo(
-    () => ({ materials, origins, suppliers }),
-    [materials, origins, suppliers],
-  );
+  const [selectedFilters, setSelectedFilters] = useState(filters);
 
   const [counter, setCounter] = useState<number>(0);
 
   const handleApply = useCallback(() => {
-    dispatch(
-      setFilters({
-        materials: materials || INITIAL_FILTERS.materials,
-        origins: origins || INITIAL_FILTERS.origins,
-        suppliers: suppliers || INITIAL_FILTERS.suppliers,
-      } as AnalysisFiltersState),
-    );
-    // setOpen(false);
-  }, [dispatch, materials, origins, suppliers]);
+    dispatch(setFilters(selectedFilters));
+  }, [dispatch, selectedFilters]);
 
   const handleClearFilters = useCallback(() => {
     dispatch(setFilters(INITIAL_FILTERS));
   }, [dispatch]);
 
-  const handleChangeFilter = useCallback(
-    // only save ids on store
-    (key, values) => {
-      dispatch(
-        setFilters({
-          ...filters,
-          [key]: values,
-        }),
-      );
-    },
-    [dispatch, filters],
-  );
+  const handleChangeFilter = useCallback((key, values) => {
+    setSelectedFilters((filters) => ({ ...filters, [key]: values }));
+  }, []);
 
   useEffect(() => {
-    const counters = Object.keys(selectedFilters).map((key) => selectedFilters[key].length);
+    const counters = Object.values(filters).map((value) => value.length);
     const total = counters.reduce((a, b) => a + b);
     setCounter(total);
-  }, [selectedFilters]);
+  }, [filters]);
 
   return (
     <Popover className="relative">
@@ -89,66 +69,77 @@ const MoreFilters: React.FC = () => {
             leaveTo="opacity-0"
           >
             <Popover.Panel
-              static
+              unmount
               className="absolute md:right-0 lg:left-0 lg:clear-right-0 mt-1 w-80 z-20"
             >
-              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-                <div className="relative p-4 bg-white rounded-lg">
-                  <div className="flex justify-between mb-4">
-                    <div>Filter by</div>
-                    <Button theme="textLight" size="text" onClick={handleClearFilters}>
-                      Clear all
-                    </Button>
-                  </div>
-                  <div className="flex flex-col gap-3">
-                    <div>
-                      <div className="mb-1">Material</div>
-                      <Materials
-                        multiple
-                        withSourcingLocations
-                        current={materials}
-                        fitContent
-                        onChange={(values) => handleChangeFilter('materials', values)}
-                      />
+              {() => {
+                // eslint-disable-next-line react-hooks/rules-of-hooks
+                useEffect(() => {
+                  return () => {
+                    setSelectedFilters({ materials, origins, suppliers });
+                  };
+                }, []);
+
+                return (
+                  <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                    <div className="relative p-4 bg-white rounded-lg">
+                      <div className="flex justify-between mb-4">
+                        <div>Filter by</div>
+                        <Button theme="textLight" size="text" onClick={handleClearFilters}>
+                          Clear all
+                        </Button>
+                      </div>
+                      <div className="flex flex-col gap-3">
+                        <div>
+                          <div className="mb-1">Material</div>
+                          <Materials
+                            multiple
+                            withSourcingLocations
+                            current={materials}
+                            fitContent
+                            onChange={(values) => handleChangeFilter('materials', values)}
+                          />
+                        </div>
+                        <div>
+                          <div className="mb-1">Origins</div>
+                          <OriginRegions
+                            multiple
+                            withSourcingLocations
+                            current={origins}
+                            fitContent
+                            onChange={(values) => handleChangeFilter('origins', values)}
+                          />
+                        </div>
+                        <div>
+                          <div className="mb-1">Suppliers</div>
+                          <Suppliers
+                            multiple
+                            withSourcingLocations
+                            current={suppliers}
+                            fitContent
+                            onChange={(values) => handleChangeFilter('suppliers', values)}
+                          />
+                        </div>
+                      </div>
+                      <div className="flex gap-2 mt-6">
+                        <Button theme="secondary" className="px-9" onClick={() => close()}>
+                          Cancel
+                        </Button>
+                        <Button
+                          theme="primary"
+                          className="flex-grow"
+                          onClick={() => {
+                            handleApply();
+                            close();
+                          }}
+                        >
+                          Apply
+                        </Button>
+                      </div>
                     </div>
-                    <div>
-                      <div className="mb-1">Origins</div>
-                      <OriginRegions
-                        multiple
-                        withSourcingLocations
-                        current={origins}
-                        fitContent
-                        onChange={(values) => handleChangeFilter('origins', values)}
-                      />
-                    </div>
-                    <div>
-                      <div className="mb-1">Suppliers</div>
-                      <Suppliers
-                        multiple
-                        withSourcingLocations
-                        current={suppliers}
-                        fitContent
-                        onChange={(values) => handleChangeFilter('suppliers', values)}
-                      />
-                    </div>
                   </div>
-                  <div className="flex gap-2 mt-6">
-                    <Button theme="secondary" className="px-9" onClick={() => close()}>
-                      Cancel
-                    </Button>
-                    <Button
-                      theme="primary"
-                      className="flex-grow"
-                      onClick={() => {
-                        handleApply();
-                        close();
-                      }}
-                    >
-                      Apply
-                    </Button>
-                  </div>
-                </div>
-              </div>
+                );
+              }}
             </Popover.Panel>
           </Transition>
         </>

5f18aa31febc91e2192ee31fa4b484f12eaa0da5
David Inga <david.inga@vizzuality.com>
2022-04-12 13:09:26 +0200

    new colors for material layer

--- client/src/utils/colors.ts
+++ client/src/utils/colors.ts
@@ -3,5 +3,5 @@ import type { ColorRamps } from 'types';
 export const COLOR_RAMPS: ColorRamps = {
   impact: ['#FFC300', '#F1920E', '#E3611C', '#C70039', '#900C3F', '#5A1846'],
   risk: ['#FEF0D9', '#FDD49E', '#FDBB84', '#FC8D59', '#E34A33', '#B30000'],
-  material: ['#FEFEE3', '#FCEEB7', '#F8CF69', '#F2A400', '#E17100', '#AA4800'],
+  material: ['#FFFFCC', '#D9F0A3', '#ADDD8E', '#78C679', '#31A354', '#006837'],
 };

ea1fb8a0a3f3e26c24519be127e8c92a789064b5
alexeh <alexurola@gmail.com>
2022-04-20 16:47:00 +0300

    Add pagination swagger docs to impact table endpoint

--- api/src/modules/impact/impact.controller.ts
+++ api/src/modules/impact/impact.controller.ts
@@ -2,14 +2,12 @@ import { Controller, Get, Query, ValidationPipe } from '@nestjs/common';
 import { GetImpactTableDto } from 'modules/impact/dto/get-impact-table.dto';
 import { ImpactService } from 'modules/impact/impact.service';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
-import {
-  ImpactTable,
-  PaginatedImpactTable,
-} from 'modules/impact/dto/response-impact-table.dto';
+import { PaginatedImpactTable } from 'modules/impact/dto/response-impact-table.dto';
 import {
   FetchSpecification,
   ProcessFetchSpecification,
 } from 'nestjs-base-service';
+import { JSONAPIPaginationQueryParams } from 'decorators/json-api-parameters.decorator';
 
 @Controller('/api/v1/impact')
 @ApiTags('Impact')
@@ -20,11 +18,12 @@ export class ImpactController {
     description: 'Get data for Impact Table',
   })
   @ApiOkResponse({
-    type: ImpactTable,
+    type: PaginatedImpactTable,
   })
+  @JSONAPIPaginationQueryParams()
   @Get('table')
   async getImpactTable(
-    @ProcessFetchSpecification({}) fetchSpecification: FetchSpecification,
+    @ProcessFetchSpecification() fetchSpecification: FetchSpecification,
     @Query(ValidationPipe) impactTableDto: GetImpactTableDto,
   ): Promise<PaginatedImpactTable> {
     return await this.impactService.getImpactTable(

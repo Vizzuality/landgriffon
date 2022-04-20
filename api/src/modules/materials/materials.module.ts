ae8b8e06aa5df4cf03473518984a15b4b2722e80
alexeh <alexurola@gmail.com>
2022-04-20 08:02:43 +0300

    Add finding descendants Ids smart filtering for AdminRegionsModule

--- api/src/modules/materials/materials.module.ts
+++ api/src/modules/materials/materials.module.ts
@@ -1,15 +1,17 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MaterialRepository } from 'modules/materials/material.repository';
 import { MaterialsController } from 'modules/materials/materials.controller';
 import { MaterialsService } from 'modules/materials/materials.service';
 import { MaterialsToH3sService } from 'modules/materials/materials-to-h3s.service';
 import { SourcingLocationsModule } from 'modules/sourcing-locations/sourcing-locations.module';
+import { AdminRegionsModule } from 'modules/admin-regions/admin-regions.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([MaterialRepository, MaterialsToH3sService]),
     SourcingLocationsModule,
+    forwardRef(() => AdminRegionsModule),
   ],
   controllers: [MaterialsController],
   providers: [MaterialsService],

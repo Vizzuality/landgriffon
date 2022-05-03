171d50413168bdc2ba0bb0f1bb2c59b94558ff6b
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-05-03 12:55:18 +0200

    Add unstyled slider as opacity control

--- client/src/components/legend/item/opacityControl.tsx
+++ client/src/components/legend/item/opacityControl.tsx
@@ -1,6 +1,8 @@
 import { Input } from 'components/forms';
 import OpacityIcon from 'components/icons/opacity';
 import ToolTip from 'components/tooltip';
+import { useEffect, useState } from 'react';
+import { useDebounce } from 'rooks';
 
 interface OpacityControlProps {
   opacity: number;
@@ -8,21 +10,32 @@ interface OpacityControlProps {
 }
 
 const OpacityControl: React.FC<OpacityControlProps> = ({ opacity, onChange }) => {
+  const debouncedChange = useDebounce(onChange, 200);
+  const [rangeValue, setRangeValue] = useState(Math.round(opacity * 100));
+
+  useEffect(() => {
+    debouncedChange(rangeValue / 100);
+  }, [debouncedChange, rangeValue]);
+
   return (
     <ToolTip
       content={
         <Input
-          unit="%"
-          type="number"
-          defaultValue={Math.round(opacity * 100)}
-          className="text-center"
+          unit={`${rangeValue} %`}
+          type="range"
+          min={0}
+          max={100}
+          step={1}
+          value={rangeValue}
+          className="appearance-none bg-yellow-700 w-full ring-2"
           onChange={(e) => {
-            if (Number.isNaN(e.target.valueAsNumber)) return;
-            onChange(e.target.valueAsNumber / 100);
+            // if (Number.isNaN(e.target.valueAsNumber)) return;
+            setRangeValue(e.currentTarget.valueAsNumber);
+            // debouncedChange(e.target.valueAsNumber / 100);
           }}
         />
       }
-      className="w-16 text-center"
+      className="w-48 text-center"
     >
       <span className="w-4 h-4 text-gray-900">
         <OpacityIcon />

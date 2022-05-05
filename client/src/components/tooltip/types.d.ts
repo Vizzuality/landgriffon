3416289cd402b600885f2c9ebd67b21f00cb7e58
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-05-05 12:33:04 +0200

    Fix tooltip arrow posititon

--- client/src/components/tooltip/types.d.ts
+++ client/src/components/tooltip/types.d.ts
@@ -1,3 +1,7 @@
 import React from 'react';
 
-export type TooltipProps = PropsWithChildren<{ className?: string; content: React.ReactNode }>;
+export type TooltipProps = PropsWithChildren<{
+  className?: string;
+  content: React.ReactNode;
+  arrow?: boolean;
+}>;

91401c1078d1e015f88c71faf7ea23f76995e67f
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-05-09 13:51:26 +0200

    Update ImpactTableEntityType

--- api/src/modules/impact/dto/paginated-entities.dto.ts
+++ api/src/modules/impact/dto/paginated-entities.dto.ts
@@ -1,12 +1,6 @@
-import { LOCATION_TYPES } from 'modules/sourcing-locations/sourcing-location.entity';
 import { ImpactTableEntityType } from 'types/impact-table-entity.type';
 import { PaginationMeta } from 'utils/app-base.service';
 export class PaginatedEntitiesDto {
-  entities: ImpactTableEntityType[] | LocationTypeEntities[];
+  entities: ImpactTableEntityType[];
   metadata?: PaginationMeta;
-}
-
-export class LocationTypeEntities {
-  name: LOCATION_TYPES;
-  children: any;
 }

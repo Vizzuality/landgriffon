f4ee1881f9a6c3073eef93af2c68f1b2f40eb281
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-05-11 11:43:06 +0200

    Use react-select for the simple selectors

--- client/src/components/select/component.tsx
+++ client/src/components/select/component.tsx
@@ -1,7 +1,6 @@
-import { Fragment, useCallback, useEffect, useState, useMemo } from 'react';
-import { Combobox, Transition } from '@headlessui/react';
-import { ChevronUpIcon, XIcon, SearchIcon } from '@heroicons/react/solid';
+import { useCallback } from 'react';
 import classNames from 'classnames';
+import ReactSelect from 'react-select';
 
 import Loading from 'components/loading';
 
@@ -40,123 +39,83 @@ const Select: React.FC<SelectProps> = ({
   allowEmpty = false,
   loading = false,
   placeholder = null,
-  searchPlaceholder = 'Search',
   onChange,
   onSearch,
   theme = 'default',
   error = false,
+  showSearch = false,
 }) => {
-  const {
-    result: optionsResult,
-    search: setSearchTerm,
-    term: searchTerm,
-    reset: resetSearch,
-  } = useFuse(options, SEARCH_OPTIONS);
-
-  const enabledOptionsResult = useMemo(
-    () => optionsResult.filter((option) => !option.disabled),
-    [optionsResult],
-  );
-
-  useEffect(() => {
-    resetSearch();
-    if (searchTerm) setSearchTerm(searchTerm);
-  }, [resetSearch, searchTerm, setSearchTerm]);
+  const { result: optionsResult, search: setSearchTerm } = useFuse(options, SEARCH_OPTIONS);
 
   const handleSearch = useCallback(
-    (e) => {
-      onSearch?.(e.currentTarget.value);
-      // resetSearch();
-      setSearchTerm(e.currentTarget.value);
+    (term: string) => {
+      setSearchTerm(term);
+      onSearch?.(term);
     },
     [onSearch, setSearchTerm],
   );
 
   const handleChange = useCallback(
-    (currentOption) => {
-      onChange?.(currentOption);
+    (option) => {
+      onChange?.(option);
     },
     [onChange],
   );
 
   return (
-    <Combobox value={current} onChange={handleChange} disabled={disabled}>
-      {({ open }) => (
-        <div className="relative">
-          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-            <Combobox.Input
-              className="text-ellipsis"
-              displayValue={(option: SelectProps['current']) => searchTerm || option?.label || ''}
-              onChange={handleSearch}
-            />
-            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-              <ChevronUpIcon
-                className={classNames('h-4 w-4', { 'rotate-180': open })}
-                aria-hidden="true"
-              />
-            </Combobox.Button>
-          </div>
-          <Transition
-            show={open}
-            as={Fragment}
-            enter="transition ease-out duration-200"
-            enterFrom="opacity-0 translate-y-1"
-            enterTo="opacity-100 translate-y-0"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100 translate-y-0"
-            leaveTo="opacity-0 translate-y-1"
-            afterLeave={() => {
-              resetSearch();
-            }}
-          >
-            <Combobox.Options
-              static
+    <ReactSelect
+      placeholder={placeholder}
+      onInputChange={handleSearch}
+      onChange={handleChange}
+      options={optionsResult.map(({ disabled, ...option }) => ({
+        ...option,
+        isDisabled: disabled,
+      }))}
+      value={current}
+      isDisabled={disabled}
+      isLoading={loading}
+      isClearable={allowEmpty}
+      isSearchable={showSearch}
+      noOptionsMessage={() => <div className="p-2 text-sm">No results</div>}
+      components={{
+        Option: ({
+          innerProps,
+          innerRef,
+          isSelected,
+          isFocused,
+          isDisabled,
+          data: { label, extraInfo },
+        }) => (
+          <div {...innerProps} ref={innerRef}>
+            <div
               className={classNames(
-                'absolute z-20 mt-1 min-w-min w-full bg-white shadow-lg max-h-60 rounded-md ring-1 ring-black ring-opacity-5 overflow-y-auto overflow-x-hidden focus:outline-none text-sm',
+                isFocused ? 'bg-green-50 text-green-700' : 'text-gray-900',
+                isSelected && 'bg-green-50 text-green-700',
+                'cursor-pointer select-none relative py-2 pl-4 pr-4',
+                isDisabled && 'text-opacity-50 cursor-default',
               )}
             >
-              {enabledOptionsResult.map((option, i) => (
-                <Combobox.Option
-                  key={option.value}
-                  className={({ active, selected, disabled }) =>
-                    classNames(
-                      'z-10',
-                      active ? 'bg-green-50 text-green-700' : 'text-gray-900',
-                      selected && 'bg-green-50 text-green-700',
-                      'cursor-pointer select-none relative py-2 pl-4 pr-4',
-                      disabled && 'text-opacity-50 cursor-default',
-                    )
-                  }
-                  disabled={option.disabled}
-                  value={option}
-                >
-                  {({ selected }) => (
-                    <div className="flex flex-row gap-x-2">
-                      <div
-                        className={classNames(
-                          selected ? 'font-semibold' : 'font-normal',
-                          'block text-sm truncate',
-                        )}
-                      >
-                        {option.label}
-                      </div>
-                      {option.extraInfo && (
-                        <div>
-                          <i className="text-gray-600 text-sm">{option.extraInfo}</i>
-                        </div>
-                      )}
-                    </div>
+              <div className="flex flex-row gap-x-2">
+                <div
+                  className={classNames(
+                    isSelected ? 'font-semibold' : 'font-normal',
+                    'block text-sm truncate',
                   )}
-                </Combobox.Option>
-              ))}
-              {enabledOptionsResult.length === 0 && searchTerm && (
-                <div className="p-2 text-sm">No results</div>
-              )}
-            </Combobox.Options>
-          </Transition>
-        </div>
-      )}
-    </Combobox>
+                >
+                  {label}
+                </div>
+                {extraInfo && (
+                  <div>
+                    <i className="text-gray-600 text-sm">{extraInfo}</i>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        ),
+        LoadingIndicator: () => <Loading />,
+      }}
+    />
   );
 };
 

12b8b61e88bdd3354a8be744c1d61ddb9af02600
David Inga <david.inga@vizzuality.com>
2022-05-11 11:26:35 +0200

    better styles for view selector

---
+++ client/src/components/button-group/types.d.ts
@@ -0,0 +1,5 @@
+import type { ButtonHTMLAttributes } from 'react';
+
+export type ButtonGroupItemProps = ButtonHTMLAttributes & {
+  active?: boolean;
+};
--- client/src/containers/analysis-visualization/mode-control/component.tsx
+++ client/src/containers/analysis-visualization/mode-control/component.tsx
@@ -1,15 +1,11 @@
 import { useCallback } from 'react';
-import classNames from 'classnames';
 import { ChartPieIcon, MapIcon, TableIcon } from '@heroicons/react/outline';
 
 import { useAppSelector, useAppDispatch } from 'store/hooks';
 import { analysisUI, setVisualizationMode } from 'store/features/analysis/ui';
 import { analysisFilters } from 'store/features/analysis/filters';
 
-const CONTROL_ITEM_CLASS_NAMES =
-  'relative inline-flex items-center px-4 py-1.5 border border-gray-200 bg-white text-sm font-medium text-gray-600 hover:bg-green-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700';
-const CONTROL_ITEM_ACTIVE_CLASS_NAMES =
-  'z-10 outline-none ring-1 ring-green-700 text-green-700 border-green-700 bg-green-50';
+import ButtonGroup, { ButtonGroupItem } from 'components/button-group';
 
 const ModeControl: React.FC = () => {
   const { visualizationMode } = useAppSelector(analysisUI);
@@ -21,32 +17,17 @@ const ModeControl: React.FC = () => {
   if (layer !== 'impact' && visualizationMode !== 'map') return null;
 
   return (
-    <div className="inline-flex shadow-sm rounded-md">
-      <button
-        onClick={() => handleClick('map')}
-        className={classNames(CONTROL_ITEM_CLASS_NAMES, 'rounded-l-md', {
-          [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'map',
-        })}
-      >
+    <ButtonGroup>
+      <ButtonGroupItem active={visualizationMode === 'map'} onClick={() => handleClick('map')}>
         <MapIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-      <button
-        onClick={() => handleClick('table')}
-        className={classNames(CONTROL_ITEM_CLASS_NAMES, '-ml-px', {
-          [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'table',
-        })}
-      >
+      </ButtonGroupItem>
+      <ButtonGroupItem active={visualizationMode === 'table'} onClick={() => handleClick('table')}>
         <TableIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-      <button
-        onClick={() => handleClick('chart')}
-        className={classNames(CONTROL_ITEM_CLASS_NAMES, '-ml-px rounded-r-md', {
-          [CONTROL_ITEM_ACTIVE_CLASS_NAMES]: visualizationMode === 'chart',
-        })}
-      >
+      </ButtonGroupItem>
+      <ButtonGroupItem active={visualizationMode === 'chart'} onClick={() => handleClick('chart')}>
         <ChartPieIcon className="h-6 w-6" aria-hidden="true" />
-      </button>
-    </div>
+      </ButtonGroupItem>
+    </ButtonGroup>
   );
 };
 

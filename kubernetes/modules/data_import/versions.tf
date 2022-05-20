229ab6be742999bc8ab15f02ecaa06c5424db4f2
alexeh <alexurola@gmail.com>
2022-05-20 15:00:24 +0200

    Update hashicorp/kubernetes provider version

--- kubernetes/modules/data_import/versions.tf
+++ kubernetes/modules/data_import/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.5.0"
+      version = "2.11.0"
     }
   }
   required_version = "~> 1.0.8"
--- kubernetes/modules/ingress/versions.tf
+++ kubernetes/modules/ingress/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.5.0"
+      version = "2.11.0"
     }
   }
   required_version = "~> 1.0.8"

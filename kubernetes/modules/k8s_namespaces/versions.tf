e476df4df38e98744adb6ca5a9848a58f7351bc6
alexeh <alexurola@gmail.com>
2022-05-20 15:00:24 +0200

    Update hashicorp/kubernetes provider version

--- kubernetes/modules/k8s_namespaces/versions.tf
+++ kubernetes/modules/k8s_namespaces/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.5.0"
+      version = "2.11.0"
     }
     aws = {
       source  = "hashicorp/aws"

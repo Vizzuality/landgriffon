765aac9fd64eae5f2863621813211a412542d15e
mluena <maria.luena@vizzuality.com>
2022-05-23 12:52:08 +0200

    fix metadata typo

--- client/src/hooks/metadata-info/index.ts
+++ client/src/hooks/metadata-info/index.ts
@@ -18,7 +18,7 @@ const INFO = {
       'Forest loss due to land use change (ha) measures the conversion of forest to other land use that has been likely produced due to the presence of nearby commodity driven agriculture, independent whether human induced or not.',
     // Carbon impact
     'impact-c71eb531-2c8e-40d2-ae49-1049543be4d1':
-      'Carbon emissions due to land use change (tCO2e) measures the release of biomass carbon associated with the forest that were lost near to a corpland area for a specific agricultural commodity.',
+      'Carbon emissions due to land use change (tCO2e) measures the release of biomass carbon associated with the forest that were lost near to a cropland area for a specific agricultural commodity.',
     // Biodiversity impact
     'impact-0594aba7-70a5-460c-9b58-fc1802d264ea':
       "Biodiversity loss due to land use change (PDF) measures the regional species loss, displacement or reduction of species that would otherwise exist in the land if it wouldn't be used for production of certain commodities.",

05c5e77aa3378d618fc2d07f22d853d610bebfc1
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-06-01 18:53:39 +0200

    Fix more filters being cropped

--- client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
@@ -14,7 +14,7 @@ import Suppliers from '../suppliers/component';
 import LocationTypes from '../location-types/component';
 
 import type { AnalysisFiltersState } from 'store/features/analysis/filters';
-import { flip, offset, shift, useFloating } from '@floating-ui/react-dom';
+import { offset, shift, useFloating } from '@floating-ui/react-dom';
 
 type MoreFiltersState = {
   materials: AnalysisFiltersState['materials'];
@@ -106,111 +106,110 @@ const MoreFilters: React.FC = () => {
               </span>
             )}
           </Popover.Button>
-          <div
+
+          <Transition
+            as="div"
+            className="z-20"
             ref={floating}
             style={{
               position: strategy,
               top: y ?? '',
               left: x ?? '',
             }}
+            show={open}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
           >
-            <Transition
-              show={open}
-              as={Fragment}
-              enter="transition duration-100 ease-out"
-              enterFrom="transform scale-95 opacity-0"
-              enterTo="transform scale-100 opacity-100"
-              leave="transition duration-75 ease-out"
-              leaveFrom="transform scale-100 opacity-100"
-              leaveTo="transform scale-95 opacity-0"
-            >
-              <Popover.Panel static className="w-80 z-20">
-                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 ">
-                  <div className="relative p-4 bg-white rounded-lg">
-                    <div className="flex justify-between mb-4">
-                      <div>Filter by</div>
-                      <Button theme="textLight" size="text" onClick={handleClearFilters}>
-                        Clear all
-                      </Button>
+            <Popover.Panel static className="w-80 z-20">
+              <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 ">
+                <div className="relative p-4 bg-white rounded-lg">
+                  <div className="flex justify-between mb-4">
+                    <div>Filter by</div>
+                    <Button theme="textLight" size="text" onClick={handleClearFilters}>
+                      Clear all
+                    </Button>
+                  </div>
+                  <div className="flex flex-col gap-3">
+                    <div>
+                      <div className="mb-1">Material</div>
+                      <Materials
+                        multiple
+                        withSourcingLocations
+                        originIds={originIds}
+                        supplierIds={supplierIds}
+                        locationTypes={locationTypesIds}
+                        current={selectedFilters.materials}
+                        fitContent
+                        onChange={(values) => handleChangeFilter('materials', values)}
+                      />
                     </div>
-                    <div className="flex flex-col gap-3">
-                      <div>
-                        <div className="mb-1">Material</div>
-                        <Materials
-                          multiple
-                          withSourcingLocations
-                          originIds={originIds}
-                          supplierIds={supplierIds}
-                          locationTypes={locationTypesIds}
-                          current={selectedFilters.materials}
-                          fitContent
-                          onChange={(values) => handleChangeFilter('materials', values)}
-                        />
-                      </div>
-                      <div>
-                        <div className="mb-1">Origins</div>
-                        <OriginRegions
-                          multiple
-                          withSourcingLocations
-                          materialIds={materialIds}
-                          supplierIds={supplierIds}
-                          locationTypes={locationTypesIds}
-                          current={selectedFilters.origins}
-                          fitContent
-                          onChange={(values) => handleChangeFilter('origins', values)}
-                        />
-                      </div>
-                      <div>
-                        <div className="mb-1">Suppliers</div>
-                        <Suppliers
-                          multiple
-                          withSourcingLocations
-                          materialIds={materialIds}
-                          originIds={originIds}
-                          locationTypes={locationTypesIds}
-                          current={selectedFilters.suppliers}
-                          fitContent
-                          onChange={(values) => handleChangeFilter('suppliers', values)}
-                        />
-                      </div>
-                      <div>
-                        <div className="mb-1">Location type</div>
-                        <LocationTypes
-                          current={selectedFilters.locationTypes}
-                          fitContent
-                          onChange={(values) => handleChangeFilter('locationTypes', values)}
-                        />
-                      </div>
+                    <div>
+                      <div className="mb-1">Origins</div>
+                      <OriginRegions
+                        multiple
+                        withSourcingLocations
+                        materialIds={materialIds}
+                        supplierIds={supplierIds}
+                        locationTypes={locationTypesIds}
+                        current={selectedFilters.origins}
+                        fitContent
+                        onChange={(values) => handleChangeFilter('origins', values)}
+                      />
                     </div>
-
-                    <div className="flex gap-2 mt-6">
-                      <Button
-                        theme="secondary"
-                        className="px-9"
-                        onClick={() => {
-                          handleClose();
-                          close();
-                        }}
-                      >
-                        Cancel
-                      </Button>
-                      <Button
-                        theme="primary"
-                        className="flex-grow"
-                        onClick={() => {
-                          handleApply();
-                          close();
-                        }}
-                        disabled={!hasChangesToApply}
-                      >
-                        Apply
-                      </Button>
+                    <div>
+                      <div className="mb-1">Suppliers</div>
+                      <Suppliers
+                        multiple
+                        withSourcingLocations
+                        materialIds={materialIds}
+                        originIds={originIds}
+                        locationTypes={locationTypesIds}
+                        current={selectedFilters.suppliers}
+                        fitContent
+                        onChange={(values) => handleChangeFilter('suppliers', values)}
+                      />
+                    </div>
+                    <div>
+                      <div className="mb-1">Location type</div>
+                      <LocationTypes
+                        current={selectedFilters.locationTypes}
+                        fitContent
+                        onChange={(values) => handleChangeFilter('locationTypes', values)}
+                      />
                     </div>
                   </div>
+
+                  <div className="flex gap-2 mt-6">
+                    <Button
+                      theme="secondary"
+                      className="px-9"
+                      onClick={() => {
+                        handleClose();
+                        close();
+                      }}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      theme="primary"
+                      className="flex-grow"
+                      onClick={() => {
+                        handleApply();
+                        close();
+                      }}
+                      disabled={!hasChangesToApply}
+                    >
+                      Apply
+                    </Button>
+                  </div>
                 </div>
-              </Popover.Panel>
-            </Transition>
-          </div>
+              </div>
+            </Popover.Panel>
+          </Transition>
         </>
       )}
     </Popover>

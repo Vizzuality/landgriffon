58efc94303d077ff9d639385b3973501332d4f24
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-06-10 11:46:31 +0200

    Update smart filter tests with location types options

--- api/test/e2e/admin-regions/admin-regions-tree-smart-filters.spec.ts
+++ api/test/e2e/admin-regions/admin-regions-tree-smart-filters.spec.ts
@@ -15,6 +15,10 @@ import { BusinessUnitsModule } from 'modules/business-units/business-units.modul
 import { BusinessUnitRepository } from 'modules/business-units/business-unit.repository';
 import { AdminRegion } from 'modules/admin-regions/admin-region.entity';
 import { Material } from 'modules/materials/material.entity';
+import {
+  LOCATION_TYPES,
+  LOCATION_TYPES_PARAMS,
+} from 'modules/sourcing-locations/sourcing-location.entity';
 
 describe('Admin Regions - Get trees - Smart Filters', () => {
   let app: INestApplication;
@@ -111,6 +115,83 @@ describe('Admin Regions - Get trees - Smart Filters', () => {
         .query({
           withSourcingLocations: true,
           'materialIds[]': [material1.id, material2.id],
+        })
+        .set('Authorization', `Bearer ${jwtToken}`);
+
+      expect(HttpStatus.OK);
+      expect(response.body.data[0].id).toEqual(parentAdminRegion1.id);
+      expect(response.body.data[0].attributes.children[0].name).toEqual(
+        childAdminRegion1.name,
+      );
+      expect(response.body.data[1].id).toEqual(parentAdminRegion2.id);
+      expect(response.body.data[1].attributes.children[0].name).toEqual(
+        childAdminRegion2.name,
+      );
+      expect(
+        response.body.data.find(
+          (adminRegion: AdminRegion) =>
+            adminRegion.id === parentAdminRegion3.id,
+        ),
+      ).toBe(undefined);
+    },
+  );
+
+  test(
+    'When I query a Admin Region Tree endpoint ' +
+      'And I query the ones with sourcing locations' +
+      'And I filter them by a related Location Types' +
+      'Then I should receive a tree list of Business Units where there are sourcing-locations for',
+    async () => {
+      const parentAdminRegion1: AdminRegion = await createAdminRegion({
+        name: 'parentAdminRegion',
+      });
+
+      const childAdminRegion1: AdminRegion = await createAdminRegion({
+        name: 'childAdminRegion',
+        parent: parentAdminRegion1,
+      });
+
+      const parentAdminRegion2: AdminRegion = await createAdminRegion({
+        name: 'parentAdminRegion2',
+      });
+
+      const childAdminRegion2: AdminRegion = await createAdminRegion({
+        name: 'childAdminRegion2',
+        parent: parentAdminRegion2,
+      });
+
+      const parentAdminRegion3: AdminRegion = await createAdminRegion({
+        name: 'parentAdminRegion3',
+      });
+
+      const childAdminRegion3: AdminRegion = await createAdminRegion({
+        name: 'childAdminRegion3',
+        parent: parentAdminRegion3,
+      });
+
+      await createSourcingLocation({
+        adminRegionId: childAdminRegion1.id,
+        locationType: LOCATION_TYPES.COUNTRY_OF_PRODUCTION,
+      });
+
+      await createSourcingLocation({
+        adminRegionId: childAdminRegion2.id,
+        locationType: LOCATION_TYPES.AGGREGATION_POINT,
+      });
+
+      await createSourcingLocation({
+        adminRegionId: childAdminRegion3.id,
+        locationType: LOCATION_TYPES.POINT_OF_PRODUCTION,
+      });
+
+      const response = await request(app.getHttpServer())
+        .get('/api/v1/admin-regions/trees')
+        .query({
+          withSourcingLocations: true,
+          'locationTypes[]': [
+            LOCATION_TYPES_PARAMS.COUNTRY_OF_PRODUCTION,
+            LOCATION_TYPES_PARAMS.AGGREGATION_POINT,
+          ],
         })
         .set('Authorization', `Bearer ${jwtToken}`);
 

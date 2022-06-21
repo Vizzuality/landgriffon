082f4c146dd284099638410bc6757028faec9337
alexeh <alexurola@gmail.com>
2022-06-21 09:05:54 +0200

    Add nest controller and service to contextual-layers

--- api/src/modules/contextual-layers/contextual-layers.module.ts
+++ api/src/modules/contextual-layers/contextual-layers.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ContextualLayerRepository } from 'modules/contextual-layers/contextual-layer.repository';
+import { ContextualLayersController } from 'modules/contextual-layers/contextual-layers.controller';
+import { ContextualLayersService } from 'modules/contextual-layers/contextual-layers.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ContextualLayerRepository])],
+  controllers: [ContextualLayersController],
+  providers: [ContextualLayersService],
 })
 export class ContextualLayersModule {}

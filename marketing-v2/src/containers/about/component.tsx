5395fac9afdb3d1b747f2e58575c7d4644ef77fb
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-06-27 11:19:05 +0200

    About: video

--- marketing-v2/src/containers/about/component.tsx
+++ marketing-v2/src/containers/about/component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Hero from 'containers/about/hero';
 import How from 'containers/about/how';
+import Video from 'containers/about/video';
 
 import Testimonials from 'containers/testimonials';
 import Newsletter from 'containers/newsletter';
@@ -11,6 +12,7 @@ const About: React.FC = () => {
     <>
       <Hero />
       <How />
+      <Video />
 
       <Testimonials />
       <Newsletter />

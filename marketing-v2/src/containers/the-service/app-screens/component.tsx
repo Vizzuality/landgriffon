b9ef7338ef44467c5e17c72bcb8f7d2c7b3a46f9
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-06-29 10:52:49 +0200

    The service: app screens

---
+++ marketing-v2/src/containers/the-service/app-screens/component.tsx
@@ -0,0 +1,15 @@
+import cx from 'classnames';
+
+const AppScreens: React.FC = () => {
+  return (
+    <div
+      className={cx({
+        'grid grid-cols-3 gap-10': true,
+      })}
+    >
+      <div></div>
+    </div>
+  );
+};
+
+export default AppScreens;

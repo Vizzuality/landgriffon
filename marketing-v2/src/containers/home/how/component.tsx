bacd62ff3ebb172da1e1b7ea6d949e0ddf895b31
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-07-04 21:32:25 +0200

    Home: Sticky content will handle better different heights

--- marketing-v2/src/containers/home/how/component.tsx
+++ marketing-v2/src/containers/home/how/component.tsx
@@ -32,135 +32,137 @@ const How: React.FC = () => {
   return (
     <section className="relative hidden bg-white md:block">
       <Wrapper>
-        <div ref={ref} className="h-[400vh] relative">
-          <div className="sticky top-0 flex items-center py-32 border-t border-black/10">
-            <div className="space-y-20">
-              <FadeIn>
-                <h2 className="w-5/12 text-4xl font-black uppercase md:text-6xl font-display">
-                  How does it work?
-                </h2>
-              </FadeIn>
+        <div ref={ref} className="h-[400vh] relative py-12 border-t border-black/10">
+          <div className="sticky top-0 flex items-center py-12 ">
+            <div className="">
+              <div className="space-y-12">
+                <FadeIn>
+                  <h2 className="w-5/12 text-4xl font-black uppercase md:text-6xl font-display">
+                    How does it work?
+                  </h2>
+                </FadeIn>
 
-              <FadeIn>
-                <ul className="grid grid-cols-4 gap-x-20">
-                  <li>
-                    <div
-                      className={cx({
-                        'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
-                        'transition-colors': true,
-                        'bg-gray-100': STEP !== 0,
-                        'bg-orange-500 ': STEP === 0,
-                      })}
-                    >
-                      <h3 className="text-xl font-black text-center uppercase font-display">
-                        Import data
-                      </h3>
-                    </div>
+                <FadeIn>
+                  <ul className="grid grid-cols-4 gap-x-20">
+                    <li>
+                      <div
+                        className={cx({
+                          'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
+                          'transition-colors': true,
+                          'bg-gray-100': STEP !== 0,
+                          'bg-orange-500 ': STEP === 0,
+                        })}
+                      >
+                        <h3 className="text-xl font-black text-center uppercase font-display">
+                          Import data
+                        </h3>
+                      </div>
 
-                    <motion.ul
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: STEP === 0 ? 1 : 0 }}
-                      className="mt-10 space-y-5"
-                    >
-                      {IMPORT_DATA_FEATURES.map((feature) => (
-                        <li key={feature.id} className="flex items-center space-x-2">
-                          <div className="flex items-center justify-center bg-orange-500 rounded-full shrink-0 w-9 h-9">
-                            <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-black" />
-                          </div>
-                          <p className="font-light">{feature.name}</p>
-                        </li>
-                      ))}
-                    </motion.ul>
-                  </li>
-                  <li>
-                    <div
-                      className={cx({
-                        'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
-                        'transition-colors': true,
-                        'bg-gray-100': STEP !== 1,
-                        'bg-green-500 text-white': STEP === 1,
-                      })}
-                    >
-                      <h3 className="text-xl font-black text-center uppercase font-display">
-                        Map suppliers
-                      </h3>
-                    </div>
+                      <motion.ul
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: STEP === 0 ? 1 : 0 }}
+                        className="mt-10 space-y-2.5"
+                      >
+                        {IMPORT_DATA_FEATURES.map((feature) => (
+                          <li key={feature.id} className="flex items-center space-x-2">
+                            <div className="flex items-center justify-center bg-orange-500 rounded-full shrink-0 w-9 h-9">
+                              <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-black" />
+                            </div>
+                            <p className="font-light">{feature.name}</p>
+                          </li>
+                        ))}
+                      </motion.ul>
+                    </li>
+                    <li>
+                      <div
+                        className={cx({
+                          'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
+                          'transition-colors': true,
+                          'bg-gray-100': STEP !== 1,
+                          'bg-green-500 text-white': STEP === 1,
+                        })}
+                      >
+                        <h3 className="text-xl font-black text-center uppercase font-display">
+                          Map suppliers
+                        </h3>
+                      </div>
 
-                    <motion.ul
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: STEP === 1 ? 1 : 0 }}
-                      className="mt-10 space-y-5"
-                    >
-                      {MAP_SUPPLIERS_FEATURES.map((feature) => (
-                        <li key={feature.id} className="flex items-center space-x-2">
-                          <div className="flex items-center justify-center bg-green-500 rounded-full shrink-0 w-9 h-9">
-                            <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-white" />
-                          </div>
-                          <p className="font-light">{feature.name}</p>
-                        </li>
-                      ))}
-                    </motion.ul>
-                  </li>
-                  <li>
-                    <div
-                      className={cx({
-                        'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
-                        'transition-colors': true,
-                        'bg-gray-100': STEP !== 2,
-                        'bg-green-500 text-white': STEP === 2,
-                      })}
-                    >
-                      <h3 className="text-xl font-black text-center uppercase font-display">
-                        Calculate impact
-                      </h3>
-                    </div>
+                      <motion.ul
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: STEP === 1 ? 1 : 0 }}
+                        className="mt-10 space-y-2.5"
+                      >
+                        {MAP_SUPPLIERS_FEATURES.map((feature) => (
+                          <li key={feature.id} className="flex items-center space-x-2">
+                            <div className="flex items-center justify-center bg-green-500 rounded-full shrink-0 w-9 h-9">
+                              <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-white" />
+                            </div>
+                            <p className="font-light">{feature.name}</p>
+                          </li>
+                        ))}
+                      </motion.ul>
+                    </li>
+                    <li>
+                      <div
+                        className={cx({
+                          'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
+                          'transition-colors': true,
+                          'bg-gray-100': STEP !== 2,
+                          'bg-green-500 text-white': STEP === 2,
+                        })}
+                      >
+                        <h3 className="text-xl font-black text-center uppercase font-display">
+                          Calculate impact
+                        </h3>
+                      </div>
 
-                    <motion.ul
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: STEP === 2 ? 1 : 0 }}
-                      className="mt-10 space-y-5"
-                    >
-                      {CALCULATE_IMPACT_FEATURES.map((feature) => (
-                        <li key={feature.id} className="flex items-center space-x-2">
-                          <div className="flex items-center justify-center bg-green-500 rounded-full shrink-0 w-9 h-9">
-                            <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-white" />
-                          </div>
-                          <p className="font-light">{feature.name}</p>
-                        </li>
-                      ))}
-                    </motion.ul>
-                  </li>
-                  <li>
-                    <div
-                      className={cx({
-                        'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
-                        'transition-colors': true,
-                        'bg-gray-100': STEP !== 3,
-                        'bg-blue-500 text-white': STEP === 3,
-                      })}
-                    >
-                      <h3 className="text-xl font-black text-center uppercase font-display">
-                        EXPLORE PATHWAYS to reduce impacts
-                      </h3>
-                    </div>
+                      <motion.ul
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: STEP === 2 ? 1 : 0 }}
+                        className="mt-10 space-y-2.5"
+                      >
+                        {CALCULATE_IMPACT_FEATURES.map((feature) => (
+                          <li key={feature.id} className="flex items-center space-x-2">
+                            <div className="flex items-center justify-center bg-green-500 rounded-full shrink-0 w-9 h-9">
+                              <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-white" />
+                            </div>
+                            <p className="font-light">{feature.name}</p>
+                          </li>
+                        ))}
+                      </motion.ul>
+                    </li>
+                    <li>
+                      <div
+                        className={cx({
+                          'flex items-center justify-center p-5 rounded-full w-52 h-52': true,
+                          'transition-colors': true,
+                          'bg-gray-100': STEP !== 3,
+                          'bg-blue-500 text-white': STEP === 3,
+                        })}
+                      >
+                        <h3 className="text-xl font-black text-center uppercase font-display">
+                          EXPLORE PATHWAYS to reduce impacts
+                        </h3>
+                      </div>
 
-                    <motion.ul
-                      initial={{ opacity: 0 }}
-                      animate={{ opacity: STEP === 3 ? 1 : 0 }}
-                      className="mt-10 space-y-5"
-                    >
-                      {EXPLORE_FEATURES.map((feature) => (
-                        <li key={feature.id} className="flex items-center space-x-2">
-                          <div className="flex items-center justify-center bg-blue-500 rounded-full shrink-0 w-9 h-9">
-                            <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-white" />
-                          </div>
-                          <p className="font-light">{feature.name}</p>
-                        </li>
-                      ))}
-                    </motion.ul>
-                  </li>
-                </ul>
-              </FadeIn>
+                      <motion.ul
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: STEP === 3 ? 1 : 0 }}
+                        className="mt-10 space-y-2.5"
+                      >
+                        {EXPLORE_FEATURES.map((feature) => (
+                          <li key={feature.id} className="flex items-center space-x-2">
+                            <div className="flex items-center justify-center bg-blue-500 rounded-full shrink-0 w-9 h-9">
+                              <Icon icon={feature.icon} className="w-3.5 h-3.5 fill-white" />
+                            </div>
+                            <p className="font-light">{feature.name}</p>
+                          </li>
+                        ))}
+                      </motion.ul>
+                    </li>
+                  </ul>
+                </FadeIn>
+              </div>
             </div>
           </div>
         </div>

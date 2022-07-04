e1b2d03510bdaed64df9b9c90e5dcc38b0a75756
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-07-04 16:40:28 +0200

    Scroll down: bounce animation

--- marketing-v2/src/containers/scroll-down/component.tsx
+++ marketing-v2/src/containers/scroll-down/component.tsx
@@ -11,25 +11,27 @@ interface ScrollDownProps {
 const ScrollDown: React.FC<ScrollDownProps> = ({ theme }: ScrollDownProps) => {
   return (
     <div className="absolute top-0 left-0 z-20 hidden w-full h-full pointer-events-none xl:block">
-      <div className="absolute -rotate-90 -right-10 bottom-40">
-        <div className="flex items-center space-x-5">
-          <Icon
-            icon={ARROW_DOWN_SVG}
-            className={cx({
-              'w-5 h-5 rotate-90': true,
-              'fill-white': theme === 'dark',
-              'fill-black': theme === 'light',
-            })}
-          />
-          <span
-            className={cx({
-              'block text-xs uppercase': true,
-              'text-white': theme === 'dark',
-              'text-black': theme === 'light',
-            })}
-          >
-            Scroll down
-          </span>
+      <div className="absolute -right-10 bottom-40 animate-bounce">
+        <div className="-rotate-90 ">
+          <div className="flex items-center space-x-5">
+            <Icon
+              icon={ARROW_DOWN_SVG}
+              className={cx({
+                'w-5 h-5 rotate-90': true,
+                'fill-white': theme === 'dark',
+                'fill-black': theme === 'light',
+              })}
+            />
+            <span
+              className={cx({
+                'block text-xs uppercase': true,
+                'text-white': theme === 'dark',
+                'text-black': theme === 'light',
+              })}
+            >
+              Scroll down
+            </span>
+          </div>
         </div>
       </div>
     </div>

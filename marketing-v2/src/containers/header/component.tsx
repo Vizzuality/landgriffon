f31b339e1c8e29347179cb04155a4b3e92a06e95
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-07-04 12:43:59 +0200

    Header: responsive

--- marketing-v2/src/containers/header/component.tsx
+++ marketing-v2/src/containers/header/component.tsx
@@ -1,106 +1,47 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
-import cx from 'classnames';
+import Icon from 'components/icon';
+
+import BURGER_SVG from 'svgs/ui/burger.svg?sprite';
+import CLOSE_SVG from 'svgs/ui/close.svg?sprite';
+
+import { useState } from 'react';
+
+import Nav from './nav';
 
 const Header: React.FC = () => {
-  const { pathname } = useRouter();
+  const [open, setOpen] = useState(false);
 
   return (
     <header className="flex items-center justify-between font-semibold bg-white h-14 lg:h-20">
       <div className="px-4 lg:px-12 shrink-0">
         <Link href="/">
-          <a className="block">
+          <a className="block w-[180px] h-[14px]">
             <Image
               src="/images/logo.svg"
               alt="Landgriffon"
               width={180}
               height={14}
               priority
-              layout="fixed"
+              layout="responsive"
             />
           </a>
         </Link>
       </div>
 
-      <nav className="hidden lg:block">
-        <ul className="flex space-x-14">
-          <li>
-            <Link href="/the-service">
-              <a
-                className={cx({
-                  'relative block font-light py-7 hover:text-green-500': true,
-                  'text-green-500': pathname === '/the-service',
-                  'after:content-[""] after:absolute after:top-0 after:left-0 after:h-1 after:w-full after:bg-green-500':
-                    pathname === '/the-service',
-                })}
-              >
-                The service
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/methodology">
-              <a
-                className={cx({
-                  'relative block font-light py-7 hover:text-green-500': true,
-                  'text-green-500': pathname === '/methodology',
-                  'after:content-[""] after:absolute after:top-0 after:left-0 after:h-1 after:w-full after:bg-green-500':
-                    pathname === '/methodology',
-                })}
-              >
-                Methodology
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a
-                className={cx({
-                  'relative block font-light py-7 hover:text-green-500': true,
-                  'text-green-500': pathname === '/about',
-                  'after:content-[""] after:absolute after:top-0 after:left-0 after:h-1 after:w-full after:bg-green-500':
-                    pathname === '/about',
-                })}
-              >
-                About
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/faq">
-              <a
-                className={cx({
-                  'relative block font-light py-7 hover:text-green-500': true,
-                  'text-green-500': pathname === '/faq',
-                  'after:content-[""] after:absolute after:top-0 after:left-0 after:h-1 after:w-full after:bg-green-500':
-                    pathname === '/faq',
-                })}
-              >
-                FAQ
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog">
-              <a
-                className={cx({
-                  'relative block font-light py-7 hover:text-green-500': true,
-                  'text-green-500': pathname === '/blog',
-                })}
-              >
-                Blog
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact">
-              <a className="block px-10 text-white bg-green-500 py-7">Contact</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <button
+        type="button"
+        className="block px-4 lg:hidden"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {!open && <Icon icon={BURGER_SVG} className="w-6 h-6" />}
+        {open && <Icon icon={CLOSE_SVG} className="w-5 h-5" />}
+      </button>
+
+      <Nav open={open} />
     </header>
   );
 };
---
+++ marketing-v2/src/containers/header/nav/component.tsx
@@ -0,0 +1,144 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+import cx from 'classnames';
+import { useMediaMatch } from 'rooks';
+import { motion, AnimatePresence } from 'framer-motion';
+
+import lockScroll from 'react-lock-scroll';
+
+export interface NavProps {
+  open: boolean;
+}
+
+const HeaderNav: React.FC<NavProps> = ({ open }: NavProps) => {
+  const { pathname } = useRouter();
+
+  const isLg = useMediaMatch('(min-width: 1024px)');
+
+  lockScroll(!isLg && open);
+
+  return (
+    <AnimatePresence>
+      {(open || isLg) && (
+        <motion.nav
+          initial={{
+            opacity: 0,
+            y: -10,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+          }}
+          exit={{
+            opacity: 0,
+            y: -10,
+          }}
+          className="flex flex-col justify-between lg:flex-row lg:space-x-10 fixed left-0 z-50 w-full h-[calc(100%_-_theme(space.14))] lg:h-auto bg-white top-14 lg:top-0 lg:w-auto lg:relative py-5 lg:py-0"
+        >
+          <ul className="flex flex-col space-y-5 lg:flex-row lg:space-y-0 lg:space-x-14">
+            {!isLg && (
+              <li>
+                <Link href="/">
+                  <a
+                    className={cx({
+                      'relative block font-light text-2xl lg:text-base py-1 lg:py-7 mx-5 lg:mx-0 px-5 lg:px-0 hover:text-green-500':
+                        true,
+                      'text-green-500': pathname === '/',
+                      'after:content-[""] after:absolute after:top-0 after:left-0 after:h-full after:w-2 lg:after:h-1 lg:after:w-full after:bg-green-500':
+                        pathname === '/',
+                    })}
+                  >
+                    Home
+                  </a>
+                </Link>
+              </li>
+            )}
+            <li>
+              <Link href="/the-service">
+                <a
+                  className={cx({
+                    'relative block font-light text-2xl lg:text-base py-1 lg:py-7 mx-5 lg:mx-0 px-5 lg:px-0 hover:text-green-500':
+                      true,
+                    'text-green-500': pathname === '/the-service',
+                    'after:content-[""] after:absolute after:top-0 after:left-0 after:h-full after:w-2 lg:after:h-1 lg:after:w-full after:bg-green-500':
+                      pathname === '/the-service',
+                  })}
+                >
+                  The service
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/methodology">
+                <a
+                  className={cx({
+                    'relative block font-light text-2xl lg:text-base py-1 lg:py-7 mx-5 lg:mx-0 px-5 lg:px-0 hover:text-green-500':
+                      true,
+                    'text-green-500': pathname === '/methodology',
+                    'after:content-[""] after:absolute after:top-0 after:left-0 after:h-full after:w-2 lg:after:h-1 lg:after:w-full after:bg-green-500':
+                      pathname === '/methodology',
+                  })}
+                >
+                  Methodology
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a
+                  className={cx({
+                    'relative block font-light text-2xl lg:text-base py-1 lg:py-7 mx-5 lg:mx-0 px-5 lg:px-0 hover:text-green-500':
+                      true,
+                    'text-green-500': pathname === '/about',
+                    'after:content-[""] after:absolute after:top-0 after:left-0 after:h-full after:w-2 lg:after:h-1 lg:after:w-full after:bg-green-500':
+                      pathname === '/about',
+                  })}
+                >
+                  About
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/faq">
+                <a
+                  className={cx({
+                    'relative block font-light text-2xl lg:text-base py-1 lg:py-7 mx-5 lg:mx-0 px-5 lg:px-0 hover:text-green-500':
+                      true,
+                    'text-green-500': pathname === '/faq',
+                    'after:content-[""] after:absolute after:top-0 after:left-0 after:h-full after:w-2 lg:after:h-1 lg:after:w-full after:bg-green-500':
+                      pathname === '/faq',
+                  })}
+                >
+                  FAQ
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog">
+                <a
+                  className={cx({
+                    'relative block font-light text-2xl lg:text-base py-1 lg:py-7 mx-5 lg:mx-0 px-5 lg:px-0 hover:text-green-500':
+                      true,
+                    'text-green-500': pathname === '/blog',
+                  })}
+                >
+                  Blog
+                </a>
+              </Link>
+            </li>
+          </ul>
+          <div>
+            <Link href="/contact">
+              <a className="block px-10 mx-5 text-base text-center text-white bg-green-500 lg:mx-0 py-7">
+                Contact
+              </a>
+            </Link>
+          </div>
+        </motion.nav>
+      )}
+    </AnimatePresence>
+  );
+};
+
+export default HeaderNav;

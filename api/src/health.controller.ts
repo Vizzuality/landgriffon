27df9c15387c78ea77463ab24bf7c975976aa18a
Tiago Garcia <tiagojsag@gmail.com>
2022-07-12 10:30:14 +0200

    Refactor API health controller

--- api/src/health.controller.ts
+++ api/src/health.controller.ts
@@ -1,18 +1,24 @@
 import {
   HealthCheck,
   HealthCheckService,
-  HttpHealthIndicator,
+  MicroserviceHealthIndicator,
+  TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
+import { Transport, RedisOptions } from '@nestjs/microservices';
 import { Controller, Get } from '@nestjs/common';
 import { HealthIndicatorResult } from '@nestjs/terminus/dist/health-indicator';
 import { HealthCheckResult } from '@nestjs/terminus/dist/health-check/health-check-result.interface';
 import { Public } from 'decorators/public.decorator';
+import * as config from 'config';
+
+const redisConfig: any = config.get('redis');
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
-    private http: HttpHealthIndicator,
+    private microservice: MicroserviceHealthIndicator,
+    private db: TypeOrmHealthIndicator,
   ) {}
 
   @Public()
@@ -21,7 +27,13 @@ export class HealthController {
   check(): Promise<HealthCheckResult> {
     return this.health.check([
       (): Promise<HealthIndicatorResult> =>
-        this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
+        this.microservice.pingCheck<RedisOptions>('redis', {
+          transport: Transport.REDIS,
+          options: {
+            url: `redis://${redisConfig.host}:${redisConfig.port}`,
+          },
+        }),
+      (): Promise<HealthIndicatorResult> => this.db.pingCheck('typeorm'),
     ]);
   }
 }

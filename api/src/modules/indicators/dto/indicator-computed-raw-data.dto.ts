7ed99d171599d265f37cd5c3859c855065a48d28
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-07-12 10:08:09 +0200

    Fix lint errors

--- api/src/modules/indicators/dto/indicator-computed-raw-data.dto.ts
+++ api/src/modules/indicators/dto/indicator-computed-raw-data.dto.ts
@@ -2,7 +2,7 @@
  * @description Raw indicator data computed in DB using stored functions of migration: XXXXXX
  * used to calculate impact for a Intervention
  */
-import { INDICATOR_TYPES } from '../indicator.entity';
+import { INDICATOR_TYPES } from 'modules/indicators/indicator.entity';
 
 export class IndicatorComputedRawDataDto {
   production: number;

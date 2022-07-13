9f2bf4b3e829b461030c18f1fb4a3969a94459cb
David Inga <david.inga@vizzuality.com>
2022-07-13 13:52:28 +0200

    styles for mode control in the analysis page

--- client/src/components/button-group/component.tsx
+++ client/src/components/button-group/component.tsx
@@ -1,5 +1,5 @@
 const ButtonGroup: React.FC = ({ children }) => (
-  <div className="relative z-0 inline-flex border border-gray-300 shadow-sm rounded-md">
+  <div className="relative z-0 inline-flex border border-gray-300 shadow-sm rounded-md bg-white">
     {children}
   </div>
 );

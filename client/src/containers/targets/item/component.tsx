26f6d7d67df8743aa915923a253c223a48949874
mariasola <mariasolahornedo@gmail.com>
2022-07-13 18:21:30 +0200

    modal started and first styles

--- client/src/containers/targets/item/component.tsx
+++ client/src/containers/targets/item/component.tsx
@@ -1,26 +1,38 @@
 import { Button } from 'components/button';
 import type { Target } from 'types';
 
+import useModal from 'hooks/modals';
+import AdminEditTargetModal from 'containers/admin/edit-target-modal';
+
 const TARGET_PILL_TITLE_CLASSNAMES = 'text-green-700 text-sm uppercase font-bold';
 const TARGET_PILL_VALUE_CLASSNAMES = '';
 
-const TargetItem: React.FC<Target> = ({ id, name, years }) => (
-  <div className="grid grid-cols-12 gap-4 items-center justify-between rounded-md bg-white shadow-sm p-4">
-    <div className="col-span-3">{name}</div>
-    <div className="col-span-9 flex items-center justify-between">
-      <div className="bg-green-50 rounded-md py-4 px-6 text-center">
-        <div className={TARGET_PILL_TITLE_CLASSNAMES}>Baseline</div>
-        <div className={TARGET_PILL_VALUE_CLASSNAMES}>2.37Mt</div>
-      </div>
-      {years.map(({ year, value }) => (
-        <div key={`${id}-${year}`} className="py-4 text-center">
-          <div className={TARGET_PILL_TITLE_CLASSNAMES}>{year}</div>
-          <div className={TARGET_PILL_VALUE_CLASSNAMES}>{value}</div>
+const TargetItem: React.FC<Target> = ({ id, name, years }) => {
+  const {
+    isOpen: isUploadEditTargetModalOpen,
+    open: openEditTargetModal,
+    close: closeEditTargetModal,
+  } = useModal();
+
+  return (
+    <div className="grid grid-cols-12 gap-4 items-center justify-between rounded-md bg-white shadow-sm p-4">
+      <div className="col-span-3">{name}</div>
+      <div className="col-span-9 flex items-center justify-between">
+        <div className="bg-green-50 rounded-md py-4 px-6 text-center">
+          <div className={TARGET_PILL_TITLE_CLASSNAMES}>Baseline</div>
+          <div className={TARGET_PILL_VALUE_CLASSNAMES}>2.37Mt</div>
         </div>
-      ))}
-      <Button theme="secondary">Edit</Button>
+        {years.map(({ year, value }) => (
+          <div key={`${id}-${year}`} className="py-4 text-center">
+            <div className={TARGET_PILL_TITLE_CLASSNAMES}>{year}</div>
+            <div className={TARGET_PILL_VALUE_CLASSNAMES}>{value}</div>
+          </div>
+        ))}
+        <Button theme="secondary" onClick={openEditTargetModal}>Edit</Button>
+      </div>
+      <AdminEditTargetModal open={isUploadEditTargetModalOpen} onDismiss={closeEditTargetModal} />
     </div>
-  </div>
-);
+  );
+};
 
 export default TargetItem;

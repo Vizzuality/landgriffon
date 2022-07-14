23d1c8fc7b9bf97c321a598320d7f12a18b970c4
David Inga <david.inga@vizzuality.com>
2022-07-14 09:29:38 +0200

    fixed warnings for search and input component

--- client/src/components/search/component.tsx
+++ client/src/components/search/component.tsx
@@ -1,38 +1,43 @@
-import { FC, useRef } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { SearchIcon, XIcon } from '@heroicons/react/solid';
-import { useButton } from '@react-aria/button';
-import { useSearchField } from '@react-aria/searchfield';
-import { useSearchFieldState } from '@react-stately/searchfield';
 
 import Input from 'components/forms/input';
 
 import type { SearchProps } from './types';
 
-export const Search: FC<SearchProps> = ({ ...props }: SearchProps) => {
-  const { placeholder } = props;
-  const state = useSearchFieldState(props);
+export const Search: FC<SearchProps> = (props: SearchProps) => {
+  const { onChange } = props;
+  const [value, setValue] = useState<SearchProps['value']>(null);
 
-  const ref = useRef();
-  const { inputProps, clearButtonProps } = useSearchField(props, state, ref);
-  const { buttonProps } = useButton(clearButtonProps, null);
+  const handleChange = useCallback(
+    (e) => {
+      if (onChange) onChange(e.target.value);
+      setValue(e.target.value);
+    },
+    [onChange],
+  );
+
+  const handleReset = useCallback(() => {
+    if (onChange) onChange('');
+    setValue('');
+  }, [onChange]);
 
   return (
     <div className="relative flex items-center w-full">
       <Input
-        {...inputProps}
+        {...props}
+        value={value}
         icon={SearchIcon}
-        ref={ref}
-        placeholder={placeholder}
         type="search"
         className="h-full"
+        onChange={handleChange}
       />
 
-      {state.value !== '' && (
+      {value && value !== '' && (
         <button
-          {...buttonProps}
-          tabIndex="clear"
           className="absolute z-10 flex items-center self-center justify-center w-5 h-5 right-3 r-2"
           type="button"
+          onClick={handleReset}
         >
           <XIcon className="w-4 h-4 text-gray-400" />
         </button>

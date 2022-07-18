d3d4257e3f3f249b7c7f9152ebea5f7b996ae7cd
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-07-18 14:40:28 +0200

    Fix showing double negative symbol

--- client/src/containers/analysis-visualization/analysis-table/comparison-cell/component.tsx
+++ client/src/containers/analysis-visualization/analysis-table/comparison-cell/component.tsx
@@ -11,16 +11,14 @@ const ComparisonCell: React.FC<ComparisonCellProps> = ({
   interventionValue,
   absoluteDifference,
 }) => {
-  const positiveDifference = interventionValue > value;
-
-  if (!interventionValue) return <>{BIG_NUMBER_FORMAT(value)}</>;
+  if (isNaN(interventionValue)) return <>{BIG_NUMBER_FORMAT(value)}</>;
   return (
     <div className="">
       <div className="my-auto text-gray-400 line-through text-xxs">{NUMBER_FORMAT(value)}</div>
       <div className="flex flex-row gap-1">
         <div className="my-auto text-sm text-gray-900">{NUMBER_FORMAT(interventionValue)}</div>
         <div className="my-auto text-xs font-semibold text-gray-500">
-          ({absoluteDifference !== 0 && positiveDifference ? '+' : '-'}
+          ({absoluteDifference > 0 && '+'}
           {NUMBER_FORMAT(absoluteDifference)})
         </div>
       </div>

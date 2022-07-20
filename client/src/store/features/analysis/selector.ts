9129cf8dc0b0cb79b7c051d9d22a85ab9aeaf0a8
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-07-20 15:41:30 +0200

    Fix build errors

--- client/src/store/features/analysis/selector.ts
+++ client/src/store/features/analysis/selector.ts
@@ -1,57 +1,8 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { analysisFilters } from './filters';
-import { analysisMap, AnalysisMapState } from './map';
 
 import type { AnalysisFiltersState } from './filters';
-import type {
-  MaterialH3APIParams,
-  RiskH3APIParams,
-  ImpactH3APIParams,
-  ImpactTabularAPIParams,
-} from 'types';
-
-export const filtersForH3API = createSelector(
-  [analysisFilters, analysisMap],
-  (analysisFiltersState: AnalysisFiltersState, analysisMapState: AnalysisMapState) => {
-    const { layers } = analysisMapState;
-    const { startYear, materials, indicator, suppliers, origins } = analysisFiltersState;
-
-    if (layers.material.active) {
-      const result: MaterialH3APIParams = {
-        year: startYear,
-        materialId: layers.material.material && layers.material.material.value,
-        resolution: 4,
-      };
-      return result;
-    }
-
-    if (layers.risk.active) {
-      const result: RiskH3APIParams = {
-        year: startYear,
-        indicatorId: indicator?.value,
-        materialId: layers.material.material && layers.material.material.value,
-        resolution: 4,
-      };
-      return result;
-    }
-
-    // when layer is impact
-    const result: ImpactH3APIParams = {
-      year: startYear,
-      indicatorId: indicator?.value && indicator?.value !== 'all' ? indicator?.value : null,
-      ...(materials && materials.length
-        ? { materialIds: materials?.map(({ value }) => value) }
-        : {}),
-      ...(suppliers && suppliers.length
-        ? { supplierIds: suppliers?.map(({ value }) => value) }
-        : {}),
-      ...(origins && origins.length ? { originIds: origins?.map(({ value }) => value) } : {}),
-      resolution: 4,
-    };
-
-    return result;
-  },
-);
+import type { ImpactTabularAPIParams } from 'types';
 
 export const filtersForTabularAPI = createSelector(
   [analysisFilters],

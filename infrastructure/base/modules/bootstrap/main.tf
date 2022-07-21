4e61198cc945e96c8e2a4af3300839b03ef85d29
Tiago Garcia <tiagojsag@gmail.com>
2022-07-21 15:22:40 +0200

    Move docker images to ECR

--- infrastructure/base/modules/bootstrap/main.tf
+++ infrastructure/base/modules/bootstrap/main.tf
@@ -6,27 +6,30 @@
 resource "aws_s3_bucket" "state_bucket" {
   bucket = var.s3_bucket
 
-  # Tells AWS to encrypt the S3 bucket at rest by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-
   # Prevents Terraform from destroying or replacing this object - a great safety mechanism
   lifecycle {
     prevent_destroy = true
   }
 
-  # Tells AWS to keep a version history of the state file
-  versioning {
-    enabled = true
-  }
-
   tags = merge({ Resource = "Terraform State" },
   var.tags)
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "state_bucket_encryption" {
+  bucket = aws_s3_bucket.state_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "state_bucket_versioning" {
+  bucket = aws_s3_bucket.state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
 
 # Build a DynamoDB to use for terraform state locking

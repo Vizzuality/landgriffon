5d56b64a43e93df181825a73ccda9653ba70a4d5
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-07-22 13:19:00 +0200

    Apply legend border radius to scrollbar

--- client/src/containers/analysis-visualization/analysis-legend/component.tsx
+++ client/src/containers/analysis-visualization/analysis-legend/component.tsx
@@ -9,6 +9,7 @@ import useContextualLayers from 'hooks/layers/getContextualLayers';
 import ContextualLegendItem from './contextual-legend-item';
 import ImpactLayer from './impact-legend-item';
 import classNames from 'classnames';
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid';
 
 const sortByOrder: (layers: Record<string, Layer>) => Layer[] = (layers) => {
   return Object.values(layers).sort((a, b) => a.order - b.order);
@@ -46,41 +47,41 @@ export const Legend: React.FC = () => {
     [],
   );
 
-  const activeLayerCount = Object.values(layers).filter((layer) => layer.active).length;
-
   return (
     <div className="relative">
       {showLegend && (
         <div className="absolute bottom-0 z-10 flex flex-col flex-grow max-w-xs overflow-hidden bg-white border border-gray-200 rounded-lg shadow-sm right-12 w-80 md:max-h-[75vh]">
-          <div className="flex items-center justify-between px-4 py-2">
-            <div className="text-sm font-semibold text-gray-900">Legend</div>
-            <button
-              type="button"
-              aria-expanded={showContextualLayers}
-              className="text-xs text-green-700"
-              onClick={onToggleActive}
-            >
-              <span>{showContextualLayers ? 'Hide' : 'Show'} contextual layers</span>
-            </button>
-          </div>
-          <Sortable
-            items={orderedLayers.map((layer) => layer.id)}
-            onChangeOrder={(orderedIds) => {
-              orderedIds.forEach((id, i) => {
-                dispatch(setLayer({ id, layer: { order: i } }));
-              });
-            }}
-          >
-            {orderedLayers.map((layer) => (
-              <SortableItem
-                key={layer.id}
-                id={layer.id}
-                className={classNames({ hidden: !showContextualLayers && layer.isContextual })}
+          <div className="overflow-y-auto">
+            <div className="flex items-center justify-between px-4 py-2">
+              <div className="text-sm font-semibold text-gray-900">Legend</div>
+              <button
+                type="button"
+                aria-expanded={showContextualLayers}
+                className="text-xs text-green-700"
+                onClick={onToggleActive}
               >
-                {LegendToShow(layer)}
-              </SortableItem>
-            ))}
-          </Sortable>
+                <span>{showContextualLayers ? 'Hide' : 'Show'} contextual layers</span>
+              </button>
+            </div>
+            <Sortable
+              items={orderedLayers.map((layer) => layer.id)}
+              onChangeOrder={(orderedIds) => {
+                orderedIds.forEach((id, i) => {
+                  dispatch(setLayer({ id, layer: { order: i } }));
+                });
+              }}
+            >
+              {orderedLayers.map((layer) => (
+                <SortableItem
+                  key={layer.id}
+                  id={layer.id}
+                  className={classNames({ hidden: !showContextualLayers && layer.isContextual })}
+                >
+                  {LegendToShow(layer)}
+                </SortableItem>
+              ))}
+            </Sortable>
+          </div>
         </div>
       )}
       <button
@@ -88,26 +89,7 @@ export const Legend: React.FC = () => {
         className="relative flex items-center justify-center w-full h-10 bg-white border border-gray-100 rounded-lg"
         onClick={handleShowLegend}
       >
-        {activeLayerCount !== 0 && (
-          <div className="absolute bottom-0 right-0 w-4 h-4 text-xs font-bold text-white translate-x-1/2 translate-y-1/2 bg-green-700 rounded-full">
-            <span className="relative my-auto h-fit">{activeLayerCount}</span>
-          </div>
-        )}
-        <div className="mt-2">
-          {orderedLayers.map((layer) => (
-            <div
-              key={layer.id}
-              className={classNames(
-                'relative border-[1px] border-white skew-x-[45deg] skew-y-[-20deg] w-[18px] h-[10px] rounded-sm transition-[margin] -mb-[6px] first:-mt-[6px] last:mb-0',
-                {
-                  'bg-gray-900': layer.active,
-                  'bg-gray-500': !layer.active,
-                },
-              )}
-              style={{ zIndex: orderedLayers.length - layer.order }}
-            />
-          ))}
-        </div>
+        <ChevronDoubleRightIcon />
       </button>
     </div>
   );

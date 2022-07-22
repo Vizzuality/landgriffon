e97b1299f34ec7585e0a5021f34d559e63da50fa
mariasola <mariasolahornedo@gmail.com>
2022-07-22 12:44:03 +0200

    remove trase from about

--- marketing-v2/src/containers/about/reason/component.tsx
+++ marketing-v2/src/containers/about/reason/component.tsx
@@ -293,18 +293,6 @@ const Rason: React.FC = () => {
                         <Icon icon={ARROW_TOP_RIGHT_SVG} className="w-3 h-3" />
                       </a>
                     </div>
-
-                    <div className="pl-5">
-                      <a
-                        href="https://www.trase.earth/"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        className="inline-flex text-xl justify-center items-center space-x-2.5 text-orange-500"
-                      >
-                        <span>Discover Trase</span>
-                        <Icon icon={ARROW_TOP_RIGHT_SVG} className="w-3 h-3" />
-                      </a>
-                    </div>
                   </div>
                   <div className="w-full space-y-5">
                     <h4 className="relative font-bold pl-5 before:absolute before:top-2 before:left-0 before:w-1.5 before:h-1.5 before:bg-white">

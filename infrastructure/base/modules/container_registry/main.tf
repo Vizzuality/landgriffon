214513385a1273b4496062ae70c90ff344d61c77
alexeh <alexurola@gmail.com>
2022-07-28 11:58:26 +0200

    Temp. comment out ecr lifecycle to avoid images from deleting from ECR

--- infrastructure/base/modules/container_registry/main.tf
+++ infrastructure/base/modules/container_registry/main.tf
@@ -7,25 +7,27 @@ resource "aws_ecr_repository" "container_registry" {
   }
 }
 
-resource "aws_ecr_lifecycle_policy" "container_registry_policy_keep_last_10" {
-  repository = aws_ecr_repository.container_registry.name
+// TEMPORARILY COMMENTED OUT TO AVOID MULTIPLE DOCKER TAGS GETTING DELETED FROM ECR
 
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep last 10 images",
-            "selection": {
-                "tagStatus": "any",
-                "countType": "imageCountMoreThan",
-                "countNumber": 1
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
+#resource "aws_ecr_lifecycle_policy" "container_registry_policy_keep_last_10" {
+#  repository = aws_ecr_repository.container_registry.name
+#
+#  policy = <<EOF
+#{
+#    "rules": [
+#        {
+#            "rulePriority": 1,
+#            "description": "Keep last 10 images",
+#            "selection": {
+#                "tagStatus": "any",
+#                "countType": "imageCountMoreThan",
+#                "countNumber": 1
+#            },
+#            "action": {
+#                "type": "expire"
+#            }
+#        }
+#    ]
+#}
+#EOF
+#}

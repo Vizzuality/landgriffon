159cf884c0bf8a2cc1ae404fdbf1df39be6c6cd0
David Inga <david.inga@vizzuality.com>
2022-08-01 13:25:09 +0200

    removed constant, moved file size to env variable

--- client/src/constants/file-uploader-size-limits.ts
+++
@@ -1,9 +0,0 @@
-/**
- * See backend counterpart here:
- * https://github.com/Vizzuality/landgriffon/blob/staging/api/config/default.json#L36
- *
- * NOTE: The API defaults to Kilobytes.
- * 'react-dropzone' which we are using in the uploader expects Bytes.
- */
-
-export const FILE_UPLOADER_MAX_SIZE = 8388608;

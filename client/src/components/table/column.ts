24050892a19437aaa3a05418763571c33e254cec
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-08-03 14:33:21 +0200

    Allow sticky columns on left and right side

--- client/src/components/table/column.ts
+++ client/src/components/table/column.ts
@@ -1,23 +1,18 @@
-import type { AccessorFn, ColumnDef, RowData } from '@tanstack/react-table';
+import type { AccessorFn, ColumnDef, ColumnMeta, RowData } from '@tanstack/react-table';
 import type { HTMLAttributes } from 'react';
+import type React from 'react';
 declare module '@tanstack/table-core' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ColumnMeta<TData extends RowData, TValue> {
-    isSticky?: boolean;
+    isSticky?: boolean | 'left' | 'right';
     align?: 'left' | 'right' | 'center';
     title?: string;
     format?: (value: TValue) => React.ReactNode;
   }
 }
 
-export type ColumnDefinition<T, C = T, F = React.ReactNode> = Omit<
-  ColumnDef<T, C>,
-  'accesorKey'
-> & {
+export type ColumnDefinition<T = unknown, C = T, F = C> = Omit<ColumnDef<T, C>, 'accesorKey'> & {
   id: string;
   fieldAccessor?: AccessorFn<T, C>;
-  isSticky?: boolean;
-  align?: 'left' | 'right' | 'center';
-  title?: string;
-  format?: (value: C) => F;
-} & Pick<HTMLAttributes<HTMLDivElement>, 'className'>;
+} & ColumnMeta<T, F> &
+  Pick<HTMLAttributes<HTMLDivElement>, 'className'>;

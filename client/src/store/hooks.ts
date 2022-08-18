3c0bc52214957d4bc6ea0182808a0a3da23620d7
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-08-18 14:43:28 +0200

    Fix redux type issues

--- client/src/store/hooks.ts
+++ client/src/store/hooks.ts
@@ -4,5 +4,5 @@ import type { RootState, AppDispatch } from '.';
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 // https://redux.js.org/recipes/usage-with-typescript#define-typed-hooks
-export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;

2e9b49f4f6268380b6125e0d20365811ce7f6cc5
Andrés González <andres.gonzalez@vizzuality.com>
2022-08-25 13:02:16 +0200

    updates test to include failed creation case

--- client/cypress/e2e/intervention-creation.cy.ts
+++ client/cypress/e2e/intervention-creation.cy.ts
@@ -4,11 +4,6 @@ beforeEach(() => {
     fixture: 'scenario/scenario-creation',
   }).as('scenarioCreation');
 
-  cy.intercept('POST', '/api/v1/scenario-interventions', {
-    statusCode: 201,
-    fixture: 'intervention-creation',
-  }).as('interventionCreation');
-
   cy.intercept('GET', '/api/v1/sourcing-records/years', {
     statusCode: 200,
     fixture: 'scenario/scenario-years',
@@ -43,7 +38,12 @@ afterEach(() => {
 });
 
 describe('Intervention creation', () => {
-  it('a user creates an intervetion – Switch to new material flow', () => {
+  it('a user creates an intervetion – Switch to new material flow (successful creation)', () => {
+    cy.intercept('POST', '/api/v1/scenario-interventions', {
+      statusCode: 201,
+      fixture: 'intervention/intervention-creation-dto',
+    }).as('successfullInterventionCreation');
+
     cy.url().should('contains', '/interventions/new');
 
     // selects a material
@@ -79,18 +79,21 @@ describe('Intervention creation', () => {
     cy.get('[data-testid="UWU_T-input"]').should('have.length', 0);
     cy.get('[data-testid="BL_LUC_T-input-input"]').should('have.length', 0);
 
+    // waits for material request and selects an option
     cy.wait('@scenarioNewMaterials').then(() => {
       const $inputSelect = cy.get('[data-testid="new-material-select"]');
       $inputSelect.click();
       $inputSelect.find('.rc-tree-list').contains('Fruits, berries and nuts').click();
     });
 
+    // waits for scenario location types request and selects an option
     cy.wait('@scenarioLocationTypes').then(() => {
       const $inputSelect = cy.get('[data-testid="new-location-select"]');
       $inputSelect.click();
       $inputSelect.find('input:not(.hidden)').type('Country of production{enter}');
     });
 
+    // waits for scenario location countries request and selects an option
     cy.wait('@scenarioLocationCountries').then(() => {
       const $inputSelect = cy.get('[data-testid="new-location-country-select"]');
       $inputSelect.click();
@@ -100,18 +103,83 @@ describe('Intervention creation', () => {
     // submits intervention
     cy.get('[data-testid="intervention-submit-btn"]').click();
 
-    cy.wait('@interventionCreation').then((interception) => {
-      const {
-        response: { statusCode, body },
-      } = interception;
-
-      expect(statusCode).to.equal(201);
-      expect(body.data.id).to.equal('random-intervention-id');
-
+    cy.wait('@successfullInterventionCreation').then(() => {
       // checks the toast message triggered after intervention creation
       cy.get('.test-toast-message')
         .find('[role="status"]')
         .should('contain', 'Intervention was created successfully');
+    });
+  });
+
+  it('a user creates an intervetion – Switch to new material flow (failed creation)', () => {
+    cy.url().should('contains', '/interventions/new');
+    cy.intercept('POST', '/api/v1/scenario-interventions', {
+      statusCode: 400,
+      fixture: 'intervention/failed-intervention-creation-dto',
+    }).as('failedInterventionCreation');
+
+    // selects a material
+    cy.wait('@scenarioRawMaterials').then(() => {
+      const $inputSelect = cy.get('[data-testid="materials-select"]');
+      $inputSelect.click();
+
+      $inputSelect.find('.rc-tree-list').contains('Cotton').click();
+    });
+
+    // selects a year
+    cy.wait('@scenarioYears').then(() => {
+      const $inputSelect = cy.get('[data-testid="startYear-select"]');
+      $inputSelect.click();
+      $inputSelect.find('input:not(.hidden)').type('2017{enter}');
+    });
+
+    // selects the first intervention type: Switch to new material
+    cy.get('[data-testid="intervention-type-option"]').first().click();
+
+    // check selectors are visible according to the intervention type selected
+    cy.get('[data-testid="new-material-select"]').should('be.visible');
+    cy.get('[data-testid="new-location-select"]').should('be.visible');
+    cy.get('[data-testid="new-location-country-select"]').should('be.visible');
+
+    // supplier options should not be visible by default
+    cy.get('[data-testid="new-t1-supplier-select"]').should('have.length', 0);
+    cy.get('[data-testid="new-producer-select"]').should('have.length', 0);
+
+    // coefficients should not be visible by default
+    cy.get('[data-testid="GHG_LUC_T-input"]').should('have.length', 0);
+    cy.get('[data-testid="DF_LUC_T-input"]').should('have.length', 0);
+    cy.get('[data-testid="UWU_T-input"]').should('have.length', 0);
+    cy.get('[data-testid="BL_LUC_T-input-input"]').should('have.length', 0);
+
+    // waits for material request and selects an option
+    cy.wait('@scenarioNewMaterials').then(() => {
+      const $inputSelect = cy.get('[data-testid="new-material-select"]');
+      $inputSelect.click();
+      $inputSelect.find('.rc-tree-list').contains('Fruits, berries and nuts').click();
+    });
+
+    // waits for scenario location types request and selects an option
+    cy.wait('@scenarioLocationTypes').then(() => {
+      const $inputSelect = cy.get('[data-testid="new-location-select"]');
+      $inputSelect.click();
+      $inputSelect.find('input:not(.hidden)').type('Country of production{enter}');
+    });
+
+    // waits for scenario location countries request and selects an option
+    cy.wait('@scenarioLocationCountries').then(() => {
+      const $inputSelect = cy.get('[data-testid="new-location-country-select"]');
+      $inputSelect.click();
+      $inputSelect.find('input:not(.hidden)').type('Botswana{enter}');
+    });
+
+    // submits intervention
+    cy.get('[data-testid="intervention-submit-btn"]').click();
+
+    cy.wait('@failedInterventionCreation').then(() => {
+      // checks the toast message triggered informing something went wrong
+      cy.get('.test-toast-message')
+        .find('[role="status"]')
+        .should('contain', 'Something went wrong during intervention creation');
     });
   });
 });

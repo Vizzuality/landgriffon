c9b75ead526c4b9df19d34481f6177142adff478
alexeh <alexurola@gmail.com>
2022-08-26 10:25:28 +0200

    Delete Suppliers join for intervention listing

--- api/src/modules/scenario-interventions/scenario-intervention.repository.ts
+++ api/src/modules/scenario-interventions/scenario-intervention.repository.ts
@@ -6,40 +6,44 @@ export class ScenarioInterventionRepository extends Repository<ScenarioIntervent
   async getScenarioInterventionsByScenarioId(
     scenarioId: string,
   ): Promise<ScenarioIntervention[]> {
-    return this.createQueryBuilder('intervention')
-      .select([
-        'intervention.id',
-        'intervention.title',
-        'intervention.description',
-        'intervention.type',
-        'intervention.startYear',
-        'intervention.status',
-        'intervention.endYear',
-        'intervention.percentage',
-        'replacedAdminRegions.id',
-        'replacedAdminRegions.name',
-        'newAdminRegion.id',
-        'newAdminRegion.name',
-        'replacedMaterials.id',
-        'replacedMaterials.name',
-        'newMaterial.id',
-        'newMaterial.name',
-        'replacedBusinessUnits.id',
-        'replacedBusinessUnits.name',
-        'newBusinessUnit.id',
-        'newBusinessUnit.name',
-        'replacedSuppliers.id',
-        'replacedSuppliers.name',
-      ])
-      .leftJoin('intervention.replacedAdminRegions', 'replacedAdminRegions')
-      .leftJoin('intervention.newAdminRegion', 'newAdminRegion')
-      .leftJoin('intervention.replacedMaterials', 'replacedMaterials')
-      .leftJoin('intervention.newMaterial', 'newMaterial')
-      .leftJoin('intervention.replacedBusinessUnits', 'replacedBusinessUnits')
-      .leftJoin('intervention.newBusinessUnit', 'newBusinessUnit')
-      .leftJoin('intervention.replacedSuppliers', 'replacedSuppliers')
+    // TODO: Join with suppliers and selecting supplier field commented out due to performance issues
+    //       This needs to be restored
+    return (
+      this.createQueryBuilder('intervention')
+        .select([
+          'intervention.id',
+          'intervention.title',
+          'intervention.description',
+          'intervention.type',
+          'intervention.startYear',
+          'intervention.status',
+          'intervention.endYear',
+          'intervention.percentage',
+          'replacedAdminRegions.id',
+          'replacedAdminRegions.name',
+          'newAdminRegion.id',
+          'newAdminRegion.name',
+          'replacedMaterials.id',
+          'replacedMaterials.name',
+          'newMaterial.id',
+          'newMaterial.name',
+          'replacedBusinessUnits.id',
+          'replacedBusinessUnits.name',
+          'newBusinessUnit.id',
+          'newBusinessUnit.name',
+          // 'replacedSuppliers.id',
+          // 'replacedSuppliers.name',
+        ])
+        .leftJoin('intervention.replacedAdminRegions', 'replacedAdminRegions')
+        .leftJoin('intervention.newAdminRegion', 'newAdminRegion')
+        .leftJoin('intervention.replacedMaterials', 'replacedMaterials')
+        .leftJoin('intervention.newMaterial', 'newMaterial')
+        .leftJoin('intervention.replacedBusinessUnits', 'replacedBusinessUnits')
+        .leftJoin('intervention.newBusinessUnit', 'newBusinessUnit')
+        // .leftJoin('intervention.replacedSuppliers', 'replacedSuppliers')
 
-      .where('intervention.scenarioId = :scenarioId', { scenarioId })
-      .getMany();
+        .where('intervention.scenarioId = :scenarioId', { scenarioId })
+        .getMany()
+    );
   }
 }

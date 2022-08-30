f5df59f5017a0dcaeda47668508c9917de438276
alexeh <alexurola@gmail.com>
2022-08-30 17:39:45 +0300

    Add single location geocoding to interface

--- api/src/modules/geo-coding/geo-coding-abstract-class.ts
+++ api/src/modules/geo-coding/geo-coding-abstract-class.ts
@@ -1,7 +1,15 @@
 import { SourcingData } from 'modules/import-data/sourcing-data/dto-processor.service';
+import { SourcingLocation } from 'modules/sourcing-locations/sourcing-location.entity';
 
 export abstract class GeoCodingAbstractClass {
   abstract geoCodeLocations(
     sourcingData: SourcingData[],
   ): Promise<SourcingData[]>;
+
+  abstract geoCodeSourcingLocation(locationInfo: {
+    locationAddressInput?: string;
+    locationLatitude?: number;
+    locationLongitude?: number;
+    locationCountryInput: string;
+  }): Promise<SourcingLocation>;
 }

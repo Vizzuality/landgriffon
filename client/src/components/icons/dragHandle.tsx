ecdfd55c85c4868b5f4e37f54ed987f760221114
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-09-06 08:50:48 +0200

    Adjust legend toggle to design

--- client/src/components/icons/dragHandle.tsx
+++ client/src/components/icons/dragHandle.tsx
@@ -1,12 +1,14 @@
-const DragHandle = () => {
+import type { SVGAttributes } from 'react';
+
+const DragHandle = (props: SVGAttributes<SVGSVGElement>) => {
   return (
-    <svg width="6" height="12" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="1" cy="1" r="1" fill="#AEB1B5" />
-      <circle cx="5" cy="1" r="1" fill="#AEB1B5" />
-      <circle cx="1" cy="6" r="1" fill="#AEB1B5" />
-      <circle cx="5" cy="6" r="1" fill="#AEB1B5" />
-      <circle cx="1" cy="11" r="1" fill="#AEB1B5" />
-      <circle cx="5" cy="11" r="1" fill="#AEB1B5" />
+    <svg width="6" height="12" fill="currentColor" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <circle cx="1" cy="1" r="1" />
+      <circle cx="5" cy="1" r="1" />
+      <circle cx="1" cy="6" r="1" />
+      <circle cx="5" cy="6" r="1" />
+      <circle cx="1" cy="11" r="1" />
+      <circle cx="5" cy="11" r="1" />
     </svg>
   );
 };

9683d8d68f467cc2abd454dc527259b4ebfdcdbb
Joaquín Triñanes <joaquin.trinanes@vizzuality.com>
2022-09-14 13:57:23 +0200

    Make select accept a generic value

--- client/src/components/select/component.tsx
+++ client/src/components/select/component.tsx
@@ -1,3 +1,4 @@
+import type { Ref } from 'react';
 import React, { useCallback, useMemo, useState } from 'react';
 import classNames from 'classnames';
 import type {
@@ -37,7 +38,7 @@ const DEFAULT_THEME: Theme = {
   },
 };
 
-const customStyles: (theme: SelectProps['theme'], error?: boolean) => StylesConfig = (
+const customStyles: <T>(theme: SelectProps<T>['theme'], error?: boolean) => StylesConfig = (
   theme,
   error = false,
 ) => {
@@ -90,201 +91,194 @@ const SEARCH_OPTIONS = {
   threshold: 0.4,
 };
 
-const Select: React.FC<SelectProps> = React.forwardRef<HTMLInputElement, SelectProps>(
-  (
-    {
-      instanceId,
-      disabled = false,
-      label,
-      options = [],
-      defaultValue = null,
-      current = null,
-      allowEmpty = false,
-      loading = false,
-      placeholder = null,
-      onChange,
-      onSearch,
-      theme = 'default',
-      error = false,
-      showSearch = false,
-      hideValueWhenMenuOpen = false,
-      numeric = false,
+const Select = <T,>({
+  instanceId,
+  disabled = false,
+  label,
+  options = [],
+  defaultValue = null,
+  current = null,
+  allowEmpty = false,
+  loading = false,
+  placeholder = null,
+  onChange,
+  onSearch,
+  theme = 'default',
+  error = false,
+  showSearch = false,
+  hideValueWhenMenuOpen = false,
+  numeric = false,
+  ref,
+}: SelectProps<T> & { ref?: Ref<HTMLInputElement> }) => {
+  const { result: optionsResult, search: setSearchTerm } = useFuse(options, SEARCH_OPTIONS);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleSearch = useCallback(
+    (term: string) => {
+      setSearchTerm(term);
+      onSearch?.(term);
     },
-    ref,
-  ) => {
-    const { result: optionsResult, search: setSearchTerm } = useFuse(options, SEARCH_OPTIONS);
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    [onSearch, setSearchTerm],
+  );
 
-    const handleSearch = useCallback(
-      (term: string) => {
-        setSearchTerm(term);
-        onSearch?.(term);
-      },
-      [onSearch, setSearchTerm],
-    );
+  const handleChange = useCallback(
+    (option) => {
+      onChange?.(option);
+    },
+    [onChange],
+  );
 
-    const handleChange = useCallback(
-      (option) => {
-        onChange?.(option);
-      },
-      [onChange],
-    );
+  const {
+    floating,
+    reference,
+    x,
+    y,
+    strategy,
+    refs: { reference: referenceElement },
+  } = useFloating({
+    whileElementsMounted: autoUpdate,
+    placement: 'bottom-start',
+    strategy: 'fixed',
+    middleware: [offset({ mainAxis: 4 }), flip(), shift()],
+  });
 
-    const {
-      floating,
-      reference,
-      x,
-      y,
-      strategy,
-      refs: { reference: referenceElement },
-    } = useFloating({
-      whileElementsMounted: autoUpdate,
-      placement: 'bottom-start',
-      strategy: 'fixed',
-      middleware: [offset({ mainAxis: 4 }), flip(), shift()],
-    });
+  const Menu: React.FC<MenuProps> = useCallback(
+    ({ children, ...rest }) => (
+      <div
+        ref={floating}
+        className="z-20"
+        style={{
+          position: strategy,
+          top: y ?? '',
+          left: x ?? '',
+          minWidth:
+            theme === 'inline-primary'
+              ? '300px'
+              : (referenceElement.current as HTMLElement)?.offsetWidth,
+        }}
+      >
+        <components.Menu {...rest}>{children}</components.Menu>
+      </div>
+    ),
+    [floating, referenceElement, strategy, theme, x, y],
+  );
 
-    const Menu: React.FC<MenuProps> = useCallback(
-      ({ children, ...rest }) => (
-        <div
-          ref={floating}
-          className="z-20"
-          style={{
-            position: strategy,
-            top: y ?? '',
-            left: x ?? '',
-            minWidth:
-              theme === 'inline-primary'
-                ? '300px'
-                : (referenceElement.current as HTMLElement)?.offsetWidth,
-          }}
-        >
-          <components.Menu {...rest}>{children}</components.Menu>
-        </div>
-      ),
-      [floating, referenceElement, strategy, theme, x, y],
-    );
-
-    const Control: React.FC<ControlProps> = useCallback(
-      ({ children, ...rest }) => (
-        <div ref={reference}>
-          <components.Control {...rest}>
-            {hideValueWhenMenuOpen && !rest.menuIsOpen && label && (
-              <div className="text-gray-600">{label}</div>
-            )}
-            {children}
-          </components.Control>
-          {theme === 'inline-primary' && (
-            <div className="mt-0.5 border-t-primary border-t-4 border-x-4 border-x-transparent mx-auto w-0 h-0" />
+  const Control: React.FC<ControlProps> = useCallback(
+    ({ children, ...rest }) => (
+      <div ref={reference}>
+        <components.Control {...rest}>
+          {hideValueWhenMenuOpen && !rest.menuIsOpen && label && (
+            <div className="text-gray-600">{label}</div>
           )}
-        </div>
-      ),
-      [hideValueWhenMenuOpen, label, reference, theme],
-    );
+          {children}
+        </components.Control>
+        {theme === 'inline-primary' && (
+          <div className="mt-0.5 border-t-primary border-t-4 border-x-4 border-x-transparent mx-auto w-0 h-0" />
+        )}
+      </div>
+    ),
+    [hideValueWhenMenuOpen, label, reference, theme],
+  );
 
-    const Input: React.FC<InputProps> = useCallback(
-      ({ children, onChange, ...rest }) => {
-        return (
-          <components.Input
-            {...rest}
-            onChange={(e) => {
-              if (numeric && !/^[0-9]*$/.test(e.currentTarget.value)) return;
-              onChange?.(e);
-            }}
-            inputClassName="ring-0 focus:ring-0 outline-none focus:outline-none truncate"
-          >
-            {children}
-          </components.Input>
-        );
-      },
-      [numeric],
-    );
+  const Input: React.FC<InputProps> = useCallback(
+    ({ children, onChange, ...rest }) => {
+      return (
+        <components.Input
+          {...rest}
+          onChange={(e) => {
+            if (numeric && !/^[0-9]*$/.test(e.currentTarget.value)) return;
+            onChange?.(e);
+          }}
+          inputClassName="ring-0 focus:ring-0 outline-none focus:outline-none truncate"
+        >
+          {children}
+        </components.Input>
+      );
+    },
+    [numeric],
+  );
 
-    const Option: React.FC<OptionProps<SelectOption>> = useCallback(
-      ({ innerProps, innerRef, isSelected, isFocused, isDisabled, data: { label, extraInfo } }) => (
-        <div {...innerProps} ref={innerRef}>
-          <div
-            className={classNames(
-              isFocused ? ' bg-green-50 text-primary' : 'text-gray-900',
-              isSelected && 'bg-green-50 text-primary',
-              'cursor-pointer select-none relative py-2 px-4',
-              isDisabled && 'text-opacity-50 cursor-default',
-            )}
-          >
-            <div className="flex flex-row gap-x-2">
-              <div
-                className={classNames(
-                  isSelected ? 'font-semibold' : 'font-normal',
-                  'block truncate',
-                )}
-              >
-                {label}
-              </div>
-              {extraInfo && (
-                <div>
-                  <span className="text-xs italic text-gray-600">{extraInfo}</span>
-                </div>
-              )}
+  const Option: React.FC<OptionProps<SelectOption<T>>> = useCallback(
+    ({ innerProps, innerRef, isSelected, isFocused, isDisabled, data: { label, extraInfo } }) => (
+      <div {...innerProps} ref={innerRef}>
+        <div
+          className={classNames(
+            isFocused ? ' bg-green-50 text-primary' : 'text-gray-900',
+            isSelected && 'bg-green-50 text-primary',
+            'cursor-pointer select-none relative py-2 px-4',
+            isDisabled && 'text-opacity-50 cursor-default',
+          )}
+        >
+          <div className="flex flex-row gap-x-2">
+            <div
+              className={classNames(isSelected ? 'font-semibold' : 'font-normal', 'block truncate')}
+            >
+              {label}
             </div>
+            {extraInfo && (
+              <div>
+                <span className="text-xs italic text-gray-600">{extraInfo}</span>
+              </div>
+            )}
           </div>
         </div>
-      ),
-      [],
-    );
-
-    const styles = useMemo(() => customStyles(theme, error), [error, theme]);
-
-    return (
-      <div className={classNames({ 'w-fit': theme === 'inline-primary' })}>
-        <input ref={ref} className="hidden" />
-        <ReactSelect
-          instanceId={instanceId}
-          defaultValue={defaultValue}
-          onMenuOpen={() => setIsMenuOpen(true)}
-          onMenuClose={() => setIsMenuOpen(false)}
-          styles={styles}
-          placeholder={placeholder}
-          onInputChange={handleSearch}
-          onChange={handleChange}
-          options={optionsResult.map(({ disabled, ...option }) => ({
-            ...option,
-            isDisabled: disabled,
-          }))}
-          value={hideValueWhenMenuOpen && isMenuOpen ? null : current}
-          isDisabled={disabled}
-          isLoading={loading}
-          isClearable={allowEmpty}
-          isSearchable={showSearch}
-          noOptionsMessage={() => <div className="p-2">No results</div>}
-          theme={
-            error
-              ? { ...DEFAULT_THEME, colors: { ...DEFAULT_THEME.colors, primary25: 'red' } }
-              : DEFAULT_THEME
-          }
-          components={{
-            Option,
-            IndicatorSeparator: null,
-            LoadingIndicator: () => <Loading className="text-primary" />,
-            DropdownIndicator:
-              theme === 'default' || theme === 'default-bordernone'
-                ? ({ selectProps: { menuIsOpen } }) => (
-                    <ChevronDownIcon
-                      className={classNames('h-4 w-4 text-gray-900', {
-                        'rotate-180': menuIsOpen,
-                        'stroke-red-700': error,
-                      })}
-                    />
-                  )
-                : null,
-            Menu,
-            Control,
-            Input,
-          }}
-        />
       </div>
-    );
-  },
-);
+    ),
+    [],
+  );
+
+  const styles = useMemo(() => customStyles(theme, error), [error, theme]);
+
+  return (
+    <div className={classNames({ 'w-fit': theme === 'inline-primary' })}>
+      <input ref={ref} className="hidden" />
+      <ReactSelect
+        instanceId={instanceId}
+        defaultValue={defaultValue}
+        onMenuOpen={() => setIsMenuOpen(true)}
+        onMenuClose={() => setIsMenuOpen(false)}
+        styles={styles}
+        placeholder={placeholder}
+        onInputChange={handleSearch}
+        onChange={handleChange}
+        options={optionsResult.map(({ disabled, ...option }) => ({
+          ...option,
+          isDisabled: disabled,
+        }))}
+        value={hideValueWhenMenuOpen && isMenuOpen ? null : current}
+        isDisabled={disabled}
+        isLoading={loading}
+        isClearable={allowEmpty}
+        isSearchable={showSearch}
+        noOptionsMessage={() => <div className="p-2">No results</div>}
+        theme={
+          error
+            ? { ...DEFAULT_THEME, colors: { ...DEFAULT_THEME.colors, primary25: 'red' } }
+            : DEFAULT_THEME
+        }
+        components={{
+          Option,
+          IndicatorSeparator: null,
+          LoadingIndicator: () => <Loading className="text-primary" />,
+          DropdownIndicator:
+            theme === 'default' || theme === 'default-bordernone'
+              ? ({ selectProps: { menuIsOpen } }) => (
+                  <ChevronDownIcon
+                    className={classNames('h-4 w-4 text-gray-900', {
+                      'rotate-180': menuIsOpen,
+                      'stroke-red-700': error,
+                    })}
+                  />
+                )
+              : null,
+          Menu,
+          Control,
+          Input,
+        }}
+      />
+    </div>
+  );
+};
 
 Select.displayName = 'Select';
 

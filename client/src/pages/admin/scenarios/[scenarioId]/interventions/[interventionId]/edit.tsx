4d16413aec391816741e31f7dab75afe1b17b2a9
Andrés González <andres.gonzalez@vizzuality.com>
2022-09-15 16:20:33 +0200

    disables intervention patch request temporary

--- client/src/pages/admin/scenarios/[scenarioId]/interventions/[interventionId]/edit.tsx
+++ client/src/pages/admin/scenarios/[scenarioId]/interventions/[interventionId]/edit.tsx
@@ -2,17 +2,18 @@ import { useCallback } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import toast from 'react-hot-toast';
+// import toast from 'react-hot-toast';
 
 import { useIntervention, useUpdateIntervention } from 'hooks/interventions';
-import { parseInterventionFormDataToDto } from 'containers/interventions/utils';
+// import { parseInterventionFormDataToDto } from 'containers/interventions/utils';
+// import type { InterventionFormData } from 'containers/interventions/types';
 
 import CleanLayout from 'layouts/clean';
 import InterventionForm from 'containers/interventions/form';
 import BackLink from 'components/back-link/component';
 import Loading from 'components/loading';
 
-import type { ErrorResponse } from 'types';
+// import type { ErrorResponse } from 'types';
 
 const EditInterventionPage: React.FC = () => {
   const router = useRouter();
@@ -39,33 +40,37 @@ const EditInterventionPage: React.FC = () => {
   const editIntervention = useUpdateIntervention();
 
   const handleSubmit = useCallback(
-    (interventionFormData) => {
-      const interventionDto = parseInterventionFormDataToDto(interventionFormData);
-
-      editIntervention.mutate(
-        { id: data.id, data: interventionDto },
-        {
-          onSuccess: () => {
-            toast.success(`Intervention edited successfully`);
-            // adding some delay to make sure the user reads the success message
-            setTimeout(() => {
-              router.replace(`/admin/scenarios/${interventionDto.scenarioId}/edit`);
-            }, 1000);
-          },
-          onError: (error: ErrorResponse) => {
-            const { errors } = error.response?.data;
-            errors.forEach(({ meta }) => toast.error(meta.rawError.response.message));
-          },
-        },
-      );
-    },
-    [editIntervention, router, data],
+    () =>
+      // interventionFormData: InterventionFormData
+      {
+        //! API is not ready to update an intervention yet. Uncomment the following lines when API is ready.
+        // const interventionDto = parseInterventionFormDataToDto(interventionFormData);
+        // editIntervention.mutate(
+        //   { id: data.id, data: interventionDto },
+        //   {
+        //     onSuccess: () => {
+        //       toast.success(`Intervention edited successfully`);
+        //       // adding some delay to make sure the user reads the success message
+        //       setTimeout(() => {
+        //         router.replace(`/admin/scenarios/${interventionDto.scenarioId}/edit`);
+        //       }, 1000);
+        //     },
+        //     onError: (error: ErrorResponse) => {
+        //       const { errors } = error.response?.data;
+        //       errors.forEach(({ meta }) => toast.error(meta.rawError.response.message));
+        //     },
+        //   },
+        // );
+      },
+    [
+      // editIntervention, router, data
+    ],
   );
 
   return (
     <CleanLayout>
       <Head>
-        <title>Edit of a intervention | Landgriffon</title>
+        <title>Edit of an intervention | Landgriffon</title>
       </Head>
       <Link href={`/admin/scenarios/${query.scenarioId}/edit`} passHref>
         <BackLink className="flex mb-6 xl:sticky xl:top-0">Back to scenario</BackLink>

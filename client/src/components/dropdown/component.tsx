9f49ba18140baabc837c0efc1e695801abf44c78
Andrés González <andres.gonzalez@vizzuality.com>
2022-09-15 18:48:42 +0200

    fixes dropdown z-index

--- client/src/components/dropdown/component.tsx
+++ client/src/components/dropdown/component.tsx
@@ -1,7 +1,7 @@
 import { Menu } from '@headlessui/react';
 
 export const Dropdown: React.FC<React.PropsWithChildren> = ({ children, ...props }) => (
-  <Menu as="div" className="relative inline-block text-left" {...props}>
+  <Menu as="div" className="relative inline-block p-2 text-left" {...props}>
     {children}
   </Menu>
 );
@@ -12,7 +12,7 @@ export const Button: React.FC<React.PropsWithChildren> = ({ children, ...props }
 
 export const Items: React.FC<React.PropsWithChildren> = ({ children, ...props }) => (
   <Menu.Items
-    className="absolute right-0 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+    className="absolute right-0 z-50 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
     {...props}
   >
     {children}

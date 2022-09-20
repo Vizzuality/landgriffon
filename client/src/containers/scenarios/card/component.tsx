06e889ae000537b22f8228a7dbb561a3c5054ae9
David Inga <david.inga@vizzuality.com>
2022-09-20 13:22:00 +0200

    changelog and scenarios redesign

--- client/src/containers/scenarios/card/component.tsx
+++ client/src/containers/scenarios/card/component.tsx
@@ -2,11 +2,12 @@ import { useCallback } from 'react';
 import Link from 'next/link';
 import { format } from 'date-fns';
 import toast from 'react-hot-toast';
+import { TrashIcon } from '@heroicons/react/outline';
+import { Popover } from '@headlessui/react';
 
 import { useDeleteScenario } from 'hooks/scenarios';
 import { useScenarioInterventions } from 'hooks/interventions';
 
-import InterventionPhrase from 'containers/interventions/phrase';
 import Loading from 'components/loading';
 import { AnchorLink, Button } from 'components/button';
 
@@ -31,9 +32,19 @@ const ScenarioCard: React.FC<ScenarioCardProps> = ({ data }) => {
   }, [data?.id, deleteScenario]);
 
   return (
-    <div className="p-6 space-y-6 bg-white rounded-md shadow-sm" data-testid="scenario-card">
-      <h2 className="text-lg">{data.title}</h2>
-      {data.description && <p className="text-xs leading-5 text-gray-500">{data.description}</p>}
+    <div
+      className="flex flex-col p-6 space-y-6 bg-white rounded-md shadow-sm"
+      data-testid="scenario-card"
+    >
+      <div className="text-xs leading-4 text-gray-400">
+        Modified: {format(new Date(data.updatedAt), 'yyyy/MM/dd')}
+      </div>
+      <div>
+        <h2 className="text-lg">{data.title}</h2>
+        {data.description && (
+          <p className="mt-4 text-xs leading-5 text-gray-500">{data.description}</p>
+        )}
+      </div>
       {/* TO-DO: Fix growth rate of 1.5% meanwhile is implemented in the API */}
       <div>
         <h3 className="text-xs">Growth rates</h3>
@@ -41,28 +52,51 @@ const ScenarioCard: React.FC<ScenarioCardProps> = ({ data }) => {
           <div className="rounded-full bg-blue py-0.5 px-3 text-xs">Entire company +1.5%/yr</div>
         </div>
       </div>
-      <div>
+      <div className="flex-1">
         <h3 className="text-xs">Interventions</h3>
         {isInterventionsLoading && <Loading />}
-        {!isInterventionsLoading &&
-          interventions &&
-          interventions.map((intervention) => (
-            <div
-              className="flex mt-2 space-x-4"
-              key={intervention.id}
-              data-testid="scenario-interventions-item"
-            >
-              <div className="rounded-full bg-yellow py-0.5 px-3 text-xs">
-                <InterventionPhrase intervention={intervention} short />
-              </div>
+        {!isInterventionsLoading && interventions && (
+          <div>
+            <div className="flex flex-wrap" data-testid="scenario-interventions-item">
+              {interventions.slice(0, 2).map((intervention) => (
+                <div
+                  key={intervention.id}
+                  className="mr-2 mb-2 rounded-full bg-yellow py-0.5 px-3 text-xs"
+                >
+                  {intervention.title}
+                </div>
+              ))}
             </div>
-          ))}
+            {interventions.length > 2 && (
+              <Popover className="relative text-xs">
+                Show{' '}
+                <Popover.Button className="mr-2 mb-2 rounded-full bg-yellow py-0.5 px-3 text-xs">
+                  +{interventions.length - 2} more
+                </Popover.Button>
+                <Popover.Panel className="absolute p-4 bg-white border border-gray-100 rounded-md shadow-md">
+                  {interventions.slice(2, interventions.length).map((intervention) => (
+                    <div
+                      key={intervention.id}
+                      className="mr-2 mb-2 last:mb-0 rounded-full bg-yellow py-0.5 px-3 text-xs"
+                    >
+                      {intervention.title}
+                    </div>
+                  ))}
+                </Popover.Panel>
+              </Popover>
+            )}
+          </div>
+        )}
         {!isInterventionsLoading && interventions.length === 0 && (
           <p className="text-xs">No interventions created for this scenario.</p>
         )}
       </div>
       <div className="flex space-between">
-        <Button theme="secondary" onClick={handleDeleteScenario}>
+        <Button
+          theme="secondary"
+          onClick={handleDeleteScenario}
+          icon={<TrashIcon className="w-4 h-4 text-gray-500" />}
+        >
           Delete
         </Button>
         <div className="flex items-center justify-end flex-1 gap-4">

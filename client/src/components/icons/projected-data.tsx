0c5e29cab160849026dc92b447c2bf08024f46f8
David Inga <david.inga@vizzuality.com>
2022-09-20 12:31:20 +0200

    added projected pattern to the chart

--- client/src/components/icons/projected-data.tsx
+++ client/src/components/icons/projected-data.tsx
@@ -6,6 +6,7 @@ function ProjectedDataIcon(props) {
       viewBox="0 0 12 12"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      id="projected-data-icon"
       {...props}
     >
       <mask
--- client/src/containers/analysis-chart/component.tsx
+++ client/src/containers/analysis-chart/component.tsx
@@ -1,53 +1,12 @@
-import { useStore } from 'react-redux';
-import omit from 'lodash/omit';
-
-import { filtersForTabularAPI } from 'store/features/analysis/selector';
-import { useImpactRanking } from 'hooks/impact';
-
-import Loading from 'components/loading';
 import ImpactChart from './impact-chart';
-
-import type { Store } from 'store';
 import type { Indicator } from 'types';
 
 type AnalysisChartProps = {
   indicator: Indicator;
 };
 
-const AnalysisChart: React.FC<AnalysisChartProps> = ({ indicator }) => {
-  const store: Store = useStore();
-  const filters = filtersForTabularAPI(store.getState());
-
-  const params = {
-    maxRankingEntities: 5,
-    sort: 'DES',
-    ...omit(filters, 'indicatorId'),
-    indicatorIds: [indicator.id],
-  };
-
-  const enabled =
-    !!filters?.indicatorId &&
-    !!filters.startYear &&
-    !!filters.endYear &&
-    filters.endYear !== filters.startYear;
-
-  const { data, isFetched, isLoading } = useImpactRanking(
-    {
-      maxRankingEntities: 5,
-      sort: 'DES',
-      ...params,
-    },
-    {
-      enabled,
-    },
-  );
-
-  return (
-    <div className="p-6 bg-white rounded-md shadow-sm">
-      {isLoading && <Loading className="w-5 h-5 m-auto text-primary" />}
-      {!isLoading && isFetched && data && <ImpactChart data={data} />}
-    </div>
-  );
-};
+const AnalysisChart: React.FC<AnalysisChartProps> = ({ indicator }) => (
+  <ImpactChart indicator={indicator} />
+);
 
 export default AnalysisChart;

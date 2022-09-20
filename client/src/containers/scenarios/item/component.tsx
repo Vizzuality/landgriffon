1819b27bcafcd5c04de22fa263edad3622818861
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-09-20 11:43:05 +0200

    Enable scenario selection on map screen

--- client/src/containers/scenarios/item/component.tsx
+++ client/src/containers/scenarios/item/component.tsx
@@ -12,7 +12,6 @@ import {
   scenarios,
   setComparisonEnabled,
 } from 'store/features/analysis/scenarios';
-import { analysisUI } from 'store/features/analysis/ui';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
 
 import ScenariosComparison from 'containers/scenarios/comparison';
@@ -23,35 +22,32 @@ import type { ErrorResponse } from 'types';
 import type { Scenario } from '../types';
 import { offset, useFloating } from '@floating-ui/react-dom';
 
-type ScenariosItemProps = {
-  data: Scenario;
+interface ScenariosItemProps {
+  scenario: Scenario;
   isComparisonAvailable: boolean;
   isSelected: boolean;
-};
+}
 
 const DROPDOWN_BUTTON_CLASSNAME =
   'w-8 h-8 inline-flex items-center justify-center text-gray-900 rounded-full bg-transparent hover:text-green-800';
 const DROPDOWN_ITEM_CLASSNAME =
   'block px-4 py-2 text-sm w-full text-left hover:bg-gray-100 hover:text-gray-900';
 
-const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) => {
+const ScenarioItem = ({ scenario, isSelected, isComparisonAvailable }: ScenariosItemProps) => {
   const dispatch = useAppDispatch();
   const { currentScenario, isComparisonEnabled } = useAppSelector(scenarios);
-  const { visualizationMode } = useAppSelector(analysisUI);
 
   const { x, y, reference, floating, strategy } = useFloating({
     placement: 'right',
     middleware: [offset({ mainAxis: 10 })],
   });
 
-  const { data, isSelected, isComparisonAvailable } = props;
-
   const deleteScenario = useDeleteScenario();
 
   const handleDelete = useCallback(() => {
-    deleteScenario.mutate(data.id, {
+    deleteScenario.mutate(scenario.id, {
       onSuccess: () => {
-        if (currentScenario === data.id) dispatch(setCurrentScenario(ACTUAL_DATA.id));
+        if (currentScenario === scenario.id) dispatch(setCurrentScenario(ACTUAL_DATA.id));
         toast.success('Scenario successfully deleted.');
       },
       onError: (error: ErrorResponse) => {
@@ -59,7 +55,7 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
         errors.forEach(({ title }) => toast.error(title));
       },
     });
-  }, [deleteScenario, data.id, currentScenario, dispatch]);
+  }, [deleteScenario, scenario.id, currentScenario, dispatch]);
 
   // const handleShare = useCallback(() => {
   //   console.log('published scenarios');
@@ -69,10 +65,6 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
     // Disabling comparison when is not selected
     if (!isSelected) dispatch(setComparisonEnabled(false));
   }, [dispatch, isSelected]);
-
-  // TO - DO remove this condition when scenarios analysis is available to show in the ma
-  // Option to select a different scenario for map view disabled temporarily
-  const disabledScenarios = visualizationMode === 'map';
 
   return (
     <li className="col-span-1 last-of-type:mb-6">
@@ -84,16 +76,15 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
       >
         <div className="flex items-center">
           <RadioGroup.Option
-            disabled={disabledScenarios}
-            key={data.id}
-            value={data}
+            disabled={!isComparisonAvailable}
+            key={scenario.id}
+            value={scenario}
             className="flex justify-between flex-1 truncate items-top"
           >
             {({ checked }) => (
               <>
                 <div className="flex justify-center flex-shrink-0 w-10 py-4 items-top">
-                  {/* TO DO - remove this condition when scenarios analysis is available to show in the map*/}
-                  {((disabledScenarios && data.id === ACTUAL_DATA.id) || !disabledScenarios) && (
+                  {(scenario.id === ACTUAL_DATA.id || isComparisonAvailable) && (
                     <span
                       className={classNames(
                         checked ? 'bg-primary border-transparent' : 'bg-white border-gray-200',
@@ -112,22 +103,22 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
                       isSelected ? 'text-primary' : 'text-gray-900',
                     )}
                   >
-                    {data.title}
+                    {scenario.title}
                   </h2>
-                  {data.id !== ACTUAL_DATA.id && (
+                  {scenario.id !== ACTUAL_DATA.id && (
                     <div className="flex">
                       <div className="bg-yellow text-xs px-2 rounded-full">
-                        {data.scenarioInterventions.length} interventions
+                        {scenario.scenarioInterventions.length} interventions
                       </div>
                     </div>
                   )}
                   <div>
-                    {data.id === ACTUAL_DATA.id && (
+                    {scenario.id === ACTUAL_DATA.id && (
                       <span className="text-sm text-gray-500">Based on your uploaded data</span>
                     )}
-                    {data.id !== ACTUAL_DATA.id && data.updatedAt && (
+                    {scenario.id !== ACTUAL_DATA.id && scenario.updatedAt && (
                       <span className="text-gray-400 text-xs">{`Last edited ${format(
-                        new Date(data.updatedAt),
+                        new Date(scenario.updatedAt),
                         'yyyy/MM/dd',
                       )}`}</span>
                     )}
@@ -136,7 +127,7 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
               </>
             )}
           </RadioGroup.Option>
-          {data.id !== ACTUAL_DATA.id && (
+          {scenario.id !== ACTUAL_DATA.id && (
             <div className="flex-shrink-0 pr-2">
               <Popover as="div" className="relative inline-block text-left">
                 {({ open }) => (
@@ -172,7 +163,7 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
                           >
                             <div className="py-1">
                               <div>
-                                <Link href={`/admin/scenarios/${data.id}/edit`}>
+                                <Link href={`/admin/scenarios/${scenario.id}/edit`}>
                                   <a
                                     className={classNames('text-gray-700', DROPDOWN_ITEM_CLASSNAME)}
                                   >
@@ -209,7 +200,7 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
             </div>
           )}
         </div>
-        {data.id === ACTUAL_DATA.id && isComparisonAvailable && (
+        {isComparisonAvailable && (
           <div className="p-4 border-t border-primary">
             <div className="flex justify-between">
               <label className="block text-sm">Compare with</label>
@@ -247,4 +238,4 @@ const ScenariosList: React.FC<ScenariosItemProps> = (props: ScenariosItemProps) 
   );
 };
 
-export default ScenariosList;
+export default ScenarioItem;

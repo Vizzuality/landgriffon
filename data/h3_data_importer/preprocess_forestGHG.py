f44cf677eb9e8d83a55a171d39e6146b42622a91
Biel Stela <biel.stela@gmail.com>
2022-09-26 13:40:35 +0200

    Fix no data handling

--- data/h3_data_importer/preprocess_forestGHG.py
+++ data/h3_data_importer/preprocess_forestGHG.py
@@ -1,8 +1,8 @@
 import argparse
 import logging
 
+import numpy as np
 import rasterio as rio
-from tqdm import tqdm
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("preprocess_forestGHG")
@@ -11,25 +11,26 @@ log = logging.getLogger("preprocess_forestGHG")
 def main(ghg_file: str, hansen_file: str, outfile: str):
     with rio.open(ghg_file) as ref:
         with rio.open(hansen_file) as hansen_src:
-            dest_profile = ref.profile.copy()
-            with rio.open(outfile, "w", **dest_profile) as dest:
-                windows = list(ref.block_windows(1))
-                log.info(f"Filtering {ghg_file}...")
-                for ij, win in tqdm(windows):
-                    # sanity check that window transform is the same
-                    ref_window_transfrom = ref.window_transform(win)
-                    if ref_window_transfrom != hansen_src.window_transform(win):
-                        log.error(
-                            f"{ghg_file} and {hansen_file} don't have the same Transform in window {ij}."
-                            f" Can't compute masking."
-                        )
-                        raise ValueError("Rasters have different transform")
+            if ref.transform != hansen_src.transform:
+                log.error(
+                    f"{ghg_file} and {hansen_file} don't have the same Transform in window {ij}."
+                    f" Can't compute masking."
+                )
+                raise ValueError("Rasters have different transform")
 
+            dest_profile = ref.profile.copy()
+            dest_profile["nodata"] = 0
+            with rio.open(outfile, "w", **dest_profile) as dest:
+                log.info(f"Filtering {ghg_file} and writing to {outfile}...")
+                for ij, win in ref.block_windows(1):
+                    # sanity check that window transform is the same
                     ghg = ref.read(1, window=win)
                     hansen = hansen_src.read(1, window=win)
                     result = ghg * hansen
-
+                    # convert nan to 0 to avoid having both nan and 0 coexisting as nodata
+                    result = np.nan_to_num(result)
                     dest.write(result, window=win, indexes=1)
+                log.info(f"Done wrinting to {outfile}...")
 
 
 if __name__ == "__main__":

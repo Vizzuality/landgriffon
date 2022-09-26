8211e9e0a7c3fcd84b8c35afbb8e5534c7964d4f
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-09-26 16:03:06 +0200

    Add close button to modal

--- client/src/components/modal/component.tsx
+++ client/src/components/modal/component.tsx
@@ -13,11 +13,12 @@ import type { ModalProps } from './types';
 export const Modal: React.FC<ModalProps> = ({
   title,
   open,
-  dismissable = true,
-  size = 'fit',
   children,
   className,
   onDismiss,
+  theme = 'default',
+  dismissable = true,
+  size = 'fit',
 }) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const { overlayProps } = useOverlay(
@@ -54,7 +55,7 @@ export const Modal: React.FC<ModalProps> = ({
                 delay: 0.125,
               },
             }}
-            className={classNames(OVERLAY_CLASSES)}
+            className={OVERLAY_CLASSES}
           >
             <FocusScope contain restoreFocus autoFocus>
               <div {...overlayProps} {...dialogProps} {...modalProps} ref={containerRef}>
@@ -80,21 +81,36 @@ export const Modal: React.FC<ModalProps> = ({
                       delay: 0,
                     },
                   }}
-                  className={classNames('max-h-[90%]', CONTENT_CLASSES[size], className)}
+                  className={classNames(
+                    'max-h-[90%]',
+                    { 'p-6': theme !== 'minimal' },
+                    CONTENT_CLASSES[size],
+                    className,
+                  )}
                 >
-                  {dismissable && (
-                    <div className="relative flex items-center mb-4">
-                      <div className="text-lg font-medium">{title}</div>
-                      <button
-                        type="button"
-                        onClick={onDismiss}
-                        className="absolute flex items-center px-4 py-4 text-sm text-gray-300 rounded-md -right-4 -top-4 focus:text-black hover:text-black"
-                      >
-                        <XIcon className="w-6 h-6 text-gray-500" />
-                      </button>
+                  <div className="overflow-hidden rounded-md bg-white">
+                    {dismissable && theme === 'default' && (
+                      <div className="relative flex items-center mb-4">
+                        <div className="text-lg font-medium">{title}</div>
+                        <button
+                          type="button"
+                          onClick={onDismiss}
+                          className="absolute flex items-center px-4 py-4 text-sm text-gray-300 rounded-md -right-4 -top-4 focus:text-black hover:text-black"
+                        >
+                          <XIcon className="w-6 h-6 text-gray-500" />
+                        </button>
+                      </div>
+                    )}
+                    {children}
+                  </div>
+                  {theme === 'minimal' && dismissable && (
+                    <div
+                      onClick={onDismiss}
+                      className="absolute cursor-pointer right-0 top-4 translate-x-1/2 p-6 bg-white rounded-full"
+                    >
+                      <XIcon className="w-4 h-4" />
                     </div>
                   )}
-                  {children}
                 </motion.div>
               </div>
             </FocusScope>

ca473649952b6ed260881c23ece166ac72525318
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-09-27 11:05:01 +0200

    Correctly set current material in settings modal

--- client/src/containers/analysis-visualization/analysis-legend/material-legend-item/component.tsx
+++ client/src/containers/analysis-visualization/analysis-legend/material-legend-item/component.tsx
@@ -12,7 +12,7 @@ import { useH3MaterialData } from 'hooks/h3-data';
 import { NUMBER_FORMAT } from 'utils/number-format';
 import { COLOR_RAMPS } from 'utils/colors';
 import Materials from 'containers/analysis-visualization/analysis-filters/materials/component';
-import { useMaterial, useMaterials } from 'hooks/materials';
+import { useMaterial } from 'hooks/materials';
 import { setFilter } from 'store/features/analysis/filters';
 import type { TreeSelectOption } from 'components/tree-select/types';
 

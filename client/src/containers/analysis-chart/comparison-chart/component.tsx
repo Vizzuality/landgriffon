eabf5d1fd283b8f5fb316e87902fd1c9819e5d50
David Inga <david.inga@vizzuality.com>
2022-09-28 16:47:53 +0200

    added line chart and comparison endpoint

--- client/src/containers/analysis-chart/comparison-chart/component.tsx
+++ client/src/containers/analysis-chart/comparison-chart/component.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useMemo } from 'react';
-import chroma from 'chroma-js';
+import { useStore } from 'react-redux';
+import omit from 'lodash/omit';
 import {
   LineChart,
   Line,
@@ -11,50 +12,61 @@ import {
   ResponsiveContainer,
 } from 'recharts';
 
+import { useAppSelector } from 'store/hooks';
+import { scenarios } from 'store/features/analysis/scenarios';
+import { filtersForTabularAPI } from 'store/features/analysis/selector';
+import { useImpactComparison } from 'hooks/impact/comparison';
+
+import Loading from 'components/loading';
 import { NUMBER_FORMAT } from 'utils/number-format';
 import LegendChart from './legend';
 
-import type { ImpactRanking } from 'types';
+import type { Store } from 'store';
+import type { Indicator } from 'types';
 
-type ComparisonChartProps = {
-  data: ImpactRanking;
+type StackedAreaChartProps = {
+  indicator: Indicator;
 };
 
-const COLOR_SCALE = chroma.scale(['#2D7A5B', '#39A163', '#9AC864', '#D9E77F', '#FFF9C7']);
+type ChartData = {
+  name: string;
+  unit: string;
+  values: {
+    year: number;
+    value: number;
+    scenarioValue: number;
+    absoluteDifference: number;
+    percentageDifference: number;
+  }[];
+};
 
-const ComparisonChart: React.FC<ComparisonChartProps> = ({ data }) => {
-  const chartData = useMemo(() => {
-    const { indicatorShortName, rows, yearSum, metadata } = data?.impactTable?.[0] || {};
-    const result = [];
-    const keys = [];
+const StackedAreaChart: React.FC<StackedAreaChartProps> = ({ indicator }) => {
+  const store: Store = useStore();
+  const { scenarioToCompare } = useAppSelector(scenarios);
+  const filters = filtersForTabularAPI(store.getState());
 
-    yearSum?.forEach(({ year }) => {
-      const items = {};
-      rows?.forEach((row) => {
-        const yearValues = row?.values.find((rowValues) => rowValues?.year === year);
-        items[row.name] = yearValues?.value;
-      });
-      result.push({ date: year, ...items });
-    });
+  const params = {
+    ...omit(filters, 'indicatorId'),
+    indicatorIds: [indicator.id],
+    scenarioId: scenarioToCompare,
+    disabledPagination: true,
+  };
 
-    if (result?.[0]) {
-      Object.keys(result[0]).forEach((key) => key !== 'date' && keys.push(key));
-    }
+  const enabled =
+    !!filters?.indicatorId &&
+    !!filters.startYear &&
+    !!filters.endYear &&
+    filters.endYear !== filters.startYear;
 
-    const colorScale = COLOR_SCALE.colors(keys.length);
+  const { data, isFetched, isLoading } = useImpactComparison(params, { enabled });
+
+  const chartData = useMemo<ChartData>(() => {
+    const { indicatorShortName, yearSum, metadata } = data?.data?.impactTable?.[0] || {};
 
     return {
-      values: result,
-      keys,
+      values: yearSum,
       name: indicatorShortName,
       unit: metadata?.unit,
-      colors: keys.reduce(
-        (acc, k, i) => ({
-          ...acc,
-          [k]: k === 'Other' || k === 'Others' ? '#E4E4E4' : colorScale[i],
-        }),
-        {},
-      ),
     };
   }, [data]);
 
@@ -63,67 +75,76 @@ const ComparisonChart: React.FC<ComparisonChartProps> = ({ data }) => {
   }, []);
 
   return (
-    <div>
-      <h2 className="flex-shrink-0 text-base">
-        {chartData.name} ({chartData.unit})
-      </h2>
-      <div className="relative flex-grow mt-3">
-        <div className="h-[370px] text-xs">
-          <ResponsiveContainer width="100%" height="100%">
-            <LineChart
-              data={chartData.values}
-              margin={{
-                top: 0,
-                right: 0,
-                left: 0,
-                bottom: 0,
-              }}
-            >
-              <Legend verticalAlign="top" content={renderLegend} height={70} />
-              <CartesianGrid
-                vertical={false}
-                stroke="#15181F"
-                strokeWidth={1}
-                strokeOpacity={0.15}
-              />
-              <XAxis
-                dataKey="date"
-                axisLine={false}
-                tick={{ fill: '#15181F', fontWeight: 300 }}
-                tickLine={false}
-              />
-              <YAxis
-                axisLine={false}
-                label={{ value: chartData.unit, angle: -90, position: 'insideLeft' }}
-                tick={{ fill: '#15181F', fontWeight: 300 }}
-                tickLine={false}
-                tickFormatter={NUMBER_FORMAT}
-              />
-              <Tooltip
-                formatter={NUMBER_FORMAT}
-                animationDuration={500}
-                itemStyle={{ color: '#15181F' }}
-              />
-              {chartData.keys.map((key) => (
-                <Line
-                  key={key}
-                  type="monotone"
-                  dataKey={key}
-                  dot={false}
-                  stroke={chartData.colors[key]}
-                  strokeWidth={2}
-                  fill={chartData.colors[key]}
-                  fillOpacity={0.9}
-                  animationEasing="ease"
-                  animationDuration={500}
-                />
-              ))}
-            </LineChart>
-          </ResponsiveContainer>
+    <div className="p-6 bg-white rounded-md shadow-sm">
+      {isLoading && <Loading className="w-5 h-5 m-auto text-primary" />}
+      {!isLoading && isFetched && data && (
+        <div>
+          <h2 className="flex-shrink-0 text-base">
+            {chartData.name} ({chartData.unit})
+          </h2>
+          <div className="relative flex-grow mt-3">
+            <div className="h-[370px] text-xs">
+              <ResponsiveContainer width="100%" height="100%">
+                <LineChart
+                  data={chartData.values}
+                  margin={{
+                    top: 0,
+                    right: 20,
+                    left: 0,
+                    bottom: 0,
+                  }}
+                >
+                  <Legend verticalAlign="top" content={renderLegend} height={70} />
+                  <CartesianGrid
+                    vertical={false}
+                    stroke="#15181F"
+                    strokeWidth={1}
+                    strokeOpacity={0.15}
+                  />
+                  <XAxis
+                    dataKey="date"
+                    axisLine={false}
+                    tick={{ fill: '#15181F', fontWeight: 300 }}
+                    tickLine={false}
+                    tickMargin={8}
+                  />
+                  <YAxis
+                    axisLine={false}
+                    label={{ value: chartData.unit, angle: -90, position: 'insideLeft' }}
+                    tick={{ fill: '#15181F', fontWeight: 300 }}
+                    tickLine={false}
+                    tickFormatter={NUMBER_FORMAT}
+                  />
+                  <Tooltip
+                    formatter={NUMBER_FORMAT}
+                    animationDuration={500}
+                    itemStyle={{ color: '#15181F' }}
+                  />
+                  <Line
+                    type="monotone"
+                    dataKey="scenarioValue"
+                    stroke="#078A3C"
+                    strokeWidth={1}
+                    fillOpacity={0.9}
+                    animationEasing="ease"
+                    animationDuration={500}
+                  />
+                  <Line
+                    type="monotone"
+                    dataKey="value"
+                    stroke="#3F59E0"
+                    strokeWidth={1}
+                    animationEasing="ease"
+                    animationDuration={500}
+                  />
+                </LineChart>
+              </ResponsiveContainer>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
 
-export default ComparisonChart;
+export default StackedAreaChart;

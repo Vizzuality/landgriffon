f4261d5276631f6fa9763be778b7f2ebfc269f50
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-10-03 14:45:01 +0200

    Make impact query fns generic

--- client/src/hooks/impact/ranking.ts
+++ client/src/hooks/impact/ranking.ts
@@ -2,11 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 
 import { apiRawService } from 'services/api';
 
-import type { UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import type { UseQueryOptions } from '@tanstack/react-query';
 import type { ImpactRanking } from 'types';
 import type { ImpactTabularAPIParams } from 'types';
 
-const DEFAULT_QUERY_OPTIONS: UseQueryOptions<ImpactRanking> = {
+const DEFAULT_QUERY_OPTIONS: UseQueryOptions<ImpactRanking, unknown, unknown> = {
   placeholderData: {
     impactTable: [],
     purchasedTonnes: [],
@@ -21,25 +21,25 @@ type ImpactRankingParams = ImpactTabularAPIParams & {
   sort: string;
 };
 
-export function useImpactRanking(
+export const useImpactRanking = <T = ImpactRanking>(
   params: Partial<ImpactRankingParams> = { maxRankingEntities: 5, sort: 'ASC' },
-  options: UseQueryOptions<ImpactRanking> = {},
-): UseQueryResult<ImpactRanking, unknown> {
-  const query = useQuery<ImpactRanking>(
+  options: UseQueryOptions<ImpactRanking, unknown, T> = {},
+) => {
+  const query = useQuery(
     ['impact-ranking', params],
     () =>
       apiRawService
-        .get('/impact/ranking', {
+        .get<ImpactRanking>('/impact/ranking', {
           params,
         })
         .then((response) => {
           return response.data;
         }),
     {
-      ...DEFAULT_QUERY_OPTIONS,
+      ...(DEFAULT_QUERY_OPTIONS as typeof options),
       ...options,
     },
   );
 
   return query;
-}
+};

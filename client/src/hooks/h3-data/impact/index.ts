19c7e35f1091a55837e152b1a7c2c2e4c2def359
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-10-04 13:39:44 +0200

    Move impact data hook to folder

---
+++ client/src/hooks/h3-data/impact/index.ts
@@ -0,0 +1,37 @@
+import type { UseQueryOptions } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
+import { apiRawService } from 'services/api';
+import type { H3APIResponse, ImpactH3APIParams } from 'types';
+import { useColors, COLOR_RAMPS } from 'utils/colors';
+import type { H3ImpactResponse } from '../utils';
+import { DEFAULT_QUERY_OPTIONS, responseParser } from '../utils';
+
+const useH3ImpactData = <T = H3ImpactResponse>(
+  params: ImpactH3APIParams,
+  options: UseQueryOptions<H3ImpactResponse, unknown, T> = {},
+) => {
+  const colors = useColors('impact', COLOR_RAMPS);
+
+  const isEnable = (options.enabled ?? true) && !!(params.indicatorId && params.year);
+
+  const query = useQuery(
+    ['h3-data-impact', params],
+    () =>
+      apiRawService
+        .get<H3APIResponse>('/h3/map/impact', {
+          params,
+        })
+        .then((response) => response.data)
+        // Adding color to the response
+        .then((response) => responseParser(response, colors)),
+    {
+      ...DEFAULT_QUERY_OPTIONS,
+      ...options,
+      enabled: isEnable,
+    },
+  );
+
+  return query;
+};
+
+export default useH3ImpactData;

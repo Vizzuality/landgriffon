33b11dad36e762322f8ece9e4ad77793ddd09603
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-10-13 12:23:37 +0200

    Add missing background to active filter count

--- client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
@@ -142,7 +142,7 @@ const MoreFilters = () => {
           <FilterIcon className="w-5 h-5 text-gray-900" aria-hidden="true" />
         </span>
         {counter !== 0 && (
-          <span className="flex items-center justify-center w-5 h-5 text-xs font-semibold text-white bg-green-700 rounded-full">
+          <span className="flex items-center justify-center w-5 h-5 text-xs font-semibold text-white bg-navy-400 rounded-full">
             {counter}
           </span>
         )}

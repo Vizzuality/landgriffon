b325386e6514ca31dc356f72cad2cf7fbcfcb17b
David Inga <david.inga@vizzuality.com>
2022-10-13 10:41:26 +0200

    collapse button with better styles

--- client/src/containers/collapse-button/component.tsx
+++ client/src/containers/collapse-button/component.tsx
@@ -2,27 +2,22 @@ import { useCallback } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
 
 import { useAppSelector, useAppDispatch } from 'store/hooks';
-import {
-  analysisUI,
-  setSidebarCollapsed,
-  setSubContentCollapsed,
-} from 'store/features/analysis/ui';
+import { analysisUI, setSidebarCollapsed } from 'store/features/analysis/ui';
 
 const ICON_CLASSNAMES = 'h-4 w-4 text-gray-900';
 
 const CollapseButton: React.FC = () => {
-  const { isSidebarCollapsed, isSubContentCollapsed } = useAppSelector(analysisUI);
+  const { isSidebarCollapsed } = useAppSelector(analysisUI);
   const dispatch = useAppDispatch();
 
   const handleClick = useCallback(() => {
-    if (!isSubContentCollapsed) dispatch(setSubContentCollapsed(true));
     dispatch(setSidebarCollapsed(!isSidebarCollapsed));
-  }, [dispatch, isSidebarCollapsed, isSubContentCollapsed]);
+  }, [dispatch, isSidebarCollapsed]);
 
   return (
     <button
       type="button"
-      className="flex items-center justify-center w-10 h-10 bg-white border border-gray-300 rounded-full cursor-pointer hover:bg-gray-100 focus:outline-none"
+      className="flex items-center justify-center w-10 h-10 bg-white border border-gray-200 rounded-full cursor-pointer hover:shadow-md hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-navy-400/20"
       onClick={handleClick}
     >
       {isSidebarCollapsed ? (

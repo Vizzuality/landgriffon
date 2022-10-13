84f528f1b2b9f43d7fc624062152e109236acf9f
David Inga <david.inga@vizzuality.com>
2022-10-13 13:13:34 +0200

    removed theme from input

--- client/src/containers/targets/input-list/component.tsx
+++ client/src/containers/targets/input-list/component.tsx
@@ -9,14 +9,14 @@ interface TargetListProps {
 }
 
 const TargetInputList: React.FC<TargetListProps> = ({ data, onChange }) => (
-  <div className="items-center justify-between rounded-md bg-gray-50 shadow-sm p-4 max-h-56 overflow-y-scroll">
-    <div className="flex flex-row pb-2 font-semibold text-sm text-gray-500 uppercase">
+  <div className="items-center justify-between p-4 overflow-y-scroll rounded-md shadow-sm bg-gray-50 max-h-56">
+    <div className="flex flex-row pb-2 text-sm font-semibold text-gray-500 uppercase">
       <span className="basis-1/6 mr-2.5">Year</span>
       <span className="basis-1/4 mr-2.5">Percentage</span>
       <span className="basis-1/2 mr-2.5">Value</span>
     </div>
     {data.map((target) => (
-      <div key={target.year} className="flex flex-row items-center space-x-4 pb-3 text-gray-900">
+      <div key={target.year} className="flex flex-row items-center pb-3 space-x-4 text-gray-900">
         <Input
           type="number"
           id="year"
@@ -36,7 +36,6 @@ const TargetInputList: React.FC<TargetListProps> = ({ data, onChange }) => (
           unit="%"
           className="w-full basis-1/4"
           defaultValue={target.percentage}
-          theme="inside-unit"
           // onInput={(ev) => onChange(target.year, ev.target)}
         />
         <Input
@@ -47,7 +46,6 @@ const TargetInputList: React.FC<TargetListProps> = ({ data, onChange }) => (
           className="w-full basis-1/2"
           defaultValue={target.value || 0}
           unit="tCO2e"
-          theme="inside-unit"
           // onInput={handleChange}
         />
         <XCircleIcon className="w-5 h-5 mr-3 fill-gray-400" />

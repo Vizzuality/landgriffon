cb1da948e9359a4fd76846ca28cd2038e7082a20
David Inga <david.inga@vizzuality.com>
2022-10-14 12:19:43 +0200

    fixed login

--- client/cypress/e2e/sign-in.cy.ts
+++ client/cypress/e2e/sign-in.cy.ts
@@ -1,7 +1,7 @@
 describe('Sign in', () => {
   it('should sign in', () => {
     cy.visit('/auth/signin');
-    cy.get('[name="username"]').type(Cypress.env('USERNAME'));
+    cy.get('[name="email"]').type(Cypress.env('USERNAME'));
     cy.get('[name="password"]').type(Cypress.env('PASSWORD'));
     cy.get('#signInForm').submit();
     cy.wait(1000);

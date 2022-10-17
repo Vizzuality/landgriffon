c53451dbf8aee42df6c5727828a7a07e973c30de
David Inga <david.inga@vizzuality.com>
2022-10-17 12:54:54 +0200

    reviewed search component and buttons

--- client/src/components/search/component.tsx
+++ client/src/components/search/component.tsx
@@ -1,41 +1,43 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { SearchIcon, XIcon } from '@heroicons/react/solid';
 import classNames from 'classnames';
 
 import Input from 'components/forms/input';
 
 import type { ChangeEvent } from 'react';
-
-type SearchProps = Omit<React.ComponentProps<typeof Input>, 'type' | 'icon' | 'onChange'> & {
-  onChange: (value: string) => void;
-};
+import type { SearchProps } from './types';
 
 export const Search: React.FC<SearchProps> = (props: SearchProps) => {
-  const { onChange, className, value } = props;
+  const [value, setValue] = useState<string>((props.defaultValue as string) || '');
+  const { onChange } = props;
 
   const handleChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
-      onChange?.(e.target.value);
+      e.stopPropagation();
+      setValue(e.target.value);
+      if (onChange) onChange(e.target.value);
     },
     [onChange],
   );
 
   const handleReset = useCallback(() => {
-    onChange?.('');
-  }, [onChange]);
+    setValue('');
+  }, []);
 
   return (
-    <div className={classNames('relative flex items-center', className)}>
+    <div className={classNames('relative', props.className)}>
       <Input
-        {...props}
         icon={<SearchIcon />}
+        {...props}
+        unit="&nbsp;"
         type="search"
-        className="w-full h-full"
+        className="w-full"
+        value={value}
         onChange={handleChange}
       />
       {value && value !== '' && (
         <button
-          className="absolute z-10 flex items-center self-center justify-center w-5 h-5 right-3 r-2"
+          className="absolute inset-y-0 right-0 flex items-center pr-3"
           type="button"
           onClick={handleReset}
         >

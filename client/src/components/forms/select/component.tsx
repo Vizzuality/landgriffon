eb63e51d5e4af8f716c2c975af91c1924f52c7f0
David Inga <david.inga@vizzuality.com>
2022-10-17 13:15:14 +0200

    added simple select

--- client/src/components/forms/select/component.tsx
+++ client/src/components/forms/select/component.tsx
@@ -1,31 +1,63 @@
-import { forwardRef, useState } from 'react';
+import { forwardRef, useCallback, useState } from 'react';
 import classnames from 'classnames';
 import { Listbox, Transition } from '@headlessui/react';
 import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/solid';
 import { flip, useFloating } from '@floating-ui/react-dom';
 
+import Hint from '../hint';
+
 import type { SelectProps, Option } from './types';
 
 const Select = forwardRef<HTMLSelectElement, SelectProps>(
-  ({ defaultValue, options, placeholder = 'Select an option', ...props }) => {
+  ({
+    defaultValue,
+    options,
+    placeholder = 'Select an option',
+    label,
+    error,
+    showHint,
+    ...props
+  }) => {
     const { onChange, value, ...selectProps } = props;
 
     const [selected, setSelected] = useState<Option>(defaultValue);
     const { x, y, reference, floating, strategy } = useFloating({
       middleware: [flip()],
+      placement: 'bottom-start',
+      strategy: 'fixed',
     });
 
+    const handleChange = useCallback(
+      (current: Option) => {
+        if (onChange) onChange(current);
+        setSelected(current);
+      },
+      [onChange],
+    );
+
     return (
-      <Listbox value={selected} onChange={setSelected} {...selectProps}>
+      <Listbox value={selected} onChange={handleChange} {...selectProps}>
         {({ open }) => (
           <>
-            <Listbox.Label className="block text-sm font-medium text-gray-700">
-              Assigned to
-            </Listbox.Label>
-            <div className="relative" ref={reference}>
-              <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white border border-gray-200 rounded-md shadow-sm cursor-default focus:border-navy-400 focus:outline-none focus:ring-0">
+            {!!label && (
+              <Listbox.Label className="block text-sm font-medium text-gray-700">
+                {label}
+              </Listbox.Label>
+            )}
+            <div className="relative">
+              <Listbox.Button
+                className={classnames(
+                  'relative py-2 pl-3 pr-10 text-left bg-white border rounded-md shadow-sm cursor-default focus:border-navy-400 focus:outline-none focus:ring-0',
+                  {
+                    'mt-1': !!label,
+                    'border-red-400': error,
+                    'border-gray-200': !error,
+                  },
+                )}
+                ref={reference}
+              >
                 <span className="block text-sm truncate">
-                  {selected ? selected.label : placeholder}
+                  {selected ? selected.label : <span className="text-gray-200">{placeholder}</span>}
                 </span>
                 <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                   {open ? (
@@ -42,7 +74,7 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
                 leave="transition ease-in duration-100"
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
-                className="z-10 w-full"
+                className="z-10"
                 style={{
                   position: strategy,
                   top: y ?? 0,
@@ -76,6 +108,10 @@ const Select = forwardRef<HTMLSelectElement, SelectProps>(
                   ))}
                 </Listbox.Options>
               </Transition>
+
+              {showHint && error && typeof error === 'string' && (
+                <Hint data-testid={`hint-input-${props.name}`}>{error}</Hint>
+              )}
             </div>
           </>
         )}
--- client/src/components/forms/select/types.d.ts
+++ client/src/components/forms/select/types.d.ts
@@ -3,9 +3,12 @@ export type Option = {
   value: string | number;
 };
 
-export type SelectProps = React.SelectHTMLAttributes<HTMLSelectElement> & {
+export type SelectProps = Omit<React.SelectHTMLAttributes<HTMLSelectElement>, 'className'> & {
   defaultValue?: Option;
+  value?: Option;
   error?: string;
+  label?: React.ReactNode;
   options: Option[];
   showHint?: boolean;
+  onChange: (value: Option) => void;
 };

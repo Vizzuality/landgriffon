f8dd1425502ac4cd1d09c535f4bdf1beb9b28642
David Inga <david.inga@vizzuality.com>
2022-10-17 16:34:47 +0200

    styles for mode control

--- client/src/components/button-group/button-group-item/component.tsx
+++ client/src/components/button-group/button-group-item/component.tsx
@@ -3,17 +3,16 @@ import classNames from 'classnames';
 import type { ButtonGroupItemProps } from '../types';
 
 const CONTROL_ITEM_CLASS_NAMES =
-  'relative inline-flex items-center px-4 py-1.5 border -ml-px first:ml-0';
-const CONTROL_ITEM_DEFAULT_CLASS_NAMES =
-  'border-gray-100 bg-white text-sm font-medium text-gray-500 hover:text-gray-900 focus:z-10 focus:outline-none focus:ring-1 focus:ring-green-700 focus:border-green-700';
-const CONTROL_ITEM_ACTIVE_CLASS_NAMES = 'z-10 text-navy-400 bg-navy-400/20 pointer-events-none';
+  'relative inline-flex h-10 px-2.5 py-1 justify-center items-center border-l border-gray-200 leading-5 ring-4 ring-inset ring-white hover:cursor-pointer hover:bg-navy-400/20 focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-navy-400/20';
+const CONTROL_ITEM_DEFAULT_CLASS_NAMES = 'bg-white text-sm text-gray-500';
+const CONTROL_ITEM_ACTIVE_CLASS_NAMES = 'text-navy-400 bg-navy-400/20';
 
 const ButtonGroupItem: React.FC<ButtonGroupItemProps> = ({ children, active, ...props }) => (
   <button
     className={classNames(
       CONTROL_ITEM_CLASS_NAMES,
       active ? CONTROL_ITEM_ACTIVE_CLASS_NAMES : CONTROL_ITEM_DEFAULT_CLASS_NAMES,
-      'first:rounded-l-md last:rounded-r-md',
+      'first:rounded-l-md first:border-l-0 last:rounded-r-md',
     )}
     type="button"
     {...props}
--- client/src/components/button-group/component.tsx
+++ client/src/components/button-group/component.tsx
@@ -1,5 +1,5 @@
 const ButtonGroup: React.FC<React.PropsWithChildren> = ({ children }) => (
-  <div className="relative z-0 inline-flex bg-white border border-gray-300 rounded-md shadow-sm">
+  <div className="relative flex bg-white border border-gray-200 rounded-md shadow-sm">
     {children}
   </div>
 );

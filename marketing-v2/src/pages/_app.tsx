85b6d2eedb2cf6564bd543dc27808bb7b821dc70
David Inga <david.inga@vizzuality.com>
2022-10-17 16:54:41 +0200

    added analytics script

--- marketing-v2/src/pages/_app.tsx
+++ marketing-v2/src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import type { AppProps } from 'next/app';
 import { Provider as ReduxProvider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { OverlayProvider } from '@react-aria/overlays';
 import { SSRProvider } from '@react-aria/ssr';
 import { Hydrate } from 'react-query/hydration';
+import Script from 'next/script';
+
 import store from 'store';
-import { MediaContextProvider } from 'components/media-query';
 import ThirdParty from 'containers/third-party-cookies';
+import { MediaContextProvider } from 'components/media-query';
+
+import type { AppProps } from 'next/app';
 
 import 'styles/globals.css';
 import 'styles/fonts.css';
@@ -23,6 +26,20 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => (
             <SSRProvider>
               <Component {...pageProps} />
               <ThirdParty />
+              {/* Global site tag (gtag.js) - Google Analytics */}
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){window.dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'GA_MEASUREMENT_ID');
+                `}
+              </Script>
             </SSRProvider>
           </MediaContextProvider>
         </OverlayProvider>

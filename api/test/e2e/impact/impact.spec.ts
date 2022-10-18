6e0cf1da606ff1924e90dd702d247b930730be54
Yulia Belyakova <yuliya.beliakova@gmail.com>
2022-10-18 10:22:55 +0200

    Update test and dto for ranking to use scenarioIds interceptor

--- api/test/e2e/impact/impact.spec.ts
+++ api/test/e2e/impact/impact.spec.ts
@@ -970,7 +970,7 @@ describe('Impact Table and Charts test suite (e2e)', () => {
         })
         .expect(HttpStatus.OK);
 
-      expect(response.body.data.impactTable[0].rows).toHaveLength(2);
+      expect(response.body.data.impactTable[0].rows).toHaveLength(4);
       expect(response.body.data.impactTable[0].rows).toEqual(
         expect.arrayContaining(groupByLocationTypeResponseData.rows),
       );

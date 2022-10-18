9b030603d0e76cde2b38e223265a8c1053637fdf
David Inga <david.inga@vizzuality.com>
2022-10-18 15:04:38 +0200

    more filters icon styles

--- client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/more-filters/component.tsx
@@ -1,5 +1,4 @@
 import React, { useCallback, useEffect, useState, useMemo } from 'react';
-import classNames from 'classnames';
 import { FilterIcon } from '@heroicons/react/solid';
 import {
   offset,
@@ -19,7 +18,7 @@ import LocationTypes from '../location-types/component';
 
 import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { analysisFilters, setFilters } from 'store/features/analysis/filters';
-import Button, { THEME } from 'components/button/component';
+import Button from 'components/button/component';
 import { scenarios, setFilter } from 'store/features/analysis';
 import { useMaterialsTrees } from 'hooks/materials';
 import { useAdminRegionsTrees } from 'hooks/admin-regions';
@@ -228,26 +227,22 @@ const MoreFilters = () => {
 
   return (
     <div className="relative">
-      <div
-        className={classNames(
-          THEME.default,
-          THEME.secondary,
-          'flex gap-x-1 justify-end rounded-md min-h-[2.5rem] border border-gray-300 shadow-sm px-2 select-none bg-white',
-        )}
+      <button
+        className="flex items-center space-x-2 px-2 py-2.5 h-10 border border-gray-200 rounded-md bg-white shadow-sm hover:cursor-pointer focus:border-navy-400 focus:outline-none focus:ring-0"
+        type="button"
         {...getReferenceProps({
           ref: reference,
         })}
       >
-        <span className="block h-5 truncate">
-          <FilterIcon className="w-5 h-5 text-gray-900" aria-hidden="true" />
-        </span>
-        {counter !== 0 && (
-          <span className="flex items-center justify-center w-5 h-5 text-xs font-semibold text-white bg-navy-400 rounded-full">
+        <FilterIcon className="block w-5 h-5 text-gray-900" aria-hidden="true" />
+        {counter > 0 && (
+          <span className="flex items-center justify-center w-5 h-5 text-xs font-semibold text-white rounded-full bg-navy-400">
             {counter}
           </span>
         )}
-      </div>
+      </button>
       <Transition
+        as="div"
         show={isOpen}
         enter="transition ease-out duration-200"
         enterFrom="opacity-0 translate-y-1"
@@ -265,69 +260,67 @@ const MoreFilters = () => {
           },
         })}
       >
-        <div className="rounded-lg shadow-lg w-80 ring-1 ring-black ring-opacity-5 ">
-          <div className="relative p-4 bg-white rounded-lg">
-            <div className="flex justify-between mb-4">
-              <div>Filter by</div>
-              <button type="button" className="text-navy-400" onClick={handleClearFilters}>
-                Clear all
-              </button>
+        <div className="p-4 bg-white rounded-md shadow-md w-80 ring-1 ring-gray-200">
+          <div className="flex justify-between mb-4">
+            <div>Filter by</div>
+            <button type="button" className="text-navy-400" onClick={handleClearFilters}>
+              Clear all
+            </button>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div>
+              <div className="mb-1">Material</div>
+              <Materials
+                options={materialOptions}
+                multiple
+                current={selectedFilters.materials}
+                fitContent
+                onChange={(values) => handleChangeFilter('materials', values)}
+              />
             </div>
-            <div className="flex flex-col gap-3">
-              <div>
-                <div className="mb-1">Material</div>
-                <Materials
-                  options={materialOptions}
-                  multiple
-                  current={selectedFilters.materials}
-                  fitContent
-                  onChange={(values) => handleChangeFilter('materials', values)}
-                />
-              </div>
-              <div>
-                <div className="mb-1">Origins</div>
-                <OriginRegions
-                  options={originOptions}
-                  multiple
-                  current={selectedFilters.origins}
-                  fitContent
-                  onChange={(values) => handleChangeFilter('origins', values)}
-                />
-              </div>
-              <div>
-                <div className="mb-1">Suppliers</div>
-                <Suppliers
-                  multiple
-                  options={supplierOptions}
-                  current={selectedFilters.suppliers}
-                  fitContent
-                  onChange={(values) => handleChangeFilter('suppliers', values)}
-                />
-              </div>
-              <div>
-                <div className="mb-1">Location type</div>
-                <LocationTypes
-                  options={locationTypeOptions}
-                  current={selectedFilters.locationTypes}
-                  fitContent
-                  onChange={(values) => handleChangeFilter('locationTypes', values)}
-                />
-              </div>
+            <div>
+              <div className="mb-1">Origins</div>
+              <OriginRegions
+                options={originOptions}
+                multiple
+                current={selectedFilters.origins}
+                fitContent
+                onChange={(values) => handleChangeFilter('origins', values)}
+              />
             </div>
+            <div>
+              <div className="mb-1">Suppliers</div>
+              <Suppliers
+                multiple
+                options={supplierOptions}
+                current={selectedFilters.suppliers}
+                fitContent
+                onChange={(values) => handleChangeFilter('suppliers', values)}
+              />
+            </div>
+            <div>
+              <div className="mb-1">Location type</div>
+              <LocationTypes
+                options={locationTypeOptions}
+                current={selectedFilters.locationTypes}
+                fitContent
+                onChange={(values) => handleChangeFilter('locationTypes', values)}
+              />
+            </div>
+          </div>
 
-            <div className="flex gap-2 mt-6">
-              <Button variant="secondary" className="px-9" onClick={handleCancel}>
-                Cancel
-              </Button>
-              <Button
-                variant="primary"
-                className="flex-grow"
-                onClick={handleApply}
-                disabled={!hasChangesToApply}
-              >
-                Apply
-              </Button>
-            </div>
+          <div className="flex gap-2 mt-6">
+            <Button variant="secondary" className="px-9" onClick={handleCancel}>
+              Cancel
+            </Button>
+            <Button
+              variant="primary"
+              className="flex-grow"
+              onClick={handleApply}
+              disabled={!hasChangesToApply}
+            >
+              Apply
+            </Button>
           </div>
         </div>
       </Transition>

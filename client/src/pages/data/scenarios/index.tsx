e4e63c4b46a312085ccaea5845ad2d6cbed285b5
Andrés González <andres.gonzalez@vizzuality.com>
2022-10-25 18:08:16 +0200

    sorts scenarios by title or most recent

--- client/src/pages/data/scenarios/index.tsx
+++ client/src/pages/data/scenarios/index.tsx
@@ -1,16 +1,55 @@
+import { useCallback, useMemo } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { PlusIcon } from '@heroicons/react/solid';
+import { useRouter } from 'next/router';
+import { PlusIcon, SortDescendingIcon } from '@heroicons/react/solid';
 
+import Select from 'components/forms/select';
 import { useScenarios } from 'hooks/scenarios';
 import AdminLayout from 'layouts/data';
 import ScenarioCard from 'containers/scenarios/card';
 import { Anchor } from 'components/button';
 import Loading from 'components/loading';
 
+const SORT_OPTIONS = [
+  {
+    label: 'Sort by most recent',
+    value: '-updatedAt',
+  },
+  {
+    label: 'Sort by name',
+    value: 'title',
+  },
+];
+
 const ScenariosAdminPage: React.FC = () => {
+  const router = useRouter();
+  const { query } = router;
+
+  const handleSort = useCallback(
+    ({ value }) => {
+      router.replace(
+        {
+          pathname: router.pathname,
+          query: {
+            ...query,
+            sortBy: value,
+          },
+        },
+        null,
+        { shallow: true },
+      );
+    },
+    [router, query],
+  );
+
+  const currentSort = useMemo(
+    () => SORT_OPTIONS.find(({ value }) => value === query.sortBy) || SORT_OPTIONS[0],
+    [query.sortBy],
+  );
+
   const { data, isLoading } = useScenarios({
-    params: { disablePagination: true },
+    params: { disablePagination: true, sort: currentSort.value },
     options: { select: (data) => data.data },
   });
 
@@ -20,22 +59,34 @@ const ScenariosAdminPage: React.FC = () => {
         <title>Admin scenarios | Landgriffon</title>
       </Head>
       <div className="flex justify-end mb-6">
-        <Link href="/data/scenarios/new" passHref>
-          <Anchor
-            variant="primary"
-            data-testid="scenario-add-button"
-            icon={
-              <div
-                aria-hidden="true"
-                className="flex items-center justify-center w-5 h-5 bg-white rounded-full"
+        <div className="flex space-x-4">
+          <Select
+            value={currentSort}
+            options={SORT_OPTIONS}
+            onChange={handleSort}
+            icon={<SortDescendingIcon className="w-4 h-4" />}
+            data-testid="sort-scenario"
+          />
+          <div>
+            {/*  view options go here */}
+            <Link href="/data/scenarios/new" passHref>
+              <Anchor
+                variant="secondary"
+                data-testid="scenario-add-button"
+                icon={
+                  <div
+                    aria-hidden="true"
+                    className="flex items-center justify-center w-5 h-5 rounded-full bg-navy-400"
+                  >
+                    <PlusIcon className="w-4 h-4 text-white" />
+                  </div>
+                }
               >
-                <PlusIcon className="w-4 h-4 text-navy-400" />
-              </div>
-            }
-          >
-            Add new scenario
-          </Anchor>
-        </Link>
+                Add scenario
+              </Anchor>
+            </Link>
+          </div>
+        </div>
       </div>
       {isLoading && (
         <div className="flex justify-center">

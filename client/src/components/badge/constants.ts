96eb8b7a83f23dc1aa841775a9a29d6ac8873b05
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2022-11-03 12:20:16 +0100

    updates tree-select pills

--- client/src/components/badge/constants.ts
+++ client/src/components/badge/constants.ts
@@ -1,6 +1,6 @@
 export const THEME_CLASSNAMES = {
   default: {
-    wrapper: 'bg-navy-50 border border-navy-200 rounded-md text-gray-900 text-xs',
-    closeIcon: 'hover:text-white hover:bg-navy-600 hover:ring-navy-200',
+    wrapper: 'bg-orange-50 border border-orange-300 rounded-md',
+    closeIcon: 'hover:text-white hover:bg-orange-300 hover:ring-orange-300',
   },
 };

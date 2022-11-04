115d9620918afd6f9243a26a4b54bab8c26921b0
Joaquín Triñanes <quin.trinanes@gmail.com>
2022-11-04 11:28:27 +0100

    Fix component name

--- client/src/containers/analysis-visualization/analysis-filters/origin-regions/index.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/origin-regions/index.tsx
@@ -5,7 +5,7 @@ import Component from './component';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
 import { analysisFilters, setFilter } from 'store/features/analysis/filters';
 
-const MaterialsFilter: React.FC<{ multiple?: boolean }> = (props) => {
+const OriginRegionsFilter: React.FC<{ multiple?: boolean }> = (props) => {
   const { multiple = false } = props;
   const dispatch = useAppDispatch();
   const filters = useAppSelector(analysisFilters);
@@ -17,4 +17,4 @@ const MaterialsFilter: React.FC<{ multiple?: boolean }> = (props) => {
   return <Component current={filters.origins} multiple={multiple} onChange={handleChange} />;
 };
 
-export default MaterialsFilter;
+export default OriginRegionsFilter;

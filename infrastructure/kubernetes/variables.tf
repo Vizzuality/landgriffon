a3d196f235a9b338755f8c844bc536b7f7a26a59
Tiago Garcia <tiagojsag@gmail.com>
2022-11-21 10:33:12 +0100

    Streamline staging and production deployment config

--- infrastructure/kubernetes/variables.tf
+++ infrastructure/kubernetes/variables.tf
@@ -50,30 +50,6 @@ variable "environments" {
   description = "A list of environments"
 }
 
-variable "load_fresh_data_staging" {
-  type        = bool
-  default     = false
-  description = "If a new data import should be triggered for the staging app. Clears the current database."
-}
-
-variable "load_fresh_data_prod" {
-  type        = bool
-  default     = false
-  description = "If a new data import should be triggered for the production app. Clears the current database."
-}
-
-variable "data_import_arguments_prod" {
-  type        = list(string)
-  default     = ["seed-data"]
-  description = "Arguments to pass to the initial data import process for the production cluster"
-}
-
-variable "data_import_arguments_staging" {
-  type        = list(string)
-  default     = ["seed-data"]
-  description = "Arguments to pass to the initial data import process for the staging cluster"
-}
-
 variable "data_s3_access_key" {
   type        = string
   sensitive   = true

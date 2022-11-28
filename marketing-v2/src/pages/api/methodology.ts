21c8161950d1c8bdd0869a64116f161e7d025ec3
David Inga <david.inga@vizzuality.com>
2022-11-28 13:50:03 +0100

    changed fromname of methodology form

--- marketing-v2/src/pages/api/methodology.ts
+++ marketing-v2/src/pages/api/methodology.ts
@@ -18,6 +18,7 @@ const Methodology = (req: NextApiRequest, res: NextApiResponse): void => {
     const msg = {
       to: email,
       from: 'hello@landgriffon.com',
+      fromname: 'LandGriffon',
       subject: `Landgriffon's Methodology`,
       text: `Hi ${name}\n\n
       Thank you for your interest in LandGriffon's Methodology!\n\n

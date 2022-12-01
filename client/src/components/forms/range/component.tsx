9d45c49f3199517d76958fbd584e1c6acb7f5265
David Inga <david.inga@vizzuality.com>
2022-12-01 11:16:18 +0100

    fixed styles for legend and opacity control

--- client/src/components/forms/range/component.tsx
+++ client/src/components/forms/range/component.tsx
@@ -34,7 +34,7 @@ const RangeSlider: React.FC<RangeSliderProps> = ({ min, max, step = 1, value, on
           <div {...props} className="relative h-6 w-full">
             {children}
             <div className="w-full h-0.5 flex flex-row top-1/2 relative -z-10">
-              <div className="h-full bg-gray-600" style={{ width: `${fillPercentage}%` }} />
+              <div className="h-full bg-navy-400" style={{ width: `${fillPercentage}%` }} />
               <div className="h-full bg-gray-100" style={{ width: `${100 - fillPercentage}%` }} />
             </div>
           </div>
@@ -43,16 +43,16 @@ const RangeSlider: React.FC<RangeSliderProps> = ({ min, max, step = 1, value, on
           <div
             {...props}
             className={classNames(
-              'relative h-6 w-6 rounded-full border-gray-600 border-2 text-center',
+              'relative h-4 w-4 rounded-full border-navy-400 border-2 text-center',
               {
                 'bg-white': !isDragged,
-                'bg-gray-600': isDragged,
+                'bg-navy-400': isDragged,
               },
             )}
           >
             {isDragged && (
               <div className="absolute top-2 -translate-y-full left-1/2 -translate-x-1/2">
-                <div className="w-16 text-white bg-black p-2 rounded-md">
+                <div className="min-w-fit text-white bg-black p-2 rounded-md text-xs whitespace-nowrap">
                   {value} {unit}
                 </div>
                 <div className="bg-black w-4 h-4 relative left-1/2 -translate-x-1/2 bottom-3 rotate-45 rounded-sm" />

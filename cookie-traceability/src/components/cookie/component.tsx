a34a146cc3093d52ebb71c78abd8f6ad2ae85323
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-12-19 13:59:41 +0100

    Cookie hero

--- cookie-traceability/src/components/cookie/component.tsx
+++ cookie-traceability/src/components/cookie/component.tsx
@@ -1,15 +1,18 @@
-import { useGLTF, useTexture } from '@react-three/drei';
-import { LinearEncoding, Mesh, MeshStandardMaterial, sRGBEncoding } from 'three';
-import { useMemo, useRef } from 'react';
-import { useFrame } from '@react-three/fiber';
+import { useEffect, useMemo, useRef } from 'react';
 
+import { useFrame } from '@react-three/fiber';
+import { useGLTF, useTexture } from '@react-three/drei';
+import { Group, LinearEncoding, MeshStandardMaterial, sRGBEncoding } from 'three';
+
+import { useMotionValue, useSpring, useTransform, MotionValue } from 'framer-motion';
+import { motion } from 'framer-motion-3d';
 interface CookieProps {
   nodes: any;
   materials: any;
 }
 
 function Cookie(props: any) {
-  const cookieRef = useRef<Mesh>(null);
+  const cookieRef = useRef<Group>();
 
   const texture = useTexture({
     map: '/models/cookie/cookie_4px_compressed.png',
@@ -18,27 +21,47 @@ function Cookie(props: any) {
 
   const { nodes }: CookieProps = useGLTF('/models/cookie/cookie.gltf');
 
+  const rotationX = useMotionValue(0);
+  const rotationY = useMotionValue(0);
+
+  const rX = useSpring(useTransform(rotationX, (v) => v));
+  const rY = useSpring(useTransform(rotationY, (v) => v));
+
   const chipMaterial = useMemo(() => {
     return new MeshStandardMaterial({
       color: '#5b210f',
+      emissive: '#5b210f',
+      emissiveIntensity: 0.5,
       metalness: 0,
       roughness: 0,
     });
   }, []);
 
   useFrame(({ mouse }) => {
-    cookieRef.current!.rotation.x = mouse.y * 0.1;
-    cookieRef.current!.rotation.y = -mouse.x * 0.1;
+    rotationX.set(mouse.y * 0.1);
+    rotationY.set(-mouse.x * 0.5);
   });
 
   return (
-    <group {...props} ref={cookieRef} dispose={null} position={[0, -1.25, 0]}>
-      <mesh receiveShadow position={[0, -0.05, 0]}>
+    <motion.group
+      {...props}
+      ref={cookieRef}
+      dispose={null}
+      position={[0, -1.25, 0]}
+      rotation-x={rX}
+      rotation-y={rY}
+    >
+      <motion.mesh receiveShadow position={[0, -0.05, 0]}>
         <boxGeometry args={[5, 0.1, 5]} />
         <shadowMaterial opacity={0.25} />
-      </mesh>
+      </motion.mesh>
 
-      <mesh castShadow geometry={nodes.Cookie.geometry} rotation={[Math.PI, Math.PI / 2, 0]}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cookie.geometry}
+        rotation={[Math.PI, Math.PI / 2, 0]}
+      >
         <meshStandardMaterial
           {...texture}
           color={'#EECDA3'}
@@ -52,13 +75,22 @@ function Cookie(props: any) {
           if (node.includes('chip')) {
             const n = nodes[node];
 
-            return <mesh key={node} {...n} geometry={n.geometry} material={chipMaterial} />;
+            return (
+              <mesh
+                key={node}
+                {...n}
+                geometry={n.geometry}
+                material={chipMaterial}
+                castShadow
+                receiveShadow
+              />
+            );
           }
 
           return null;
         })}
       </mesh>
-    </group>
+    </motion.group>
   );
 }
 

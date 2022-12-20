6cdc88c6f8b6274b3f4ab5d84f2d5b2a82a71730
David Inga <david.inga@vizzuality.com>
2022-12-20 11:52:10 +0100

    removed d3-map

--- cookie-traceability/src/components/map/component.tsx
+++ cookie-traceability/src/components/map/component.tsx
@@ -1,61 +1,26 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { feature } from 'topojson-client';
-import { geoPath, geoMercator } from 'd3-geo';
 import { v4 as uuidv4 } from 'uuid';
+import DeckGL from '@deck.gl/react/typed';
+import { FlowmapLayer, FlowmapLayerPickingInfo, PickingType } from '@flowmap.gl/layers';
+import { Map as ReactMapGl, ViewState as ViewportProps } from 'react-map-gl';
 
-import type { GeometryCollection } from 'topojson-specification';
-
-const fetchCountries = async () => axios.get('/data/updated_countries-50m.json');
-
-const WIDTH = 1000;
-const HEIGHT = 600;
-const projection = geoMercator().translate([WIDTH / 2, HEIGHT / 2]);
-const countryPath = geoPath().projection(projection);
+const fetchData = async () => axios.get('/data/map_flow.json').then((res) => res.data);
 
 const Map: React.FC = () => {
-  const elementRef = useRef<SVGSVGElement>(null);
-  const { data, isLoading } = useQuery(['countries'], fetchCountries, {
+  const [viewState, setViewState] = useState<ViewportProps>();
+  const { data, isLoading } = useQuery(['map-flow'], fetchData, {
     refetchOnWindowFocus: false,
-    refetchOnMount: false,
     refetchOnReconnect: false,
+    refetchOnMount: false,
   });
 
-  const countries = useMemo(() => {
-    if (data?.data) {
-      return feature(data.data, data.data.objects.countries as GeometryCollection).features.map(
-        (feature) => ({
-          ...feature,
-          uuid: uuidv4(),
-        }),
-      );
-    }
-    return [];
-  }, [data]);
+  console.log(data);
 
   return (
-    <div>
-      {isLoading && <p>Loading...</p>}
-      {countries && (
-        <svg
-          width={WIDTH}
-          height={HEIGHT}
-          viewBox={`0 0 ${WIDTH} ${HEIGHT}`}
-          ref={elementRef}
-          className="mx-auto"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          {countries.map((country) => (
-            <path
-              key={country.uuid}
-              d={countryPath(country) as string}
-              className="stroke-primary fill-none"
-            />
-          ))}
-        </svg>
-      )}
+    <div className="w-full h-[760px]">
+      <DeckGL width="100%" height="100%" initialViewState={viewState}></DeckGL>
     </div>
   );
 };

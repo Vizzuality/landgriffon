70783e306c5a35a2b875b2cd9eec18df490f97e2
David Inga <david.inga@vizzuality.com>
2022-12-21 12:25:20 +0100

    added countries borders

--- cookie-traceability/src/components/map/types.d.ts
+++ cookie-traceability/src/components/map/types.d.ts
@@ -1,5 +1,10 @@
-import { Ingredient } from 'types';
+import { Ingredient, LocationDatum, FlowDatum } from 'types';
 
 export type MapProps = {
   ingredientId: Ingredient['id'];
 };
+
+export type MapFlowData = {
+  locations: LocationDatum[];
+  flows: FlowDatum[];
+} | null;

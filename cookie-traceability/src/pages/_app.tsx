ba1c1bab7aaa8dd5779f6fff70a28f8e3d6cc2b8
David Inga <david.inga@vizzuality.com>
2022-12-22 16:39:27 +0100

    added analytics to supply chain

--- cookie-traceability/src/pages/_app.tsx
+++ cookie-traceability/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 import type { AppProps } from 'next/app';
 
@@ -10,6 +11,7 @@ const queryClient = new QueryClient();
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </QueryClientProvider>
   );

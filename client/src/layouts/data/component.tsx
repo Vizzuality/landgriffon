6ba9682a1639467d96f1103d993c6a9e74095ebe
David Inga <david.inga@vizzuality.com>
2023-01-12 11:26:05 +0100

    adding muted styles to targets link in data page

--- client/src/layouts/data/component.tsx
+++ client/src/layouts/data/component.tsx
@@ -2,7 +2,12 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import classNames from 'classnames';
 
-import { ADMIN_TABS } from './constants';
+import {
+  ADMIN_TABS,
+  MENU_ITEM_STYLE,
+  MENU_ITEM_ACTIVE_STYLE,
+  MENU_ITEM_DISABLED_STYLE,
+} from './constants';
 
 import ApplicationLayout from 'layouts/application';
 
@@ -21,20 +26,36 @@ const AdminLayout: React.FC<AdminLayoutProps> = ({ title = 'Admin', children }) 
           <div className="flex items-stretch justify-between px-12 bg-white border-b border-gray-100 rounded-tl-3xl">
             <h1 className="mt-8 text-left">{title}</h1>
 
-            <nav className="flex mt-12 space-x-10 text-sm">
-              {Object.values(ADMIN_TABS).map((tab) => (
-                <Link key={tab.href} href={tab.href}>
-                  <a
-                    className={classNames('flex items-center pb-5 -mb-[2px]', {
-                      'text-navy-400 border-b-2 border-navy-400': pathname === tab.href,
-                    })}
-                    data-testid="admin-menu-item"
-                    data-testisactive={pathname === tab.href}
-                  >
-                    {tab.name}
-                  </a>
-                </Link>
-              ))}
+            <nav className="flex mt-12 space-x-10 text-sm" data-testid="admin-menu-list">
+              {Object.values(ADMIN_TABS).map((tab) => {
+                if (tab.disabled) {
+                  return (
+                    <span
+                      key={tab.href}
+                      className={classNames(MENU_ITEM_STYLE, MENU_ITEM_DISABLED_STYLE)}
+                      data-testid="admin-menu-item"
+                      data-testisactive={false}
+                      data-testname={`admin-menu-item-${tab.name}`}
+                    >
+                      {tab.name}
+                    </span>
+                  );
+                }
+                return (
+                  <Link key={tab.href} href={tab.href}>
+                    <a
+                      className={classNames(MENU_ITEM_STYLE, {
+                        [MENU_ITEM_ACTIVE_STYLE]: pathname === tab.href,
+                      })}
+                      data-testid="admin-menu-item"
+                      data-testisactive={pathname === tab.href}
+                      data-testname={`admin-menu-item-${tab.name}`}
+                    >
+                      {tab.name}
+                    </a>
+                  </Link>
+                );
+              })}
             </nav>
           </div>
         </header>

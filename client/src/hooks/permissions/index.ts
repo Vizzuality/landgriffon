08f464d41556f160bd3992506a2bdccdef02e12c
barbara-chaves <barbarabarcellos90@gmail.com>
2023-01-19 17:31:09 +0100

    Fix usePermissions hook to return roles as array and add a hasRole function

--- client/src/hooks/permissions/index.ts
+++ client/src/hooks/permissions/index.ts
@@ -1,16 +1,25 @@
 import { useProfile } from 'hooks/profile';
 
-import type { Permission } from './enums';
+import type { Permission, RoleName } from './enums';
 
 export function usePermissions() {
   const { data, isLoading } = useProfile();
-  const roles = data?.roles[0];
-  const role = roles?.name;
-  const permissions = roles?.permissions;
 
-  const hasPermissions = (permission: Permission) => {
+  const roles: RoleName[] = [];
+  const permissions: Permission[] = [];
+
+  data?.roles?.forEach((role) => {
+    roles.push(role.name);
+    role.permissions?.forEach((permission) => permissions.push(permission));
+  });
+
+  const hasRole = (role: RoleName) => {
+    return !isLoading && !!roles?.includes(role);
+  };
+
+  const hasPermission = (permission: Permission) => {
     return !isLoading && !!permissions?.includes(permission);
   };
 
-  return { role, permissions, hasPermissions, permissionsLoading: isLoading };
+  return { roles, hasRole, permissions, hasPermission, permissionsLoading: isLoading };
 }

9702188ffe0b87709af619ec34e159403c3013a0
Maria Sola <maria.sola@vizzuality.com>
2023-01-19 14:14:38 +0100

    hook and example to check user role and permissions
    Fix usePermissions hook to return roles as array and add a hasRole function
    
    fixes in change.log and small fixes

---
+++ client/src/hooks/permissions/enums.ts
@@ -0,0 +1,12 @@
+/** User roles */
+export enum RoleName {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+/** User permissions */
+export enum Permission {
+  CAN_CREATE_SCENARIO = 'canCreateScenario',
+  CAN_EDIT_SCENARIO = 'canEditScenario',
+  CAN_DELETE_SCENARIO = 'canDeleteScenario',
+}

d4496c6f3ae5e44ff2ed429347d955842570852b
barbara-chaves <barbarabarcellos90@gmail.com>
2023-01-24 18:52:31 +0100

    Rebase

--- client/cypress/e2e/actual-data.cy.ts
+++ client/cypress/e2e/actual-data.cy.ts
@@ -9,12 +9,12 @@ describe('Actual data', () => {
   });
 
   it('admin should be able to upload data source', () => {
-    cy.intercept('/api/v1/users/me', { fixture: 'profile/admin.json' }).as('profile').as('profile');
+    cy.intercept('/api/v1/users/me', { fixture: 'profiles/admin.json' });
     cy.get('[data-testid=upload-data-source-btn]').should('not.be.disabled');
   });
 
   it('user should not be able to upload data source', () => {
-    cy.intercept('/api/v1/users/me', { fixture: 'profile/user.json' });
+    cy.intercept('/api/v1/users/me', { fixture: 'profiles/user.json' });
     cy.get('[data-testid=upload-data-source-btn]').should('be.disabled');
   });
 });

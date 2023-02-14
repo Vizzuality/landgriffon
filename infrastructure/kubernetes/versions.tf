7d91179bb35db01cd5f003203e36c7e7dac5ea93
alexeh <alexurola@gmail.com>
2023-02-14 13:16:19 +0300

    Add tiler TF deployment

--- infrastructure/kubernetes/versions.tf
+++ infrastructure/kubernetes/versions.tf
@@ -52,7 +52,7 @@ provider "helm" {
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = [
+      args = [
         "eks",
         "get-token",
         "--cluster-name",
@@ -65,5 +65,5 @@ provider "helm" {
 
 # https://github.com/integrations/terraform-provider-github/issues/667#issuecomment-1182340862
 provider "github" {
-#  owner = "vizzuality"
+  #  owner = "vizzuality"
 }

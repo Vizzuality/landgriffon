81a4c57d91655645d5d82db17f83f6baf309e105
alexeh <alexurola@gmail.com>
2023-02-23 12:09:45 +0300

    bypass auth middleware for health and openapi docs in tiler

--- tiler/app/main.py
+++ tiler/app/main.py
@@ -6,7 +6,7 @@ from titiler.core.middleware import TotalTimeMiddleware, LoggerMiddleware
 from .middlewares.auth_middleware import AuthMiddleware
 from .middlewares.url_injector import inject_s3_url
 
-app = FastAPI(title="LandGriffon Tiler! Because why keep life simple?")
+app = FastAPI(title="LandGriffon Tiler")
 app.add_middleware(TotalTimeMiddleware)
 app.add_middleware(LoggerMiddleware)
 app.add_middleware(AuthMiddleware)

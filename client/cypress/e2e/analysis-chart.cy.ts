ae898ee0e9fe1d738f242ba49f007850c7e27e14
David Inga <david.inga@vizzuality.com>
2023-02-23 14:03:16 +0100

    fixed analysis tests

--- client/cypress/e2e/analysis-chart.cy.ts
+++ client/cypress/e2e/analysis-chart.cy.ts
@@ -54,29 +54,4 @@ describe('Analysis charts', () => {
       cy.get('@chart').find('.recharts-responsive-container').and('have.length', 5);
     });
   });
-
-  it('should be able to select a scenario vs actual data in the comparison select', () => {
-    cy.wait('@scenariosNoPaginated');
-
-    cy.intercept(
-      'GET',
-      '/api/v1/**/trees?withSourcingLocations=true&scenarioIds[]=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7',
-    ).as('treesSelectorsWithScenarioId');
-
-    cy.get('[data-testid="scenario-item-null"]') // actual data
-      .find('[data-testid="select-comparison"]')
-      .click()
-      .find('input:visible')
-      .type('Test{enter}');
-
-    cy.url().should('contain', 'compareScenarioId=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7');
-
-    // checking tree selectors on more filers
-    cy.wait('@treesSelectorsWithScenarioId')
-      .its('request.url')
-      .should('contain', 'scenarioIds[]=8dfd0ce0-67b7-4f1d-be9c-41bc3ceafde7');
-
-    // checking the charts
-    cy.get('[data-testid="analysis-chart"]').should('not.be.empty');
-  });
 });

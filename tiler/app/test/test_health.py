ef8f68b8cb5c2cebff8ee28278f8e3903b81c333
alexeh <alexurola@gmail.com>
2023-02-25 11:37:44 +0300

    integrate s3 in tiler

--- tiler/app/test/test_health.py
+++ tiler/app/test/test_health.py
@@ -4,7 +4,7 @@ from ..main import app
 client = TestClient(app)
 
 
-def test_auth_middleware():
+def test_health():
     """Should throw a Unauthorized Exception if no token has been provided"""
     response = client.get("/cog/info")
     assert response.status_code == 400

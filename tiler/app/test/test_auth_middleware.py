25a05bca69c9a089d3ebad11478485cec8d4d0c5
alexeh <alexurola@gmail.com>
2023-03-07 07:45:39 +0300

    Add tiler tests

--- tiler/app/test/test_auth_middleware.py
+++ tiler/app/test/test_auth_middleware.py
@@ -1,16 +1,34 @@
+import os
+
 import pytest
 from unittest.mock import patch, Mock
 from starlette.testclient import TestClient
 
 from ..main import app
+from ..middlewares.url_injector import s3_presigned_access
 
 test_client = TestClient(app)
 
+def override_s3_pressigned_access(url: str | None):
+    return url
+
+# Inject a mock dependency
+app.dependency_overrides[s3_presigned_access] = override_s3_pressigned_access
+
 
 @patch('app.middlewares.auth_middleware.requests.get')
-def test_auth(mock_get):
+def test_auth_failing_api_validation(mock_get):
     mock = Mock()
-    mock.status_code = 100
+    mock.status_code = 401
     mock_get.return_value = mock
     response = test_client.get("/cog/info", headers={"Authorization": "Bearer my_token"})
-    assert response.status_code != 400
+    assert response.status_code == 401
+
+@patch('app.middlewares.auth_middleware.requests.get')
+def test_auth_correct_api_validation(mock_get):
+    mock = Mock()
+    mock.status_code = 200
+    mock_get.return_value = mock
+    response = test_client.get("/cog/info", headers={"Authorization": "Bearer my_token"})
+    assert response.status_code != 401
+

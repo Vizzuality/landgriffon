34370306efa8cfb38686765f4ff1271135b96f75
Andrés González Muñoz <andres.gonzalez@vizzuality.com>
2023-03-09 18:12:24 +0100

    WIP - client

---
+++ client/src/pages/ts-rest.tsx
@@ -0,0 +1,20 @@
+import { initQueryClient } from '@ts-rest/react-query';
+
+// import { PokemonCard } from '../components/PokemonCard';
+// import { pokemonApi } from '../pokeApi';
+
+// export const queryClient = initQueryClient(pokemonApi, {
+//   baseUrl: 'https://pokeapi.co/api/v2',
+//   baseHeaders: {},
+// });
+
+export default function TestRS() {
+  // const query = queryClient.getAllPokemon.useQuery(
+  //   ['pokemon'],
+  //   { query: { limit: 10 } },
+  //   { staleTime: Infinity },
+  // );
+
+  // const pokemon = query.data?.status === 200 ? query.data.body : null;
+  return null;
+}

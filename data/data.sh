297fa1182cf2f69f302bae47d9297c217ba9f8d3
elpamart <elena.palao@vizzuality.com>
2023-04-25 18:01:52 +0200

    update excel data ingestion with updated aggregation materials

--- data/data.sh
+++ data/data.sh
@@ -3,5 +3,5 @@ set -e
 
 COMPOSE_PROJECT_NAME="landgriffon"
 
-docker compose -f ./docker-compose.yml --project-name ${COMPOSE_PROJECT_NAME} build
-docker compose -f ./docker-compose.yml --project-name ${COMPOSE_PROJECT_NAME} run --rm landgriffon-seed-data "$@"
+docker-compose -f ./docker-compose.yml --project-name ${COMPOSE_PROJECT_NAME} build
+docker-compose -f ./docker-compose.yml --project-name ${COMPOSE_PROJECT_NAME} run --rm landgriffon-seed-data "$@"

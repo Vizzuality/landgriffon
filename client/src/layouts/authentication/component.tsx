da985fc92f5e367d454395c81f18d9850d893b65
David Inga <david.inga@vizzuality.com>
2023-04-27 15:01:48 +0200

    fixed warning on image on the login page

--- client/src/layouts/authentication/component.tsx
+++ client/src/layouts/authentication/component.tsx
@@ -6,7 +6,7 @@ import ToastContainer from 'containers/toaster';
 const AuthenticationLayout: React.FC<React.PropsWithChildren> = ({ children }) => (
   <div className="flex flex-col min-h-screen py-12 bg-auth sm:px-6 lg:px-8">
     <div className="text-center">
-      <Image src="/landgriffon-logo-white.svg" width={196} height={16} alt="Landgriffon logo" />
+      <Image src="/landgriffon-logo-white.svg" width={196} height={17} alt="Landgriffon logo" />
     </div>
     <div className="flex flex-col justify-center flex-1 pt-12">{children}</div>
     <ToastContainer position="bottom-center" />

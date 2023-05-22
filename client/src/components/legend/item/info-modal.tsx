71b3bf1c0895296ee6b2c29c3f2dd652c17b86b0
barbara-chaves <barbarabarcellos90@gmail.com>
2023-05-22 11:21:52 +0200

    Change layer legend info from tooltip to modal

---
+++ client/src/components/legend/item/info-modal.tsx
@@ -0,0 +1,46 @@
+import { InformationCircleIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+
+import Modal from 'components/modal/component';
+
+export type InfoModalProps = {
+  info: { title: string; description: string; source: string | string[] };
+};
+
+const InfoModal = ({ info: { title, description, source } }: InfoModalProps) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <button type="button" className="py-0 px-0" onClick={() => setOpen(true)}>
+        <InformationCircleIcon className="w-4 h-4" />
+      </button>
+      <Modal
+        onDismiss={() => setOpen(false)}
+        title={typeof title === 'string' ? title : ''}
+        open={open}
+      >
+        <div>
+          {typeof title !== 'string' && title}
+          <p className="text-gray-500 text-sm mb-4">{description}</p>
+          <p className="font-semibold text-sm mb-2">
+            Data layer source{Array.isArray(source) && source.length > 1 ? 's' : ''}:
+          </p>
+          {Array.isArray(source) ? (
+            <ul className="space-y-2">
+              {source.map((s) => (
+                <li key={s} className="text-gray-500 text-sm">
+                  {s}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p className="text-gray-500 text-sm">{source}</p>
+          )}
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default InfoModal;

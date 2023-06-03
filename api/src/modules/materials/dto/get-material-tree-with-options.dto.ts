c5c8dd2b7fe1eac4ea2e60d86f59fd202e26b23b
alexeh <alexurola@gmail.com>
2023-06-03 09:30:47 +0300

    Adds BaseQueryBuilder, CommonFilters dto

--- api/src/modules/materials/dto/get-material-tree-with-options.dto.ts
+++ api/src/modules/materials/dto/get-material-tree-with-options.dto.ts
@@ -1,18 +1,9 @@
-/**
- * Get Supplier with options:
- */
-import {
-  IsBoolean,
-  IsEnum,
-  IsNumber,
-  IsOptional,
-  IsUUID,
-} from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional, IsUUID } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { LOCATION_TYPES } from 'modules/sourcing-locations/sourcing-location.entity';
+import { CommonFiltersDto } from 'utils/base.query-builder';
 
-export class GetMaterialTreeWithOptionsDto {
+export class GetMaterialTreeWithOptionsDto extends CommonFiltersDto {
   @ApiPropertyOptional({
     description:
       'Return Materials with related Sourcing Locations. Setting this to true will override depth param',
@@ -28,51 +19,8 @@ export class GetMaterialTreeWithOptionsDto {
   @IsNumber()
   depth?: number;
 
-  // Below fields for smart filtering
-  @IsUUID('4', { each: true })
-  @ApiPropertyOptional()
-  @IsOptional()
-  supplierIds?: string[];
-
-  @IsUUID('4', { each: true })
-  @ApiPropertyOptional()
-  @IsOptional()
-  businessUnitIds?: string[];
-
-  @IsUUID('4', { each: true })
-  @ApiPropertyOptional()
-  @IsOptional()
-  originIds?: string[];
-
-  @IsUUID('4', { each: true })
-  @ApiPropertyOptional()
-  @IsOptional()
-  materialIds?: string[];
-
-  @ApiPropertyOptional({
-    description: 'Types of Sourcing Locations, written with hyphens',
-    enum: Object.values(LOCATION_TYPES),
-    name: 'locationTypes[]',
-  })
-  @IsOptional()
-  @IsEnum(LOCATION_TYPES, {
-    each: true,
-    message:
-      'Available options: ' +
-      Object.values(LOCATION_TYPES).toString().toLowerCase(),
-  })
-  @Type(() => String)
-  locationTypes?: LOCATION_TYPES[];
-
   @ApiPropertyOptional()
   @IsOptional()
   @IsUUID('4')
   scenarioId?: string;
-
-  @ApiPropertyOptional({
-    description: 'Array of Scenario Ids to include in the material search',
-  })
-  @IsOptional()
-  @IsUUID('4', { each: true })
-  scenarioIds?: string[];
 }
---
+++ api/src/utils/base.query-builder.ts
@@ -0,0 +1,136 @@
+import {
+  Brackets,
+  ObjectLiteral,
+  SelectQueryBuilder,
+  WhereExpressionBuilder,
+} from 'typeorm';
+import { IsEnum, IsOptional, IsUUID } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  LOCATION_TYPES,
+  SourcingLocation,
+} from 'modules/sourcing-locations/sourcing-location.entity';
+import { Type } from 'class-transformer';
+import {
+  SCENARIO_INTERVENTION_STATUS,
+  ScenarioIntervention,
+} from 'modules/scenario-interventions/scenario-intervention.entity';
+
+export class BaseQueryBuilder {
+  static addFilters<Entity extends ObjectLiteral>(
+    queryBuilder: SelectQueryBuilder<Entity>,
+    filters: CommonFiltersDto,
+  ): SelectQueryBuilder<Entity> {
+    queryBuilder.innerJoin(SourcingLocation, 'sl', 'sl.materialId = m.id');
+
+    if (filters.materialIds) {
+      queryBuilder.andWhere('sl.materialId IN (:...materialIds)', {
+        materialIds: filters.materialIds,
+      });
+    }
+    if (filters.t1SupplierIds) {
+      queryBuilder.andWhere('sl."t1SupplierId" IN (:...t1SupplierIds)', {
+        t1SupplierIds: filters.t1SupplierIds,
+      });
+    }
+    if (filters.producerIds) {
+      queryBuilder.andWhere('sl."producerId" IN (:...producerIds)', {
+        producerIds: filters.producerIds,
+      });
+    }
+    if (filters.businessUnitIds) {
+      queryBuilder.andWhere('sl.businessUnitId IN (:...businessUnitIds)', {
+        businessUnitIds: filters.businessUnitIds,
+      });
+    }
+    if (filters.originIds) {
+      queryBuilder.andWhere('sl.adminRegionId IN (:...originIds)', {
+        originIds: filters.originIds,
+      });
+    }
+
+    if (filters.locationTypes) {
+      queryBuilder.andWhere('sl.locationType IN (:...locationTypes)', {
+        locationTypes: filters.locationTypes,
+      });
+    }
+    // TODO: we could externalise this to something generic as well
+    if (filters.scenarioIds) {
+      queryBuilder.leftJoin(
+        ScenarioIntervention,
+        'scenarioIntervention',
+        'sl.scenarioInterventionId = scenarioIntervention.id',
+      );
+
+      queryBuilder.andWhere(
+        new Brackets((qb: WhereExpressionBuilder) => {
+          qb.where('sl.scenarioInterventionId is null').orWhere(
+            new Brackets((qbInterv: WhereExpressionBuilder) => {
+              qbInterv
+                .where('scenarioIntervention.scenarioId IN (:...scenarioIds)', {
+                  scenarioIds: filters.scenarioIds,
+                })
+                .andWhere(`scenarioIntervention.status = :status`, {
+                  status: SCENARIO_INTERVENTION_STATUS.ACTIVE,
+                });
+            }),
+          );
+        }),
+      );
+    } else {
+      queryBuilder.andWhere('sl.scenarioInterventionId is null');
+      queryBuilder.andWhere('sl.interventionType is null');
+    }
+
+    return queryBuilder;
+  }
+}
+
+export class CommonFiltersDto {
+  @IsUUID('4', { each: true })
+  @ApiPropertyOptional()
+  @IsOptional()
+  t1SupplierIds?: string[];
+
+  @IsUUID('4', { each: true })
+  @ApiPropertyOptional()
+  @IsOptional()
+  producerIds?: string[];
+
+  @IsUUID('4', { each: true })
+  @ApiPropertyOptional()
+  @IsOptional()
+  businessUnitIds?: string[];
+
+  @IsUUID('4', { each: true })
+  @ApiPropertyOptional()
+  @IsOptional()
+  originIds?: string[];
+
+  @IsUUID('4', { each: true })
+  @ApiPropertyOptional()
+  @IsOptional()
+  materialIds?: string[];
+
+  @ApiPropertyOptional({
+    description: 'Types of Sourcing Locations, written with hyphens',
+    enum: Object.values(LOCATION_TYPES),
+    name: 'locationTypes[]',
+  })
+  @IsOptional()
+  @IsEnum(LOCATION_TYPES, {
+    each: true,
+    message:
+      'Available options: ' +
+      Object.values(LOCATION_TYPES).toString().toLowerCase(),
+  })
+  @Type(() => String)
+  locationTypes?: LOCATION_TYPES[];
+
+  @ApiPropertyOptional({
+    description: 'Array of Scenario Ids to include in the material search',
+  })
+  @IsOptional()
+  @IsUUID('4', { each: true })
+  scenarioIds?: string[];
+}

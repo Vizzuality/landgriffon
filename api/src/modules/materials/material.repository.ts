7375a5533930a54f79002209842c6aaa5a7dfcba
alexeh <alexurola@gmail.com>
2023-06-03 09:30:47 +0300

    Adds BaseQueryBuilder, CommonFilters dto

--- api/src/modules/materials/material.repository.ts
+++ api/src/modules/materials/material.repository.ts
@@ -1,19 +1,10 @@
-import {
-  Brackets,
-  DataSource,
-  SelectQueryBuilder,
-  WhereExpressionBuilder,
-} from 'typeorm';
+import { DataSource, SelectQueryBuilder } from 'typeorm';
 import { Material } from 'modules/materials/material.entity';
 import { ExtendedTreeRepository } from 'utils/tree.repository';
 import { CreateMaterialDto } from 'modules/materials/dto/create.material.dto';
 import { Injectable, Logger } from '@nestjs/common';
-import { SourcingLocation } from 'modules/sourcing-locations/sourcing-location.entity';
 import { GetMaterialTreeWithOptionsDto } from 'modules/materials/dto/get-material-tree-with-options.dto';
-import {
-  SCENARIO_INTERVENTION_STATUS,
-  ScenarioIntervention,
-} from 'modules/scenario-interventions/scenario-intervention.entity';
+import { BaseQueryBuilder } from 'utils/base.query-builder';
 
 @Injectable()
 export class MaterialRepository extends ExtendedTreeRepository<
@@ -36,72 +27,14 @@ export class MaterialRepository extends ExtendedTreeRepository<
     materialTreeOptions: GetMaterialTreeWithOptionsDto,
     withAncestry: boolean = true,
   ): Promise<Material[]> {
-    // Join and filters over materials present in sourcing-locations. Resultant query returns IDs of elements meeting the filters
-    const queryBuilder: SelectQueryBuilder<Material> = this.createQueryBuilder(
-      'm',
-    )
-      .innerJoin(SourcingLocation, 'sl', 'sl.materialId = m.id')
-      .distinct(true);
+    const initialQueryBuilder: SelectQueryBuilder<Material> =
+      this.createQueryBuilder('m').distinct(true);
 
-    if (materialTreeOptions.materialIds) {
-      queryBuilder.andWhere('sl.materialId IN (:...materialIds)', {
-        materialIds: materialTreeOptions.materialIds,
-      });
-    }
-    if (materialTreeOptions.supplierIds) {
-      queryBuilder.andWhere(
-        new Brackets((qb: WhereExpressionBuilder) => {
-          qb.where('sl."t1SupplierId" IN (:...suppliers)', {
-            suppliers: materialTreeOptions.supplierIds,
-          }).orWhere('sl."producerId" IN (:...suppliers)', {
-            suppliers: materialTreeOptions.supplierIds,
-          });
-        }),
+    const queryBuilder: SelectQueryBuilder<Material> =
+      BaseQueryBuilder.addFilters<Material>(
+        initialQueryBuilder,
+        materialTreeOptions,
       );
-    }
-    if (materialTreeOptions.businessUnitIds) {
-      queryBuilder.andWhere('sl.businessUnitId IN (:...businessUnitIds)', {
-        businessUnitIds: materialTreeOptions.businessUnitIds,
-      });
-    }
-    if (materialTreeOptions.originIds) {
-      queryBuilder.andWhere('sl.adminRegionId IN (:...originIds)', {
-        originIds: materialTreeOptions.originIds,
-      });
-    }
-
-    if (materialTreeOptions.locationTypes) {
-      queryBuilder.andWhere('sl.locationType IN (:...locationTypes)', {
-        locationTypes: materialTreeOptions.locationTypes,
-      });
-    }
-    // TODO: we could externalise this to something generic as well
-    if (materialTreeOptions.scenarioIds) {
-      queryBuilder.leftJoin(
-        ScenarioIntervention,
-        'scenarioIntervention',
-        'sl.scenarioInterventionId = scenarioIntervention.id',
-      );
-
-      queryBuilder.andWhere(
-        new Brackets((qb: WhereExpressionBuilder) => {
-          qb.where('sl.scenarioInterventionId is null').orWhere(
-            new Brackets((qbInterv: WhereExpressionBuilder) => {
-              qbInterv
-                .where('scenarioIntervention.scenarioId IN (:...scenarioIds)', {
-                  scenarioIds: materialTreeOptions.scenarioIds,
-                })
-                .andWhere(`scenarioIntervention.status = :status`, {
-                  status: SCENARIO_INTERVENTION_STATUS.ACTIVE,
-                });
-            }),
-          );
-        }),
-      );
-    } else {
-      queryBuilder.andWhere('sl.scenarioInterventionId is null');
-      queryBuilder.andWhere('sl.interventionType is null');
-    }
 
     if (!withAncestry) {
       return queryBuilder.getMany();

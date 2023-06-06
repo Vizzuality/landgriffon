348a9b9e3d29b7fbf8b6bc051426b01e7cff71c9
alexeh <alexurola@gmail.com>
2023-06-06 12:50:22 +0300

    import asyncparser in a Non-ESM way to avoid module resolution conflicts

--- api/src/modules/tasks/report.service.ts
+++ api/src/modules/tasks/report.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { AsyncParser } from '@json2csv/node';
+
+// import { AsyncParser } from '@json2csv/node';
+// Above blows because of node module resolution. if updated to node16 or nodenext, other
+// dependencies cant be transpiled. We could update some, but i.e class-validator is at its last version
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { AsyncParser } = require('@json2csv/node');
 
 export interface ErrorRecord {
   line: number;
@@ -15,7 +20,7 @@ export class ReportService {
       fields: ['line', 'error'],
     };
 
-    const asyncParser: AsyncParser<any, any> = new AsyncParser(json2csvOptions);
+    const asyncParser: typeof AsyncParser = new AsyncParser(json2csvOptions);
 
     return asyncParser.parse(errors).promise();
   }

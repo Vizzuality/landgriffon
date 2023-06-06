cd87b2f29d47e07da77f4cf5885d785021aae451
alexeh <alexurola@gmail.com>
2023-06-06 07:52:04 +0300

    update deforestation column name

--- api/src/migrations/1664874035162-UpdatedMethodologyStoredFunctions.ts
+++ api/src/migrations/1664874035162-UpdatedMethodologyStoredFunctions.ts
@@ -51,7 +51,7 @@ $$
         material_h3_column_name varchar;
         h3_resolution integer;
         deforestation_h3_table_name varchar := 'h3_grid_deforestation_global'; --update with new deforestation table
-        deforestation_h3_column_name varchar := 'hansenLoss2020HaBuffered';
+        deforestation_h3_column_name varchar := 'hansenLossBuffered2021';
         sum float;
 
     BEGIN

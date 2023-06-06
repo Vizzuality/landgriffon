54b0664ceb5a0698df7a0f1375da6853b17cade9
alexeh <alexurola@gmail.com>
2023-06-06 07:54:30 +0300

    update ghg indicator column name

--- api/src/migrations/1664874035162-UpdatedMethodologyStoredFunctions.ts
+++ api/src/migrations/1664874035162-UpdatedMethodologyStoredFunctions.ts
@@ -86,7 +86,7 @@ $$
         material_h3_column_name varchar;
         h3_resolution integer;
         carbon_h3_table_name varchar := 'h3_grid_ghg_global';
-        carbon_h3_column_name varchar := 'forestGhg2020Buffered';
+        carbon_h3_column_name varchar := 'forestGhgBuffered2021';
         sum float;
 
     BEGIN

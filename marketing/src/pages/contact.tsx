683356b4a80a4d9e6bcea939e15471d389ce6756
Maria Sola <maria.sola@vizzuality.com>
2023-06-14 16:20:54 +0200

    OG descriptions updated

--- marketing/src/pages/contact.tsx
+++ marketing/src/pages/contact.tsx
@@ -11,7 +11,7 @@ const ContactPage: FC = () => {
     <ApplicationLayout>
       <>
         <Head>
-          <title>Contact us - Landgriffon</title>
+          <title>Sustainable Supply Chain Solutions: Contact Us! | LandGriffon</title>
           <link rel="icon" href="/favicon.ico" />
           <meta name="viewport" content="width=device-width" />
 

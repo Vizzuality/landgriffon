d15d6665ef8125e3750b6b3f3dca07ae532f9ce8
Maria Sola <maria.sola@vizzuality.com>
2023-06-20 13:54:38 +0200

    fixes in img and underlined links

--- marketing/src/containers/the-service/steps/03/component.tsx
+++ marketing/src/containers/the-service/steps/03/component.tsx
@@ -51,6 +51,7 @@ const Step03: React.FC = () => {
                   href="https://resourcewatch.org/data/explore/wat050-Aqueduct-Baseline-Water-Stress?section=Discover&selectedCollection=&zoom=3&lat=0&lng=0&pitch=0&bearing=0&basemap=dark&labels=light&layers=%255B%257B%2522dataset%2522%253A%2522c66d7f3a-d1a8-488f-af8b-302b0f2c3840%2522%252C%2522opacity%2522%253A1%252C%2522layer%2522%253A%2522fdf06d8c-72e9-48a7-80f1-27bd5f19342c%2522%257D%255D&aoi=&page=1&sort=most-viewed&sortDirection=-1"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   Aqueduct
                 </a>
@@ -59,6 +60,7 @@ const Step03: React.FC = () => {
                   href="https://waterfootprint.org/en/resources/waterstat/product-water-footprint-statistics/"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   Water Footprint Network
                 </a>
@@ -67,11 +69,21 @@ const Step03: React.FC = () => {
             <li className="relative pl-5 before:absolute before:top-2 before:left-0 before:w-1.5 before:h-1.5 before:bg-black">
               <p className="font-semibold">Cropland & yields:</p>
               <p>
-                <a href="https://www.mapspam.info/data/" target="_blank" rel="noreferrer noopener">
+                <a
+                  href="https://www.mapspam.info/data/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="underline"
+                >
                   MapSPAM
                 </a>
                 ,{' '}
-                <a href="http://www.earthstat.org/" target="_blank" rel="noreferrer noopener">
+                <a
+                  href="http://www.earthstat.org/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="underline"
+                >
                   Earthstat
                 </a>
                 ,{' '}
@@ -79,6 +91,7 @@ const Step03: React.FC = () => {
                   href="https://www.fao.org/faostat/en/#home"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   FAOSTAT
                 </a>
@@ -87,6 +100,7 @@ const Step03: React.FC = () => {
                   href="https://www.fao.org/land-water/land/land-governance/land-resources-planning-toolbox/category/details/en/c/1236449/"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   Gridded Livestock of The World
                 </a>
@@ -96,7 +110,12 @@ const Step03: React.FC = () => {
               <p className="font-semibold">Forest loss & Land Carbon:</p>
 
               <p>
-                <a href="https://land.copernicus.eu/" target="_blank" rel="noreferrer noopener">
+                <a
+                  href="https://land.copernicus.eu/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="underline"
+                >
                   Copernicus
                 </a>
                 ,{' '}
@@ -104,6 +123,7 @@ const Step03: React.FC = () => {
                   href="https://satelligence.com/solutions"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   Satelligence
                 </a>
@@ -112,6 +132,7 @@ const Step03: React.FC = () => {
                   href="https://www.globalforestwatch.org/map/?map=eyJkYXRhc2V0cyI6W3siZGF0YXNldCI6InRyZWUtY292ZXItbG9zcyIsIm9wYWNpdHkiOjEsInZpc2liaWxpdHkiOnRydWUsImxheWVycyI6WyJ0cmVlLWNvdmVyLWxvc3MiXX0seyJkYXRhc2V0IjoicG9saXRpY2FsLWJvdW5kYXJpZXMiLCJsYXllcnMiOlsiZGlzcHV0ZWQtcG9saXRpY2FsLWJvdW5kYXJpZXMiLCJwb2xpdGljYWwtYm91bmRhcmllcyJdLCJvcGFjaXR5IjoxLCJ2aXNpYmlsaXR5Ijp0cnVlfV19&mapMenu=eyJtZW51U2VjdGlvbiI6ImRhdGFzZXRzIiwiZGF0YXNldENhdGVnb3J5IjoiZm9yZXN0Q2hhbmdlIn0%3D"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   Global Forest Watch
                 </a>
@@ -124,11 +145,17 @@ const Step03: React.FC = () => {
                   href="https://www.worldwildlife.org/pages/conservation-science-data-and-tools"
                   target="_blank"
                   rel="noreferrer noopener"
+                  className="underline"
                 >
                   WWF Eco
                 </a>
                 ,{' '}
-                <a href="https://www.ibat-alliance.org/" target="_blank" rel="noreferrer noopener">
+                <a
+                  href="https://www.ibat-alliance.org/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                  className="underline"
+                >
                   IBAT
                 </a>
               </p>

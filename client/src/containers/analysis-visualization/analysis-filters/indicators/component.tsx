2fbed817d867a196865e702eed165c374ae87a06
barbara-chaves <barbarabarcellos90@gmail.com>
2023-07-05 12:19:06 -0300

    Fix

--- client/src/containers/analysis-visualization/analysis-filters/indicators/component.tsx
+++ client/src/containers/analysis-visualization/analysis-filters/indicators/component.tsx
@@ -72,7 +72,7 @@ const IndicatorsFilter = () => {
   );
 
   return (
-    <div className="bg-white rounded-md w-[280px]">
+    <div className="bg-white rounded-md">
       <Select
         data-testid="indicators-filter"
         value={current}

d2edf053b5f304c6641d6b6cf175774af9194a85
barbara-chaves <barbarabarcellos90@gmail.com>
2023-07-06 10:27:51 -0300

    Add avatar to user list

--- client/src/containers/user-dropdown/component.tsx
+++ client/src/containers/user-dropdown/component.tsx
@@ -1,14 +1,14 @@
 import { useCallback } from 'react';
 import Link from 'next/link';
 import { createPortal } from 'react-dom';
-import { signOut, useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import { Menu } from '@headlessui/react';
 import { offset, useFloating } from '@floating-ui/react-dom';
 import { shift } from '@floating-ui/core';
 
-import StringAvatar from 'components/string-avatar';
-import Avatar from 'components/avatar';
 import Loading from 'components/loading';
+import UserAvatar from 'containers/user-avatar';
+import { useProfile } from 'hooks/profile';
 
 const MENU_ITEM_CLASSNAME =
   'block w-full py-2 px-4 text-sm text-left text-gray-900 h-9 hover:bg-navy-50 focus-visible:outline-navy-50';
@@ -19,32 +19,21 @@ const UserDropdown: React.FC = () => {
     middleware: [offset({ crossAxis: 20, mainAxis: 10 }), shift()],
   });
 
-  const { data: session, status } = useSession(); // TO-DO: replace by useMe
+  const { data: user, status } = useProfile();
 
   const handleSignOut = useCallback(() => signOut({ callbackUrl: '/auth/signin' }), []);
 
-  const renderAvatar = useCallback(
-    (className?: string) => {
-      if (!session) return null;
-      const { user } = session || {};
-      if (user?.image) return <Avatar alt="user avatar" src={user?.image} className={className} />;
-      if (user?.name) return <StringAvatar fullName={user.name} className={className} />;
-      return <StringAvatar fullName={user.email} className={className} />;
-    },
-    [session],
-  );
-
   return (
     <Menu as="div" className="flex justify-center flex-col items-center w-full mb-5">
-      {(!session || status === 'loading') && <Loading className="w-5 h-5 text-white" />}
+      {(!user || status === 'loading') && <Loading className="w-5 h-5 text-white" />}
       <Menu.Button
         className="focus-visible:shadow-button-focused focus-visible:outline-none rounded-lg shadow-menu hover:shadow-button-hovered"
         ref={reference}
       >
-        {renderAvatar('bg-black/20')}
+        <UserAvatar user={user} className="bg-black/20 h-[50px] w-[50px] " />
       </Menu.Button>
       <span className="text-white text-xs mt-3">Account</span>
-      {!!session &&
+      {!!user &&
         createPortal(
           <Menu.Items
             ref={floating}
@@ -56,11 +45,11 @@ const UserDropdown: React.FC = () => {
             className="z-40 p-6 bg-white rounded-md shadow-lg focus:outline-none"
           >
             <div className="flex mb-3 items-center">
-              {renderAvatar('w-14 h-14')}
+              <UserAvatar user={user} className="w-14 h-14" />
 
               <div className="ml-4">
-                <span className="block text-lg leading-8 text-gray-900">{session?.user.name}</span>
-                <span className="block text-sm leading-7 text-gray-400">{session?.user.email}</span>
+                <span className="block text-lg leading-8 text-gray-900">{user.displayName}</span>
+                <span className="block text-sm leading-7 text-gray-400">{user.email}</span>
               </div>
             </div>
 

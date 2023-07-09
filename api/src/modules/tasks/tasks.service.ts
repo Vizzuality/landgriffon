3c2e95751ce1ebc0437d07837291a2ab1c873be6
alexeh <alexurola@gmail.com>
2023-07-09 16:04:42 +0300

    Revert "Generate basic error report"
    This reverts commit 0148475c6ce40c9388fc5b04800f7c7ef39d81cb.

--- api/src/modules/tasks/tasks.service.ts
+++ api/src/modules/tasks/tasks.service.ts
@@ -16,8 +16,6 @@ import {
   UpdateImportTask,
   UpdateTaskDto,
 } from 'modules/tasks/dto/update-task.dto';
-import { GetReportsDto } from 'modules/tasks/dto/get-reports.dto';
-import { ErrorRecord, ReportService } from 'modules/tasks/report.service';
 
 @Injectable()
 export class TasksService extends AppBaseService<
@@ -26,10 +24,7 @@ export class TasksService extends AppBaseService<
   UpdateTaskDto,
   AppInfoDTO
 > {
-  constructor(
-    public readonly taskRepository: TasksRepository,
-    public readonly reportService: ReportService,
-  ) {
+  constructor(public readonly taskRepository: TasksRepository) {
     super(
       taskRepository,
       taskResource.name.singular,
@@ -128,21 +123,5 @@ export class TasksService extends AppBaseService<
         'Task failed due to a system error. Please contact support.';
       await stalledTask.save();
     }
-  }
-
-  async getTaskErrorReport(
-    taskId: string,
-    reportDto: GetReportsDto,
-  ): Promise<string> {
-    const task: Task | null = await this.taskRepository.findOne({
-      where: { id: taskId, type: reportDto.type },
-    });
-    if (!task) {
-      throw new NotFoundException(`Could not found Task with ID: ${taskId}`);
-    }
-    const { errors } = task;
-    return this.reportService.generateImportErrorReportStream(
-      errors as ErrorRecord[],
-    );
   }
 }

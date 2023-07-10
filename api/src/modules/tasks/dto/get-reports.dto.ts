f6e6481cfb04c1dfcd5080a9022f811c5cb120b4
alexeh <alexurola@gmail.com>
2023-07-10 08:37:32 +0300

    Revert "Generate basic error report"
    This reverts commit 0148475c6ce40c9388fc5b04800f7c7ef39d81cb.

--- api/src/modules/tasks/dto/get-reports.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsEnum } from 'class-validator';
-import { TASK_TYPE } from 'modules/tasks/task.entity';
-
-export class GetReportsDto {
-  @ApiProperty()
-  @IsEnum(TASK_TYPE)
-  type!: TASK_TYPE;
-}

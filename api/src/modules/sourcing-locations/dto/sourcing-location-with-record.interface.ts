24f03be8a72aeed324bc5f6d6e0f37480916c697
alexeh <alexurola@gmail.com>
2023-08-07 08:23:52 +0300

    t1supplier and producer required for sourcing locations

--- api/src/modules/sourcing-locations/dto/sourcing-location-with-record.interface.ts
+++
@@ -1,6 +0,0 @@
-import { SourcingLocation } from 'modules/sourcing-locations/sourcing-location.entity';
-
-export interface SourcingLocationWithRecord extends SourcingLocation {
-  year: number;
-  tonnage: number;
-}

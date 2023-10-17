32006245c400ef29c202a9a82bdd38a3e407ddce
elpamart <elena.palao@vizzuality.com>
2023-10-17 20:00:10 +0200

    includes harvest area preprocessing

--- data/preprocessing/livestock_processed/preprocess_faostats.py
+++ data/preprocessing/livestock_processed/preprocess_faostats.py
@@ -39,38 +39,42 @@ def get_country_geometry():
     return countries_df
 
 
-def open_clean(file):
+def open_clean(file, data_type):
     # Define the units available for conversion
-    units_to_tonnes = {"100 g/An": 100000, "100 mg/An": 1000000}
+    units_to_tonnes = {"100 g/An": 100000, "100 mg/An": 1000000, "LSU/ha": 1}
 
     # Open the file
     df = pd.read_csv(file)
 
-    # check the units from the dataframe and the conversionneeded
+    # Check unique units from the dataframe
     unit_list = list(df["Unit"].unique())
-    # check the conversion from the unit_to_tonnes dictionary
-    if len(unit_list) == 1:
-        conversion = units_to_tonnes[unit_list[0]]
+
+    # Check if there is a single unit and it exists in the conversion dictionary
+    if len(unit_list) == 1 and unit_list[0] in units_to_tonnes:
         unit = unit_list[0]
+        conversion = units_to_tonnes[unit]
     else:
-        log.info("There is more than one unit in the dataframe")
-        # check which of my units of the dataframe exist in the units_to_tonnes dictionary
-        conversion_list = [units_to_tonnes[unit] for unit in unit_list if unit in units_to_tonnes.keys()]
-        unit_list = [unit for unit in unit_list if unit in units_to_tonnes.keys()]
-        if len(conversion_list) == 1:
-            conversion = conversion_list[0]
-            unit = unit_list[0]
+        # Check which units from the dataframe exist in the conversion dictionary
+        valid_units = [unit for unit in unit_list if unit in units_to_tonnes]
+        if len(valid_units) == 1:
+            unit = valid_units[0]
+            conversion = units_to_tonnes[unit]
         else:
-            log.info("There is more than one conversion in the dictionary")
-            conversion = None
-            unit = None
-    # Clean file and get just the 100 mg by animal value
-    df = df[["Area Code (ISO3)", "Year", "Unit", "Value"]]
-    df = df[df["Unit"] == unit]
+            log.info("Multiple valid units or conversions found.")
+            return None
+
+    # Clean file and get just the specified unit value
+    df = df.loc[df["Unit"] == unit, ["Area Code (ISO3)", "Year", "Unit", "Value"]]
 
     # Convert the 100mg /Animal to T/Animal
     df["Value"] = df["Value"] / conversion
-    df["Unit"] = df["Unit"].replace(unit, "T/An")
+    if data_type == "production":
+        df["Unit"] = "T/An"
+    elif data_type == "harvest":
+        df["Unit"] = "LSU/ha"
+    else:
+        log.warning("Invalid data type specified.")
+        return None
 
     return df
 
@@ -89,16 +93,18 @@ def merge_faostat_country(df, countries_df, output_file):
     return df
 
 
-def process_faostats_data(input_file, output_file):
+def process_faostats_data(input_file, output_file, data_type):
     # Get the country geometry
     countries_df = get_country_geometry()
 
     # Open and clean the file
-    df = open_clean(input_file)
+    df = open_clean(input_file, data_type)
 
     # create a preprocess directory if it does not exist
-    if not os.path.exists("./data/faostats_processed"):
-        os.makedirs("./data/faostats_processed")
+    if not os.path.exists("./data/faostats_processed") and data_type == "production":
+        os.makedirs("./data/faostats_processed/production")
+    elif not os.path.exists("./data/faostats_processed") and data_type == "harvest":
+        os.makedirs("./data/faostats_processed/harvest")
 
     # Merge the data with the country geometry
     df = merge_faostat_country(df, countries_df, output_file)
@@ -111,10 +117,11 @@ def main():
     parser = argparse.ArgumentParser(description="Process livestock preprocessed faostats data.")
     parser.add_argument("input_file", type=str, help="Path to the input file containing vector files")
     parser.add_argument("output_file", type=str, help="Path to the output file to save processed data")
+    parser.add_argument("data_type", type=str, help="Type of data to process")
     args = parser.parse_args()
 
     # Process the specified folder
-    process_faostats_data(args.input_file, args.output_file)
+    process_faostats_data(args.input_file, args.output_file, args.data_type)
 
 
 if __name__ == "__main__":

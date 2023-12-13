262be5bd55a9a39c0382e8c2797ae427f1c33996
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2023-12-13 12:52:21 +0100

    Area charts: filtered by legend

--- client/src/containers/analysis-chart/impact-chart/legend/component.tsx
+++ client/src/containers/analysis-chart/impact-chart/legend/component.tsx
@@ -14,27 +14,39 @@ interface ExtendedPayload extends Payload {
 
 export interface ExtendedLegendProps extends LegendProps {
   payload: ExtendedPayload[];
+  legendKey: string | null;
 }
 
-const LegendChart: React.FC<ExtendedLegendProps> = ({ payload, onClick = () => null }) => {
+const LegendChart: React.FC<ExtendedLegendProps> = ({
+  payload,
+  legendKey,
+  onClick = () => null,
+}) => {
   const handleClick = useCallback(onClick, [onClick]);
 
   return (
-    <div className="flex justify-between">
-      <ul className="flex flex-wrap gap-x-2 gap-y-4">
+    <div className="flex justify-between space-x-2.5">
+      <ul className="flex flex-wrap gap-1.5">
         {payload
           .filter(({ type }) => type !== 'none')
           .map((item, index) => (
             <li
               key={item.value}
-              className={classNames('flex items-center mr-2 space-x-1 cursor-pointer', {
-                'opacity-50': item.payload.fillOpacity === 0.1,
-              })}
+              className={classNames(
+                'flex items-center space-x-1 cursor-pointer border py-0.5 px-1 rounded',
+                {
+                  'opacity-50': legendKey && legendKey !== item.id,
+                },
+              )}
               onClick={handleClick.bind(null, item, index)}
             >
               <div
-                className="w-[6px] h-3 rounded shrink-0 grow-0"
-                style={{ backgroundColor: `${item.color}` }}
+                className="w-[6px] h-3 rounded shrink-0 grow-0 bg-gray-400"
+                style={{
+                  ...((!legendKey || legendKey === item.id) && {
+                    backgroundColor: `${item.color}`,
+                  }),
+                }}
               />
               <div
                 className="overflow-hidden text-xs whitespace-nowrap text-ellipsis max-w-[100px]"
@@ -48,7 +60,7 @@ const LegendChart: React.FC<ExtendedLegendProps> = ({ payload, onClick = () => n
       <div>
         <div className="flex items-center space-x-1">
           <ProjectedDataIcon />
-          <div className="text-xs whitespace-nowrap">Projected data</div>
+          <div className="text-xs whitespace-nowrap py-1">Projected data</div>
         </div>
       </div>
     </div>

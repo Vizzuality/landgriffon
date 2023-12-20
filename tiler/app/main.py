323d9177098cc85db4d6e3877652f56f556128ce
Biel Stela <biel.stela@gmail.com>
2023-12-20 10:23:51 +0100

    Adds contextual rasters, fix s3 url in tiler and format tiler style

--- tiler/app/main.py
+++ tiler/app/main.py
@@ -1,6 +1,4 @@
-import os
-
-from fastapi import FastAPI, Query
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from titiler.core import TilerFactory
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
@@ -11,20 +9,24 @@ from .middlewares.auth_middleware import AuthMiddleware
 from .middlewares.s3_access import s3_presigned_access
 
 root_path = get_settings().root_path
-titiler_router_prefix = get_settings().titiler_router_prefix if get_settings().titiler_router_prefix is not None else "/cog"
+titiler_router_prefix = (
+    get_settings().titiler_router_prefix if get_settings().titiler_router_prefix is not None else "/cog"
+)
 titiler_prefix = get_settings().titiler_prefix if get_settings().titiler_prefix is not None else "/cog"
 
-app = FastAPI(title="LandGriffon Tiler", docs_url='/tiler/docs', openapi_url='/tiler')
+app = FastAPI(title="LandGriffon Tiler", docs_url="/tiler/docs", openapi_url="/tiler", debug=True)
 app.add_middleware(TotalTimeMiddleware)
 app.add_middleware(LoggerMiddleware)
 app.add_middleware(AuthMiddleware)
-app.add_middleware(CORSMiddleware, allow_origins=["*"],
-                   allow_methods=["GET"],
-                   allow_headers=["*"], )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 # single COG tiler. One file can have multiple bands
 cog = TilerFactory(router_prefix=titiler_router_prefix, path_dependency=s3_presigned_access)
-#cog = TilerFactory(router_prefix="/cog")
 app.include_router(cog.router, tags=["Cloud Optimized GeoTIFF"], prefix=titiler_prefix)
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
 
--- tiler/app/middlewares/s3_access.py
+++ tiler/app/middlewares/s3_access.py
@@ -3,30 +3,32 @@ from fastapi.params import Query
 import boto3
 from ..config.config import get_settings
 
-s3 = boto3.client("s3", region_name="eu-west-3", config=Config(signature_version='s3v4'))
+s3 = boto3.client("s3", region_name="eu-west-3", config=Config(signature_version="s3v4"))
 bucket_name = get_settings().s3_bucket_name
 default_cog = get_settings().default_cog
-DATA_PATH_IN_S3 = 'processed/satelligence/'
+DATA_PATH_IN_S3 = "processed/cogs/"
 
 
 # # TODO: This only allows to access data hosted on owr s3 bucket. At some point we will need to discriminate
 #         external resources i.e datasets that are publicly available
 
+
 def s3_presigned_access(url: str | None = Query(default=None, description="Optional dataset URL")) -> str:
     """
-        Generate a pre-signed URL for an Amazon S3 object.
+    Generate a pre-signed URL for an Amazon S3 object.
 
-        Args:
-            url (str | None, optional): The URL of the S3 object to generate a pre-signed URL for. If not provided, a default URL is used. Defaults to None.
+    Args:
+        url (str | None, optional): The URL of the S3 object to generate a pre-signed URL for. If not provided,
+        a default URL is used. Defaults to None.
 
-        Returns:
-            str: A pre-signed URL that can be used to access the S3 object.
+    Returns:
+        str: A pre-signed URL that can be used to access the S3 object.
 
-        Raises:
-            botocore.exceptions.NoCredentialsError: If AWS credentials are not found.
+    Raises:
+        botocore.exceptions.NoCredentialsError: If AWS credentials are not found.
 
-        Note:
-            This function requires the `boto3` library to be installed, and valid AWS credentials to be configured.
+    Note:
+        This function requires the `boto3` library to be installed, and valid AWS credentials to be configured.
 
     """
     if not url:
@@ -34,14 +36,8 @@ def s3_presigned_access(url: str | None = Query(default=None, description="Optio
             raise ValueError("DEFAULT_COG env var is not set. It is required if no URL is passed to the tiler")
         url = default_cog
     presigned_url = s3.generate_presigned_url(
-        'get_object',
-        Params={
-            'Bucket': bucket_name,
-            'Key': DATA_PATH_IN_S3 + url
-        },
-        ExpiresIn=3600
+        "get_object",
+        Params={"Bucket": bucket_name, "Key": DATA_PATH_IN_S3 + url},
+        ExpiresIn=3600,
     )
     return presigned_url
-
-
-

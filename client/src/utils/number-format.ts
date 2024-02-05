15dd857456a9f120b09f95865410eae0a9755743
Biel Stela <biel.stela@gmail.com>
2024-02-05 11:47:03 +0100

    Fix small number handling with threshold and formatting

--- client/src/utils/number-format.ts
+++ client/src/utils/number-format.ts
@@ -1,14 +1,17 @@
 import { format } from 'd3-format';
 
 // for numbers bigger than 1
-export const NUMBER_FORMAT = format('.3~s');
+export const NUMBER_FORMAT = format('.2~s');
 
 // for numbers smaller than 1
-export const SMALL_NUMBER_FORMAT = format('.3~g');
+export const SMALL_NUMBER_FORMAT = format('.2~g');
 
 export function formatNumber(number: number): string {
-  if (number < 1) {
-    return SMALL_NUMBER_FORMAT(number);
+  if (Math.abs(number) < 1) {
+    if (Math.abs(number) < 0.001) {
+      return "< 0.001";
+    }
+    return SMALL_NUMBER_FORMAT(number)
   }
   return NUMBER_FORMAT(number);
 }

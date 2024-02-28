f92f821fe3e78389d3a3634c7c285eac2dc01a73
alexeh <alexurola@gmail.com>
2024-02-28 09:10:05 +0300

    Add initial get eudr georegions

--- api/src/modules/geo-regions/geo-regions.module.ts
+++ api/src/modules/geo-regions/geo-regions.module.ts
@@ -4,9 +4,17 @@ import { GeoRegionsController } from 'modules/geo-regions/geo-regions.controller
 import { GeoRegionsService } from 'modules/geo-regions/geo-regions.service';
 import { GeoRegion } from 'modules/geo-regions/geo-region.entity';
 import { GeoRegionRepository } from 'modules/geo-regions/geo-region.repository';
+import { AdminRegionsModule } from 'modules/admin-regions/admin-regions.module';
+import { MaterialsModule } from 'modules/materials/materials.module';
+import { SuppliersModule } from 'modules/suppliers/suppliers.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GeoRegion])],
+  imports: [
+    TypeOrmModule.forFeature([GeoRegion]),
+    AdminRegionsModule,
+    MaterialsModule,
+    SuppliersModule,
+  ],
   controllers: [GeoRegionsController],
   providers: [GeoRegionsService, GeoRegionRepository],
   exports: [GeoRegionsService, GeoRegionRepository],

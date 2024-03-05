a41010a56cd7cc2d44c111e1f4687f468eb3472e
Andrés González <andres.gonzalez@vizzuality.com>
2024-03-05 18:24:42 +0100

    eudr - categories breakdown

--- client/src/containers/analysis-eudr/category-list/breakdown/breakdown-item/index.tsx
+++ client/src/containers/analysis-eudr/category-list/breakdown/breakdown-item/index.tsx
@@ -1,4 +1,4 @@
-import { formatPercentage } from '@/utils/number-format';
+import type { ReactNode } from 'react';
 
 const BreakdownItem = ({
   name,
@@ -8,21 +8,21 @@ const BreakdownItem = ({
 }: {
   name: string;
   color: string;
-  icon: string;
+  icon: ReactNode;
   value: number;
 }): JSX.Element => {
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center">
-        <div className="mr-2 h-2 w-2 rounded-full bg-[#4AB7F3]" />
+    <div className="flex items-center justify-between space-x-6 py-3 after:ml-6 after:w-[98px]">
+      <div className="flex flex-1 items-center space-x-4">
+        {icon ?? null}
         <span>{name}</span>
       </div>
       <div className="shrink-0 grow-0">
         <div className="text-center">
-          {formatPercentage(value)} <span className="text-xs">of suppliers</span>
+          {`${value}%`} <span className="text-2xs">of suppliers</span>
         </div>
         <div className="h-[2px] w-[340px] bg-gray-200">
-          <div className={`h-[2px] ${color}`} style={{ width: formatPercentage(value) }} />
+          <div className="h-[2px]" style={{ width: `${value}%`, backgroundColor: color }} />
         </div>
       </div>
     </div>

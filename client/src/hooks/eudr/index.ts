88ccf68934c0785af19d7776a26e2ab10c237fb9
Andrés González <andres.gonzalez@vizzuality.com>
2024-03-07 16:53:35 +0100

    starts connecting filters and fetching of filters

--- client/src/hooks/eudr/index.ts
+++ client/src/hooks/eudr/index.ts
@@ -2,7 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 
 import { apiService } from 'services/api';
 
-import type { Supplier } from '@/containers/analysis-eudr/supplier-list-table/table';
+import type { AdminRegionsTreesParams } from '@/hooks/admin-regions';
+import type { MaterialTreeItem, OriginRegion, Supplier } from '@/types';
 import type { UseQueryOptions } from '@tanstack/react-query';
 
 export const useEUDRSuppliers = <T = Supplier[]>(
@@ -44,6 +45,96 @@ export const usePlotGeometries = <T = Supplier[]>(
           params,
         })
         .then((response) => response.data.geojson),
+    {
+      ...options,
+    },
+  );
+};
+
+export const useEUDRMaterialsTree = <T = MaterialTreeItem[]>(
+  params?: { producersIds: string[]; originsId: string[]; materialsId: string[] },
+  options: UseQueryOptions<MaterialTreeItem[], unknown, T> = {},
+) => {
+  return useQuery(
+    ['eudr-materials', params],
+    () =>
+      apiService
+        .request<{ data: MaterialTreeItem[] }>({
+          method: 'GET',
+          url: '/eudr/materials',
+          params,
+        })
+        .then(({ data: responseData }) => responseData.data),
+    {
+      ...options,
+    },
+  );
+};
+
+export const useEUDRAdminRegionsTree = <T = OriginRegion[]>(
+  params: AdminRegionsTreesParams,
+  options: UseQueryOptions<OriginRegion[], unknown, T> = {},
+) => {
+  const query = useQuery(
+    ['eudr-admin-regions', params],
+    () =>
+      apiService
+        .request<{ data: OriginRegion[] }>({
+          method: 'GET',
+          url: '/eudr/admin-regions',
+          params,
+        })
+        .then(({ data: responseData }) => responseData.data),
+    {
+      ...options,
+    },
+  );
+
+  return query;
+};
+
+export const useEUDRPlotsTree = <T = OriginRegion[]>(
+  params: AdminRegionsTreesParams,
+  options: UseQueryOptions<OriginRegion[], unknown, T> = {},
+) => {
+  const query = useQuery(
+    ['eudr-geo-regions', params],
+    () =>
+      apiService
+        .request<{ data: OriginRegion[] }>({
+          method: 'GET',
+          url: '/eudr/geo-regions',
+          params,
+        })
+        .then(({ data: responseData }) => responseData.data),
+    {
+      ...options,
+    },
+  );
+
+  return query;
+};
+
+interface Alert {
+  alertDate: {
+    value: string;
+  };
+}
+
+export const useEUDRAlertDates = <T = Alert[]>(
+  params?: { producersIds: string[]; originsId: string[]; materialsId: string[] },
+  options: UseQueryOptions<Alert[], unknown, T> = {},
+) => {
+  return useQuery(
+    ['eudr-dates', params],
+    () =>
+      apiService
+        .request({
+          method: 'GET',
+          url: '/eudr/dates',
+          params,
+        })
+        .then(({ data: responseData }) => responseData.data),
     {
       ...options,
     },
--- client/src/store/features/eudr/index.ts
+++ client/src/store/features/eudr/index.ts
@@ -1,15 +1,38 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { DATES_RANGE } from 'containers/analysis-eudr/filters/years-range';
+
+import type { Option } from '@/components/forms/select';
 import type { VIEW_BY_OPTIONS } from 'containers/analysis-eudr/suppliers-stacked-bar';
 import type { PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from 'store';
 
 export type EUDRState = {
   viewBy: (typeof VIEW_BY_OPTIONS)[number]['value'];
+  filters: {
+    materials: Option[];
+    origins: Option[];
+    plots: Option[];
+    suppliers: Option[];
+    dates: {
+      from: Date;
+      to: Date;
+    };
+  };
 };
 
 export const initialState: EUDRState = {
   viewBy: 'commodities',
+  filters: {
+    materials: [],
+    origins: [],
+    plots: [],
+    suppliers: [],
+    dates: {
+      from: DATES_RANGE[0],
+      to: DATES_RANGE[1],
+    },
+  },
 };
 
 export const EUDRSlice = createSlice({
@@ -20,10 +43,17 @@ export const EUDRSlice = createSlice({
       ...state,
       viewBy: action.payload,
     }),
+    setFilters: (state, action: PayloadAction<Partial<EUDRState['filters']>>) => ({
+      ...state,
+      filters: {
+        ...state.filters,
+        ...action.payload,
+      },
+    }),
   },
 });
 
-export const { setViewBy } = EUDRSlice.actions;
+export const { setViewBy, setFilters } = EUDRSlice.actions;
 
 export const eudr = (state: RootState) => state['eudr'];
 

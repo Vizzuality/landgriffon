2b41f81bc89118623bbde3ff7549dd76c88a9d3f
alexeh <alexurola@gmail.com>
2024-03-07 09:45:26 +0300

    Add metadata to geo features

--- api/test/e2e/geo-regions/fixtures.ts
+++ api/test/e2e/geo-regions/fixtures.ts
@@ -3,6 +3,7 @@ import {
   createGeoRegion,
   createMaterial,
   createSourcingLocation,
+  createSourcingRecord,
   createSupplier,
 } from '../../entity-mocks';
 import * as request from 'supertest';
@@ -13,7 +14,8 @@ import {
 } from '../../../src/modules/sourcing-locations/sourcing-location.entity';
 import { TestManager } from '../../utils/test-manager';
 import { Feature } from 'geojson';
-import { SUPPLIER_TYPES } from 'modules/suppliers/supplier.entity';
+import { SourcingRecord } from '../../../src/modules/sourcing-records/sourcing-record.entity';
+import { Supplier } from '../../../src/modules/suppliers/supplier.entity';
 
 export class GeoRegionsTestManager extends TestManager {
   constructor(manager: TestManager) {
@@ -76,6 +78,18 @@ export class GeoRegionsTestManager extends TestManager {
       producerId: supplier2.id,
       materialId: material2.id,
     });
+    for (const year of [2018, 2019, 2020, 2021, 2022, 2023]) {
+      await createSourcingRecord({
+        sourcingLocationId: sourcingLocation1.id,
+        year,
+        tonnage: 100 * year,
+      });
+      await createSourcingRecord({
+        sourcingLocationId: sourcingLocation2.id,
+        year,
+        tonnage: 100 * year,
+      });
+    }
     return {
       eudrGeoRegions: [geoRegion, geoRegion2],
       eudrSourcingLocations: [sourcingLocation1, sourcingLocation2],
@@ -86,22 +100,22 @@ export class GeoRegionsTestManager extends TestManager {
     'geoRegionIds[]': string[];
     collection?: boolean;
   }): Promise<void> => {
-    this.response = await request(this.testApp.getHttpServer())
-      .get('/api/v1/eudr/geo-features')
-      .query(filters)
-      .set('Authorization', `Bearer ${this.jwtToken}`);
+    this.response = await this.getRequest({
+      url: '/api/v1/eudr/geo-features',
+      query: filters,
+    });
   };
 
-  WhenIRequestEUDRGeoFeatureCollection = async (filters: {
+  WhenIRequestEUDRGeoFeatureCollection = async (filters?: {
     'geoRegionIds[]'?: string[];
     'producerIds[]'?: string[];
     'materialIds[]'?: string[];
     'originIds[]'?: string[];
   }): Promise<void> => {
-    this.response = await request(this.testApp.getHttpServer())
-      .get('/api/v1/eudr/geo-features/collection')
-      .query(filters)
-      .set('Authorization', `Bearer ${this.jwtToken}`);
+    this.response = await this.getRequest({
+      url: '/api/v1/eudr/geo-features/collection',
+      query: filters,
+    });
   };
 
   ThenIShouldOnlyRecieveCorrespondingGeoFeatures = (
@@ -125,6 +139,40 @@ export class GeoRegionsTestManager extends TestManager {
           ),
         ).toBeDefined();
       }
+    }
+  };
+
+  ThenTheGeoFeaturesShouldHaveCorrectMetadata = async (
+    sourceLocations: SourcingLocation[],
+  ) => {
+    for (const feature of this.response!.body.geojson.features) {
+      const sourcingLocation = sourceLocations.find(
+        (r: any) => r.geoRegionId === feature.properties.id,
+      );
+      const expectedMetadataContentForEachFeature = await this.dataSource
+        .createQueryBuilder()
+        .from(SourcingRecord, 'sr')
+        .select('sr.tonnage', 'baselineVolume')
+        .addSelect('gr.id', 'id')
+        .addSelect('gr.name', 'plotName')
+        .addSelect('s.name', 'supplierName')
+        .innerJoin(SourcingLocation, 'sl', 'sr."sourcingLocationId" = sl.id')
+        .innerJoin(Supplier, 's', 'sl."producerId" = s.id')
+        .innerJoin(GeoRegion, 'gr', 'sl."geoRegionId" = gr.id')
+        .where('sr."sourcingLocationId" = :sourcingLocationId', {
+          sourcingLocationId: sourcingLocation!.id,
+        })
+        .andWhere('sr.year = :year', { year: 2020 })
+        .getRawOne();
+      expect(sourcingLocation).toBeDefined();
+      expect(feature.properties).toEqual({
+        id: expectedMetadataContentForEachFeature.id,
+        supplierName: expectedMetadataContentForEachFeature.supplierName,
+        plotName: expectedMetadataContentForEachFeature.plotName,
+        baselineVolume: parseInt(
+          expectedMetadataContentForEachFeature.baselineVolume,
+        ),
+      });
     }
   };
 }

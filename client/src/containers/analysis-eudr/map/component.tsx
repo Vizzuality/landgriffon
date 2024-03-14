0224fa4d7f858ade787f1172e0bb60d6b6fa5266
David Inga <david.inga@vizzuality.com>
2024-03-14 17:13:05 +0100

    planet comparison and contextual layers

--- client/src/containers/analysis-eudr/map/component.tsx
+++ client/src/containers/analysis-eudr/map/component.tsx
@@ -3,35 +3,42 @@ import DeckGL from '@deck.gl/react/typed';
 import { BitmapLayer, GeoJsonLayer } from '@deck.gl/layers/typed';
 import Map from 'react-map-gl/maplibre';
 import { WebMercatorViewport, MapView } from '@deck.gl/core/typed';
-import { View } from '@deck.gl/core';
 import { TileLayer } from '@deck.gl/geo-layers/typed';
+import { CartoLayer, setDefaultCredentials, MAP_TYPES, API_VERSIONS } from '@deck.gl/carto/typed';
 import bbox from '@turf/bbox';
 
 import ZoomControl from './zoom';
 import LegendControl from './legend';
 import BasemapControl from './basemap';
 
+import { useAppSelector } from '@/store/hooks';
+// import { setBasemap, setPlanetCompare } from '@/store/features/eudr';
 import { INITIAL_VIEW_STATE, MAP_STYLES } from '@/components/map';
 import { usePlotGeometries } from '@/hooks/eudr';
 import { formatNumber } from '@/utils/number-format';
 
 import type { PickingInfo, MapViewState } from '@deck.gl/core/typed';
-import type { BasemapValue } from '@/components/map/controls/basemap/types';
-import type { MapStyle } from '@/components/map/types';
 
-const PlanetQ32023_ID = 'PlanetQ32023';
-const PlanetQ32019_ID = 'PlanetQ32019';
-const DEFORESTATION_QUADBIN_LAYER_ID = 'deforestationQuadbinLayer';
+setDefaultCredentials({
+  // apiBaseUrl: 'https://eudr.carto.com',
+  apiBaseUrl: 'https://gcp-us-east1.api.carto.com',
+  accessToken:
+    'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfemsydWhpaDYiLCJqdGkiOiJjZDk0ZWIyZSJ9.oqLagnOEc-j7Z4hY-MTP1yoZA_vJ7WYYAkOz_NUmCJo',
+});
 
 const EUDRMap = () => {
+  const { basemap, planetCompare, supplierLayer, contextualLayers } = useAppSelector(
+    (state) => state.eudr,
+  );
+
   const [hoverInfo, setHoverInfo] = useState<PickingInfo>(null);
-  const [mapStyle, setMapStyle] = useState<MapStyle>('terrain');
   const [viewState, setViewState] = useState<MapViewState>(INITIAL_VIEW_STATE);
 
   const plotGeometries = usePlotGeometries();
 
+  // Supplier plot layer
   const layer: GeoJsonLayer = new GeoJsonLayer({
-    id: 'top-plots-layer',
+    id: 'full-plots-layer',
     data: plotGeometries.data,
     // Styles
     filled: true,
@@ -44,30 +51,13 @@ const EUDRMap = () => {
     pickable: true,
     autoHighlight: true,
     highlightColor: [255, 176, 0, 255],
+    visible: supplierLayer.active,
     onHover: setHoverInfo,
   });
 
-  const layer2: GeoJsonLayer = new GeoJsonLayer({
-    id: 'bottom-plots-layer',
-    data: plotGeometries.data,
-    // Styles
-    filled: true,
-    getFillColor: [255, 0, 0, 255],
-    stroked: true,
-    getLineColor: [255, 0, 0, 255],
-    getLineWidth: 1,
-    lineWidthUnits: 'pixels',
-    // Interactive props
-    pickable: true,
-    autoHighlight: true,
-    highlightColor: [255, 176, 0, 255],
-    onHover: setHoverInfo,
-  });
-
-  const planetQ32019Layer = new TileLayer({
-    // id: PlanetQ32023_ID,
-    id: 'top-planet-q3-2019-layer',
-    data: 'https://tiles.planet.com/basemaps/v1/planet-tiles/global_quarterly_2019q3_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK6679039df83f414faf798ba4ad4530db',
+  const planetLayer = new TileLayer({
+    id: 'top-planet-monthly-layer',
+    data: 'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2020_12_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK6679039df83f414faf798ba4ad4530db',
     minZoom: 0,
     maxZoom: 20,
     tileSize: 256,
@@ -94,10 +84,9 @@ const EUDRMap = () => {
     },
   });
 
-  const planetQ32023Layer = new TileLayer({
-    // id: PlanetQ32023_ID,
-    id: 'bottom-planet-q3-2023-layer',
-    data: 'https://tiles.planet.com/basemaps/v1/planet-tiles/global_quarterly_2023q3_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK6679039df83f414faf798ba4ad4530db',
+  const planetCompareLayer = new TileLayer({
+    id: 'bottom-planet-monthly-layer',
+    data: 'https://tiles.planet.com/basemaps/v1/planet-tiles/global_monthly_2024_02_mosaic/gmap/{z}/{x}/{y}.png?api_key=PLAK6679039df83f414faf798ba4ad4530db',
     minZoom: 0,
     maxZoom: 20,
     tileSize: 256,
@@ -124,11 +113,35 @@ const EUDRMap = () => {
     },
   });
 
-  const layers = [planetQ32019Layer, planetQ32023Layer, layer, layer2];
+  const forestCoverLayer = new CartoLayer({
+    id: 'full-forest-cover-2020-ec-jrc',
+    type: MAP_TYPES.TILESET,
+    connection: 'eudr',
+    data: 'cartobq.eudr.JRC_2020_Forest_d_TILE',
+    pointRadiusMinPixels: 2,
+    getLineColor: [114, 169, 80],
+    getFillColor: [114, 169, 80],
+    lineWidthMinPixels: 1,
+    visible: contextualLayers['forest-cover-2020-ec-jrc'].active,
+    credentials: {
+      apiVersion: API_VERSIONS.V3,
+      apiBaseUrl: 'https://gcp-us-east1.api.carto.com',
+      accessToken:
+        'eyJhbGciOiJIUzI1NiJ9.eyJhIjoiYWNfemsydWhpaDYiLCJqdGkiOiJjY2JlMjUyYSJ9.LoqzuDp076ESVYmHm1mZNtfhnqOVGmSxzp60Fht8PQw',
+    },
+  });
 
-  const handleMapStyleChange = useCallback((newStyle: BasemapValue) => {
-    setMapStyle(newStyle);
-  }, []);
+  const deforestationLayer = new CartoLayer({
+    id: 'full-deforestation-alerts-2020-2022-hansen',
+    type: MAP_TYPES.QUERY,
+    connection: 'eudr',
+    data: 'SELECT * FROM `cartobq.eudr.TCL_hansen_year`',
+    pointRadiusMinPixels: 2,
+    getLineColor: [224, 191, 36],
+    getFillColor: [224, 191, 36],
+    lineWidthMinPixels: 1,
+    visible: contextualLayers['deforestation-alerts-2020-2022-hansen'].active,
+  });
 
   const handleZoomIn = useCallback(() => {
     const zoom = viewState.maxZoom === viewState.zoom ? viewState.zoom : viewState.zoom + 1;
@@ -180,36 +193,48 @@ const EUDRMap = () => {
         viewState={{ ...viewState }}
         onViewStateChange={({ viewState }) => setViewState(viewState as MapViewState)}
         controller={{ dragRotate: false }}
-        layers={layers}
-        layerFilter={({ layer, viewport }) => viewport.id.startsWith(layer.id.split('-')[0])}
-        onResize={handleResize}
-        views={[
-          new MapView({
-            id: 'full',
-            controller: true,
-          }),
-          new MapView({
-            id: 'top',
-            y: 0,
-            height: '50%',
-            padding: { top: '100%' },
-            controller: true,
-          }),
-          new MapView({
-            id: 'bottom',
-            y: '50%',
-            height: '50%',
-            padding: { bottom: '100%' },
-            // controller: true,
-          }),
+        layers={[
+          basemap === 'planet' && !planetCompare ? [planetLayer] : null,
+          basemap === 'planet' && planetCompare ? [planetLayer, planetCompareLayer] : null,
+          forestCoverLayer,
+          deforestationLayer,
+          layer,
         ]}
+        layerFilter={({ layer, viewport }) => {
+          return !planetCompare || viewport.id.startsWith(layer.id.split('-')[0]);
+        }}
+        onResize={handleResize}
+        {...(planetCompare
+          ? {
+              views: [
+                new MapView({
+                  id: 'top',
+                  y: 0,
+                  height: '50%',
+                  padding: { top: '100%' },
+                }),
+                new MapView({
+                  id: 'bottom',
+                  y: '50%',
+                  height: '50%',
+                  padding: { bottom: '100%' },
+                }),
+                new MapView({
+                  id: 'full',
+                  y: 0,
+                  x: 0,
+                  width: '100%',
+                  height: '100%',
+                }),
+              ],
+            }
+          : {})}
       >
-        <View id="full">
-          <Map reuseMaps mapStyle={MAP_STYLES[mapStyle]} styleDiffing={false} />
-        </View>
-        <View id="top" />
-        <View id="bottom" />
+        <Map reuseMaps mapStyle={MAP_STYLES.terrain} styleDiffing={false} />
       </DeckGL>
+      {planetCompare && (
+        <div className="pointer-events-none absolute left-0 top-1/2 z-20 h-[2px] w-full bg-white" />
+      )}
       {hoverInfo?.object && (
         <div
           className="pointer-events-none absolute z-10 max-w-32 rounded-md bg-white p-2 text-2xs shadow-md"

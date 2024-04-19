53416ddccda0b1d11a32289cdff4edb1eca8dcb9
alexeh <alexurola@gmail.com>
2024-04-19 10:16:11 +0300

    Add tracking for import and impact chunk insert

--- api/src/utils/app-base.repository.ts
+++ api/src/utils/app-base.repository.ts
@@ -3,6 +3,7 @@ import { SaveOptions } from 'typeorm/repository/SaveOptions';
 import { chunk } from 'lodash';
 import { Injectable, Logger } from '@nestjs/common';
 import * as config from 'config';
+import { ImportProgressEmitter } from '../modules/cqrs/import-data/import-progress.emitter';
 
 const dbConfig: any = config.get('db');
 const batchChunkSize: number = parseInt(`${dbConfig.batchChunkSize}`, 10);
@@ -13,15 +14,27 @@ export abstract class AppBaseRepository<
 > extends Repository<Entity> {
   logger: Logger = new Logger(this.constructor.name);
   protected dataSource: DataSource;
+  protected importProgress: ImportProgressEmitter;
 
   async saveChunks<Entity>(
     entities: Entity[],
     options?: SaveOptions,
+    trackingOptions?: {
+      step: string;
+      progressStartingPoint: number;
+    },
   ): Promise<Entity[]> {
     const queryRunner: QueryRunner = this.dataSource.createQueryRunner();
     await queryRunner.connect();
     await queryRunner.startTransaction();
     const result: Entity[][] = [];
+    const totalEntities: number = entities.length;
+    const totalChunks: number = Math.ceil(totalEntities / batchChunkSize);
+    const progressStartingPoint: number =
+      trackingOptions?.progressStartingPoint ?? 0;
+    let progress: number = progressStartingPoint;
+    const progressPerChunk: number =
+      (100 - progressStartingPoint ?? 0) / totalChunks; // Distribuir el progreso restante entre los chunks
 
     try {
       for (const [index, dataChunk] of chunk(
@@ -29,13 +42,24 @@ export abstract class AppBaseRepository<
         batchChunkSize,
       ).entries()) {
         this.logger.debug(
-          `Inserting chunk #${index} (${dataChunk.length} items)...`,
+          `Inserting chunk #${index} (${dataChunk.length} items) from a total of ${totalChunks}...`,
         );
         const promises: Promise<Entity>[] = dataChunk.map((row: Entity) =>
           queryRunner.manager.save(row, options),
         );
         const saved: Entity[] = await Promise.all(promises);
         result.push(saved);
+        progress += progressPerChunk;
+        if (trackingOptions?.step === 'IMPORTING_DATA') {
+          this.importProgress.emitImportProgress({
+            progress,
+          });
+        }
+        if (trackingOptions?.step === 'CALCULATING_IMPACT') {
+          this.importProgress.emitImpactCalculationProgress({
+            progress,
+          });
+        }
       }
 
       // commit transaction if every chunk was saved successfully

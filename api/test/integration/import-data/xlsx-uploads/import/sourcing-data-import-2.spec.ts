81127d4199edb6bc4642cc4618e54a57e848685f
alexeh <alexurola@gmail.com>
2024-05-06 10:43:03 +0300

    Delete old tests

--- api/test/integration/import-data/xlsx-uploads/import/sourcing-data-import-2.spec.ts
+++ api/test/integration/import-data/xlsx-uploads/import/sourcing-data-import-2.spec.ts
@@ -24,6 +24,14 @@ describe('Sourcing Data Correct Import (Integration Tests)', () => {
   afterEach(async () => {
     await sourcingDataImportTestManager.clearDatabase();
   });
+  test('should throw an error if no materials are found when importing the data', async () => {
+    const error = await sourcingDataImportTestManager.WhenIImportACorrectFile();
+    await sourcingDataImportTestManager.ButThereIsNoBaseDataInThePlatform();
+    sourcingDataImportTestManager.ThenAnErrorShouldBeThrown(
+      error,
+      'No Materials found present in the DB. Please check the LandGriffon installation manual',
+    );
+  });
 
   test('Should correctly import a file with no errors in it', async () => {
     await sourcingDataImportTestManager.GivenThereIsBaseDataInThePlatform();

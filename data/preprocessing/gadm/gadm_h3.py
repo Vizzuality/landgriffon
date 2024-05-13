502c3cb117e2d15197cef379b3b3f5ee30ec1b70
Biel Stela <biel.stela@gmail.com>
2024-05-13 16:59:41 +0200

    Fixes python script to match the table schema

--- data/preprocessing/gadm/gadm_h3.py
+++ data/preprocessing/gadm/gadm_h3.py
@@ -1,3 +1,8 @@
+"""Converts the GADM Shapefile to a csv with h3 cells for ingesting
+into geo_region table.
+"""
+
+import uuid
 from pathlib import Path
 
 import click
@@ -13,21 +18,26 @@ H3_RESOLUTION = 6
 @click.argument("output", type=click.Path(path_type=Path))
 def main(filename: Path, output) -> None:
     """Convert gadm shapefile to csv with h3 columns"""
-    print("Reading file...")
     gdf = gpd.read_file(filename)
     print("Making h3 cells...")
-    gdf["h3flat"] = geoseries_to_cells(gdf["geometry"], resolution=H3_RESOLUTION, compact=False)
-    print('Compacting h3 cells...')
-    gdf["h3Compact"] = [list(compact(x)) for x in gdf["h3flat"]]
-    print('Converting to cell indexes to hexadecimal...')
+    gdf["h3Flat"] = geoseries_to_cells(gdf["geometry"], resolution=H3_RESOLUTION, compact=False)
+    gdf["h3Compact"] = [list(compact(x)) for x in gdf["h3Flat"]]
     gdf["h3Compact"] = gdf["h3Compact"].apply(lambda arr: [hex(x)[2:] for x in arr])
-    gdf["h3flat"] = gdf["h3flat"].apply(lambda arr: [hex(x)[2:] for x in arr])
-    gdf = gdf.to_wkb(hex=True)
-    gdf = gdf.rename({"geometry": "theGeom"})
+    gdf["h3Flat"] = gdf["h3Flat"].apply(lambda arr: [hex(x)[2:] for x in arr])
 
-    gdf["h3FlatLength"] = gdf["h3flat"].apply(lambda x: len(x))
-    print(f"writing to {output}...")
-    gdf.to_csv(output, index=False)
+    # convert h3 lists to sql literal arrays
+    gdf["h3Compact"] = gdf["h3Compact"].apply(lambda x: f"{{{','.join(e for e in x)}}}")
+    gdf["h3Flat"] = gdf["h3Flat"].apply(lambda x: f"{{{','.join(e for e in x)}}}")
+
+    gdf = gdf.to_wkb(hex=True)
+    gdf = gdf.drop(["name"], axis=1)
+    gdf = gdf.rename(columns={"geometry": "theGeom", "mpath": "name"})
+
+    gdf["h3FlatLength"] = gdf["h3Flat"].apply(lambda x: len(x))
+    gdf["id"] = [str(uuid.uuid4()) for _ in range(len(gdf))]
+    print(f"Writing to {output}...")
+    gdf.to_csv(output, index=False, columns=["id", "h3Compact", "h3Flat", "h3FlatLength", "name", "theGeom"])
+    # gdf.to_parquet(output, index=False, columns=["id", "h3Compact", "h3Flat", "h3FlatLength", "name", "theGeom"])
 
 
 if __name__ == "__main__":

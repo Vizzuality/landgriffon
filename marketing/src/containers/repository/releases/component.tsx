6b41dace0f55007d0618eed5b6d25746e3e37ca3
María <maluenarod@gmail.com>
2024-05-16 20:51:59 +0200

    datasources links

--- marketing/src/containers/repository/releases/component.tsx
+++ marketing/src/containers/repository/releases/component.tsx
@@ -7,42 +7,86 @@ import Icon from 'components/icon';
 
 import ARROW_SVG from 'svgs/ui/arrow-top-right.svg?sprite';
 
-const DATASET_RELEASES_1 = {
-  title: 'Datasets for EUDR:',
+const LANDGRIFFON_ANALYSIS = {
+  title: 'Foundational datasets that underpin LandGriffon analysis:',
   datasets: [
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'Mekonnen and Hoekstra. The green, blue and grey water footprint of crops and derived crop products',
+      url: 'https://doi.org/10.5194/hess-15-1577-2011',
     },
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'Kuzma et al. Aqueduct 4.0: Updated decision-relevant global water risk indicators',
+      url: 'http://doi.org/10.46830/writn.23.00061',
     },
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'McDowell et al. Global Mapping of Freshwater Nutrient Enrichment and Periphyton Growth Potential',
+      url: 'https://doi.org/10.1038/s41598-020-60279-w',
     },
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'McDowell et al. Global Database of Diffuse Riverine Nitrogen and Phosphorus Loads and Yields',
+      url: 'https://doi.org/10.1002/gdj3.111',
+    },
+    {
+      name: 'International Food Policy Research Institute. Global Spatially-Disaggregated Crop Production Statistics Data for 2010 Version 2.0',
+      url: 'https://doi.org/10.7910/DVN/PRFF8V',
+    },
+    {
+      name: 'Halpern et al. The Environmental Footprint of Global Food Production.',
+      url: 'https://doi.org/10.1038/s41893-022-00965-x',
+    },
+    {
+      name: 'Noon et al. Mapping the Irrecoverable Carbon in Earth’s Ecosystems.',
+      url: 'https://doi.org/10.1038/s41893-021-00803-6',
+    },
+    {
+      name: 'Hansen et al. High-Resolution Global Maps of 21st-Century Forest Cover Change. ',
+      url: 'https://doi.org/10.1126/science.1244693',
+    },
+    {
+      name: 'Mazur et al. SBTN Natural Lands Map: Technical Documentation.',
+      url: 'https://sciencebasedtargetsnetwork.org/wp-content/uploads/2023/05/Technical-Guidance-2023-Step3-Land-v0.3-Natural-Lands-Map.pdf',
+    },
+    {
+      name: 'Karra et al. Global Land Use / Land Cover with Sentinel 2 and Deep Learning.',
+      url: 'https://doi.org/10.1109/IGARSS47720.2021.9553499',
+    },
+    {
+      name: 'Grantham et al. Anthropogenic Modification of Forests Means Only 40% of Remaining Forests Have High Ecosystem Integrity.',
+      url: 'https://doi.org/10.1038/s41467-020-19493-3',
+    },
+    {
+      name: 'Gassert et al. Global 100m Projections of Biodiversity Intactness for the Years 2017 - 2020.',
+      url: 'https://ai4edatasetspublicassets.blob.core.windows.net/assets/pdfs/io-biodiversity/Biodiversity_Intactness_whitepaper.pdf',
     },
   ],
 };
 
-const DATASET_RELEASES_2 = {
+const DATASET_FOR_EUDR = {
   title: 'Datasets for EUDR:',
   datasets: [
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'Bourgoin et al. Global map of forest cover 2020 - version 1.',
+      url: 'http://data.europa.eu/89h/10d1b337-b7d1-4938-a048-686c8185b290',
     },
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'Bourgoin et al. Mapping Global Forest Cover of the Year 2020 to Support the EU Regulation on Deforestation-free Supply Chains.',
+      url: 'https://www.researchgate.net/publication/379119723_Mapping_Global_Forest_Cover_of_the_Year_2020_to_Support_the_EU_Regulation_on_Deforestation-free_Supply_Chains',
     },
     {
-      name: 'Name of the Data set',
-      url: '',
+      name: 'Hansen et al. High-Resolution Global Maps of 21st-Century Forest Cover Change.',
+      url: 'https://glad.earthengine.app/view/global-forest-change',
+    },
+    {
+      name: 'Reiche et al. Forest disturbance alerts for the Congo Basin using Sentinel-1.',
+      url: 'https://doi.org/10.1088/1748-9326/abd0a8',
+    },
+    {
+      name: 'Adrià et al. High resolution global industrial and smallholder oil palm map for 2019 (Version v1).',
+      url: 'https://zenodo.org/records/4473715',
+    },
+    {
+      name: 'Vancutsem et al. Long-term (1990-2019) monitoring of forest cover changes in the humid tropics. ',
+      url: 'https://www.science.org/doi/10.1126/sciadv.abe1603',
     },
   ],
 };
@@ -56,12 +100,12 @@ const DatasetReleases: React.FC = () => (
           intro="Explore the datasets that are central to our analysis."
         >
           <div className="space-y-6">
-            <h4 className="font-bold">{DATASET_RELEASES_1.title}</h4>
+            <h4 className="font-bold">{DATASET_FOR_EUDR.title}</h4>
             <ul className="space-y-[5px]">
-              {DATASET_RELEASES_1.datasets.map((dataset) => (
+              {DATASET_FOR_EUDR.datasets.map((dataset) => (
                 <li
                   key={dataset.name}
-                  className="flex justify-between items-center bg-gray-100 p-[15px]"
+                  className="flex justify-between items-center bg-gray-100 p-[15px] space-x-2"
                 >
                   <span className="font-light">{dataset.name}</span>
                   <Link href={dataset.url}>
@@ -70,7 +114,7 @@ const DatasetReleases: React.FC = () => (
                       rel="noopener noreferrer"
                       className="flex space-x-5 font-bold items-center"
                     >
-                      <span className="underline">Go to source</span>
+                      <span className="underline whitespace-nowrap">Go to source</span>
                       <Icon icon={ARROW_SVG} className="w-3 h-3 fil-current" />
                     </a>
                   </Link>
@@ -80,12 +124,12 @@ const DatasetReleases: React.FC = () => (
           </div>
 
           <div className="space-y-6">
-            <h4 className="font-bold">{DATASET_RELEASES_2.title}</h4>
+            <h4 className="font-bold">{LANDGRIFFON_ANALYSIS.title}</h4>
             <ul className="space-y-[5px]">
-              {DATASET_RELEASES_2.datasets.map((dataset) => (
+              {LANDGRIFFON_ANALYSIS.datasets.map((dataset) => (
                 <li
                   key={dataset.name}
-                  className="flex justify-between items-center bg-gray-100 p-[15px]"
+                  className="flex justify-between items-center bg-gray-100 p-[15px] space-x-2"
                 >
                   <span className="font-light">{dataset.name}</span>
                   <Link href={dataset.url}>
@@ -94,7 +138,7 @@ const DatasetReleases: React.FC = () => (
                       rel="noopener noreferrer"
                       className="flex space-x-5 font-bold items-center"
                     >
-                      <span className="underline">Go to source</span>
+                      <span className="underline whitespace-nowrap">Go to source</span>
                       <Icon icon={ARROW_SVG} className="w-3 h-3 fil-current" />
                     </a>
                   </Link>

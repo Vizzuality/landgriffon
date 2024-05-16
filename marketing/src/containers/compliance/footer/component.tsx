66f8c03a364596188a2b55a5e5a560f29f0d6922
María <maluenarod@gmail.com>
2024-05-16 20:34:53 +0200

    image fixed in compliance

--- marketing/src/containers/compliance/footer/component.tsx
+++ marketing/src/containers/compliance/footer/component.tsx
@@ -9,7 +9,7 @@ const ContactUs: FC = () => {
       <Wrapper>
         <div className="relative z-10 py-14 bg-orange-500 xl:-mt-14 xl:px-20 xl:-mx-20 space-y-10">
           <h2 className="text-4xl font-black uppercase md:text-6xl font-display">
-          Drive positive impact with your supply chain.
+            Drive positive impact with your supply chain.
           </h2>
 
           <Link href="/contact">

f193c75d6d6fe9729d461291c871727b5a460d55
alexeh <alexurola@gmail.com>
2024-12-14 05:43:04 +0100

    fix missing row in geocoding

--- api/src/modules/geo-coding/geo-coding.service.ts
+++ api/src/modules/geo-coding/geo-coding.service.ts
@@ -93,7 +93,7 @@ export class GeoCodingService extends GeoCodingAbstractClass {
         progressTracker.trackProgress();
       } catch (e: any) {
         errors.push({
-          line: i + 5,
+          row: i + 5,
           error: e.message,
           type: 'geo-coding-error',
           sheet: 'sourcingData',

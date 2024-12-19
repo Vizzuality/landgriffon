c38979cc191e26909e0c6e128d3bd16ea39829f3
alexeh <alexurola@gmail.com>
2024-12-19 06:56:43 +0100

    sourcing-data events and view

--- api/src/modules/events/import-data-events/import-data.event-handler.ts
+++ api/src/modules/events/import-data-events/import-data.event-handler.ts
@@ -1,19 +1,19 @@
 import { EventsHandler, IEvent, IEventHandler } from '@nestjs/cqrs';
-import { API_EVENT_KINDS } from 'modules/api-events/api-event.entity';
+import { API_EVENT_KINDS, ApiEvent } from 'modules/api-events/api-event.entity';
 import { ApiEventsService } from 'modules/api-events/api-events.service';
 import { Task } from 'modules/tasks/task.entity';
 
 export enum IMPORT_DATA_EVENTS {
-  STARTED = API_EVENT_KINDS.data__importStarted__v1alpha1,
-  FAILED = API_EVENT_KINDS.data__importFailed__v1alpha1,
-  SUCCEED = API_EVENT_KINDS.data__importSucceeded__v1alpha1,
+  STARTED = API_EVENT_KINDS.sourcing_data__importStarted__v1alpha1,
+  FAILED = API_EVENT_KINDS.sourcing_data__importFailed__v1alpha1,
+  SUCCEED = API_EVENT_KINDS.sourcing_data__importSucceeded__v1alpha1,
 }
 
 export class ImportDataEvent implements IEvent {
   constructor(
     public readonly taskId: Task['id'],
     public readonly kind: IMPORT_DATA_EVENTS,
-    public readonly payload: any,
+    public readonly data: any,
   ) {}
 }
 
@@ -22,10 +22,11 @@ export class ImportDataEventHandler implements IEventHandler<ImportDataEvent> {
   constructor(private readonly apiEvents: ApiEventsService) {}
 
   async handle(event: ImportDataEvent): Promise<void> {
+    const { taskId, kind, data } = event;
     await this.apiEvents.create({
-      topic: event.taskId,
-      kind: event.kind as unknown as API_EVENT_KINDS,
-      data: event.payload,
+      topic: taskId,
+      kind: kind as unknown as API_EVENT_KINDS,
+      data: data,
     });
   }
 }

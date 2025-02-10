6368aba7653ca9c454082e6bbe0a5394d9edcd8e
Jorge Martí Marín <jorge.marti@vizzuality.com>
2025-02-10 09:53:19 +0100

    LANDGRIF-1602: Refactor import data CQRS

---
+++ api/src/modules/import-data/cqrs/import-data-processing.handler.ts
@@ -0,0 +1,40 @@
+import { CommandHandler, ICommandHandler, EventBus } from '@nestjs/cqrs';
+import { ImportDataService } from 'modules/import-data/import-data.service';
+import { Logger } from '@nestjs/common';
+import {
+  IMPORT_DATA_EVENTS,
+  ImportDataEvent,
+} from 'modules/events/import-data-events/import-data.event-handler';
+import { ExcelImportJob } from '../workers/import-data.producer';
+
+export class StartImportProcessingCommand {
+  constructor(public readonly jobData: ExcelImportJob) {}
+}
+
+@CommandHandler(StartImportProcessingCommand)
+export class StartImportProcessingHandler
+  implements ICommandHandler<StartImportProcessingCommand>
+{
+  private readonly logger = new Logger(StartImportProcessingHandler.name);
+
+  constructor(
+    private readonly importDataService: ImportDataService,
+    private readonly eventBus: EventBus,
+  ) {}
+
+  async execute(command: StartImportProcessingCommand): Promise<void> {
+    const { jobData } = command;
+    // Optionally publish an event to mark the import as "Started"
+    // this.eventBus.publish(...);
+    this.eventBus.publish(
+      new ImportDataEvent(jobData.taskId, IMPORT_DATA_EVENTS.STARTED, {
+        file: jobData.xlsxFileData.originalname,
+      }),
+    );
+
+    // Now do the actual heavy lifting here
+    await this.importDataService.processImportJob({ data: jobData } as any);
+
+    // Optionally publish a "Finished" event or let OnQueueCompleted handle that
+  }
+}

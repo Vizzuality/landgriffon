54e25383d8fb13bdf9ce17ee284e0f2721345170
Biel Stela <biel.stela@gmail.com>
2025-03-13 18:19:33 +0100

    (wip) gadm to h3

--- data-preprocessing/src/data_preprocessing/pipelines/gadm/nodes.py
+++ data-preprocessing/src/data_preprocessing/pipelines/gadm/nodes.py
@@ -1,4 +1,32 @@
-"""
-This is a boilerplate pipeline 'gadm'
-generated using Kedro 0.19.11
-"""
+import logging
+import uuid
+
+import geopandas as gpd
+import pandas as pd
+from h3ronpy import cells_to_string, compact
+from h3ronpy.pandas.vector import geoseries_to_cells
+
+log = logging.getLogger(__name__)
+
+
+def gadm_to_h3(gdf: gpd.GeoDataFrame, h3_resolution: int) -> pd.DataFrame:
+    log.info("Converting GADM data to H3")
+    gdf["h3Flat"] = geoseries_to_cells(gdf["geometry"], resolution=h3_resolution, compact=False)
+    gdf["h3Compact"] = [list(compact(x)) for x in gdf["h3Flat"]]
+    breakpoint()
+    log.info("Converting H3 cells to hex strings")
+    gdf["h3Compact"] = gdf["h3Compact"].apply(lambda arr: cells_to_string(arr))
+    gdf["h3Flat"] = gdf["h3Flat"].apply(lambda arr: cells_to_string(arr))
+    gdf["h3FlatLength"] = gdf["h3Flat"].apply(lambda x: len(x))
+
+    # convert h3 lists to sql literal arrays
+    gdf["h3Compact"] = gdf["h3Compact"].apply(lambda x: f"{{{','.join(e for e in x)}}}")
+    gdf["h3Flat"] = gdf["h3Flat"].apply(lambda x: f"{{{','.join(e for e in x)}}}")
+
+    log.info("Serializing geometries to WKB")
+    df = gdf.to_wkb(hex=True)
+    df = df.drop(["name"], axis=1)
+    df = df.rename(columns={"geometry": "theGeom", "mpath": "name"})
+
+    df["id"] = [str(uuid.uuid4()) for _ in range(len(df))]
+    return df
--- data-preprocessing/src/data_preprocessing/pipelines/gadm/pipeline.py
+++ data-preprocessing/src/data_preprocessing/pipelines/gadm/pipeline.py
@@ -1,5 +1,10 @@
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from data_preprocessing.pipelines.gadm.nodes import gadm_to_h3
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([node(lambda x: x, "input", "output")])
+    return pipeline(
+        [
+            node(gadm_to_h3, ["gadm", "params:h3_resolution"], "geo_region"),
+        ]
+    )

f4cc8de415515a4744b5c5afb8edb7aaff5e8fa0
Biel Stela <biel.stela@gmail.com>
2025-03-17 17:55:47 +0100

    Switch to gadm partitioned by level dataset

--- data-preprocessing/src/data_preprocessing/pipelines/gadm/nodes.py
+++ data-preprocessing/src/data_preprocessing/pipelines/gadm/nodes.py
@@ -1,13 +1,15 @@
 import logging
-import uuid
 import re
+import uuid
 
 import geopandas as gpd
 import pandas as pd
+import polars as pl
 from h3ronpy import cells_to_string
 from h3ronpy.pandas.vector import geoseries_to_cells
 
 log = logging.getLogger(__name__)
+
 
 def _collapse_gid_and_name(row: pd.Series, remove_version: bool = True) -> tuple[str, str, int]:
     """Unify GIDs to the smallest non-null value"""
@@ -31,22 +33,30 @@ def _collapse_gid_and_name(row: pd.Series, remove_version: bool = True) -> tuple
     return gadm_id, name, level
 
 
-def gadm_to_full_table(gdf: gpd.GeoDataFrame, h3_resolution: int) -> pd.DataFrame:
-    log.info("Converting GADM data to H3")
+def gadm_to_h3(gdf: gpd.GeoDataFrame, h3_resolution: int, tolerance: float | None) -> pl.DataFrame:
+    if tolerance is not None:
+        log.info("Simplifying geometries with tolerance: %s", tolerance)
+        gdf["geometry"] = gdf.simplify(tolerance)
     gdf["h3Compact"] = geoseries_to_cells(gdf["geometry"], resolution=h3_resolution, compact=True)
     gdf["h3Compact"] = gdf["h3Compact"].apply(lambda arr: cells_to_string(arr).to_numpy())
-    log.info("Serializing geometries to WKB")
     df = gdf.to_wkb(hex=True)
-    df = df.rename(columns={"geometry": "theGeom"})
+    return pl.DataFrame(df)
+
+
+def join_gadm_levels(adm0: pl.DataFrame, adm1: pl.DataFrame, adm2: pl.DataFrame) -> pl.DataFrame:
+    df: pl.DataFrame = adm2.join(adm1, how="left").join(adm0, how="left")
+
+    df = df.rename({"geometry": "theGeom"})
+    df = df.with_columns(pl.col("GID_0").alias("isoA3"))
     df[["gadmId", "name", "level"]] = df.apply(_collapse_gid_and_name, axis=1, result_type="expand")
     df["id"] = [str(uuid.uuid4()) for _ in range(len(df))]
     df["isoA3"] = df["GID_0"]
     return df
 
 
-def reshape_to_geo_region_table(df: pd.DataFrame, params: dict) -> pd.DataFrame:
-    df["isCreatedByUser"] = False
-    df = df[params["columns"]]
+def reshape_to_geo_region_table(df: pl.DataFrame, params: dict) -> pd.DataFrame:
+    df = df.with_columns(pl.lit(False).alias("isCreatedByUser"))
+    df = df.with_columns(params["columns"])
     return df
 
 
--- data-preprocessing/src/data_preprocessing/pipelines/gadm/pipeline.py
+++ data-preprocessing/src/data_preprocessing/pipelines/gadm/pipeline.py
@@ -1,13 +1,37 @@
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
-from data_preprocessing.pipelines.gadm.nodes import gadm_to_full_table, reshape_to_geo_region_table, \
-    reshape_to_admin_region_table
+
+from data_preprocessing.pipelines.gadm.nodes import (
+    reshape_to_geo_region_table,
+    reshape_to_admin_region_table,
+    gadm_to_h3,
+    join_gadm_levels,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            node(gadm_to_full_table, ["gadm", "params:h3_resolution"], "gadm_h3"),
-            node(reshape_to_geo_region_table, ["gadm_h3", "params:geo_region"], "geo_regions"),
-            node(reshape_to_admin_region_table, ["gadm_h3", "params:admin_region"], "admin_regions")
+            node(
+                gadm_to_h3,
+                ["gadm_adm0", "params:h3_resolution", "params:adm0.tolerance"],
+                "gadm_adm0_h3",
+            ),
+            node(
+                gadm_to_h3,
+                ["gadm_adm1", "params:h3_resolution", "params:adm1.tolerance"],
+                "gadm_adm1_h3",
+            ),
+            node(
+                gadm_to_h3,
+                ["gadm_adm2", "params:h3_resolution", "params:adm2.tolerance"],
+                "gadm_adm2_h3",
+            ),
+            node(join_gadm_levels, ["gadm_adm0_h3", "gadm_adm1_h3", "gadm_adm2_h3"], "gadm_h3_all"),
+            node(reshape_to_geo_region_table, ["gadm_h3_all", "params:geo_region"], "geo_regions"),
+            node(
+                reshape_to_admin_region_table,
+                ["gadm_h3_all", "params:admin_region"],
+                "admin_regions",
+            ),
         ]
     )

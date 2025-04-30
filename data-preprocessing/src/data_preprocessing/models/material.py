5f7256035c8a52a14846d0bd610922f707594b24
Biel Stela <biel.stela@gmail.com>
2025-04-30 17:12:50 +0200

    Add pydantic json dataset

---
+++ data-preprocessing/src/data_preprocessing/models/material.py
@@ -0,0 +1,31 @@
+from typing import Any, Literal
+
+from pydantic import UUID4, BaseModel, ConfigDict, RootModel
+from pydantic.alias_generators import to_camel
+
+type NameCode = str
+
+
+class Material(BaseModel):
+    model_config = ConfigDict(
+        # Remember it must use by_alias=True parameter when serialization to get camelCase json.
+        alias_generator=to_camel,
+        # https://docs.pydantic.dev/latest/api/config/#pydantic.config.ConfigDict.validate_by_alias
+        validate_by_alias=True,
+        validate_by_name=True,
+    )
+    id: UUID4
+    parent_id: UUID4 | None
+    name: str
+    description: str
+    hs_code_id: str
+    earthstat_id: str | None
+    mapspam_id: str | None
+    status: Literal["active", "inactive"]
+    metadata: dict[str, Any]
+    dataset_id: str
+    mpath: str
+
+
+class Materials(RootModel):
+    root: list[Material]

dca133c564e1c101766bd8e0d140bff013f99fac
alexeh <alexurola@gmail.com>
2025-08-21 18:13:12 +0200

    switch to new approach

--- api/src/modules/import-data/sourcing-data/sourcing-data-import.service.ts
+++ api/src/modules/import-data/sourcing-data/sourcing-data-import.service.ts
@@ -155,13 +155,13 @@ export class SourcingDataImportService {
       this.logger.log('Generating Indicator Records...');
 
       try {
-        await this.impactCalculator.calculateImpactForAllSourcingRecords(
+        // await this.impactCalculator.calculateImpactForAllSourcingRecords(
+        // activeIndicators,
+        //);
+        this.logger.warn('Using new impact calculation flow');
+        await this.impactCalculatorV2.calculateImpactForAllLocations(
           activeIndicators,
         );
-        // this.logger.warn('Using new impact calculation flow');
-        // await this.impactCalculatorV2.calculateImpactForAllLocations(
-        //   activeIndicators,
-        // );
         this.logger.log('Indicator Records generated');
         await this.impactService.updateImpactView();
       } catch (err: any) {
